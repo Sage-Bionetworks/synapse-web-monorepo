859e89e7a9b7b3bc69ea3105bc0b370dc76e42ba
Jay Hodgson <jay.hodgson@sagebase.org>
2016-02-19 11:48:06 -0800

    generated browser based js via browserfy, and created minified version

--- dist/markdown-it-synapse.js
+++ dist/markdown-it-synapse.js
@@ -1,27 +1,28 @@
-/*! markdown-it-sub 1.0.0 https://github.com//markdown-it/markdown-it-sub @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitSub = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-// Process ~subscript~
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// Process ${widgetname?param1=1&param2=2}
 
 'use strict';
 
 // same as UNESCAPE_MD_RE plus a space
 var UNESCAPE_RE = /\\([ \\!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~-])/g;
 
-
-function subscript(state, silent) {
+var suffix;
+var widgetIndex;
+function synapse(state, silent) {
   var found,
       content,
       token,
       max = state.posMax,
       start = state.pos;
-
-  if (state.src.charCodeAt(start) !== 0x7E/* ~ */) { return false; }
+  if (start + 3 >= max) { return false; }
+  if (state.src.charCodeAt(start) !== 0x24/* $ */) { return false; }
+  if (state.src.charCodeAt(start + 1) !== 0x7B/* { */) { return false; }
   if (silent) { return false; } // don't run any pairs in validation mode
-  if (start + 2 >= max) { return false; }
 
-  state.pos = start + 1;
+  state.pos = start + 2;
 
   while (state.pos < max) {
-    if (state.src.charCodeAt(state.pos) === 0x7E/* ~ */) {
+    if (state.src.charCodeAt(state.pos) === 0x7D/* } */) {
       found = true;
       break;
     }
@@ -29,12 +30,12 @@ function subscript(state, silent) {
     state.md.inline.skipToken(state);
   }
 
-  if (!found || start + 1 === state.pos) {
+  if (!found || start + 2 === state.pos) {
     state.pos = start;
     return false;
   }
 
-  content = state.src.slice(start + 1, state.pos);
+  content = state.src.slice(start + 2, state.pos);
 
   // don't allow unescaped spaces/newlines inside
   if (content.match(/(^|[^\\])(\\\\)*\s/)) {
@@ -44,27 +45,29 @@ function subscript(state, silent) {
 
   // found!
   state.posMax = state.pos;
-  state.pos = start + 1;
+  state.pos = start + 2;
 
   // Earlier we checked !silent, but this implementation does not need it
-  token         = state.push('sub_open', 'sub', 1);
-  token.markup  = '~';
+  token         = state.push('synapse_open', 'span', 1);
+  token.markup  = '${';
+  token.attrs = [ [ 'widgetparams', content.replace(UNESCAPE_RE, '$1') ],
+    [ 'class', 'widgetContainer' ],
+    [ 'id', 'widget-' + widgetIndex + suffix ] ];
 
-  token         = state.push('text', '', 0);
-  token.content = content.replace(UNESCAPE_RE, '$1');
-
-  token         = state.push('sub_close', 'sub', -1);
-  token.markup  = '~';
+  token         = state.push('synapse_close', 'span', -1);
+  token.markup  = '}';
 
   state.pos = state.posMax + 1;
   state.posMax = max;
+  widgetIndex = widgetIndex + 1;
   return true;
 }
 
 
-module.exports = function sub_plugin(md) {
-  md.inline.ruler.after('emphasis', 'sub', subscript);
+module.exports = function synapse_plugin(md, _suffix) {
+  widgetIndex = 0;
+  suffix = _suffix;
+  md.inline.ruler.after('emphasis', 'synapse', synapse);
 };
 
-},{}]},{},[1])(1)
-});
+},{}]},{},[1]);
