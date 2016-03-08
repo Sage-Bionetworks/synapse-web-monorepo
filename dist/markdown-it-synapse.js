b058cf3e9c86e2819f1e6905e33c80ae80103f95
Jay Hodgson <jay.hodgson@sagebase.org>
2016-03-07 17:51:46 -0800

    changes to support Synapse references (export footnotes)

--- dist/markdown-it-synapse.js
+++ dist/markdown-it-synapse.js
@@ -1,19 +1,28 @@
-/*! markdown-it-synapse 1.0.2 https://github.com/jay-hodgson/markdown-it-synapse @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitSynapse = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! markdown-it-synapse 1.0.3 https://github.com/jay-hodgson/markdown-it-synapse @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitSynapse = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // Process ${widgetname?param1=1&param2=2}
 
 'use strict';
 
 // same as UNESCAPE_MD_RE plus a space
 var UNESCAPE_RE = /\\([ \\!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~-])/g;
+var TEXT_PARAM_RE = /.*text=(.*)[&]{1}/gmi;
+var REFERENCE_START = 'reference?';
+var BOOKMARK_START = 'bookmark?';
 
 var suffix;
 var widgetIndex;
+var footnoteId;
+var footnotes;
 function synapse(state, silent) {
   var found,
       content,
       token,
       max = state.posMax,
-      start = state.pos;
+      start = state.pos,
+      widgetParams,
+      matchResults,
+      footnoteText,
+      widgetContainerClass = 'widgetContainer';
   if (start + 3 >= max) { return false; }
   if (state.src.charCodeAt(start) !== 0x24/* $ */) { return false; }
   if (state.src.charCodeAt(start + 1) !== 0x7B/* { */) { return false; }
@@ -36,6 +45,35 @@ function synapse(state, silent) {
   }
 
   content = state.src.slice(start + 2, state.pos);
+  widgetParams = content.replace(UNESCAPE_RE, '$1');
+  if (content.lastIndexOf(REFERENCE_START, 0) === 0) {
+    // If reference widget, then add an addditional widget param and output token.
+    widgetParams += '&footnoteId=' + footnoteId;
+    token         = state.push('synapse_reference_open', 'span', 1);
+    token.attrs = [ [ 'id', 'wikiReference' + footnoteId ] ];
+    token         = state.push('synapse_reference_close', 'span', -1);
+    // also push bookmark markdown into the environment variable
+    // (will be reprocessed and appended to the html output from the first pass)
+    matchResults = TEXT_PARAM_RE.exec(widgetParams);
+    if (matchResults) {
+      footnoteText = matchResults[1];
+      footnotes += '${bookmark?text=[' + footnoteId +
+      ']&bookmarkID=wikiReference' + footnoteId + '} ' + decodeURIComponent(footnoteText) + '\n\n';
+    }
+    footnoteId++;
+    widgetContainerClass = 'inlineWidgetContainer';
+  } else if (content.lastIndexOf(BOOKMARK_START, 0) === 0) {
+    // If bookmark widget, add additional span (target)
+    token         = state.push('synapse_footnote_target_open', 'span', 1);
+    token.attrs = [ [ 'id', 'wikiFootnote' + footnoteId ] ];
+    token         = state.push('synapse_footnote_target_close', 'span', -1);
+
+    token         = state.push('synapse_footnote_target_newline_open', 'br', 1);
+    token         = state.push('synapse_footnote_target_newline_close', 'br', -1);
+
+    footnoteId++;
+    widgetContainerClass = 'inlineWidgetContainer';
+  }
 
   // NOTE: allows unescaped spaces/newlines inside content
   // found!
@@ -45,8 +83,8 @@ function synapse(state, silent) {
   // Earlier we checked !silent, but this implementation does not need it
   token         = state.push('synapse_open', 'span', 1);
   token.markup  = '${';
-  token.attrs = [ [ 'widgetparams', content.replace(UNESCAPE_RE, '$1') ],
-    [ 'class', 'widgetContainer' ],
+  token.attrs = [ [ 'widgetparams', widgetParams ],
+    [ 'class', widgetContainerClass ],
     [ 'id', 'widget-' + widgetIndex + suffix ] ];
 
   token         = state.push('synapse_close', 'span', -1);
@@ -61,8 +99,18 @@ function synapse(state, silent) {
 
 module.exports = function synapse_plugin(md, _suffix) {
   widgetIndex = 0;
+  footnoteId = 1;
   suffix = _suffix;
+  footnotes = '';
   md.inline.ruler.after('emphasis', 'synapse', synapse);
+};
+
+module.exports.footnotes = function() {
+  return footnotes;
+};
+
+module.exports.resetFootnoteId = function() {
+  footnoteId = 1;
 };
 
 },{}]},{},[1])(1)
--- index.js
+++ index.js
@@ -4,15 +4,24 @@
 
 // same as UNESCAPE_MD_RE plus a space
 var UNESCAPE_RE = /\\([ \\!"#$%&'()*+,.\/:;<=>?@[\]^_`{|}~-])/g;
+var TEXT_PARAM_RE = /.*text=(.*)[&]{1}/gmi;
+var REFERENCE_START = 'reference?';
+var BOOKMARK_START = 'bookmark?';
 
 var suffix;
 var widgetIndex;
+var footnoteId;
+var footnotes;
 function synapse(state, silent) {
   var found,
       content,
       token,
       max = state.posMax,
-      start = state.pos;
+      start = state.pos,
+      widgetParams,
+      matchResults,
+      footnoteText,
+      widgetContainerClass = 'widgetContainer';
   if (start + 3 >= max) { return false; }
   if (state.src.charCodeAt(start) !== 0x24/* $ */) { return false; }
   if (state.src.charCodeAt(start + 1) !== 0x7B/* { */) { return false; }
@@ -35,6 +44,35 @@ function synapse(state, silent) {
   }
 
   content = state.src.slice(start + 2, state.pos);
+  widgetParams = content.replace(UNESCAPE_RE, '$1');
+  if (content.lastIndexOf(REFERENCE_START, 0) === 0) {
+    // If reference widget, then add an addditional widget param and output token.
+    widgetParams += '&footnoteId=' + footnoteId;
+    token         = state.push('synapse_reference_open', 'span', 1);
+    token.attrs = [ [ 'id', 'wikiReference' + footnoteId ] ];
+    token         = state.push('synapse_reference_close', 'span', -1);
+    // also push bookmark markdown into the environment variable
+    // (will be reprocessed and appended to the html output from the first pass)
+    matchResults = TEXT_PARAM_RE.exec(widgetParams);
+    if (matchResults) {
+      footnoteText = matchResults[1];
+      footnotes += '${bookmark?text=[' + footnoteId +
+      ']&bookmarkID=wikiReference' + footnoteId + '} ' + decodeURIComponent(footnoteText) + '\n\n';
+    }
+    footnoteId++;
+    widgetContainerClass = 'inlineWidgetContainer';
+  } else if (content.lastIndexOf(BOOKMARK_START, 0) === 0) {
+    // If bookmark widget, add additional span (target)
+    token         = state.push('synapse_footnote_target_open', 'span', 1);
+    token.attrs = [ [ 'id', 'wikiFootnote' + footnoteId ] ];
+    token         = state.push('synapse_footnote_target_close', 'span', -1);
+
+    token         = state.push('synapse_footnote_target_newline_open', 'br', 1);
+    token         = state.push('synapse_footnote_target_newline_close', 'br', -1);
+
+    footnoteId++;
+    widgetContainerClass = 'inlineWidgetContainer';
+  }
 
   // NOTE: allows unescaped spaces/newlines inside content
   // found!
@@ -44,8 +82,8 @@ function synapse(state, silent) {
   // Earlier we checked !silent, but this implementation does not need it
   token         = state.push('synapse_open', 'span', 1);
   token.markup  = '${';
-  token.attrs = [ [ 'widgetparams', content.replace(UNESCAPE_RE, '$1') ],
-    [ 'class', 'widgetContainer' ],
+  token.attrs = [ [ 'widgetparams', widgetParams ],
+    [ 'class', widgetContainerClass ],
     [ 'id', 'widget-' + widgetIndex + suffix ] ];
 
   token         = state.push('synapse_close', 'span', -1);
@@ -60,6 +98,16 @@ function synapse(state, silent) {
 
 module.exports = function synapse_plugin(md, _suffix) {
   widgetIndex = 0;
+  footnoteId = 1;
   suffix = _suffix;
+  footnotes = '';
   md.inline.ruler.after('emphasis', 'synapse', synapse);
+};
+
+module.exports.footnotes = function() {
+  return footnotes;
+};
+
+module.exports.resetFootnoteId = function() {
+  footnoteId = 1;
 };
