5cc0e929c3ef43c7fe34a3bd06e0c3c254bf9c08
Yongjoo Yoo <yjoo.eyoo@gmail.com>
2022-04-28 15:22:01 -0700

    PORTALS-2160: added with profile pic(PORTALS-2161)

--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -1,67 +1,80 @@
 import React, { useState, useEffect } from 'react'
 import { displayToast } from 'synapse-react-client/dist/containers/ToastMessage'
-import { getUserProfile, updateMyUserProfile } from 'synapse-react-client/dist/utils/SynapseClient'
-import { UserProfile } from 'synapse-react-client/dist/utils/synapseTypes'
+import { getFileHandleByIdURL, getMyUserBundle, updateMyUserProfile, uploadFile } from 'synapse-react-client/dist/utils/SynapseClient'
+import { FileUploadComplete, UserBundle, UserProfile } from 'synapse-react-client/dist/utils/synapseTypes'
 import { useSynapseContext } from 'synapse-react-client/dist/utils/SynapseContext'
-import { Col, Container, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap'
+import { Button, Col, Container, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap'
 import MailIcon from 'assets/mail.svg'
 import LinkIcon from 'assets/link.svg'
-import EditIcon from '../assets/RedEditPencil.svg'
-import { Typography } from 'synapse-react-client'
-
-// import { useGetUserProfileWithProfilePic } from 'synapse-react-client/src/lib/utils/hooks/SynapseAPI/useUserBundle'
+import EditIcon from 'assets/RedEditPencil.svg'
+import VerifiedBorder from 'assets/VerifiedProfilePic.svg'
+import { SynapseConstants, Typography } from 'synapse-react-client'
 
 export type ProfilePageProps={}
 
 export const ProfilePage = (props: ProfilePageProps) => {
     const { accessToken } = useSynapseContext()
-    const [ userProfile, setUserProfile ] = useState<UserProfile>()
-    const [userName, setUserName] = useState<string|undefined>()
-    const [firstName, setFirstName] = useState<string|undefined>()
-    const [lastName, setLastName] = useState<string|undefined>()
-    const [position, setPosition] = useState<string|undefined>()
-    const [company, setCompany] = useState<string|undefined>()
-    const [location, setLocation] = useState<string|undefined>()
-    const [summary, setSummary] = useState<string|undefined>()
-    const [url, setUrl] = useState<string|undefined>()
-    const [editing, setEditing] = useState(false)
 
-    useEffect(()=>{
-        const getProfile = async () => {
+    const [ userProfile, setUserProfile ] = useState<UserProfile>()
+    const [ firstName, setFirstName ] = useState<string|undefined>()
+    const [ lastName, setLastName ] = useState<string|undefined>()
+    const [ position, setPosition ] = useState<string|undefined>()
+    const [ company, setCompany ] = useState<string|undefined>()
+    const [ location, setLocation ] = useState<string|undefined>()
+    const [ summary, setSummary ] = useState<string|undefined>()
+    const [ url, setUrl ] = useState<string|undefined>()
+    const [ profilePicUrl, setProfilePicUrl ] = useState<string|undefined>()
+    const [ fileHandleId, setFileHandleId ] = useState<string|undefined>()
+    const [ verified, setVerfied ] = useState<boolean>()
+    const [ editing, setEditing ] = useState(false)
+
+    const updateUserProfile = async () => {
         try{
-            const user = await getUserProfile(accessToken)
-            setUserProfile(user)
-            setUserName(user.userName)
-            setFirstName(user.firstName)
-            setLastName(user.lastName)
-            setPosition(user.position)
-            setCompany(user.company)
-            setLocation(user.location)
-            setSummary(user.summary)
-            setUrl(user.url)
+            if(userProfile){
+                userProfile.firstName = firstName as string
+                userProfile.lastName = lastName as string
+                userProfile.position = position as string
+                userProfile.company = company as string
+                userProfile.location = location as string
+                userProfile.summary = summary as string
+                userProfile.url = url as string
+                userProfile.profilePicureFileHandleId = fileHandleId as string
+                await updateMyUserProfile(userProfile, accessToken)
+                displayToast('Profile has been successfully updated', 'success')
+                getProfile()
+                setEditing(false)
+            }
+        } catch(err:any){
+            displayToast(err.reason as string, 'danger')
+        }
+    }
+
+    const getProfile = async () => {
+        try{
+            const mask =
+            SynapseConstants.USER_BUNDLE_MASK_IS_VERIFIED |
+            SynapseConstants.USER_BUNDLE_MASK_USER_PROFILE
+
+            const bundle: UserBundle = await getMyUserBundle(mask, accessToken)
+            const picUrl = await getFileHandleByIdURL(bundle.userProfile?.profilePicureFileHandleId as string,accessToken)
+            setUserProfile(bundle.userProfile)
+            setFirstName(bundle.userProfile?.firstName)
+            setLastName(bundle.userProfile?.lastName)
+            setPosition(bundle.userProfile?.position)
+            setCompany(bundle.userProfile?.company)
+            setLocation(bundle.userProfile?.location)
+            setSummary(bundle.userProfile?.summary)
+            setUrl(bundle.userProfile?.url)
+            setProfilePicUrl(picUrl)
+            setVerfied(bundle.isVerified)
         } catch(err:any) {
             displayToast(err.reason as string, 'danger')
         }
     }
-    getProfile()
-    },[])
-    
 
-    // Profile Pic - PORTALS-2161
-    // const { data: profileAndImage } = useGetUserProfileWithProfilePic(
-    //     userProfile?.ownerId as string
-    // )
-
-    // console.log(userProfile && profileAndImage)
-    // const profilePic = () => {
-    //     return(
-    //         <svg width="134" height="132" viewBox="0 0 134 132" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //             <circle cx="66" cy="66" r="65.5" stroke="black"/>
-    //             <path d="M114.928 92.4041C115.126 92.187 115.468 92.187 115.666 92.4041L119.883 97.0199C119.994 97.1408 120.156 97.1999 120.319 97.1783L126.516 96.353C126.807 96.3142 127.069 96.5339 127.082 96.8276L127.345 103.074C127.352 103.238 127.438 103.387 127.577 103.475L132.855 106.827C133.103 106.984 133.162 107.321 132.983 107.554L129.169 112.508C129.07 112.638 129.04 112.808 129.089 112.964L130.978 118.924C131.067 119.204 130.896 119.5 130.609 119.564L124.503 120.908C124.343 120.943 124.21 121.054 124.148 121.205L121.764 126.985C121.652 127.257 121.331 127.374 121.07 127.238L115.529 124.343C115.384 124.267 115.211 124.267 115.066 124.343L109.524 127.238C109.264 127.374 108.943 127.257 108.831 126.985L106.447 121.205C106.384 121.054 106.252 120.943 106.092 120.908L99.9859 119.564C99.6988 119.5 99.5279 119.204 99.6167 118.924L101.506 112.964C101.555 112.808 101.525 112.638 101.425 112.508L97.6118 107.554C97.4325 107.321 97.4919 106.984 97.74 106.827L103.018 103.475C103.156 103.387 103.243 103.238 103.25 103.074L103.513 96.8276C103.525 96.5339 103.787 96.3142 104.079 96.353L110.276 97.1783C110.438 97.1999 110.601 97.1408 110.711 97.0199L114.928 92.4041Z" fill="#2A2A2A"/>
-    //             <path d="M108.436 109.725L113.697 115.279L123.303 105.723" stroke="#FEFEFE" stroke-width="2"/>
-    //         </svg>
-    //     )
-    // }
+    useEffect(()=>{
+        getProfile()
+    },[accessToken,])
 
     const EditField = (label: string, updatedValue: string|undefined, updateFn: Function) => {
         return(
@@ -76,24 +89,58 @@ export const ProfilePage = (props: ProfilePageProps) => {
         )
     }
 
-    const updateUserProfile = async () => {
-        try{
-            if(userProfile){
-                userProfile.firstName = firstName as string
-                userProfile.lastName = lastName as string
-                userProfile.position = position as string
-                userProfile.company = company as string
-                userProfile.location = location as string
-                userProfile.summary = summary as string
-                userProfile.url = url as string
-                await updateMyUserProfile(userProfile, accessToken)
-                displayToast('Profile has been successfully updated', 'success')
-                setEditing(false)
+    const onCancel = async () => {
+        getProfile()
+        setEditing(false)
+    }
+
+    const uploadHandler = async(e: React.ChangeEvent<HTMLInputElement>) => {
+        if(e.target.files){
+            const file = e.target.files[0]
+            try{
+                const resp: FileUploadComplete = await uploadFile(accessToken, file.name, file)
+                const picUrl = await getFileHandleByIdURL(resp.fileHandleId,accessToken)
+                setFileHandleId(resp.fileHandleId)
+                setProfilePicUrl(picUrl)
+            } catch(err:any){
+                console.error(err)
             }
-        } catch(err:any){
-            displayToast(err.reason as string, 'danger')
         }
     }
+
+    const hiddenFileInput = React.useRef<HTMLInputElement>(null)
+    const clickHandler = () => {
+        if (hiddenFileInput?.current!) {
+          hiddenFileInput.current?.click()
+        }
+    }
+
+    const uploadImg = (
+        <>
+            <input
+                type={'file'}
+                ref={hiddenFileInput}
+                onChange={uploadHandler}
+                style={{display: 'none'}}
+            />
+            <Button style={{marginTop:'16px'}} variant='secondary' onClick={clickHandler}>Upload File</Button>
+        </>
+    )
+
+    const validProfilePic = (
+        <>
+            {verified ? <div className='verified-img-container'>
+                <img className='verified-border' src={VerifiedBorder}/>
+                <img className='verified-img' src={profilePicUrl}/>
+                {editing && uploadImg}
+            </div> :
+            <div className='non-verified-profile-pic'>
+                <img className='non-verified-img' src={profilePicUrl}/>
+                {editing && uploadImg}
+            </div>
+            }
+        </>
+    ) 
 
     return(
         <div className="bootstrap-4-backport blue-background profile-page">
@@ -106,20 +153,20 @@ export const ProfilePage = (props: ProfilePageProps) => {
                     </>
                     :
                     <>
-                    <button onClick={()=>updateUserProfile()}>Save Changes</button>
-                    <button onClick={()=>setEditing(false)}>Cancel</button>
+                        <button onClick={()=>updateUserProfile()}>Save Changes</button>
+                        <button onClick={onCancel}>Cancel</button>
                     </> 
                     }
                 </div>
                 <Row>
-                    <Col sm={4} >
-                        IMAGE GOES HERE
+                    <Col sm={3} >
+                        {validProfilePic}   
                     </Col>
-                    <Col sm={8}>
+                    <Col sm={9}>
                         <div className='grid-container'>
                             <div className='containers'>
                                 {!!!editing ? 
-                                    <Typography variant='headline3'>{`${firstName} ${lastName}`}</Typography>
+                                    <Typography variant='headline3'>{`${userProfile?.firstName} ${userProfile?.lastName}`}</Typography>
                                 : 
                                 <FormGroup style={{display:'inline-block'}}>
                                     {EditField('First name', firstName, setFirstName)}
@@ -130,9 +177,9 @@ export const ProfilePage = (props: ProfilePageProps) => {
                             <div className='containers'>
                                 {!!!editing ? 
                                 <div>
-                                    {position} <br/>
-                                    {company} <br/>
-                                    {location}
+                                    {userProfile?.position} <br/>
+                                    {userProfile?.company} <br/>
+                                    {userProfile?.location}
                                 </div> : 
                                 <FormGroup>
                                     {EditField('Position', position, setPosition)}
@@ -142,7 +189,7 @@ export const ProfilePage = (props: ProfilePageProps) => {
                             }
                             </div>
                             <div className='containers'>
-                                {!!!editing ? summary : 
+                                {!!!editing ? userProfile?.summary : 
                                 <FormGroup>
                                 <FormLabel>Summary</FormLabel>
                                 <FormControl
@@ -156,10 +203,10 @@ export const ProfilePage = (props: ProfilePageProps) => {
                                 }
                             </div>
                             <div className='containers'>
-                                <a href={"mailto:" + userName}><img className='contact-icon' src={MailIcon}/>{userName}</a>
+                                <a href={"mailto:" + userProfile?.userName}><img className='contact-icon' src={MailIcon}/>{userProfile?.userName}</a>
                             </div>
                             <div className='containers'>
-                                {!!!editing ? <a href={url}><img className='contact-icon' src={LinkIcon}/>{url}</a> : EditField('Website', url, setUrl)}
+                                {!!!editing ? <a href={userProfile?.url}><img className='contact-icon' src={LinkIcon}/>{userProfile?.url}</a> : EditField('Website', url, setUrl)}
                             </div>
                         </div>
                     </Col>
