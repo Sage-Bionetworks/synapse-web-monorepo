f751514a5e395ab8ab00dac303b1babc7ef9005d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-08-09 16:16:15 -0400

    Update src/OAuth2Form.tsx

--- src/OAuth2Form.tsx
+++ src/OAuth2Form.tsx
@@ -35,6 +35,7 @@ export const OAuth2Form = () => {
     }
   };
 
+  // In addition to fetching the current user profile, the success of this request will determine if the current access token is valid.
   const { data: profile } = useGetCurrentUserProfile({
     enabled: !!accessToken,
     onError,
