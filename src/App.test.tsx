0c1718981b22fbe7c4b8276ed65595f4e7347463
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-08-09 08:29:15 -0400

    Add test todos, remove Enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import App from './App';
-import { shallow } from 'enzyme'
+import React from "react";
+import App from "./App";
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  window.history.pushState({}, 'Synapse Sign-In', '/?client_id=1234');  
-  shallow(<App />)
+describe("App integration tests", () => {
+  test("renders without crashing", () => {
+    window.history.pushState({}, "Synapse Sign-In", "/?client_id=1234");
+    render(<App />);
+  });
+
+  test.todo("Shows login when no token is provided");
+  test.todo(
+    "Shows login and does not redirect when an expired token is provided"
+  );
+  test.todo("Stores the token when login is successful");
+  test.todo(
+    "Shows consent screen if a token is provided but the user has not consented"
+  );
+  test.todo("Consent to app terms");
+  test.todo("Deny consent to app terms");
+  test.todo(
+    "Redirects if a token is provided and the user has already consented"
+  );
+
+  test.todo("Redirects if a the redirect URI is invalid");
+  test.todo("Redirects if the backend provides an unhandled error");
+  test.todo("Shows warning if the client is unverified");
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,30 +1,26 @@
-import 'isomorphic-fetch' // polyfill for fetch
-import 'raf/polyfill' // polyfill for requestAnimationFrame
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import "isomorphic-fetch"; // polyfill for fetch
+import "raf/polyfill"; // polyfill for requestAnimationFrame
 
-declare var global: any
-global.markdownit = require('markdown-it')
-global.markdownitSynapse = require('markdown-it-synapse')
-global.markdownitSub = require('markdown-it-sub-alt')
-global.markdownitSup = require('markdown-it-sup-alt')
-global.markdownitCentertext = require('markdown-it-center-text')
-global.markdownitSynapseHeading = require('markdown-it-synapse-heading')
-global.markdownitSynapseTable = require('markdown-it-synapse-table')
-global.markdownitStrikethroughAlt = require('markdown-it-strikethrough-alt')
-global.markdownitContainer = require('markdown-it-container')
-global.markdownitEmphasisAlt = require('markdown-it-emphasis-alt')
-global.markdownitInlineComments = require('markdown-it-inline-comments')
-global.markdownitBr = require('markdown-it-br')
-global.sanitizeHtml = require('sanitize-html')
-global.markdownitMath = require('markdown-it-synapse-math')
-
-configure({ adapter: new Adapter() })
+declare var global: any;
+global.markdownit = require("markdown-it");
+global.markdownitSynapse = require("markdown-it-synapse");
+global.markdownitSub = require("markdown-it-sub-alt");
+global.markdownitSup = require("markdown-it-sup-alt");
+global.markdownitCentertext = require("markdown-it-center-text");
+global.markdownitSynapseHeading = require("markdown-it-synapse-heading");
+global.markdownitSynapseTable = require("markdown-it-synapse-table");
+global.markdownitStrikethroughAlt = require("markdown-it-strikethrough-alt");
+global.markdownitContainer = require("markdown-it-container");
+global.markdownitEmphasisAlt = require("markdown-it-emphasis-alt");
+global.markdownitInlineComments = require("markdown-it-inline-comments");
+global.markdownitBr = require("markdown-it-br");
+global.sanitizeHtml = require("sanitize-html");
+global.markdownitMath = require("markdown-it-synapse-math");
 
 // Line below is used because plotly has a dependency on mapbox-gl
 // which requires a browser env and doesn't provide support for headless
 // js testing, so we shim the function below.
 // View - https://github.com/mapbox/mapbox-gl-js/issues/3436
-window.URL.createObjectURL = function() {
-  return ''
-}
+window.URL.createObjectURL = function () {
+  return "";
+};
