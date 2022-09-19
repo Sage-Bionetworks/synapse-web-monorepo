baf127dc84c66e5cdc6819813a75c82a0b12878c
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2022-09-19 12:09:45 -0400

    PORTALS-2322

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import AppInitializer from './AppInitializer'
 import { SynapseContextConsumer, SynapseContextType } from 'synapse-react-client/dist/utils/SynapseContext'
 import {
