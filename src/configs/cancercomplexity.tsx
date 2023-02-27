bbb64424d931ffd5eb899fc52dabf63874685367
Jay Hodgson <jay.hodgson@sagebase.org>
2023-02-27 10:08:41 -0800

    trim CCKP description for Sage Resources page

--- src/configs/cancercomplexity.tsx
+++ src/configs/cancercomplexity.tsx
@@ -6,7 +6,7 @@ import LogoSvg from '../assets/CCKPLogo.svg'
 const logo = <img src={LogoSvg} alt="Logo" />
 
 const description =
-  'The NIH National Cancer Institute-sponsored Cancer Systems Biology Consortium (CSBC) and Physical Sciences in Oncology Network (PS-ON) aim to tackle the most perplexing issues in cancer to increase our understanding of tumor biology, treatment options, and patient outcome. The CSBC PS-ON Data Portal highlights ongoing research efforts in these richly interdisciplinary fields, including the data generated and methods developed in the addresssing fundamental questions in cancer research.'
+  "Data and tools from the NIH National Cancer Institute-sponsored Cancer Systems Biology Consortium (CSBC) and Physical Sciences in Oncology Network (PS-ON) for the study of Alzheimer's disease, related dementias, and healthy aging."
 
 export const cckp: SourceAppConfig = {
   appId: 'CCKP',
