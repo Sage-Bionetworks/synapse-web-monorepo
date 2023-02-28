52d519ab234562a8d7269b73dec8030fc94f28d6
jay-hodgson <jay.hodgson@sagebase.org>
2023-02-28 16:53:57 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@890f4a6f1225471f39c70c5ecc997bcbe5a20517 🚀

---
+++ assets/UserCard.stories-15a9dddd.js
@@ -0,0 +1,19 @@
+import{a as I,F as z,j as m}from"./jsx-runtime-670450c2.js";import{h as L,H as E,u as M,L as f,as as D}from"./SynapseConstants-41b9b1f3.js";import{b4 as v}from"./SynapseContext-52fddc23.js";import{U as p}from"./UserCard-f3b3c3b8.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-96c5f47c.js";import"./extends-98964cd2.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./Fade-49f10167.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./useTheme-dfe229ea.js";import"./styled-42ab246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./Clear-16219090.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-dd8a6e5c.js";import"./useIsFocusVisible-83eed526.js";import"./Transition-550b3899.js";import"./TransitionGroupContext-ab7f4b4e.js";import"./SvgIcon-74ace867.js";import"./isArray-5e3f9107.js";import"./getEndpoint-5374ab4d.js";import"./IconSvg-2e0d14e4.js";import"./Box-b7ffd230.js";import"./extendSxProp-63b32eb6.js";import"./Button-86068eae.js";import"./ButtonBase-4a394d72.js";import"./emotion-react.browser.esm-e553699c.js";import"./assertThisInitialized-081f9914.js";import"./Button-7d415009.js";import"./Alert-4131c431.js";import"./hook-c05d8d9f.js";import"./createWithBsPrefix-e09f51dd.js";import"./divWithClassName-95211f4d.js";import"./index-4d501b15.js";import"./IconCopy-9a72c17a.js";import"./SkeletonTable-f3eb718f.js";import"./times-d23216df.js";import"./toInteger-287eaf08.js";import"./isSymbol-7c514724.js";import"./Skeleton-18b555b0.js";import"./ToastMessage-47a48bcd.js";import"./FullWidthAlert-9afcbd85.js";import"./Typography-81d78865.js";import"./removeClass-164fd327.js";import"./hasClass-ec9efd32.js";import"./uniqueId-4d05949d.js";import"./toString-cc90cb98.js";import"./Overlay-b843e17b.js";import"./contains-33365353.js";import"./usePopperMarginModifiers-45f6b36e.js";import"./useWaitForDOMRef-54076dc2.js";const wr={title:"UI/UserCard",component:p,render:a=>{const{data:i}=v();let s=i==null?void 0:i.ownerId;return s===D.toString()&&(s=void 0),I(z,{children:[m("p",{children:"If you are logged in, your avatar or card will appear. If you are not logged in, enter an ownerId (e.g. 273960) or alias (e.g. brucehoff) below."}),m(p,{ownerId:a.ownerId??s,size:a.size??L,...a})]})}},r={args:{size:E,avatarSize:"SMALL"}},o={args:{size:L}},e={args:{size:M}},t={args:{size:f}};var n,d,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: AVATAR,
+    avatarSize: 'SMALL'
+  }
+}`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,A,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: SMALL_USER_CARD
+  }
+}`,...(g=(A=o.parameters)==null?void 0:A.docs)==null?void 0:g.source}}};var S,U,l;e.parameters={...e.parameters,docs:{...(S=e.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: MEDIUM_USER_CARD
+  }
+}`,...(l=(U=e.parameters)==null?void 0:U.docs)==null?void 0:l.source}}};var R,C,_;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    size: LARGE_USER_CARD
+  }
+}`,...(_=(C=t.parameters)==null?void 0:C.docs)==null?void 0:_.source}}};const hr=["Avatar","SmallUserCard","MediumUserCard","LargeUserCard"];export{r as Avatar,t as LargeUserCard,e as MediumUserCard,o as SmallUserCard,hr as __namedExportsOrder,wr as default};
+//# sourceMappingURL=UserCard.stories-15a9dddd.js.map
---
+++ assets/times-d23216df.js
@@ -0,0 +1,2 @@
+import{b9 as s,bS as f}from"./SynapseContext-52fddc23.js";import{t as m}from"./toInteger-287eaf08.js";function u(t){return typeof t=="function"?t:s}var c=9007199254740991,i=4294967295,e=Math.min;function p(t,r){if(t=m(t),t<1||t>c)return[];var n=i,o=e(t,i);r=u(r),t-=i;for(var a=f(o,r);++n<t;)r(n);return a}export{u as c,p as t};
+//# sourceMappingURL=times-d23216df.js.map
