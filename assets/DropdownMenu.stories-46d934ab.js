1c90f2c45f19b8853307c3d8c0936182c3f19fc5
jay-hodgson <jay.hodgson@sagebase.org>
2023-03-03 21:02:43 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@73c36d09c066f3845747f93a825e3e103073ca82 🚀

---
+++ assets/DropdownMenu.stories-46d934ab.js
@@ -0,0 +1,29 @@
+import{j as s}from"./jsx-runtime-670450c2.js";import{D as p}from"./DropdownMenu-19489ded.js";import{I as m}from"./IconSvg-bec9c070.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./Button-86068eae.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./extends-98964cd2.js";import"./styled-42ab246b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./ButtonBase-4a394d72.js";import"./emotion-react.browser.esm-e553699c.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./TransitionGroupContext-ab7f4b4e.js";import"./useForkRef-dd8a6e5c.js";import"./useIsFocusVisible-83eed526.js";import"./Typography-81d78865.js";import"./extendSxProp-63b32eb6.js";import"./Clear-16219090.js";import"./useTheme-dfe229ea.js";import"./isHostComponent-fa76b8d9.js";import"./index-96c5f47c.js";import"./Transition-550b3899.js";import"./SvgIcon-74ace867.js";import"./Fade-49f10167.js";import"./Paper-67bb1ac6.js";import"./MenuList-e2fa804a.js";import"./index-1aacdabe.js";import"./List-c7e152cc.js";import"./Divider-195b40c4.js";const F={title:"UI/DropdownMenu",component:p},o=n=>()=>{console.log("Item clicked",n)},t={args:{dropdownButtonText:"Dropdown Menu Button",buttonProps:{endIcon:s(m,{icon:"verticalEllipsis",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}};var i,r,e;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    dropdownButtonText: 'Dropdown Menu Button',
+    buttonProps: {
+      endIcon: <IconSvg icon="verticalEllipsis" wrap={false} />
+    },
+    items: [[{
+      text: 'Do something cool',
+      onClick: onClickHandler(0)
+    }, {
+      text: 'This one has a tooltip',
+      tooltipText: 'Some more info',
+      onClick: onClickHandler(1)
+    }, {
+      text: 'This one is disabled',
+      disabled: true,
+      onClick: onClickHandler(3)
+    }, {
+      text: 'This one has both',
+      disabled: true,
+      tooltipText: "You can't do this for reasons",
+      onClick: onClickHandler(4)
+    }], [{
+      text: 'You can organize actions into groups',
+      onClick: onClickHandler(5)
+    }]]
+  }
+}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const G=["Demo"];export{t as Demo,G as __namedExportsOrder,F as default};
+//# sourceMappingURL=DropdownMenu.stories-46d934ab.js.map
---
+++ assets/cloneDeep-2b54e8ad.js
@@ -0,0 +1,2 @@
+import{b as o}from"./_baseClone-27736ebe.js";var r=1,n=4;function a(e){return o(e,r|n)}export{a as c};
+//# sourceMappingURL=cloneDeep-2b54e8ad.js.map
