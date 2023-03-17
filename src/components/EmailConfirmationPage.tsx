9a0e4774868577fa2ec900e5543e57fd8e817179
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-03-17 09:48:20 -0700

    PORTALS-2561: polish 'please check your email' page

--- src/components/EmailConfirmationPage.tsx
+++ src/components/EmailConfirmationPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined'
 import { Button, Link } from '@mui/material'
-import IconSvg from 'synapse-react-client/dist/containers/IconSvg'
+import Avatar from '@mui/material/Avatar'
+import React from 'react'
 import { Container } from 'react-bootstrap'
 
 export type EmailConfirmationPageProps = {
@@ -15,31 +16,40 @@ export const EmailConfirmationPage: React.FunctionComponent<
     color: 'white',
   }
 
+  const avatarSx = {
+    backgroundColor: 'primary.main',
+    height: '100px',
+    width: '100px',
+    marginBottom: '40px',
+  }
+
   const iconSx = {
-    color: 'primary.main',
     fontSize: '60px',
   }
 
   return (
     <Container className="thankYouContainer">
       <div className="thankYouPanel">
-        <IconSvg icon="email" sx={iconSx} />
+        <Avatar sx={avatarSx}>
+          <EmailOutlinedIcon sx={iconSx} />
+        </Avatar>
         <div className="thankYouText">
           <h3>Please check your email.</h3>
           <p>
-            We’ve sent an email to <strong>{email}</strong>
+            We've sent an email to <strong>{email}</strong>
           </p>
           <p>
             Click the link in the message to verify your address, then continue
             registration.
           </p>
           <Link
+            color="primary"
             href={
               'https://sagebionetworks.jira.com/servicedesk/customer/portal/9'
             }
             target="_blank"
           >
-            I didn’t receive an email
+            I didn't receive an email
           </Link>
         </div>
         <Button
