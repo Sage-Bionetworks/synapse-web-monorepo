432dbc42a8d3e6facc06a932f03441edc014f7b8
hallieswan <26949006+hallieswan@users.noreply.github.com>
2023-03-22 22:54:33 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@971c9bbb1be7bdd4eea852dbe2ae9c7ebbdb45d7 🚀

---
+++ assets/IconSvg.stories-fe184f8b.js
@@ -0,0 +1,10 @@
+import{j as t,a as s}from"./jsx-runtime-670450c2.js";import{I as i,b as a}from"./IconSvg-4faf2a65.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./SvgIcon-50b3abbe.js";import"./objectWithoutPropertiesLoose-df62547b.js";import"./styled-233e7b47.js";import"./extends-98964cd2.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./Clear-10ac5e31.js";import"./useTheme-4a3c7035.js";import"./isHostComponent-fa76b8d9.js";import"./index-96c5f47c.js";import"./useForkRef-dd8a6e5c.js";import"./TransitionGroupContext-a2b6e27b.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";const D={title:"UI/IconSvg",component:i,render:n=>t("div",{style:{display:"flex",flexWrap:"wrap",flexDirection:"row"},children:a.sort().map(r=>s("div",{style:{margin:"10px",textAlign:"center"},children:[t("div",{children:t(i,{...n,icon:r,label:r})}),t("div",{style:{fontSize:"10px"},children:r})]},r))})},o={args:{sx:{color:"primary.main",width:"40px",height:"40px"}}};var e,p,m;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    sx: {
+      color: 'primary.main',
+      width: '40px',
+      height: '40px'
+    }
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const E=["Icon"];export{o as Icon,E as __namedExportsOrder,D as default};
+//# sourceMappingURL=IconSvg.stories-fe184f8b.js.map
