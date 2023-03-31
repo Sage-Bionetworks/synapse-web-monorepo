7647331427f49782b98e82d5894743b188f3c645
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-03-30 17:10:49 -0700

    PORTALS-2567: polish items for verification step 4 (upload)

--- apps/SageAccountWeb/src/components/ProfileValidation/Attestation.tsx
+++ apps/SageAccountWeb/src/components/ProfileValidation/Attestation.tsx
@@ -1,6 +1,6 @@
-import { Box, Button } from '@mui/material'
+import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined'
+import { Box } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import theme from 'style/theme'
 import { Typography, SynapseComponents } from 'synapse-react-client'
 import { displayToast } from 'synapse-react-client/dist/containers/ToastMessage'
 import {
@@ -9,10 +9,51 @@ import {
   FileUploadComplete,
 } from 'synapse-react-client/dist/utils/synapseTypes'
 import { ContinueButton } from './ContinueButton'
+import { ReturnToAppButton } from './ReturnToAppButton'
 
 export type AttestationProps = {
   verificationSubmission: VerificationSubmission
   onNext: (vs: VerificationSubmission) => void
+}
+
+export type UploadButtonProps = {
+  uploadCallback: (data: UploadCallbackResp) => void
+}
+
+const UploadButton: React.FC<UploadButtonProps> = ({ uploadCallback }) => {
+  const hackSx = {
+    position: 'relative',
+    'button#uploadButton': {
+      fontWeight: 700,
+      border: '1px solid #E0E0E0',
+      borderRadius: '3px',
+      width: '100%',
+      color: 'grey.800',
+      paddingLeft: '32px',
+    },
+  }
+  const iconSx = {
+    zIndex: 10,
+    position: 'absolute',
+    left: '56.6px',
+    top: '50%',
+    transform: 'translate(-50%,-50%)',
+    color: 'grey.800',
+    pointerEvents: 'none',
+  }
+
+  /* agendel TODO: we need a component that takes a child component and wraps it in a button */
+  return (
+    <Box id="hack" sx={hackSx}>
+      <CloudUploadOutlinedIcon sx={iconSx} />
+      <SynapseComponents.FileUpload
+        uploadCallback={uploadCallback}
+        label={'Upload from your computer'}
+        variant="light-primary-base"
+        id="uploadButton"
+      />
+    </Box>
+  )
 }
 
 const Attestation: React.FC<AttestationProps> = (props: AttestationProps) => {
@@ -41,44 +82,25 @@ const Attestation: React.FC<AttestationProps> = (props: AttestationProps) => {
 
   return (
     <>
-      <Box
-        sx={{
-          '& #hack > button': {
-            border: '1px solid #E0E0E0',
-            borderRadius: '3px',
-            width: '100%',
-          },
-        }}
-      >
-        {/* agendel TODO: we need a component that takes a child component and wraps it in a button 
-                    import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined' for the icon here*/}
-        <div id="hack">
-          <SynapseComponents.FileUpload
-            uploadCallback={uploadCallback}
-            label={isAttachment ? 'Replace' : 'Select ID File to Upload'}
-            variant={/*isAttachment ? */ 'light-primary-base' /*: 'secondary'*/}
-          />
-          {isAttachment && (
-            <>
-              <Typography
-                style={{ margin: theme.spacing(5, 0) }}
-                variant="body2"
-              >
-                {attachments[0].fileName}
-              </Typography>
-              <Button variant="outlined" onClick={() => setAttachments([])}>
-                Remove
-              </Button>
-            </>
-          )}
-        </div>
-      </Box>
+      {isAttachment && (
+        <>
+          <Typography variant="body2" sx={{ fontWeight: 700 }}>
+            Selected file:
+          </Typography>
+          <Typography variant="body2" sx={{ marginBottom: '20px' }}>
+            {attachments[0].fileName}
+          </Typography>
+        </>
+      )}
+      <UploadButton uploadCallback={uploadCallback} />
       <ContinueButton
         disabled={!attachments?.length}
         onClick={() => props.onNext(verificationSubmission)}
+        sx={{ marginTop: '40px', marginBottom: '10px' }}
       >
-        Request Validation
+        Request validation
       </ContinueButton>
+      <ReturnToAppButton />
     </>
   )
 }
