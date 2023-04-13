a5910e7d92ad9782517fe32875c5393202625f6a
nickgros <nickgrosenbacher@gmail.com>
2023-04-13 13:58:00 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@bd581c55dd4dd38de7b3e74438e02f79f30593dc 🚀

---
+++ assets/MarkdownSynapse.stories-08567a6c.js
@@ -0,0 +1,85 @@
+import{b as W}from"./AccessRequirementList-8dc77531.js";import"./jsx-runtime-ad672792.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./SynapseContext-1c9e8220.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./isArray-5e3f9107.js";import"./index-96c5f47c.js";import"./extends-98964cd2.js";import"./getEndpoint-5374ab4d.js";import"./SynapseConstants-c11fd777.js";import"./SvgIcon-ed1b2f06.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./styled-ff065be4.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-eaaa54fe.js";import"./IconSvg-0c61ea2c.js";import"./Clear-55e06940.js";import"./useTheme-16c0fb65.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-dd8a6e5c.js";import"./TransitionGroupContext-a2b6e27b.js";import"./Box-b7c6e0f8.js";import"./extendSxProp-1b40785b.js";import"./Button-5da6eef4.js";import"./ButtonBase-4fdac2d3.js";import"./emotion-react.browser.esm-d0daa96f.js";import"./assertThisInitialized-081f9914.js";import"./Button-7d415009.js";import"./Alert-84d62a08.js";import"./hook-c05d8d9f.js";import"./createWithBsPrefix-e09f51dd.js";import"./divWithClassName-1aeead00.js";import"./index-4d501b15.js";import"./useGetInfoFromIds-21bef76b.js";import"./use-deep-compare-effect.esm-2b920fea.js";import"./index-5d0cf067.js";import"./uniq-9958a14f.js";import"./_baseSlice-cf92e063.js";import"./_isIterateeCall-82c5d97e.js";import"./_MapCache-c47e0ef4.js";import"./_baseTimes-8cccc40f.js";import"./_isIndex-af14b756.js";import"./toInteger-97099945.js";import"./isSymbol-7c514724.js";import"./_cacheHas-f4513965.js";import"./without-92533977.js";import"./toString-cc90cb98.js";import"./isArrayLikeObject-ca102021.js";import"./_setToArray-3d6ec6fd.js";import"./StandaloneLoginForm-c2e7ff5f.js";import"./LoginFlowBackButton-aa551300.js";import"./useMutation-b1f5db20.js";import"./mutation-3a29db04.js";import"./FullWidthAlert-c9084138.js";import"./Typography-7519d429.js";import"./TextField-848ad8ce.js";import"./FormControl-f09e7ea9.js";import"./ownerWindow-2c76219e.js";import"./isMuiElement-d529b771.js";import"./index-1aacdabe.js";import"./Modal-0ebcf51b.js";import"./Fade-1906ca66.js";import"./getScrollbarSize-ac846fe6.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-d7d4034b.js";import"./MenuList-0503a3ae.js";import"./List-0aecca37.js";import"./TextField-9da304f4.js";import"./Link-f8d2bb3d.js";import"./IconButton-f574beef.js";import"./LoadingScreen-c3ef81a7.js";import"./Modal-6ffa84ac.js";import"./contains-33365353.js";import"./useWillUnmount-7fb15b8a.js";import"./usePrevious-9f30f8c7.js";import"./removeClass-164fd327.js";import"./hasClass-ec9efd32.js";import"./useWaitForDOMRef-54076dc2.js";import"./UserCard-503957e2.js";import"./IconCopy-4d77a0f7.js";import"./SkeletonTable-7d243484.js";import"./times-87f43916.js";import"./Skeleton-3a50b3e0.js";import"./ToastMessage-8a4a525a.js";import"./uniqueId-4d05949d.js";import"./Overlay-013cd108.js";import"./usePopperMarginModifiers-b2172580.js";import"./RegularExpressions-bc0adf55.js";import"./react-intersection-observer.modern-5866bfdc.js";import"./useInfiniteQuery-81bc7454.js";import"./infiniteQueryBehavior-dfe33a71.js";import"./DateFormatter-6340bc5a.js";import"./dayjs.min-e0adaab4.js";import"./utc-8a3e1a17.js";import"./EntityIcon-cf233242.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./uniq-b503d6a4.js";import"./_baseIteratee-dd2a3782.js";import"./hasIn-6ae41e3d.js";import"./_baseUniq-7e37c67e.js";import"./index.browser-7a1de4f7.js";import"./core.esm-08cdfe54.js";import"./pick-63b75f03.js";import"./_baseClone-11a40197.js";import"./_baseMap-39e03039.js";import"./isEqual-97aef6b7.js";import"./_baseFor-d254fa1e.js";import"./isPlainObject-6d05c27d.js";import"./union-43d319b4.js";import"./CustomSelectWidget-ef3541f2.js";import"./_basePickBy-38ad6c16.js";import"./Select-40119e12.esm-928a24e6.js";import"./toConsumableArray-85fcc1b5.js";import"./getPrototypeOf-e4242ba0.js";import"./isNativeReflectConstruct-ff095bc2.js";import"./isString-b52675be.js";import"./now-030ccb2b.js";import"./mapValues-1c647352.js";import"./factory-83f07b08.js";import"./sqlFunctions-264b8cff.js";import"./QueryFilter-bbc0aec9.js";import"./useEntity-3a70b169.js";import"./SynapseTableConstants-942d2b0b.js";import"./FacetNav-1a871e5c.js";import"./queryUtils-bf02e297.js";import"./cloneDeep-6c099e3c.js";import"./NoSearchResults-79eea014.js";import"./NoData-80d05b97.js";import"./unCamelCase-ec1c2b3d.js";import"./ColumnType-5116adf5.js";import"./useGetQueryResultBundle-2327db36.js";import"./merge-f11e1b52.js";import"./ElementWithTooltip-f35725e8.js";import"./Dropdown-fe0fb5f2.js";import"./isRequiredForA11y-0a29da5f.js";import"./Checkbox-9c5dca81.js";import"./RadioGroup-2b4593e6.js";import"./RangeSlider-cf7db10e.js";import"./Stack-60ddb7e2.js";import"./react-sizeme-2217af01.js";import"./ColorGradient-fc5a110b.js";import"./colorPalette-43310703.js";import"./SelectionCriteriaPill-059a36af.js";import"./Close-2535a2b3.js";import"./relativeTime-93b00284.js";import"./useDownloadList-4394037e.js";import"./QueryCount-c5314da9.js";import"./react-select.esm-9cf201a4.js";import"./IconList-99054850.js";import"./UserCardList-16e98235.js";import"./useAccessRequirements-ed158043.js";import"./RestrictionInformation-4ed2591e.js";import"./DialogTitle-e0a7d646.js";import"./DialogContent-b66fe0a3.js";import"./ManagedACTAccessRequirementStatus-9671fe8d.js";import"./Form-532b074a.js";import"./FormLabel-be95a1b6.js";import"./Col-1f1afacc.js";import"./FormGroup-672b2c7b.js";import"./FileUpload-fdcd3952.js";import"./UserSearchBox-d93d4d15.js";import"./UserOrTeamBadge-0eef46f6.js";import"./EntityLink-97e47fda.js";import"./ErrorChip-5f986b57.js";import"./Avatar-82471a64.js";import"./SynapseVideo-45ccf3b0.js";const Xo={title:"Markdown/MarkdownSynapse",component:W},r={args:{markdown:"*markdown* given to the **component**"}},t={args:{ownerId:"syn12666371",wikiId:"585317",loadingSkeletonRowCount:20}},o={args:{ownerId:"syn18142975"}},e={args:{markdown:`Button link demo 
+
+
+\${buttonlink?text=Align%20Left&align=left} 
+
+
+\${buttonlink?text=Align%20Right&align=right} 
+
+
+\${buttonlink?text=Align%20Center&align=center} 
+
+
+\${buttonlink?text=Highlight&highlight=true} 
+
+
+Links to synapse: 
+
+\${buttonlink?text=This%20Button%20Links%20to%20Synapse&url=https://synapse.org/}
+  `}},i={args:{markdown:"Note: you must be signed in to see this image\n${image?synapseId=syn36695878}"}},n={args:{markdown:'${plot?query=select "id"%2C "createdOn" from syn23567477&title=&type=BAR&barmode=GROUP&horizontal=false&showlegend=true}'}},p={args:{markdown:`# Synapse Table
+Modify the markdown control to change the parameters
+\${synapsetable?query=SELECT %2A FROM syn26302617&showquery=false&tableonly=false}`}},m={args:{markdown:`# Provenance Graphs
+Multiple start nodes
+\${provenance?entityList=syn12548902%2Csyn33344762&depth=3&displayHeightPx=800&showExpand=false}
+Specify the entity version
+\${provenance?entityList=syn12548902%2Fversion%2F34&depth=1&displayHeightPx=500&showExpand=true}`}},s={args:{ownerId:"syn5585645",wikiId:"493662"}},a={args:{ownerId:"syn38870076"}},d={args:{markdown:"${iduReport?accessRestrictionId=9605700}"}};var c,l,g;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    markdown: '*markdown* given to the **component**'
+  }
+}`,...(g=(l=r.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var u,y,k;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    ownerId: 'syn12666371',
+    wikiId: '585317',
+    loadingSkeletonRowCount: 20
+  }
+}`,...(k=(y=t.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var h,w,S;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    ownerId: 'syn18142975'
+  }
+}`,...(S=(w=o.parameters)==null?void 0:w.docs)==null?void 0:S.source}}};var I,b,$;e.parameters={...e.parameters,docs:{...(I=e.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    markdown: \`Button link demo \\n
+
+\\\${buttonlink?text=Align%20Left&align=left} \\n
+
+\\\${buttonlink?text=Align%20Right&align=right} \\n
+
+\\\${buttonlink?text=Align%20Center&align=center} \\n
+
+\\\${buttonlink?text=Highlight&highlight=true} \\n
+
+Links to synapse: \\n
+\\\${buttonlink?text=This%20Button%20Links%20to%20Synapse&url=https://synapse.org/}
+  \`
+  }
+}`,...($=(b=e.parameters)==null?void 0:b.docs)==null?void 0:$.source}}};var f,x,R;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    markdown: 'Note: you must be signed in to see this image\\n\${image?synapseId=syn36695878}'
+  }
+}`,...(R=(x=i.parameters)==null?void 0:x.docs)==null?void 0:R.source}}};var L,M,v;n.parameters={...n.parameters,docs:{...(L=n.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    markdown: '\${plot?query=select "id"%2C "createdOn" from syn23567477&title=&type=BAR&barmode=GROUP&horizontal=false&showlegend=true}'
+  }
+}`,...(v=(M=n.parameters)==null?void 0:M.docs)==null?void 0:v.source}}};var P,C,H;p.parameters={...p.parameters,docs:{...(P=p.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    markdown: '# Synapse Table\\nModify the markdown control to change the parameters\\n\${synapsetable?query=SELECT %2A FROM syn26302617&showquery=false&tableonly=false}'
+  }
+}`,...(H=(C=p.parameters)==null?void 0:C.docs)==null?void 0:H.source}}};var T,A,B;m.parameters={...m.parameters,docs:{...(T=m.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    markdown: '# Provenance Graphs\\nMultiple start nodes\\n\${provenance?entityList=syn12548902%2Csyn33344762&depth=3&displayHeightPx=800&showExpand=false}\\nSpecify the entity version\\n\${provenance?entityList=syn12548902%2Fversion%2F34&depth=1&displayHeightPx=500&showExpand=true}'
+  }
+}`,...(B=(A=m.parameters)==null?void 0:A.docs)==null?void 0:B.source}}};var E,F,O;s.parameters={...s.parameters,docs:{...(E=s.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    ownerId: 'syn5585645',
+    wikiId: '493662'
+  }
+}`,...(O=(F=s.parameters)==null?void 0:F.docs)==null?void 0:O.source}}};var q,D,G;a.parameters={...a.parameters,docs:{...(q=a.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    ownerId: 'syn38870076'
+  }
+}`,...(G=(D=a.parameters)==null?void 0:D.docs)==null?void 0:G.source}}};var U,z,N;d.parameters={...d.parameters,docs:{...(U=d.parameters)==null?void 0:U.docs,source:{originalSource:`{
+  args: {
+    markdown: '\${iduReport?accessRestrictionId=9605700}'
+  }
+}`,...(N=(z=d.parameters)==null?void 0:z.docs)==null?void 0:N.source}}};const Yo=["HardCodedMarkdown","WikiPage","ImageDemo","ButtonLink","ImageBySynID","Plot","SynapseTable","MarkdownProvenanceGraph","HtmlRenderingTest","LargeHtmlFileRenderingTest","MarkdownIDUReport"];export{e as ButtonLink,r as HardCodedMarkdown,s as HtmlRenderingTest,i as ImageBySynID,o as ImageDemo,a as LargeHtmlFileRenderingTest,d as MarkdownIDUReport,m as MarkdownProvenanceGraph,n as Plot,p as SynapseTable,t as WikiPage,Yo as __namedExportsOrder,Xo as default};
+//# sourceMappingURL=MarkdownSynapse.stories-08567a6c.js.map
