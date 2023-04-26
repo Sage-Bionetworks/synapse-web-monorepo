3c367e531b3988258b9111d13e69108a208a9dea
Jay Hodgson <jay.hodgson@sagebase.org>
2023-04-26 10:55:55 -0700

    clean up ELITE portal explore sections

--- apps/portals/src/configurations/elportal/resources.ts
+++ apps/portals/src/configurations/elportal/resources.ts
@@ -1,4 +1,3 @@
-export const computationalSql = 'Select * from syn20337467'
 export const dataSql = 'SELECT * FROM syn51228429'
 export const dataOnStudiesPageSql =
   "SELECT id, name, metadataType, dataType, assay FROM syn11346063.34 WHERE `resourceType` = 'metadata'"
@@ -6,11 +5,3 @@ export const peopleSql = 'SELECT * FROM syn51209684'
 export const projectsSql = 'SELECT * FROM syn51209786 ORDER BY isFeatured DESC'
 export const publicationsSql = 'SELECT * FROM syn51209321'
 export const studiesSql = 'SELECT * FROM syn51209885 ORDER BY isFeatured DESC'
-
-export const experimentalModelsSql = 'select * from syn22219805 ORDER BY isFeatured DESC'
-
-// Convert the `title` column to markdown, linking to the `link` column
-export const targetEnablingResourcesExploreSql =
-  'select * from syn26146692 WHERE `isPublic` = true'
-export const targetEnablingResourcesDetailsPageSql =
-  'select * from syn26146692 WHERE `isPublic` = true'
