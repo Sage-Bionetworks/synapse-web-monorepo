0f635addd5ec65eab956889bd4154dadeecd6239
nickgros <nickgrosenbacher@gmail.com>
2023-04-27 11:53:56 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@bc4c68faaf63fb479a995ef60395a2a4038398d2 🚀

---
+++ assets/LoadingScreen.stories-a217f3c0.js
@@ -0,0 +1,10 @@
+import{B as s}from"./LoadingScreen-43345a4f.js";import"./jsx-runtime-ad672792.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./extends-98964cd2.js";import"./Button-7d415009.js";import"./createWithBsPrefix-e09f51dd.js";import"./contains-60f9209b.js";import"./useWaitForDOMRef-b2ebcbef.js";import"./divWithClassName-1aeead00.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./index-96c5f47c.js";import"./index-4d501b15.js";import"./usePrevious-9f30f8c7.js";import"./removeClass-164fd327.js";import"./hasClass-ec9efd32.js";import"./Typography-f5dd8748.js";import"./styled-8837a0b3.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-3d5299f2.js";import"./extendSxProp-a6801df0.js";import"./useTheme-c4678cf9.js";import"./emotion-react.browser.esm-a8a50fc7.js";const I={title:"UI/Loaders/BlockingLoader",component:s},o={args:{show:!0,currentProgress:50,totalProgress:100,headlineText:"Loading some content",hintText:"This blocks the whole page, so use sparingly"}};var r,t,e;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    show: true,
+    currentProgress: 50,
+    totalProgress: 100,
+    headlineText: 'Loading some content',
+    hintText: 'This blocks the whole page, so use sparingly'
+  }
+}`,...(e=(t=o.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const O=["Demo"];export{o as Demo,O as __namedExportsOrder,I as default};
+//# sourceMappingURL=LoadingScreen.stories-a217f3c0.js.map
--- assets/isRequiredForA11y-0a29da5f.js
+++
@@ -1,2 +0,0 @@
-var n={},h={get exports(){return n},set exports(r){n=r}};(function(r,t){Object.defineProperty(t,"__esModule",{value:!0}),t.default=c;function c(l){return function(a,u,i,o,f){var v=i||"<<anonymous>>",g=f||u;if(a[u]==null)return new Error("The "+o+" `"+g+"` is required to make "+("`"+v+"` accessible for users of assistive ")+"technologies such as screen readers.");for(var s=arguments.length,d=Array(s>5?s-5:0),e=5;e<s;e++)d[e-5]=arguments[e];return l.apply(void 0,[a,u,i,o,f].concat(d))}}r.exports=t.default})(h,n);
-//# sourceMappingURL=isRequiredForA11y-0a29da5f.js.map
