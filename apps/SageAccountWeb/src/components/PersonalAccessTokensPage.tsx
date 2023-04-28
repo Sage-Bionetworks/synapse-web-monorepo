20c3823e305ce4821d0a687761b16cb0d049e51f
Jay Hodgson <jay.hodgson@sagebase.org>
2023-04-28 10:07:42 -0700

    give personal access tokens full width of inner container

--- apps/SageAccountWeb/src/components/PersonalAccessTokensPage.tsx
+++ apps/SageAccountWeb/src/components/PersonalAccessTokensPage.tsx
@@ -1,35 +1,34 @@
 import React from 'react'
 import { SourceAppLogo } from './SourceApp'
-import { Typography } from '@mui/material'
-import { LeftRightPanel } from './LeftRightPanel'
 import { SynapseComponents } from 'synapse-react-client'
+import { StyledOuterContainer } from './StyledComponents'
+import { Box, Paper, Typography } from '@mui/material'
 
 export const PersonalAccessTokensPage = () => {
   return (
-    <LeftRightPanel
-      className={'PersonalAccessTokensPage'}
-      leftContent={
-        <div>
-          <SourceAppLogo sx={{ textAlign: 'center' }} />
-          <SynapseComponents.AccessTokenPage title="" body="" />
-        </div>
-      }
-      rightContent={
-        <div className={'panel-right-text'}>
-          <Typography
-            variant="headline3"
-            sx={{ paddingBottom: '20px', fontWeight: 600 }}
-          >
-            Personal Access Tokens
-          </Typography>
-          <Typography variant="body1" sx={{ paddingBottom: '10px' }}>
-            Issue personal access tokens to access your Synapse resources in the
+    <StyledOuterContainer className="PersonalAccessTokenPage">
+      <Paper
+        className="SageResourcesPage"
+        sx={{
+          margin: '0 auto',
+          width: '900px',
+          '& .SourceAppLogo': {
+            display: 'block',
+          },
+        }}
+      >
+        <Box sx={{ padding: '60px' }}>
+          <SourceAppLogo sx={{ display: 'block' }} />
+          <Typography variant="headline3">Personal Access Tokens</Typography>
+          <SynapseComponents.AccessTokenPage
+            title=""
+            body="Issue personal access tokens to access your Synapse resources in the
             command line clients. A personal access token will expire if it is
             unused for 180 consecutive days. You may create up to 100 personal
-            access tokens.
-          </Typography>
-        </div>
-      }
-    ></LeftRightPanel>
+            access tokens."
+          />
+        </Box>
+      </Paper>
+    </StyledOuterContainer>
   )
 }
