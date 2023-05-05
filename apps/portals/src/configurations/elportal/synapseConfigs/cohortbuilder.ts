7396a2365fe5cdf709a86110fa0463280eebdb4e
Jay Hodgson <jay.hodgson@sagebase.org>
2023-05-05 14:44:48 -0700

    EC-109: do the items that were more straightforward.  Add placeholder cohortbuilder route definition

---
+++ apps/portals/src/configurations/elportal/synapseConfigs/cohortbuilder.ts
@@ -0,0 +1,66 @@
+import { SynapseConfig } from 'types/portal-config'
+import { dataSql } from '../resources'
+
+const rgbIndex = 1
+
+const cohortbuilder: SynapseConfig = {
+  name: 'QueryWrapperPlotNav',
+  props: {
+    rgbIndex,
+    name: 'Data',
+    // showExportToCavatica: true,
+    visibleColumnCount: 10,
+    tableConfiguration: {
+      showAccessColumn: true,
+      showDownloadColumn: true,
+      columnLinks: [
+        {
+          matchColumnName: 'study',
+          isMarkdown: false,
+          baseURL: 'Explore/Studies/DetailsPage',
+          URLColumnName: 'Study_Name',
+          wrapValueWithParens: true,
+        },
+      ],
+    },
+    sql: dataSql,
+    searchConfiguration: {
+      searchable: [
+        'study',
+        'dataType',
+        'assay',
+        'organ',
+        'tissue',
+        'species',
+        'diagnosis',
+        'sex',
+        'consortium',
+        'grant',
+        'modelSystemName',
+        'treatmentType',
+        'specimenID',
+        'individualID',
+        'individualIdSource',
+        'specimenIdSource',
+        'resourceType',
+        'dataSubtype',
+        'metadataType',
+        'assayTarget',
+        'analysisType',
+        'cellType',
+        'nucleicAcidSource',
+        'fileFormat',
+        'group',
+        'name',
+        'isModelSystem',
+        'isConsortiumAnalysis',
+        'isMultiSpecimen',
+        'metaboliteType',
+        'chromosome',
+      ],
+    },
+    shouldDeepLink: true,
+  },
+}
+
+export default cohortbuilder
