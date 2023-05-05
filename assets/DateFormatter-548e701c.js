e442efd6c2a6a8eb9e88546d77e5bc9263491145
nickgros <nickgrosenbacher@gmail.com>
2023-05-05 18:06:45 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@64d7aa38302073a6559c6c5d6ecce7035ee4bd9b 🚀

---
+++ assets/DateFormatter-548e701c.js
@@ -0,0 +1,2 @@
+import{x as m}from"./EntityTypeUtils-5f3d9964.js";import"./index-8db94870.js";import"./getEndpoint-ac94413e.js";import"./jsx-runtime-095bf462.js";import{d as o}from"./dayjs.min-8d4ef725.js";import{u as e}from"./utc-c90baebf.js";o.extend(e);function c(r,t="M/D/YYYY h:mm A"){return m()?o.utc(r).format(t)+" UTC":o(r).format(t)}export{c as f};
+//# sourceMappingURL=DateFormatter-548e701c.js.map
