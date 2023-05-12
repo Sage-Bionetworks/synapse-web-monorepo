e3621b7fa985843e1022aeb380e6be4a6e2176ea
nickgros <nickgrosenbacher@gmail.com>
2023-05-12 14:19:24 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@d3e3ac9da52918b43574386e5e00a3c0a6c683d7 🚀

---
+++ assets/UserCard.stories-4afbf3a2.js
@@ -0,0 +1,19 @@
+import{a as I,F as z,j as m}from"./jsx-runtime-095bf462.js";import{S as L,H as E,y as M,L as f,at as D}from"./SynapseConstants-7100b9c0.js";import{bw as w}from"./EntityTypeUtils-37551199.js";import{U as p}from"./UserCard-a3377195.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./inheritsLoose-c82a83d4.js";import"./isArray-5e3f9107.js";import"./index-8ce4a492.js";import"./extends-98964cd2.js";import"./getEndpoint-ac94413e.js";import"./SvgIcon-4c933a0b.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./styled-f64e11ba.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-29d231ba.js";import"./IconSvg-6ba2e879.js";import"./Tooltip-d0fc02c5.js";import"./useTheme-14d0ce71.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./TransitionGroupContext-0df7f063.js";import"./Clear-72bafe95.js";import"./Box-13f1aabd.js";import"./extendSxProp-29a14132.js";import"./Button-891a522f.js";import"./ButtonBase-4f599170.js";import"./emotion-react.browser.esm-4b1ddfee.js";import"./assertThisInitialized-081f9914.js";import"./Button-5637ed55.js";import"./Alert-57f06f06.js";import"./hook-f132a7c0.js";import"./createWithBsPrefix-57e9f06a.js";import"./divWithClassName-ac73dc31.js";import"./index-58d3fd43.js";import"./SkeletonTable-9b354783.js";import"./times-85a1b1aa.js";import"./_baseTimes-8cccc40f.js";import"./isSymbol-7c514724.js";import"./Skeleton-3aad52b9.js";import"./ToastMessage-f9dfa353.js";import"./FullWidthAlert-1ed130e0.js";import"./Typography-0933b2f6.js";import"./removeClass-164fd327.js";import"./hasClass-ec9efd32.js";import"./uniqueId-4d05949d.js";import"./toString-cc90cb98.js";import"./Popover-872c5707.js";import"./Modal-efa0c968.js";import"./Fade-e4fb8afd.js";import"./getScrollbarSize-ac846fe6.js";import"./ownerWindow-2c76219e.js";import"./createChainedFunction-0bab83cf.js";import"./Paper-2b624eab.js";import"./Link-2921f78c.js";const vr={title:"UI/UserCard",component:p,render:a=>{const{data:i}=w();let s=i==null?void 0:i.ownerId;return s===D.toString()&&(s=void 0),I(z,{children:[m("p",{children:"If you are logged in, your avatar or card will appear. If you are not logged in, enter an ownerId (e.g. 273960) or alias (e.g. brucehoff) below."}),m(p,{ownerId:a.ownerId??s,size:a.size??L,...a})]})}},r={args:{size:E,avatarSize:"SMALL"}},o={args:{size:L}},e={args:{size:M}},t={args:{size:f}};var n,d,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    size: AVATAR,
+    avatarSize: 'SMALL'
+  }
+}`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,A,S;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: SMALL_USER_CARD
+  }
+}`,...(S=(A=o.parameters)==null?void 0:A.docs)==null?void 0:S.source}}};var g,U,l;e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: MEDIUM_USER_CARD
+  }
+}`,...(l=(U=e.parameters)==null?void 0:U.docs)==null?void 0:l.source}}};var R,C,_;t.parameters={...t.parameters,docs:{...(R=t.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    size: LARGE_USER_CARD
+  }
+}`,...(_=(C=t.parameters)==null?void 0:C.docs)==null?void 0:_.source}}};const xr=["Avatar","SmallUserCard","MediumUserCard","LargeUserCard"];export{r as Avatar,t as LargeUserCard,e as MediumUserCard,o as SmallUserCard,xr as __namedExportsOrder,vr as default};
+//# sourceMappingURL=UserCard.stories-4afbf3a2.js.map
