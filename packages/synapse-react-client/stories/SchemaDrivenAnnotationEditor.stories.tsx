8d5c74dfbe2bb9bd13fc1cdfe4850008cb8768db
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-05-12 13:45:39 -0700

    PORTALS-2624: refactor SchemaDrivenAnnotationEditor to use MUI components

--- packages/synapse-react-client/stories/SchemaDrivenAnnotationEditor.stories.tsx
+++ packages/synapse-react-client/stories/SchemaDrivenAnnotationEditor.stories.tsx
@@ -5,11 +5,7 @@ import { SchemaDrivenAnnotationEditor } from '../src/lib/containers/entity/annot
 const meta = {
   title: 'Synapse/SchemaDrivenAnnotationEditor',
   component: SchemaDrivenAnnotationEditor,
-  render: args => (
-    <div className="bootstrap-4-backport">
-      <SchemaDrivenAnnotationEditor {...args} />
-    </div>
-  ),
+  render: args => <SchemaDrivenAnnotationEditor {...args} />,
 } satisfies Meta
 export default meta
 type Story = StoryObj<typeof meta>
@@ -17,5 +13,11 @@ type Story = StoryObj<typeof meta>
 export const ComplexSchema: Story = {
   args: {
     schemaId: 'nkauer-ad.main',
+  },
+}
+
+export const ErrorsEntity: Story = {
+  args: {
+    entityId: 'syn25959288',
   },
 }
