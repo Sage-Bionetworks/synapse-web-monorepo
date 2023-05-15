1038e0650ab88e5ab5d912eaaa45c197034039da
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2023-05-15 09:13:46 -0700

    clean up story per PR

--- packages/synapse-react-client/stories/SchemaDrivenAnnotationEditor.stories.ts
+++ packages/synapse-react-client/stories/SchemaDrivenAnnotationEditor.stories.ts
@@ -1,11 +1,9 @@
-import React from 'react'
 import { Meta, StoryObj } from '@storybook/react'
 import { SchemaDrivenAnnotationEditor } from '../src/lib/containers/entity/annotations/SchemaDrivenAnnotationEditor'
 
 const meta = {
   title: 'Synapse/SchemaDrivenAnnotationEditor',
   component: SchemaDrivenAnnotationEditor,
-  render: args => <SchemaDrivenAnnotationEditor {...args} />,
 } satisfies Meta
 export default meta
 type Story = StoryObj<typeof meta>
