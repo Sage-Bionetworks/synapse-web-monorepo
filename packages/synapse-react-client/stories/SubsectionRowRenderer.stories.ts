9bd666df5b39d2b1863fae32777cf39730a540f2
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-05-19 09:50:51 -0400

    Stabilize a few UI checks in Chromatic - Load Lato font in Storybook preview-head - Update SubsectionRowRenderer demo to use existing table

--- packages/synapse-react-client/stories/SubsectionRowRenderer.stories.ts
+++ packages/synapse-react-client/stories/SubsectionRowRenderer.stories.ts
@@ -9,9 +9,12 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Demo: Story = {
+  name: 'SubsectionRowRenderer',
   args: {
-    sql: 'SELECT funderName as "Funding Agency" FROM syn26344829',
-    searchParams: { Resource_id: '844b598c-0171-4972-91c3-27aa21b45d52' },
+    sql: 'SELECT abstract as "Summary" FROM syn21918972',
+    searchParams: {
+      grantNumber: 'CA217655',
+    },
     isMarkdown: false,
   },
 }
