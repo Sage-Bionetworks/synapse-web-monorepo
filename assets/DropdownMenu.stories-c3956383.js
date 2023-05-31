fea7cd9897c56325fd1b0bfc19fb11510ef0e336
jay-hodgson <jay.hodgson@sagebase.org>
2023-05-31 17:43:49 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@4c173bc0c153c12eb3d38a48e2eb54ceb8de1134 🚀

---
+++ assets/DropdownMenu.stories-c3956383.js
@@ -0,0 +1,29 @@
+import{j as p}from"./jsx-runtime-095bf462.js";import{D as m}from"./DropdownMenu-a950ca8e.js";import{I as s}from"./IconSvg-d5f86620.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Button-7a4e3dd9.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./extends-98964cd2.js";import"./styled-8ebe2b7d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-29d231ba.js";import"./ButtonBase-92b85091.js";import"./emotion-react.browser.esm-cc724fea.js";import"./hoist-non-react-statics.cjs-775f1375.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-53ae1513.js";import"./useForkRef-2674f3de.js";import"./useEventCallback-65e61675.js";import"./useEnhancedEffect-c45cae33.js";import"./Typography-fa1f2f14.js";import"./extendSxProp-8b7baf65.js";import"./Tooltip-fcb4b0de.js";import"./useTheme-df5273c4.js";import"./ownerDocument-613eb639.js";import"./index-8ce4a492.js";import"./isHostComponent-73d6e646.js";import"./useControlled-be22aa93.js";import"./utils-8d96ae5c.js";import"./Fade-79bf68a5.js";import"./Paper-ca64eb44.js";import"./ClickAwayListener-73754b4f.js";import"./MenuList-3617d530.js";import"./react-is.production.min-a192e302.js";import"./getScrollbarSize-ac846fe6.js";import"./MenuItem-2c9fad3f.js";import"./dividerClasses-017f8642.js";import"./Divider-110b9a1e.js";import"./SvgIcon-147e032d.js";import"./createSvgIcon-075f7759.js";import"./ErrorOutlined-0f626467.js";import"./InfoOutlined-3ee3099a.js";import"./CheckCircleTwoTone-64ad58ef.js";const X={title:"UI/DropdownMenu",component:m},o=n=>()=>{console.log("Item clicked",n)},t={args:{dropdownButtonText:"Dropdown Menu Button",buttonProps:{endIcon:p(s,{icon:"verticalEllipsis",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}};var i,r,e;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    dropdownButtonText: 'Dropdown Menu Button',
+    buttonProps: {
+      endIcon: <IconSvg icon="verticalEllipsis" wrap={false} />
+    },
+    items: [[{
+      text: 'Do something cool',
+      onClick: onClickHandler(0)
+    }, {
+      text: 'This one has a tooltip',
+      tooltipText: 'Some more info',
+      onClick: onClickHandler(1)
+    }, {
+      text: 'This one is disabled',
+      disabled: true,
+      onClick: onClickHandler(3)
+    }, {
+      text: 'This one has both',
+      disabled: true,
+      tooltipText: "You can't do this for reasons",
+      onClick: onClickHandler(4)
+    }], [{
+      text: 'You can organize actions into groups',
+      onClick: onClickHandler(5)
+    }]]
+  }
+}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const Z=["Demo"];export{t as Demo,Z as __namedExportsOrder,X as default};
+//# sourceMappingURL=DropdownMenu.stories-c3956383.js.map
---
+++ assets/IconSvg.stories-ee823ff0.js
@@ -0,0 +1,10 @@
+import{j as t,a as s}from"./jsx-runtime-095bf462.js";import{I as i,b as a}from"./IconSvg-d5f86620.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./SvgIcon-147e032d.js";import"./extends-98964cd2.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./styled-8ebe2b7d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-29d231ba.js";import"./Tooltip-fcb4b0de.js";import"./useTheme-df5273c4.js";import"./ownerDocument-613eb639.js";import"./index-8ce4a492.js";import"./useForkRef-2674f3de.js";import"./useEnhancedEffect-c45cae33.js";import"./isHostComponent-73d6e646.js";import"./useControlled-be22aa93.js";import"./useEventCallback-65e61675.js";import"./utils-8d96ae5c.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-53ae1513.js";import"./createSvgIcon-075f7759.js";import"./ErrorOutlined-0f626467.js";import"./InfoOutlined-3ee3099a.js";import"./CheckCircleTwoTone-64ad58ef.js";const B={title:"UI/IconSvg",component:i,render:n=>t("div",{style:{display:"flex",flexWrap:"wrap",flexDirection:"row"},children:a.sort().map(r=>s("div",{style:{margin:"10px",textAlign:"center"},children:[t("div",{children:t(i,{...n,icon:r,label:r})}),t("div",{style:{fontSize:"10px"},children:r})]},r))})},o={args:{sx:{color:"primary.main",width:"40px",height:"40px"}}};var p,m,e;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    sx: {
+      color: 'primary.main',
+      width: '40px',
+      height: '40px'
+    }
+  }
+}`,...(e=(m=o.parameters)==null?void 0:m.docs)==null?void 0:e.source}}};const C=["Icon"];export{o as Icon,C as __namedExportsOrder,B as default};
+//# sourceMappingURL=IconSvg.stories-ee823ff0.js.map
