048911c811ee9c453c3695dedf25553e7d76a7e5
Jay Hodgson <jay.hodgson@sagebase.org>
2023-06-01 15:51:53 -0700

    Refactor custom commands into new UI based on the selection design

--- packages/synapse-react-client/src/components/SynapseTable/TopLevelControls.tsx
+++ packages/synapse-react-client/src/components/SynapseTable/TopLevelControls.tsx
@@ -20,6 +20,7 @@ import { Icon } from '../row_renderers/utils'
 import MissingQueryResultsWarning from '../MissingQueryResultsWarning'
 import { useExportToCavatica } from '../../synapse-queries/entity/useExportToCavatica'
 import { Cavatica } from '../../assets/icons/Cavatica'
+import { RowSelectionControls } from './RowSelectionControls'
 
 export type TopLevelControlsProps = {
   name?: string
@@ -33,7 +34,7 @@ export type TopLevelControlsProps = {
   showExportToCavatica?: boolean
 }
 
-type Control = {
+export type Control = {
   key: keyof TopLevelControlsState
   icon: string
   tooltipText: string
@@ -45,7 +46,7 @@ export type CustomControlCallbackData = {
   refresh: () => void
 }
 
-type CustomControl = {
+export type CustomControl = {
   buttonText: string
   onClick: (event: CustomControlCallbackData) => void
   classNames?: string
@@ -214,25 +215,6 @@ const TopLevelControls = (props: TopLevelControlsProps) => {
           )}
         </div>
         <div className="TopLevelControls__actions">
-          {customControls &&
-            customControls.map(customControl => {
-              return (
-                <button
-                  key={customControl.buttonText}
-                  className={`btn SRC-roundBorder SRC-primary-background-color SRC-whiteText ${
-                    customControl.classNames ?? ''
-                  }`}
-                  style={{ marginRight: '5px' }}
-                  type="button"
-                  onClick={() =>
-                    customControl.onClick({ data, selectedRows, refresh })
-                  }
-                >
-                  {customControl.icon}&nbsp;
-                  {customControl.buttonText}
-                </button>
-              )
-            })}
           {showExportToCavatica && (
             <Button
               variant="text"
@@ -271,6 +253,15 @@ const TopLevelControls = (props: TopLevelControlsProps) => {
               />
             )
           })}
+          {customControls && (
+            <RowSelectionControls
+              customControls={customControls}
+              data={data}
+              refresh={refresh}
+              selectedRows={selectedRows}
+              setSelectedRows={setSelectedRows}
+            />
+          )}
           {showColumnSelection && (
             <ColumnSelection
               headers={data?.selectColumns}
