2d3a5652c4f8b8008b7dd5fd5248bb7fc7dea90f
Jay Hodgson <jay.hodgson@sagebase.org>
2023-06-06 11:05:30 -0700

    PORTALS-2685: Small fixes, now with a No Content UI if no plots are defined for the Featured Data Tabs component

--- apps/portals/src/configurations/elportal/resources.ts
+++ apps/portals/src/configurations/elportal/resources.ts
@@ -1,10 +1,10 @@
 export const dataSql = 'SELECT * FROM syn51228429'
 export const dataOnStudiesPageSql =
   "SELECT * FROM syn51228429 WHERE `resourceType` = 'metadata'"
-export const peopleSql = 'SELECT * FROM syn51209684 ORDER BY firstName'
+export const peopleSql = 'SELECT * FROM syn51209684'
 export const projectsSql = 'SELECT * FROM syn51209786'
 export const publicationsSql = 'SELECT * FROM syn51209321'
 export const studiesSql = 'SELECT * FROM syn51209885 ORDER BY isFeatured DESC'
 export const speciesSql = 'SELECT * FROM syn51449110 ORDER BY ItemOrder'
 export const computationalSql = 'SELECT * FROM syn51469335'
-export const upsetPlotSql = 'SELECT individualID, assay FROM syn51581771'
+export const upsetPlotSql = 'SELECT files, assay FROM syn51581771'
