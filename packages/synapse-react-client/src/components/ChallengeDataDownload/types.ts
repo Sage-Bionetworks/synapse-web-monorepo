0e9c36f07c801c9b70394e10dd0501d16c87c74a
Geoffrey Roth <geoffroth@gmail.com>
2023-06-07 10:20:09 -0400

    Remove disused types

--- packages/synapse-react-client/src/components/ChallengeDataDownload/types.ts
+++ packages/synapse-react-client/src/components/ChallengeDataDownload/types.ts
@@ -1,6 +1,4 @@
-import { EntityHeader, EntityType } from '@sage-bionetworks/synapse-types'
 import BaseTable, { ColumnShape } from '@sage-bionetworks/react-base-table'
-import { VersionSelectionType } from '../EntityFinder/VersionSelectionType'
 
 export type CellRendererProps<T> = {
   cellData: any
@@ -11,29 +9,4 @@ export type CellRendererProps<T> = {
   rowIndex: number
   container: BaseTable<T>
   isScrolling?: boolean
-}
-
-export interface ChallengeDataTableProps {
-  hasNextPage?: boolean
-  isFetchingNextPage?: boolean
-  isLoading?: boolean
-  minTableWidth?: number
-  tableData: EntityHeader[]
-  tableHeight?: number
-  versionSelection: VersionSelectionType
-}
-
-/**
- * Describes the shape of the data passed to the BaseTable
- */
-export type ChallengeDataTableRowData = EntityHeader & {
-  currentSelectedVersion?: number
-  entityId: string
-  entityType: EntityType
-  isDisabled: boolean
-  isSelected: boolean
-  isVersionableEntity: boolean
-  onChangeSelected: (newValue: boolean) => void
-  size: number
-  versionNumber?: number
 }
