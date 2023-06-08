84352b64064b59f9ac3157d25650f7714f7952d3
jay-hodgson <jay.hodgson@sagebase.org>
2023-06-08 23:21:18 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@5e38b559457a4e086efa8a8153de53c4a3e014f3 🚀

---
+++ assets/Login.stories-8b236713.js
@@ -0,0 +1,15 @@
+import{S as p}from"./StandaloneLoginForm-6f98e2fc.js";import{d as e}from"./ToastMessage-c60353c5.js";import{s as n}from"./StorybookComponentWrapper-59437fdd.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./LoginFlowBackButton-2546bbc2.js";import"./SynapseConstants-fca8ab5f.js";import"./OrientationBanner-ec8fcbed.js";import"./FullWidthAlert-ac112c77.js";import"./AlertTitle-1c4986af.js";import"./styled-0d0c058b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./createSvgIcon-c04c1e3c.js";import"./Paper-c5bb6f3a.js";import"./IconButton-055a3ecb.js";import"./ButtonBase-89336bd6.js";import"./emotion-react.browser.esm-a1e35fdb.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";import"./Typography-bbc222e1.js";import"./extendSxProp-32377841.js";import"./Stack-fc7a4170.js";import"./Box-ef0af35d.js";import"./useTheme-48a0a3fd.js";import"./Grow-abebdd1c.js";import"./isHostComponent-fa76b8d9.js";import"./index-8ce4a492.js";import"./utils-a7612588.js";import"./ClickAwayListener-36b79525.js";import"./Tooltip-ada0ed7d.js";import"./Button-6910d769.js";import"./SynapseClient-f341520e.js";import"./getEndpoint-ac94413e.js";import"./Link-c55ec6e2.js";import"./isArray-5e3f9107.js";import"./dayjs.min-8d4ef725.js";import"./index-58d3fd43.js";import"./CheckCircleTwoTone-a72e35ba.js";import"./inputBaseClasses-2d1e355f.js";import"./Fade-8ab202b4.js";import"./useMutation-90a3309e.js";import"./TextField-7bb2cab4.js";import"./InputLabel-a4beb275.js";import"./ownerWindow-2c76219e.js";import"./isMuiElement-08f54e3c.js";import"./index-953d39fd.js";import"./Popover-35c1d8c6.js";import"./Modal-1e12e009.js";import"./Backdrop-85605ced.js";import"./getScrollbarSize-ac846fe6.js";import"./createChainedFunction-0bab83cf.js";import"./MenuList-e8a7bfde.js";import"./TextField-b3bab9ad.js";import"./IconSvg-b6907485.js";import"./ErrorOutlined-81a5221b.js";import"./InfoOutlined-1e8e00a7.js";import"./hasClass-ec9efd32.js";import"./uniqueId-4d05949d.js";import"./toString-cc90cb98.js";import"./isSymbol-7c514724.js";const yo={title:"Authentication/Login",component:p},o={args:{sessionCallback:()=>{n().then(({profile:m})=>{e(`You are currently logged in as ${m.userName}`,"info",{autoCloseInMs:5e3})})},renderBackButton:!0}};var r,t,i;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    sessionCallback: () => {
+      sessionChangeHandler().then(({
+        profile
+      }) => {
+        displayToast(\`You are currently logged in as \${profile.userName}\`, 'info', {
+          autoCloseInMs: 5000
+        });
+      });
+    },
+    renderBackButton: true
+  }
+}`,...(i=(t=o.parameters)==null?void 0:t.docs)==null?void 0:i.source}}};const Bo=["LoginDemo"];export{o as LoginDemo,Bo as __namedExportsOrder,yo as default};
+//# sourceMappingURL=Login.stories-8b236713.js.map
