a111d1c46a44b0378b70d956c5b6e20d858a6d15
hallieswan <26949006+hallieswan@users.noreply.github.com>
2023-06-08 20:13:48 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@baca985f9989c29af94931dd327426bacf220761 🚀

---
+++ assets/ComplexMenu.stories-e5511b9c.js
@@ -0,0 +1,69 @@
+import{j as i}from"./jsx-runtime-095bf462.js";import{I as n}from"./IconSvg-b6907485.js";import{C as a}from"./ComplexMenu-dda74349.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./createSvgIcon-c04c1e3c.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./styled-0d0c058b.js";import"./Tooltip-ada0ed7d.js";import"./useTheme-48a0a3fd.js";import"./Grow-abebdd1c.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-a7612588.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-81a5221b.js";import"./InfoOutlined-1e8e00a7.js";import"./CheckCircleTwoTone-a72e35ba.js";import"./DropdownMenu-29c176a8.js";import"./Button-6910d769.js";import"./ButtonBase-89336bd6.js";import"./emotion-react.browser.esm-a1e35fdb.js";import"./assertThisInitialized-081f9914.js";import"./Typography-bbc222e1.js";import"./extendSxProp-32377841.js";import"./Fade-8ab202b4.js";import"./Paper-c5bb6f3a.js";import"./ClickAwayListener-36b79525.js";import"./MenuList-e8a7bfde.js";import"./index-953d39fd.js";import"./getScrollbarSize-ac846fe6.js";import"./MenuItem-9b4a4b29.js";import"./dividerClasses-a42a4a88.js";import"./ListItemText-774eab98.js";import"./Divider-ac94dde5.js";import"./Box-ef0af35d.js";import"./IconButton-055a3ecb.js";const R={title:"UI/ComplexMenu",component:a},o=s=>()=>{console.log("Item clicked",s)},t={args:{iconButtons:[{icon:"edit",tooltipText:"This is an icon button",onClick:o("edit icon")},{icon:"label",tooltipText:"View annotations",onClick:o("label icon")},{icon:"createVersion",tooltipText:"Create new version",onClick:o("createVersion icon")}],dropdownMenus:[{dropdownButtonText:"You can have...",buttonProps:{endIcon:i(n,{icon:"upload",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]},{dropdownButtonText:"...multiple dropdown menus!",buttonProps:{endIcon:i(n,{icon:"download",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}]}};var e,r,l;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    iconButtons: [{
+      icon: 'edit',
+      tooltipText: 'This is an icon button',
+      onClick: onClickHandler('edit icon')
+    }, {
+      icon: 'label',
+      tooltipText: 'View annotations',
+      onClick: onClickHandler('label icon')
+    }, {
+      icon: 'createVersion',
+      tooltipText: 'Create new version',
+      onClick: onClickHandler('createVersion icon')
+    }],
+    dropdownMenus: [{
+      dropdownButtonText: 'You can have...',
+      buttonProps: {
+        endIcon: <IconSvg icon="upload" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }, {
+      dropdownButtonText: '...multiple dropdown menus!',
+      buttonProps: {
+        endIcon: <IconSvg icon="download" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }]
+  }
+}`,...(l=(r=t.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const W=["Demo"];export{t as Demo,W as __namedExportsOrder,R as default};
+//# sourceMappingURL=ComplexMenu.stories-e5511b9c.js.map
---
+++ assets/TextField.stories-f2919193.js
@@ -0,0 +1,9 @@
+import{T as m}from"./TextField-b3bab9ad.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./InputLabel-a4beb275.js";import"./styled-0d0c058b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./inputBaseClasses-2d1e355f.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./ownerWindow-2c76219e.js";import"./isHostComponent-fa76b8d9.js";import"./emotion-react.browser.esm-a1e35fdb.js";import"./isMuiElement-08f54e3c.js";const D={title:"UI/SDS/TextField",component:m},r={args:{label:"Label",placeholder:"my placeholder",fullWidth:!0,required:!0}};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    label: 'Label',
+    placeholder: 'my placeholder',
+    fullWidth: true,
+    required: true
+  }
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const S=["Demo"];export{r as Demo,S as __namedExportsOrder,D as default};
+//# sourceMappingURL=TextField.stories-f2919193.js.map
