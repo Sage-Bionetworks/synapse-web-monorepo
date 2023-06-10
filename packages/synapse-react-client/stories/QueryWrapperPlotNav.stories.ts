510f960bfc8a51b59e56cf844a94eeaaf77a16a8
Jay Hodgson <jay.hodgson@sagebase.org>
2023-06-09 17:10:02 -0700

    with cavatica help URL

--- packages/synapse-react-client/stories/QueryWrapperPlotNav.stories.ts
+++ packages/synapse-react-client/stories/QueryWrapperPlotNav.stories.ts
@@ -194,6 +194,8 @@ export const SendToCavatica: Story = {
     hideSqlEditorControl: false,
     shouldDeepLink: false,
     showExportToCavatica: true,
+    cavaticaHelpURL:
+      'https://staging.eliteportal.synapse.org/Limited%20Data%20Commons',
   },
 }
 
