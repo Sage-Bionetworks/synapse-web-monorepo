0bd35d5fa42dcc846e9731459f8956d04348904a
nickgros <nickgrosenbacher@gmail.com>
2023-07-05 17:51:40 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@f5241601c22484d344d997ab64882c3308c4396e 🚀

---
+++ assets/ComplexMenu.stories-eb521980.js
@@ -0,0 +1,69 @@
+import{j as i}from"./jsx-runtime-095bf462.js";import{I as n}from"./IconSvg-1d8aeeb4.js";import{C as a}from"./ComplexMenu-5b7970cf.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./createSvgIcon-e7cd5941.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./styled-6ec5f85d.js";import"./Tooltip-add08016.js";import"./useTheme-9d382341.js";import"./Grow-93737182.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-67eec175.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-ddaff4fd.js";import"./GetAppTwoTone-5ba04530.js";import"./InfoOutlined-eb1a3679.js";import"./CheckCircleTwoTone-108bcaf3.js";import"./DropdownMenu-463fae66.js";import"./Button-ff2bc4b7.js";import"./ButtonBase-43867210.js";import"./emotion-react.browser.esm-b991c11c.js";import"./assertThisInitialized-081f9914.js";import"./Typography-65c9d67d.js";import"./extendSxProp-fe165b74.js";import"./Fade-fdeca465.js";import"./Paper-586cb0df.js";import"./ClickAwayListener-36b79525.js";import"./MenuList-449dd9dd.js";import"./index-953d39fd.js";import"./getScrollbarSize-ac846fe6.js";import"./MenuItem-02ba3f84.js";import"./dividerClasses-252d6ebf.js";import"./ListItemText-dbb51b38.js";import"./Divider-509ab497.js";import"./Box-3a3e81db.js";import"./IconButton-2455b4de.js";const W={title:"UI/ComplexMenu",component:a},o=s=>()=>{console.log("Item clicked",s)},t={args:{iconButtons:[{icon:"edit",tooltipText:"This is an icon button",onClick:o("edit icon")},{icon:"label",tooltipText:"View annotations",onClick:o("label icon")},{icon:"createVersion",tooltipText:"Create new version",onClick:o("createVersion icon")}],dropdownMenus:[{dropdownButtonText:"You can have...",buttonProps:{endIcon:i(n,{icon:"upload",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]},{dropdownButtonText:"...multiple dropdown menus!",buttonProps:{endIcon:i(n,{icon:"download",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}]}};var e,r,l;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    iconButtons: [{
+      icon: 'edit',
+      tooltipText: 'This is an icon button',
+      onClick: onClickHandler('edit icon')
+    }, {
+      icon: 'label',
+      tooltipText: 'View annotations',
+      onClick: onClickHandler('label icon')
+    }, {
+      icon: 'createVersion',
+      tooltipText: 'Create new version',
+      onClick: onClickHandler('createVersion icon')
+    }],
+    dropdownMenus: [{
+      dropdownButtonText: 'You can have...',
+      buttonProps: {
+        endIcon: <IconSvg icon="upload" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }, {
+      dropdownButtonText: '...multiple dropdown menus!',
+      buttonProps: {
+        endIcon: <IconSvg icon="download" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }]
+  }
+}`,...(l=(r=t.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const X=["Demo"];export{t as Demo,X as __namedExportsOrder,W as default};
+//# sourceMappingURL=ComplexMenu.stories-eb521980.js.map
