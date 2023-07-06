4e1f71d1d90f0ebfbedaaf156f179f52857338fc
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-07-06 15:54:40 -0400

    dependency maintenance - Bump dependencies shown in `pnpm audit` that have valid patches - Update SBOM workflow for security updates - Remove project-specific `resolutions` fields (these had no effect on dependency resolution)

--- packages/vite-config/src/vitest-config.ts
+++ packages/vite-config/src/vitest-config.ts
@@ -1,22 +1,22 @@
-import { defineConfig, mergeConfig } from "vitest/config";
-import viteConfig from "./vite-config.js";
-
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite-config.js'
+import type { UserConfig } from 'vite'
 export default mergeConfig(
-  viteConfig,
+  viteConfig as UserConfig,
   defineConfig({
     optimizeDeps: {
-      exclude: ["vitest/utils"],
-      include: ["@vitest/utils", "vitest/browser"],
+      exclude: ['vitest/utils'],
+      include: ['@vitest/utils', 'vitest/browser'],
     },
     test: {
-      environment: "jsdom",
-      reporters: ["default", "html"],
-      outputFile: { html: "./coverage/report/index.html" },
+      environment: 'jsdom',
+      reporters: ['default', 'html'],
+      outputFile: { html: './coverage/report/index.html' },
       coverage: {
-        provider: "c8",
-        reporter: ["text-summary", "html-spa"],
-        reportsDirectory: "./coverage/cov",
+        provider: 'c8',
+        reporter: ['text-summary', 'html-spa'],
+        reportsDirectory: './coverage/cov',
       },
     },
-  })
-);
+  }) as UserConfig,
+)
