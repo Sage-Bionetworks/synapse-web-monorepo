f2a3cb5b2f9b9a4d5790f4251e7c5e1dfd122a60
jay-hodgson <jay.hodgson@sagebase.org>
2023-07-06 18:04:40 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@0665e11e038f4685d8996967838eaecbc5227841 🚀

---
+++ assets/DropdownMenu.stories-aaed41af.js
@@ -0,0 +1,29 @@
+import{j as p}from"./jsx-runtime-095bf462.js";import{D as s}from"./DropdownMenu-463fae66.js";import{I as m}from"./IconSvg-1d8aeeb4.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Button-ff2bc4b7.js";import"./styled-6ec5f85d.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./ButtonBase-43867210.js";import"./emotion-react.browser.esm-b991c11c.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";import"./Typography-65c9d67d.js";import"./extendSxProp-fe165b74.js";import"./Tooltip-add08016.js";import"./useTheme-9d382341.js";import"./Grow-93737182.js";import"./isHostComponent-fa76b8d9.js";import"./index-8ce4a492.js";import"./utils-67eec175.js";import"./Fade-fdeca465.js";import"./Paper-586cb0df.js";import"./ClickAwayListener-36b79525.js";import"./MenuList-449dd9dd.js";import"./index-953d39fd.js";import"./getScrollbarSize-ac846fe6.js";import"./MenuItem-02ba3f84.js";import"./dividerClasses-252d6ebf.js";import"./ListItemText-dbb51b38.js";import"./Divider-509ab497.js";import"./createSvgIcon-e7cd5941.js";import"./ErrorOutlined-ddaff4fd.js";import"./GetAppTwoTone-5ba04530.js";import"./InfoOutlined-eb1a3679.js";import"./CheckCircleTwoTone-108bcaf3.js";const Q={title:"UI/DropdownMenu",component:s},o=n=>()=>{console.log("Item clicked",n)},t={args:{dropdownButtonText:"Dropdown Menu Button",buttonProps:{endIcon:p(m,{icon:"verticalEllipsis",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}};var i,r,e;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    dropdownButtonText: 'Dropdown Menu Button',
+    buttonProps: {
+      endIcon: <IconSvg icon="verticalEllipsis" wrap={false} />
+    },
+    items: [[{
+      text: 'Do something cool',
+      onClick: onClickHandler(0)
+    }, {
+      text: 'This one has a tooltip',
+      tooltipText: 'Some more info',
+      onClick: onClickHandler(1)
+    }, {
+      text: 'This one is disabled',
+      disabled: true,
+      onClick: onClickHandler(3)
+    }, {
+      text: 'This one has both',
+      disabled: true,
+      tooltipText: "You can't do this for reasons",
+      onClick: onClickHandler(4)
+    }], [{
+      text: 'You can organize actions into groups',
+      onClick: onClickHandler(5)
+    }]]
+  }
+}`,...(e=(r=t.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const R=["Demo"];export{t as Demo,R as __namedExportsOrder,Q as default};
+//# sourceMappingURL=DropdownMenu.stories-aaed41af.js.map
---
+++ assets/Topic.stories-daf1f2e3.js
@@ -0,0 +1,10 @@
+import{T as m}from"./Topic-efec10c9.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./IconSvg-1d8aeeb4.js";import"./createSvgIcon-e7cd5941.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-8498a53a.js";import"./styled-6ec5f85d.js";import"./Tooltip-add08016.js";import"./useTheme-9d382341.js";import"./Grow-93737182.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-67eec175.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-ddaff4fd.js";import"./GetAppTwoTone-5ba04530.js";import"./InfoOutlined-eb1a3679.js";import"./CheckCircleTwoTone-108bcaf3.js";import"./ConditionalWrapper-51b8d1ed.js";import"./Stack-f176d42e.js";import"./extendSxProp-fe165b74.js";import"./Button-ff2bc4b7.js";import"./ButtonBase-43867210.js";import"./emotion-react.browser.esm-b991c11c.js";import"./assertThisInitialized-081f9914.js";import"./Skeleton-b660800e.js";import"./Link-a20b00c0.js";import"./Typography-65c9d67d.js";const C={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!"}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Topic',
+  args: {
+    isSubscribed: true,
+    isLoading: false,
+    icon: 'discussion',
+    name: 'Help me understand this project!'
+  }
+}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const G=["Demo"];export{o as Demo,G as __namedExportsOrder,C as default};
+//# sourceMappingURL=Topic.stories-daf1f2e3.js.map
