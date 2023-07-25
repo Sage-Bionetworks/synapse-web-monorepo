7893d2c39f3623b9e255a09ad280b46509eac21f
Jay Hodgson <jay.hodgson@sagebase.org>
2023-07-25 11:42:58 -0700

    PORTALS-2750: in progress

--- apps/portals/src/configurations/nf/synapseConfigs/datasets.ts
+++ apps/portals/src/configurations/nf/synapseConfigs/datasets.ts
@@ -1,33 +1,49 @@
 import { columnAliases } from './commonProps'
 import { datasetsSql } from '../resources'
 import { SynapseConfig } from 'types/portal-config'
-import { SynapseConstants } from 'synapse-react-client'
+import { CardConfiguration, SynapseConstants } from 'synapse-react-client'
+import { ColumnSingleValueFilterOperator } from '@sage-bionetworks/synapse-types'
+import { DetailsPageProps } from 'types/portal-util-types'
 
 export const newDatasetsSql = `${datasetsSql} order by ROW_ID desc limit 3`
-const type = 'dataset'
 const rgbIndex = 8
 
+export const datasetCardConfiguration: CardConfiguration = {
+  type: SynapseConstants.GENERIC_CARD,
+  genericCardSchema: {
+    type: SynapseConstants.DATASET,
+    title: 'title',
+    description: 'description',
+    secondaryLabels: [
+      'diseaseFocus',
+      'fundingAgency',
+      'datasetItemCount',
+      'dataType',
+      'series',
+      'yearProcessed',
+      'datasetSizeInBytes',
+    ],
+  },
+  titleLinkConfig: {
+    isMarkdown: false,
+    matchColumnName: 'id',
+    URLColumnName: 'id',
+    baseURL: 'Explore/Datasets/DetailsPage',
+  },
+}
 const datasets: SynapseConfig = {
   name: 'QueryWrapperPlotNav',
   props: {
     rgbIndex,
     shouldDeepLink: true,
     sql: datasetsSql,
-    cardConfiguration: {
-      type,
-      genericCardSchema: {
-        type: SynapseConstants.DATASET,
-        title: 'datasetName',
-        secondaryLabels: ['series', 'yearProcessed', 'dataType'],
-      },
-      secondaryLabelLimit: 6,
-    },
+    cardConfiguration: datasetCardConfiguration,
     name: 'Datasets',
     columnAliases,
     searchConfiguration: {
       searchable: [
-        'datasetName',
-        'summary',
+        'title',
+        'description',
         'studyName',
         'diseaseFocus',
         'manifestation',
@@ -36,5 +52,63 @@ const datasets: SynapseConfig = {
     },
   },
 }
+
+export const datasetDetailsPageConfig: DetailsPageProps = {
+  sql: datasetsSql,
+  sqlOperator: ColumnSingleValueFilterOperator.EQUAL,
+  showMenu: true,
+
+  synapseConfigArray: [
+    // {
+    //   name: 'Markdown',
+    //   columnName: 'datasetDescription',
+    //   title: 'Description',
+    //   props: {},
+    // },
+    // {
+    //   name: 'Markdown',
+    //   columnName: 'acknowledgmentStatement',
+    //   title: 'Acknowledgment',
+    //   props: {},
+    // },
+    {
+      name: 'QueryWrapperPlotNav',
+      title: 'Files',
+      props: {
+        rgbIndex,
+        sql: '',
+        visibleColumnCount: 7,
+        tableConfiguration: {
+          showAccessColumn: true,
+          showDownloadColumn: true,
+        },
+        shouldDeepLink: false,
+        columnAliases,
+        defaultShowFacetVisualization: false,
+      },
+      // tableSqlKeys: ['id'],  // Do not modify the sql where condition based on search params
+      overrideSqlSourceTable: true, // Instead, modify the sql (SELECT * FROM <search_param_value>).<rowVersionNumber>
+      columnName: 'id',
+    },
+  ],
+}
+
+export const datasetsDetailsPage: SynapseConfig[] = [
+  {
+    name: 'CardContainerLogic',
+    isOutsideContainer: true,
+    props: {
+      ...datasetCardConfiguration,
+      sql: datasetsSql,
+      isHeader: true,
+    },
+  },
+  {
+    name: 'DetailsPage',
+    isOutsideContainer: false,
+    props: datasetDetailsPageConfig,
+    containerClassName: 'DatasetDetailPage container-full-width',
+  },
+]
 
 export default datasets
