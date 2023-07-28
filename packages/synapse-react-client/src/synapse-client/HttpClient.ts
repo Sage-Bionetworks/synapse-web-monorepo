a4fc8eb3dbb5f773afc5f7109821387700585c1e
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-07-28 16:21:35 -0400

    PORTALS-2748 - fix types, refactor SynapseClient for testability
    - Refactor SynapseClient and move utilities to new SynapseClientUtils or new HttpClient. This makes SynapseClient more testable (we can mock out the utility to determine the current hostname)
    - Add tests for SynapseClient.getAccessTokenFromCookie to confirm the return type

---
+++ packages/synapse-react-client/src/synapse-client/HttpClient.ts
@@ -0,0 +1,178 @@
+import {
+  BackendDestinationEnum,
+  getEndpoint,
+} from '../utils/functions/getEndpoint'
+import { SynapseClientError } from '../utils/SynapseClientError'
+import { NETWORK_UNAVAILABLE_MESSAGE } from '../utils/SynapseConstants'
+import { SynapseError } from '../utils/SynapseError'
+
+/**
+ * Waits t number of milliseconds
+ *
+ * @export
+ * @param {number} t milliseconds
+ * @returns after t milliseconds
+ */
+export function delay(t: number) {
+  return new Promise(resolve => {
+    setTimeout(resolve.bind(null, {}), t)
+  })
+}
+
+/*
+  0 - no internet connection
+  429 - Too Many Requests
+  502 - Bad Gateway
+  503 - Service Unavailable
+  504 - Gateway Timeout
+*/
+const RETRY_STATUS_CODES = [0, 429, 502, 503, 504]
+const MAX_RETRY_STATUS_CODES = [502, 503]
+const MAX_RETRY = 3
+/**
+ * Fetches data, retrying if the HTTP status code indicates that it could be retried. Contains custom logic for
+ * handling errors returned by the Synapse backend.
+ * @throws SynapseClientError
+ */
+const fetchWithExponentialTimeout = async <TResponse>(
+  url: RequestInfo,
+  options: RequestInit,
+  delayMs = 1000,
+): Promise<TResponse> => {
+  let response
+  try {
+    response = await fetch(url, options)
+  } catch (err) {
+    console.error(err)
+    throw new SynapseClientError(0, NETWORK_UNAVAILABLE_MESSAGE, url.toString())
+  }
+
+  let numOfTry = 1
+  while (response.status && RETRY_STATUS_CODES.includes(response.status)) {
+    await delay(delayMs)
+    // Exponential backoff if we re-fetch
+    delayMs = delayMs * 2
+    response = await fetch(url, options)
+    if (MAX_RETRY_STATUS_CODES.includes(response.status)) {
+      numOfTry++
+      if (numOfTry == MAX_RETRY) {
+        break
+      }
+    }
+  }
+
+  const contentType = response.headers.get('Content-Type')
+  const responseBody = await response.text()
+  let responseObject: TResponse | SynapseError | string = responseBody
+  try {
+    // try to parse it as json
+    if (contentType && contentType.includes('application/json')) {
+      responseObject = JSON.parse(responseBody) as TResponse | SynapseError
+    }
+  } catch (error) {
+    console.warn('Failed to parse response as JSON', responseBody)
+  }
+
+  if (response.ok) {
+    return responseObject as TResponse
+  } else if (
+    responseObject !== null &&
+    typeof responseObject === 'object' &&
+    'reason' in responseObject
+  ) {
+    throw new SynapseClientError(
+      response.status,
+      responseObject.reason,
+      url.toString(),
+      responseObject,
+    )
+  } else {
+    throw new SynapseClientError(
+      response.status,
+      JSON.stringify(responseObject),
+      url.toString(),
+    )
+  }
+}
+export const doPost = <T>(
+  url: string,
+  requestJsonObject: unknown,
+  accessToken: string | undefined,
+  endpoint: BackendDestinationEnum,
+  additionalOptions: RequestInit = {},
+): Promise<T> => {
+  const options: RequestInit = {
+    body: JSON.stringify(requestJsonObject),
+    headers: {
+      Accept: '*/*',
+      'Access-Control-Request-Headers': 'authorization',
+      'Content-Type': 'application/json',
+      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+    },
+    method: 'POST',
+    mode: 'cors',
+    ...additionalOptions,
+  }
+  const usedEndpoint = getEndpoint(endpoint)
+  return fetchWithExponentialTimeout<T>(usedEndpoint + url, options)
+}
+export const doGet = <T>(
+  url: string,
+  accessToken: string | undefined,
+  endpoint: BackendDestinationEnum,
+  additionalOptions: RequestInit = {},
+) => {
+  const options: RequestInit = {
+    headers: {
+      Accept: '*/*',
+      'Access-Control-Request-Headers': 'authorization',
+      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+    },
+    method: 'GET',
+    mode: 'cors',
+    ...additionalOptions,
+  }
+  const usedEndpoint = getEndpoint(endpoint)
+  return fetchWithExponentialTimeout<T>(usedEndpoint + url, options)
+}
+export const doDelete = (
+  url: string,
+  accessToken: string | undefined,
+  endpoint: BackendDestinationEnum,
+  additionalOptions: RequestInit = {},
+) => {
+  const options: RequestInit = {
+    headers: {
+      Accept: '*/*',
+      'Access-Control-Request-Headers': 'authorization',
+      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+    },
+    method: 'DELETE',
+    mode: 'cors',
+    ...additionalOptions,
+  }
+  const usedEndpoint = getEndpoint(endpoint)
+  return fetchWithExponentialTimeout<void>(usedEndpoint + url, options)
+}
+export const doPut = <T>(
+  url: string,
+  requestJsonObject: unknown,
+  accessToken: string | undefined,
+  endpoint: BackendDestinationEnum,
+  additionalOptions: RequestInit = {},
+): Promise<T> => {
+  const options: RequestInit = {
+    body: JSON.stringify(requestJsonObject),
+    headers: {
+      Accept: '*/*',
+      'Access-Control-Request-Headers': 'authorization',
+      'Content-Type': 'application/json',
+      ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+    },
+    method: 'PUT',
+    mode: 'cors',
+    ...additionalOptions,
+  }
+  const usedEndpoint = getEndpoint(endpoint)
+  return fetchWithExponentialTimeout<T>(usedEndpoint + url, options)
+}
--- packages/synapse-react-client/src/synapse-client/SynapseClient.test.ts
+++ packages/synapse-react-client/src/synapse-client/SynapseClient.test.ts
@@ -1,331 +1,133 @@
-import SynapseClient from './index'
-import { ASYNCHRONOUS_JOB_TOKEN } from '../utils/APIConstants'
-import {
-  BackendDestinationEnum,
-  getEndpoint,
-} from '../utils/functions/getEndpoint'
-import { FunctionReturningPaginatedResults } from './index'
+import SynapseClient, { FunctionReturningPaginatedResults } from './index'
 import { SynapseClientError } from '../utils/SynapseClientError'
-import { NETWORK_UNAVAILABLE_MESSAGE } from '../utils/SynapseConstants'
 import {
-  AsynchronousJobStatus,
-  PaginatedResults,
-} from '@sage-bionetworks/synapse-types'
-import { rest, server } from '../mocks/msw/server'
-import {
-  ErrorResponseCode,
-  TwoFactorAuthErrorResponse,
-} from '@sage-bionetworks/synapse-types'
+  ACCESS_TOKEN_COOKIE_KEY,
+  NETWORK_UNAVAILABLE_MESSAGE,
+} from '../utils/SynapseConstants'
+import { PaginatedResults } from '@sage-bionetworks/synapse-types'
+import UniversalCookies from 'universal-cookie'
+import * as SynapseClientUtils from './SynapseClientUtils'
+import * as HttpClient from './HttpClient'
+
+const isOutsideSynapseOrgSpy = jest.spyOn(
+  SynapseClientUtils,
+  'isOutsideSynapseOrg',
+)
+
+const doGetSpy = jest.spyOn(HttpClient, 'doGet')
 
 describe('SynapseClient tests', () => {
-  describe('SynapseClient integration tests', () => {
-    beforeAll(() => server.listen())
-    afterEach(() => server.restoreHandlers())
-    afterAll(() => server.close())
-
-    describe('getAsyncResultFromJobId tests', () => {
-      const asyncJobId = 'async-123'
-      const responseBodyServerEndpoint = `/repo/v1/entity/123/table/query/async/get/:jobId`
-      const responseBodyRequestEndpoint = `/repo/v1/entity/123/table/query/async/get/${asyncJobId}`
-      const accessToken = `synapseAccessToken`
-      const setCurrentAsyncStatus = jest.fn()
-
-      const resultObject = { arbitrary: 'data' }
-
-      const processingJob: Partial<AsynchronousJobStatus<any, any>> = {
-        jobState: 'PROCESSING',
-        responseBody: resultObject,
-      }
-      const completeJob: Partial<AsynchronousJobStatus<any, any>> = {
-        jobState: 'COMPLETE',
-        responseBody: resultObject,
-      }
-      const failedJob: Partial<AsynchronousJobStatus<any, any>> = {
-        jobState: 'FAILED',
-      }
-
-      const requestCaptor = jest.fn()
-
-      beforeEach(() => {
-        jest.clearAllMocks()
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+  describe('getAccessTokenFromCookie', () => {
+    it('No cookie outside of *.synapse.org', async () => {
+      isOutsideSynapseOrgSpy.mockReturnValue(true)
+      new UniversalCookies().remove(ACCESS_TOKEN_COOKIE_KEY, { path: '/' })
+      window.location.assign('http://localhost:3000/')
+      expect(await SynapseClient.getAccessTokenFromCookie()).toBeUndefined()
+    })
+    it('Has cookie outside of *.synapse.org', async () => {
+      isOutsideSynapseOrgSpy.mockReturnValue(true)
+      const token = 'fake-token'
+      new UniversalCookies().set(ACCESS_TOKEN_COOKIE_KEY, token, {
+        path: '/',
       })
+      window.location.assign('http://localhost:3000/')
+      expect(await SynapseClient.getAccessTokenFromCookie()).toBe(token)
+    })
+    it('Calls portal servlet to get token', async () => {
+      isOutsideSynapseOrgSpy.mockReturnValue(false)
+      const token = 'fake-token'
+      window.location.assign('http://somesubdomain.synapse.org/')
+      doGetSpy.mockResolvedValue(token)
+      expect(await SynapseClient.getAccessTokenFromCookie()).toBe(token)
 
-      it('Returns a complete response', async () => {
-        // The first call returns a successful response.
-        server.use(
-          rest.get(
-            `${getEndpoint(
-              BackendDestinationEnum.REPO_ENDPOINT,
-            )}${ASYNCHRONOUS_JOB_TOKEN(':jobId')}`,
+      expect(doGetSpy).toHaveBeenCalled()
+    })
+    it('Calls portal servlet and is unauthenticated', async () => {
+      isOutsideSynapseOrgSpy.mockReturnValue(false)
+      window.location.assign('http://somesubdomain.synapse.org/')
+      // Calling the sessioncookie service without a cookie set will result in a 401
+      doGetSpy.mockRejectedValue(
+        new SynapseClientError(
+          401,
+          'token unavailable',
+          expect.getState().currentTestName!,
+        ),
+      )
+      await expect(() =>
+        SynapseClient.getAccessTokenFromCookie(),
+      ).rejects.toThrow()
 
-            (req, res, ctx) => {
-              requestCaptor(req)
-              const status = 200
-              const response = completeJob
-              return res(ctx.status(status), ctx.json(response))
-            },
-          ),
-        )
-
-        // Call under test
-        const response = await SynapseClient.getAsyncResultFromJobId(
-          asyncJobId,
-          responseBodyRequestEndpoint,
-          accessToken,
-          setCurrentAsyncStatus,
-        )
-
-        expect(requestCaptor).toHaveBeenCalledTimes(1)
-        expect(requestCaptor).toHaveBeenCalledWith(
-          expect.objectContaining({
-            params: {
-              jobId: asyncJobId,
-            },
-          }),
-        )
-
-        expect(setCurrentAsyncStatus).toHaveBeenCalledTimes(1)
-        expect(setCurrentAsyncStatus).toHaveBeenCalledWith(response)
-      })
-
-      it('Re-fetches a processing response until complete', async () => {
-        // The first two calls return "PROCESSING" response, and the third call will return a "COMPLETE" response.
-        server.use(
-          rest.get(
-            `${getEndpoint(
-              BackendDestinationEnum.REPO_ENDPOINT,
-            )}${ASYNCHRONOUS_JOB_TOKEN(':jobId')}`,
-            // Use a generator to simulate returning a processing result twice followed by a complete result
-            function* (req, res, ctx) {
-              const status = 200
-              let count = 0
-              while (count < 2) {
-                count++
-                requestCaptor(req)
-                yield res(ctx.status(status), ctx.json(processingJob))
-              }
-              requestCaptor(req)
-              yield res(ctx.status(status), ctx.json(completeJob))
-            },
-          ),
-        )
-
-        // Call under test
-        const response = await SynapseClient.getAsyncResultFromJobId(
-          asyncJobId,
-          responseBodyRequestEndpoint,
-          accessToken,
-          setCurrentAsyncStatus,
-        )
-
-        expect(response).toEqual(completeJob)
-
-        expect(requestCaptor).toHaveBeenCalledTimes(3)
-        expect(requestCaptor).toHaveBeenCalledWith(
-          expect.objectContaining({
-            params: {
-              jobId: asyncJobId,
-            },
-          }),
-        )
-
-        expect(setCurrentAsyncStatus).toHaveBeenCalledTimes(3)
-        expect(setCurrentAsyncStatus).toHaveBeenCalledWith(processingJob)
-        expect(setCurrentAsyncStatus).toHaveBeenCalledWith(completeJob)
-      })
-
-      it('Fetches a failed response on alternate endpoint to get HTTP failure status code', async () => {
-        const errorStatus = 400
-        const errorObject = { reason: 'bad request' }
-
-        server.use(
-          rest.get(
-            `${getEndpoint(
-              BackendDestinationEnum.REPO_ENDPOINT,
-            )}${ASYNCHRONOUS_JOB_TOKEN(':jobId')}`,
-            function (req, res, ctx) {
-              const status = 200
-              return res(ctx.status(status), ctx.json(failedJob))
-            },
-          ),
-          rest.get(
-            `${getEndpoint(
-              BackendDestinationEnum.REPO_ENDPOINT,
-            )}${responseBodyServerEndpoint}`,
-            function (req, res, ctx) {
-              const status = errorStatus
-              return res(ctx.status(status), ctx.json(errorObject))
-            },
-          ),
-        )
-
-        // Call under test
-        await expect(
-          SynapseClient.getAsyncResultFromJobId(
-            asyncJobId,
-            responseBodyRequestEndpoint,
-            accessToken,
-            setCurrentAsyncStatus,
-          ),
-        ).rejects.toEqual(
-          expect.objectContaining({ status: errorStatus, ...errorObject }),
-        )
-        expect(setCurrentAsyncStatus).toHaveBeenCalledTimes(1)
-        expect(setCurrentAsyncStatus).toHaveBeenCalledWith(failedJob)
-      })
+      expect(doGetSpy).toHaveBeenCalled()
     })
   })
 
-  describe('SynapseClient unit tests', () => {
-    describe('allowNotFoundError', () => {
-      it('Passes along a non-404 successful response', async () => {
-        const expected = {
-          arbitrary: 'data',
-        }
-        const fn = jest.fn().mockResolvedValue(expected)
-
-        const actual = await SynapseClient.allowNotFoundError(fn)
-
-        expect(expected).toEqual(actual)
-      })
-      it('Passes null on a 404 response', async () => {
-        const expected = null
-        const fn = jest
-          .fn()
-          .mockRejectedValue(
-            new SynapseClientError(
-              404,
-              'Not found!',
-              expect.getState().currentTestName!,
-            ),
-          )
-
-        const actual = await SynapseClient.allowNotFoundError(fn)
-
-        expect(expected).toEqual(actual)
-      })
-      it('Passes along a non-404 error response', async () => {
-        const expected = new SynapseClientError(
-          400,
-          'Bad request!',
-          expect.getState().currentTestName!,
-        )
-        const fn = jest.fn().mockRejectedValue(expected)
-
-        await expect(() =>
-          SynapseClient.allowNotFoundError(fn),
-        ).rejects.toEqual(expected)
-      })
+  describe('fetch tests', () => {
+    it('fetch error results in a nice network unavailable message', async () => {
+      const expected = new SynapseClientError(
+        0,
+        NETWORK_UNAVAILABLE_MESSAGE,
+        expect.getState().currentTestName!,
+      )
+      jest.spyOn(global, 'fetch').mockRejectedValue(new Error())
+      await expect(() => SynapseClient.getVersion()).rejects.toEqual(expected)
     })
-    describe('returnIfTwoFactorAuthError', () => {
-      it('Passes along a successful response', async () => {
-        const expected = {
-          arbitrary: 'data',
-        }
-        const fn = jest.fn().mockResolvedValue(expected)
-
-        const actual = await SynapseClient.returnIfTwoFactorAuthError(fn)
-
-        expect(expected).toEqual(actual)
-      })
-      it('Rethrows a non-2fa error', async () => {
-        const notFoundError = new SynapseClientError(
-          404,
-          'Not found!',
-          expect.getState().currentTestName!,
-        )
-        const fn = jest.fn().mockRejectedValue(notFoundError)
-
-        await expect(
-          SynapseClient.returnIfTwoFactorAuthError(fn),
-        ).rejects.toEqual(notFoundError)
-      })
-      it('Passes along a two factor authentication error response', async () => {
-        const twoFactorAuthError: TwoFactorAuthErrorResponse = {
-          reason: 'Need 2fa',
-          errorCode: ErrorResponseCode.TWO_FA_REQUIRED,
-          userId: 12345,
-          twoFaToken: 'abc123',
-          concreteType:
-            'org.sagebionetworks.repo.model.auth.TwoFactorAuthErrorResponse',
-        }
-        const expected = new SynapseClientError(
-          401,
-          'Need 2fa',
-          expect.getState().currentTestName!,
-          twoFactorAuthError,
-        )
-        const fn = jest.fn().mockRejectedValue(expected)
-
-        const actual = await SynapseClient.returnIfTwoFactorAuthError(fn)
-        expect(actual).toBe(twoFactorAuthError)
-      })
+  })
+  describe('getAllOfPaginatedService', () => {
+    it('works with < 50 results', async () => {
+      const results = ['a']
+      const mockPaginatedObject: PaginatedResults<string> = {
+        results,
+      }
+      const mockFn: FunctionReturningPaginatedResults<string> = jest
+        .fn()
+        .mockResolvedValueOnce(mockPaginatedObject)
+      const data = await SynapseClient.getAllOfPaginatedService(mockFn)
+      expect(data).toEqual(results)
+      expect(mockFn).toHaveBeenCalledTimes(1)
+      expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
     })
-    describe('fetch tests', () => {
-      it('fetch error results in a nice network unavailable message', async () => {
-        const expected = new SynapseClientError(
-          0,
-          NETWORK_UNAVAILABLE_MESSAGE,
-          expect.getState().currentTestName!,
-        )
-        jest.spyOn(global, 'fetch').mockRejectedValue(new Error())
-        await expect(() => SynapseClient.getVersion()).rejects.toEqual(expected)
-      })
+
+    it('works with exacty 50 results', async () => {
+      const totalResults = Array(50).fill('a')
+      const mockFirstReturn: PaginatedResults<string> = {
+        results: totalResults,
+      }
+      const mockSecondReturn: PaginatedResults<string> = {
+        results: [],
+      }
+      const mockFn: FunctionReturningPaginatedResults<string> = jest
+        .fn()
+        .mockResolvedValueOnce(mockFirstReturn)
+        .mockResolvedValueOnce(mockSecondReturn)
+      const data = await SynapseClient.getAllOfPaginatedService(mockFn)
+      expect(data).toEqual(totalResults)
+      expect(mockFn).toHaveBeenCalledTimes(2)
+      expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
+      expect(mockFn).toHaveBeenNthCalledWith(2, 50, 50)
     })
-    describe('getAllOfPaginatedService', () => {
-      it('works with < 50 results', async () => {
-        const results = ['a']
-        const mockPaginatedObject: PaginatedResults<string> = {
-          results,
-        }
-        const mockFn: FunctionReturningPaginatedResults<string> = jest
-          .fn()
-          .mockResolvedValueOnce(mockPaginatedObject)
-        const data = await SynapseClient.getAllOfPaginatedService(mockFn)
-        expect(data).toEqual(results)
-        expect(mockFn).toHaveBeenCalledTimes(1)
-        expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
-      })
 
-      it('works with exacty 50 results', async () => {
-        const totalResults = Array(50).fill('a')
-        const mockFirstReturn: PaginatedResults<string> = {
-          results: totalResults,
-        }
-        const mockSecondReturn: PaginatedResults<string> = {
-          results: [],
-        }
-        const mockFn: FunctionReturningPaginatedResults<string> = jest
-          .fn()
-          .mockResolvedValueOnce(mockFirstReturn)
-          .mockResolvedValueOnce(mockSecondReturn)
-        const data = await SynapseClient.getAllOfPaginatedService(mockFn)
-        expect(data).toEqual(totalResults)
-        expect(mockFn).toHaveBeenCalledTimes(2)
-        expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
-        expect(mockFn).toHaveBeenNthCalledWith(2, 50, 50)
-      })
-
-      it('works with > 50 results', async () => {
-        const totalResults = Array(75).fill('a')
-        const firstResult = totalResults.slice(0, 50)
-        const secondResult = totalResults.slice(50)
-        const mockFirstReturn: PaginatedResults<string> = {
-          results: firstResult,
-        }
-        const mockSecondReturn: PaginatedResults<string> = {
-          results: secondResult,
-        }
-        const mockFn: FunctionReturningPaginatedResults<string> = jest
-          .fn()
-          .mockResolvedValueOnce(mockFirstReturn)
-          .mockResolvedValueOnce(mockSecondReturn)
-        const data = await SynapseClient.getAllOfPaginatedService(mockFn)
-        expect(data).toEqual(totalResults)
-        expect(mockFn).toHaveBeenCalledTimes(2)
-        expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
-        expect(mockFn).toHaveBeenNthCalledWith(2, 50, 50)
-      })
+    it('works with > 50 results', async () => {
+      const totalResults = Array(75).fill('a')
+      const firstResult = totalResults.slice(0, 50)
+      const secondResult = totalResults.slice(50)
+      const mockFirstReturn: PaginatedResults<string> = {
+        results: firstResult,
+      }
+      const mockSecondReturn: PaginatedResults<string> = {
+        results: secondResult,
+      }
+      const mockFn: FunctionReturningPaginatedResults<string> = jest
+        .fn()
+        .mockResolvedValueOnce(mockFirstReturn)
+        .mockResolvedValueOnce(mockSecondReturn)
+      const data = await SynapseClient.getAllOfPaginatedService(mockFn)
+      expect(data).toEqual(totalResults)
+      expect(mockFn).toHaveBeenCalledTimes(2)
+      expect(mockFn).toHaveBeenNthCalledWith(1, 50, 0)
+      expect(mockFn).toHaveBeenNthCalledWith(2, 50, 50)
     })
   })
 })
