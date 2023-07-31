ec632ed597d48ddc243e5f0224313c1b93b16c67
jay-hodgson <jay.hodgson@sagebase.org>
2023-07-31 17:57:00 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@97b9152b6acaf9f69e8fc7043ef418c87edaf0d5 🚀

---
+++ assets/CardContainerLogic.stories-2b9a9706.js
@@ -0,0 +1,56 @@
+import{G as p,P as a,O as h,F as I,D}from"./SynapseConstants-099130fd.js";import{C as F}from"./ConfirmationDialog-26bcc7b3.js";import"./OrientationBanner-3e5e2da7.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./FullWidthAlert-7a6f936f.js";import"./AlertTitle-d92dfb78.js";import"./styled-96220216.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./createSvgIcon-102248e7.js";import"./Paper-8ac7942c.js";import"./IconButton-1e0dd4f6.js";import"./ButtonBase-0195d275.js";import"./emotion-react.browser.esm-8516f11f.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";import"./Typography-c5b8eeae.js";import"./extendSxProp-d749f662.js";import"./Stack-f7ee2fdc.js";import"./Box-891dc3c7.js";import"./useTheme-a5fe342b.js";import"./Grow-15d1c4f6.js";import"./isHostComponent-fa76b8d9.js";import"./index-8ce4a492.js";import"./utils-60a28f01.js";import"./ClickAwayListener-36b79525.js";import"./Tooltip-e6a8fad5.js";import"./Button-d3d5b3a8.js";import"./jsx-runtime_commonjs-proxy-0d22bf03.js";import"./createChainedFunction-0bab83cf.js";import"./ownerWindow-2c76219e.js";import"./isMuiElement-08f54e3c.js";import"./SynapseClient-d9a6d554.js";import"./getEndpoint-ac94413e.js";import"./Link-9ad54ce1.js";import"./isArray-5e3f9107.js";import"./dayjs.min-59ef0ea6.js";import"./index-58d3fd43.js";import"./CheckCircleTwoTone-cd25e954.js";import"./inputBaseClasses-f1eaf41c.js";import"./Fade-5a6f6506.js";import"./WideButton-44390645.js";import"./LoadingScreen-2548bf16.js";import"./LinearProgress-ddf61131.js";import"./Backdrop-1b2a4bbc.js";import"./UserCard-55e2670e.js";import"./SkeletonTable-76bee6b7.js";import"./times-fc1da7cc.js";import"./_baseTimes-8cccc40f.js";import"./isSymbol-7c514724.js";import"./Skeleton-a24326d8.js";import"./IconSvg-dff6a879.js";import"./ErrorOutlined-b4a8c67a.js";import"./GetAppTwoTone-9a0869d1.js";import"./InfoOutlined-1265fd90.js";import"./ToastMessage-08f9c422.js";import"./hasClass-ec9efd32.js";import"./uniqueId-4d05949d.js";import"./toString-cc90cb98.js";import"./Popover-3ba117be.js";import"./Modal-3a262027.js";import"./getScrollbarSize-ac846fe6.js";import"./useTeamMembers-f9055749.js";import"./useMutation-bfff4f77.js";import"./RegularExpressions-bc0adf55.js";import"./index-95cfb7c2.js";import"./useInfiniteQuery-27efa7a2.js";import"./DateFormatter-205b0c5b.js";import"./EntityIcon-ea068193.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index.modern-2873e19a.js";import"./mapValues-b26bbf14.js";import"./isPlainObject-c2262b51.js";import"./now-bf937ae8.js";import"./unCamelCase-c4c930f1.js";import"./_baseSlice-cf92e063.js";import"./without-03410b6a.js";import"./_cacheHas-5929c6a8.js";import"./_MapCache-7a0e0573.js";import"./_baseRest-517ed69f.js";import"./isArrayLikeObject-46349da7.js";import"./hasIn-359c7c5f.js";import"./_Uint8Array-c94d498b.js";import"./_isIndex-af14b756.js";import"./pick-152b5fb9.js";import"./_baseClone-2337a707.js";import"./sortBy-b19543b9.js";import"./_baseIsEqual-e7489ede.js";import"./_setToArray-3d6ec6fd.js";import"./_baseFor-d254fa1e.js";import"./_isIterateeCall-5eddd9fd.js";import"./uniq-81954552.js";import"./isNil-b85a85f2.js";import"./RequestDownloadCard-9808f16d.js";import"./ActionRequiredCard-b58fff9f.js";import"./Icon-c0f74fa0.js";import"./factory-275e83af.js";import"./SqlFunctions-2c20cf81.js";import"./useEntity-3bec655f.js";import"./use-deep-compare-effect.esm-7a830988.js";import"./NoSearchResults-460db635.js";import"./NoData-59ea70fa.js";import"./cloneDeep-80e59d72.js";import"./index-953d39fd.js";import"./queryUtils-e30b863d.js";import"./LastLoginInfo-bf5793a9.js";import"./useGetQueryResultBundle-187486ef.js";import"./merge-a1814bb4.js";import"./useAccessRequirements-f993c294.js";import"./UserSearchBoxV2-e61258f4.js";import"./Select-ce3716e1.esm-8435c103.js";import"./isNativeReflectConstruct-e378569d.js";import"./UserOrTeamBadge-e9e7bdfa.js";import"./StandaloneLoginForm-e7530507.js";import"./LoginFlowBackButton-f3ff526d.js";import"./TextField-7d949b7a.js";import"./InputLabel-e8578cb2.js";import"./MenuList-f2b66e01.js";import"./TextField-97b69b15.js";import"./LoginMethodButton-e30fb11f.js";import"./DialogTitle-87c8bdee.js";import"./Grid-c86b3808.js";import"./useDataAccessSubmission-a3e1b6c1.js";import"./RadioGroup-eb876a69.js";import"./Form-18a51c1e.js";import"./ThemeProvider-c7f6a893.js";import"./Col-9f9a4d0f.js";import"./createWithBsPrefix-3758f106.js";import"./FileUpload-d55cba03.js";import"./Divider-a5888bbc.js";import"./dividerClasses-30b81617.js";import"./AuthenticatedRequirement-985fb104.js";import"./RequirementItem-24c0533f.js";import"./ConditionalWrapper-51b8d1ed.js";import"./Avatar-15b73dbe.js";import"./CheckTwoTone-8ffb1a26.js";import"./CertificationRequirement-4f490249.js";import"./ValidationRequirement-80ca1228.js";import"./TwoFactorAuthEnabledRequirement-8e75bf54.js";import"./TwoFactorEnrollmentForm-a3854027.js";import"./LeftRightPanel-d94a31dd.js";import"./ElementWithTooltip-c2b7523b.js";import"./SynapseTableConstants-0be13e3a.js";import"./Tabs-e906015a.js";import"./QueryCount-8a6ffbc8.js";import"./SkeletonButton-1cc36769.js";import"./RangeSlider-e66fc33f.js";import"./react-sizeme-5bfc490a.js";import"./ColorGradient-3d3e1969.js";import"./colorPalette-c43701fe.js";import"./SelectionCriteriaPill-1a888a01.js";import"./ErrorChip-3b26e46f.js";import"./IconList-8281bbc0.js";import"./UserCardList-93866351.js";import"./useDownloadList-7110329e.js";import"./EntityLink-f9bd16e9.js";import"./SynapseVideo-a4a5683a.js";const Mo={title:"Explore/CardContainerLogic",component:F},t={args:{sql:"SELECT * FROM syn22095937.4 order by authors asc",limit:2,type:p,genericCardSchema:{type:a,title:"title",description:"abstract",subTitle:"authors",secondaryLabels:["year","journal","study","grants","DOI"]},sortConfig:{defaultColumn:"authors",defaultDirection:"ASC",sortableColumns:["authors","title","createdOn","journal"]}}},r={args:{sql:"SELECT * FROM syn22095937.4 WHERE study='not a study value'",type:p,genericCardSchema:{type:a,title:"title",description:"abstract",subTitle:"authors",secondaryLabels:["year","journal","study","grants","DOI"]}}},o={args:{sql:'SELECT "Observation Submitter Name" as "submitterName", Synapse_id as "submitterUserId", "Observation Time" as "time", "Observation Time Units" as "timeUnits", "Observation Text" as "text", "Observation Type" as "tag" FROM syn26344832 WHERE "Observation Time" IS NOT NULL',type:h,limit:3}},i={args:{sql:"SELECT * FROM syn16858699",type:I,limit:3}},m={args:{sql:"SELECT * FROM syn16859580",type:D,limit:3}},v={type:a,title:"publicationTitle",subTitle:"authors",secondaryLabels:["pubMedLink","journal","publicationYear","theme","tumorType","tissue","assay","keywords","doi","consortium"]},e={args:{sql:`SELECT * FROM syn21868591 WHERE ( ( "grantNumber" HAS ( 'CA209988' ) ) )`,type:p,genericCardSchema:v}};var s,n,c;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    sql: 'SELECT * FROM syn22095937.4 order by authors asc',
+    limit: 2,
+    type: GENERIC_CARD,
+    genericCardSchema: {
+      type: PUBLICATION,
+      title: 'title',
+      description: 'abstract',
+      subTitle: 'authors',
+      secondaryLabels: ['year', 'journal', 'study', 'grants', 'DOI']
+    },
+    sortConfig: {
+      defaultColumn: 'authors',
+      defaultDirection: 'ASC',
+      sortableColumns: ['authors', 'title', 'createdOn', 'journal']
+    }
+  }
+}`,...(c=(n=t.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var u,l,d;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    sql: "SELECT * FROM syn22095937.4 WHERE study='not a study value'",
+    type: GENERIC_CARD,
+    genericCardSchema: {
+      type: PUBLICATION,
+      title: 'title',
+      description: 'abstract',
+      subTitle: 'authors',
+      secondaryLabels: ['year', 'journal', 'study', 'grants', 'DOI']
+    }
+  }
+}`,...(d=(l=r.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var y,C,E;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    sql: \`SELECT "Observation Submitter Name" as "submitterName", Synapse_id as "submitterUserId", "Observation Time" as "time", "Observation Time Units" as "timeUnits", "Observation Text" as "text", "Observation Type" as "tag" FROM syn26344832 WHERE "Observation Time" IS NOT NULL\`,
+    type: OBSERVATION_CARD,
+    limit: 3
+  }
+}`,...(E=(C=o.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};var b,O,S;i.parameters={...i.parameters,docs:{...(b=i.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    sql: \`SELECT * FROM syn16858699\`,
+    type: FUNDER,
+    limit: 3
+  }
+}`,...(S=(O=i.parameters)==null?void 0:O.docs)==null?void 0:S.source}}};var T,g,R;m.parameters={...m.parameters,docs:{...(T=m.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    sql: \`SELECT * FROM syn16859580\`,
+    type: DATASET,
+    limit: 3
+  }
+}`,...(R=(g=m.parameters)==null?void 0:g.docs)==null?void 0:R.source}}};var L,A,N;e.parameters={...e.parameters,docs:{...(L=e.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    sql: \`SELECT * FROM syn21868591 WHERE ( ( "grantNumber" HAS ( 'CA209988' ) ) )\`,
+    type: GENERIC_CARD,
+    genericCardSchema: publicationSchema
+  }
+}`,...(N=(A=e.parameters)==null?void 0:A.docs)==null?void 0:N.source}}};const Uo=["GenericCard","EmptyResults","ObservationCard","FunderCard","DatasetCard","PublicationCard"];export{m as DatasetCard,r as EmptyResults,i as FunderCard,t as GenericCard,o as ObservationCard,e as PublicationCard,Uo as __namedExportsOrder,Mo as default};
+//# sourceMappingURL=CardContainerLogic.stories-2b9a9706.js.map
---
+++ assets/RequirementItem.stories-2509af93.js
@@ -0,0 +1,45 @@
+import{j as e}from"./jsx-runtime-095bf462.js";import{R as s,a as t}from"./RequirementItem-24c0533f.js";import{d as a}from"./ToastMessage-08f9c422.js";import{b as P}from"./ConfirmationDialog-26bcc7b3.js";import{m as O}from"./mockWiki-23617a07.js";import{P as T}from"./Paper-8ac7942c.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./ConditionalWrapper-51b8d1ed.js";import"./Avatar-15b73dbe.js";import"./styled-96220216.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./createSvgIcon-102248e7.js";import"./useTheme-a5fe342b.js";import"./Skeleton-a24326d8.js";import"./emotion-react.browser.esm-8516f11f.js";import"./CheckTwoTone-8ffb1a26.js";import"./Box-891dc3c7.js";import"./extendSxProp-d749f662.js";import"./Button-d3d5b3a8.js";import"./ButtonBase-0195d275.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";import"./FullWidthAlert-7a6f936f.js";import"./AlertTitle-d92dfb78.js";import"./IconButton-1e0dd4f6.js";import"./Typography-c5b8eeae.js";import"./Stack-f7ee2fdc.js";import"./Grow-15d1c4f6.js";import"./isHostComponent-fa76b8d9.js";import"./index-8ce4a492.js";import"./utils-60a28f01.js";import"./ClickAwayListener-36b79525.js";import"./Tooltip-e6a8fad5.js";import"./hasClass-ec9efd32.js";import"./uniqueId-4d05949d.js";import"./toString-cc90cb98.js";import"./isArray-5e3f9107.js";import"./isSymbol-7c514724.js";import"./jsx-runtime_commonjs-proxy-0d22bf03.js";import"./createChainedFunction-0bab83cf.js";import"./ownerWindow-2c76219e.js";import"./isMuiElement-08f54e3c.js";import"./SynapseClient-d9a6d554.js";import"./SynapseConstants-099130fd.js";import"./OrientationBanner-3e5e2da7.js";import"./getEndpoint-ac94413e.js";import"./Link-9ad54ce1.js";import"./dayjs.min-59ef0ea6.js";import"./index-58d3fd43.js";import"./CheckCircleTwoTone-cd25e954.js";import"./inputBaseClasses-f1eaf41c.js";import"./Fade-5a6f6506.js";import"./WideButton-44390645.js";import"./LoadingScreen-2548bf16.js";import"./LinearProgress-ddf61131.js";import"./Backdrop-1b2a4bbc.js";import"./UserCard-55e2670e.js";import"./SkeletonTable-76bee6b7.js";import"./times-fc1da7cc.js";import"./_baseTimes-8cccc40f.js";import"./IconSvg-dff6a879.js";import"./ErrorOutlined-b4a8c67a.js";import"./GetAppTwoTone-9a0869d1.js";import"./InfoOutlined-1265fd90.js";import"./Popover-3ba117be.js";import"./Modal-3a262027.js";import"./getScrollbarSize-ac846fe6.js";import"./useTeamMembers-f9055749.js";import"./useMutation-bfff4f77.js";import"./RegularExpressions-bc0adf55.js";import"./index-95cfb7c2.js";import"./useInfiniteQuery-27efa7a2.js";import"./DateFormatter-205b0c5b.js";import"./EntityIcon-ea068193.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index.modern-2873e19a.js";import"./mapValues-b26bbf14.js";import"./isPlainObject-c2262b51.js";import"./now-bf937ae8.js";import"./unCamelCase-c4c930f1.js";import"./_baseSlice-cf92e063.js";import"./without-03410b6a.js";import"./_cacheHas-5929c6a8.js";import"./_MapCache-7a0e0573.js";import"./_baseRest-517ed69f.js";import"./isArrayLikeObject-46349da7.js";import"./hasIn-359c7c5f.js";import"./_Uint8Array-c94d498b.js";import"./_isIndex-af14b756.js";import"./pick-152b5fb9.js";import"./_baseClone-2337a707.js";import"./sortBy-b19543b9.js";import"./_baseIsEqual-e7489ede.js";import"./_setToArray-3d6ec6fd.js";import"./_baseFor-d254fa1e.js";import"./_isIterateeCall-5eddd9fd.js";import"./uniq-81954552.js";import"./isNil-b85a85f2.js";import"./RequestDownloadCard-9808f16d.js";import"./ActionRequiredCard-b58fff9f.js";import"./Icon-c0f74fa0.js";import"./factory-275e83af.js";import"./SqlFunctions-2c20cf81.js";import"./useEntity-3bec655f.js";import"./use-deep-compare-effect.esm-7a830988.js";import"./NoSearchResults-460db635.js";import"./NoData-59ea70fa.js";import"./cloneDeep-80e59d72.js";import"./index-953d39fd.js";import"./queryUtils-e30b863d.js";import"./LastLoginInfo-bf5793a9.js";import"./useGetQueryResultBundle-187486ef.js";import"./merge-a1814bb4.js";import"./useAccessRequirements-f993c294.js";import"./UserSearchBoxV2-e61258f4.js";import"./Select-ce3716e1.esm-8435c103.js";import"./isNativeReflectConstruct-e378569d.js";import"./UserOrTeamBadge-e9e7bdfa.js";import"./StandaloneLoginForm-e7530507.js";import"./LoginFlowBackButton-f3ff526d.js";import"./TextField-7d949b7a.js";import"./InputLabel-e8578cb2.js";import"./MenuList-f2b66e01.js";import"./TextField-97b69b15.js";import"./LoginMethodButton-e30fb11f.js";import"./DialogTitle-87c8bdee.js";import"./Grid-c86b3808.js";import"./useDataAccessSubmission-a3e1b6c1.js";import"./RadioGroup-eb876a69.js";import"./Form-18a51c1e.js";import"./ThemeProvider-c7f6a893.js";import"./Col-9f9a4d0f.js";import"./createWithBsPrefix-3758f106.js";import"./FileUpload-d55cba03.js";import"./Divider-a5888bbc.js";import"./dividerClasses-30b81617.js";import"./AuthenticatedRequirement-985fb104.js";import"./CertificationRequirement-4f490249.js";import"./ValidationRequirement-80ca1228.js";import"./TwoFactorAuthEnabledRequirement-8e75bf54.js";import"./TwoFactorEnrollmentForm-a3854027.js";import"./LeftRightPanel-d94a31dd.js";import"./ElementWithTooltip-c2b7523b.js";import"./SynapseTableConstants-0be13e3a.js";import"./Tabs-e906015a.js";import"./QueryCount-8a6ffbc8.js";import"./SkeletonButton-1cc36769.js";import"./RangeSlider-e66fc33f.js";import"./react-sizeme-5bfc490a.js";import"./ColorGradient-3d3e1969.js";import"./colorPalette-c43701fe.js";import"./SelectionCriteriaPill-1a888a01.js";import"./ErrorChip-3b26e46f.js";import"./IconList-8281bbc0.js";import"./UserCardList-93866351.js";import"./useDownloadList-7110329e.js";import"./EntityLink-f9bd16e9.js";import"./SynapseVideo-a4a5683a.js";import"./mock_user_profile-f88f9ec5.js";const No={title:"Governance/Data Access Request Flow/Requirements/RequirementItem",component:s,argTypes:{status:{control:"select",options:[...new Set(Object.values(t))]}},tags:["autodocs"],render:A=>e(T,{sx:{p:5,margin:"auto",maxWidth:"700px"},children:e(s,{...A,children:e(P,{markdown:O.markdown})})})},r={args:{status:t.COMPLETE,actions:[{variant:"outlined",children:"Learn More",onClick:()=>{a("Learn More clicked")}}]}},o={args:{status:t.PENDING,actions:[{variant:"outlined",children:"Learn More",onClick:()=>{a("Learn More clicked")}}]}},i={args:{status:t.LOADING,actions:[]}},m={args:{status:t.COMPLETE,actions:[]}},p={args:{status:t.LOCKED,actions:[{variant:"outlined",children:"Accept terms",onClick:()=>{a("Accept terms clicked")}}]}};var n,c,d;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    status: RequirementItemStatus.COMPLETE,
+    actions: [{
+      variant: 'outlined',
+      children: 'Learn More',
+      onClick: () => {
+        displayToast('Learn More clicked');
+      }
+    }]
+  }
+}`,...(d=(c=r.parameters)==null?void 0:c.docs)==null?void 0:d.source}}};var u,l,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    status: RequirementItemStatus.PENDING,
+    actions: [{
+      variant: 'outlined',
+      children: 'Learn More',
+      onClick: () => {
+        displayToast('Learn More clicked');
+      }
+    }]
+  }
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var L,k,C;i.parameters={...i.parameters,docs:{...(L=i.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    status: RequirementItemStatus.LOADING,
+    actions: []
+  }
+}`,...(C=(k=i.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var M,E,S;m.parameters={...m.parameters,docs:{...(M=m.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    status: RequirementItemStatus.COMPLETE,
+    actions: []
+  }
+}`,...(S=(E=m.parameters)==null?void 0:E.docs)==null?void 0:S.source}}};var I,R,q;p.parameters={...p.parameters,docs:{...(I=p.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    status: RequirementItemStatus.LOCKED,
+    actions: [{
+      variant: 'outlined',
+      children: 'Accept terms',
+      onClick: () => {
+        displayToast('Accept terms clicked');
+      }
+    }]
+  }
+}`,...(q=(R=p.parameters)==null?void 0:R.docs)==null?void 0:q.source}}};const fo=["Complete","Pending","Loading","NoActions","Locked"];export{r as Complete,i as Loading,p as Locked,m as NoActions,o as Pending,fo as __namedExportsOrder,No as default};
+//# sourceMappingURL=RequirementItem.stories-2509af93.js.map
