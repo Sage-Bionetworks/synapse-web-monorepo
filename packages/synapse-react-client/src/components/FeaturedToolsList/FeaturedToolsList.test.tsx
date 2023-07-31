f42b72b2dd17092f674432f0a21116c49b6aa31d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-07-31 08:06:53 -0400

    PORTALS-2748 - fix test failure

--- packages/synapse-react-client/src/components/FeaturedToolsList/FeaturedToolsList.test.tsx
+++ packages/synapse-react-client/src/components/FeaturedToolsList/FeaturedToolsList.test.tsx
@@ -4,33 +4,31 @@ import { QueryResultBundle } from '@sage-bionetworks/synapse-types'
 import syn26344826Json from '../../mocks/query/syn26344826.json'
 import { render } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
-import { SynapseTestContext } from '../../mocks/MockSynapseContext'
 import SynapseClient from '../../synapse-client'
+import { createWrapper } from '../../testutils/TestingLibraryUtils'
 
 jest.mock('../../synapse-client', () => ({
   getQueryTableResults: jest.fn(),
+  getUseUtcTimeFromCookie: jest.fn(() => false),
 }))
 
 const mockGetQueryTableResults = jest.mocked(SynapseClient.getQueryTableResults)
 
 const data = syn26344826Json as QueryResultBundle
 
-let container: HTMLElement
-
 function init() {
-  container = render(
-    <SynapseTestContext>
-      <FeaturedToolsList
-        entityId={'syn26344826'}
-        idColumnName={'Resource_id'}
-        nameColumnName={'Resource Name'}
-        descriptionColumnName={'Description'}
-        typeColumnName={'Resource Type'}
-        dateColumnName={'Date Added'}
-        toolDetailPageURL={'/ToolDetailsPage/resourceId='}
-      />
-    </SynapseTestContext>,
-  ).container
+  return render(
+    <FeaturedToolsList
+      entityId={'syn26344826'}
+      idColumnName={'Resource_id'}
+      nameColumnName={'Resource Name'}
+      descriptionColumnName={'Description'}
+      typeColumnName={'Resource Type'}
+      dateColumnName={'Date Added'}
+      toolDetailPageURL={'/ToolDetailsPage/resourceId='}
+    />,
+    { wrapper: createWrapper() },
+  )
 }
 
 describe('basic tests', () => {
@@ -41,7 +39,7 @@ describe('basic tests', () => {
   it('displays featured tool cards from a Synapse Table', async () => {
     // We must await asynchronous events for our assertions to pass
     // eslint-disable-next-line @typescript-eslint/require-await
-    await act(async () => init())
+    const { container } = await act(async () => init())
     expect(container.querySelector('.FeaturedToolCard')).toBeDefined()
     expect(container.querySelectorAll('.FeaturedToolCard')).toHaveLength(3)
   })
