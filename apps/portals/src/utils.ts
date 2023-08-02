099838387d7fb3e9bac7bdd11ba7de27788c0895
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-08-02 13:57:39 -0400

    PORTALS-2634, PORTALS-2636 - TopLevelControls/TotalQueryResults appears to be full width in portals

--- apps/portals/src/utils.ts
+++ apps/portals/src/utils.ts
@@ -14,9 +14,17 @@ export const DESKTOP_VIEWPORT_MIN_WIDTH_MEDIA_QUERY = `(min-width: ${DESKTOP_VIE
 export const useShowDesktop = () =>
   useMediaQuery(DESKTOP_VIEWPORT_MIN_WIDTH_MEDIA_QUERY)
 
-export const RESPONSIVE_SIDE_PADDING: SxProps = {
+export const RESPONSIVE_SIDE_PADDING = {
   px: {
     xs: 2,
     md: 5,
   },
-}
+} satisfies SxProps
+
+// Should stay in sync with RESPONSIVE_SIDE_PADDING
+export const NEGATIVE_RESPONSIVE_SIDE_MARGIN = {
+  mx: {
+    xs: -2,
+    md: -5,
+  },
+} satisfies SxProps
