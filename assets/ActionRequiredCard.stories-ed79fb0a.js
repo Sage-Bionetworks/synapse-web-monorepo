f861a51966b9044a4e8a144382efa8eb897be4c8
jay-hodgson <jay.hodgson@sagebase.org>
2023-08-04 00:49:17 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@a4e88e44469ef4e1d8a41fd7a08a63fdb7aa0650 🚀

---
+++ assets/ActionRequiredCard.stories-ed79fb0a.js
@@ -0,0 +1,15 @@
+import{j as r,a as s,F as d}from"./jsx-runtime-095bf462.js";import{A as c}from"./ActionRequiredCard-8a178e8c.js";import{W as g}from"./WideButton-45d668d2.js";import{n as u}from"./SynapseConstants-0807672a.js";import{T as l}from"./Typography-c5b8eeae.js";import{B as h}from"./Button-d3d5b3a8.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Icon-0c9cd4d0.js";import"./createSvgIcon-102248e7.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./styled-96220216.js";import"./IconSvg-a8479f34.js";import"./Tooltip-e6a8fad5.js";import"./useTheme-a5fe342b.js";import"./Grow-15d1c4f6.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-60a28f01.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-b4a8c67a.js";import"./GetAppTwoTone-9a0869d1.js";import"./InfoOutlined-1265fd90.js";import"./CheckCircleTwoTone-cd25e954.js";import"./Paper-8ac7942c.js";import"./Box-891dc3c7.js";import"./extendSxProp-d749f662.js";import"./Skeleton-a24326d8.js";import"./emotion-react.browser.esm-8516f11f.js";import"./OrientationBanner-d057b0e6.js";import"./FullWidthAlert-7e419f2a.js";import"./AlertTitle-d92dfb78.js";import"./IconButton-1e0dd4f6.js";import"./ButtonBase-0195d275.js";import"./assertThisInitialized-081f9914.js";import"./Stack-f7ee2fdc.js";import"./ClickAwayListener-36b79525.js";const rt={title:"Download/ActionRequiredCard",component:c,tags:["autodocs"],argTypes:{actionNode:{options:["Button","Button with text (Sharing Settings)"],mapping:{Button:r(g,{variant:"contained",children:"Start"}),"Button with text (Sharing Settings)":s(d,{children:[r(l,{variant:"smallText1",sx:{color:"grey.700"},children:"Contact an administrator to request download permission"}),r(h,{variant:"outlined",children:"View Sharing Settings"})]})}}}},t={args:{isLoading:!0}},o={args:{isLoading:!1,title:"The title of the card",description:"The description of the card",actionNode:"Button",iconType:u,count:42}};var i,a,e;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(e=(a=t.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};var n,p,m;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    title: 'The title of the card',
+    description: 'The description of the card',
+    actionNode: 'Button',
+    iconType: EASY_DIFFICULTY,
+    count: 42
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const it=["Loading","Card"];export{o as Card,t as Loading,it as __namedExportsOrder,rt as default};
+//# sourceMappingURL=ActionRequiredCard.stories-ed79fb0a.js.map
