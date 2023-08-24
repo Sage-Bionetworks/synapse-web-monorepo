1fe3c7679e6869e6bd5a753e0203a6eea67f0a1c
jay-hodgson <jay.hodgson@sagebase.org>
2023-08-24 16:33:34 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@31ae998d3016c400f84969e0cd77cf67e3cb7b42 🚀

---
+++ assets/ActionRequiredCard.stories-f7820628.js
@@ -0,0 +1,15 @@
+import{j as r,a as s,F as d}from"./jsx-runtime-095bf462.js";import{A as c}from"./ActionRequiredCard-df0f6b4c.js";import{W as g}from"./WideButton-21e8230b.js";import{n as u}from"./SynapseConstants-76f49806.js";import{T as l}from"./Typography-c5b8eeae.js";import{B as h}from"./Button-d3d5b3a8.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Icon-62c5918d.js";import"./createSvgIcon-102248e7.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./styled-96220216.js";import"./IconSvg-fff0cc14.js";import"./Tooltip-e6a8fad5.js";import"./useTheme-a5fe342b.js";import"./Grow-15d1c4f6.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-60a28f01.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-b4a8c67a.js";import"./GetAppTwoTone-9a0869d1.js";import"./InfoOutlined-1265fd90.js";import"./CheckCircleTwoTone-cd25e954.js";import"./Paper-8ac7942c.js";import"./Box-891dc3c7.js";import"./extendSxProp-d749f662.js";import"./Skeleton-a24326d8.js";import"./emotion-react.browser.esm-8516f11f.js";import"./OrientationBanner-3e5e2da7.js";import"./FullWidthAlert-7a6f936f.js";import"./AlertTitle-d92dfb78.js";import"./IconButton-1e0dd4f6.js";import"./ButtonBase-0195d275.js";import"./assertThisInitialized-081f9914.js";import"./Stack-f7ee2fdc.js";import"./ClickAwayListener-36b79525.js";const rt={title:"Download/ActionRequiredCard",component:c,tags:["autodocs"],argTypes:{actionNode:{options:["Button","Button with text (Sharing Settings)"],mapping:{Button:r(g,{variant:"contained",children:"Start"}),"Button with text (Sharing Settings)":s(d,{children:[r(l,{variant:"smallText1",sx:{color:"grey.700"},children:"Contact an administrator to request download permission"}),r(h,{variant:"outlined",children:"View Sharing Settings"})]})}}}},t={args:{isLoading:!0}},o={args:{isLoading:!1,title:"The title of the card",description:"The description of the card",actionNode:"Button",iconType:u,count:42}};var i,a,e;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...(e=(a=t.parameters)==null?void 0:a.docs)==null?void 0:e.source}}};var n,p,m;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    title: 'The title of the card',
+    description: 'The description of the card',
+    actionNode: 'Button',
+    iconType: EASY_DIFFICULTY,
+    count: 42
+  }
+}`,...(m=(p=o.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const it=["Loading","Card"];export{o as Card,t as Loading,it as __namedExportsOrder,rt as default};
+//# sourceMappingURL=ActionRequiredCard.stories-f7820628.js.map
---
+++ assets/ThemesPlot.stories-7dc92fde.js
@@ -0,0 +1,56 @@
+import{T as p}from"./ThemesPlot-60d64142.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./ElementWithTooltip-6dfe0df4.js";import"./SynapseTableConstants-0be13e3a.js";import"./IconSvg-fff0cc14.js";import"./createSvgIcon-102248e7.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./styled-96220216.js";import"./Tooltip-e6a8fad5.js";import"./useTheme-a5fe342b.js";import"./Grow-15d1c4f6.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-60a28f01.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-b4a8c67a.js";import"./GetAppTwoTone-9a0869d1.js";import"./InfoOutlined-1265fd90.js";import"./CheckCircleTwoTone-cd25e954.js";import"./RegularExpressions-bc0adf55.js";import"./Icon-62c5918d.js";import"./SynapseConstants-76f49806.js";import"./OrientationBanner-3e5e2da7.js";import"./FullWidthAlert-7a6f936f.js";import"./AlertTitle-d92dfb78.js";import"./Paper-8ac7942c.js";import"./IconButton-1e0dd4f6.js";import"./ButtonBase-0195d275.js";import"./emotion-react.browser.esm-8516f11f.js";import"./assertThisInitialized-081f9914.js";import"./Typography-c5b8eeae.js";import"./extendSxProp-d749f662.js";import"./Stack-f7ee2fdc.js";import"./Box-891dc3c7.js";import"./ClickAwayListener-36b79525.js";import"./Button-d3d5b3a8.js";import"./ThemeProvider-c7f6a893.js";import"./index-58d3fd43.js";import"./hasClass-ec9efd32.js";import"./createWithBsPrefix-3758f106.js";import"./unCamelCase-90cb6394.js";import"./_baseSlice-cf92e063.js";import"./toString-cc90cb98.js";import"./isArray-5e3f9107.js";import"./isSymbol-7c514724.js";import"./without-34cd7bc5.js";import"./_cacheHas-475ed0c9.js";import"./_MapCache-79d78a83.js";import"./SynapseClient-773213a5.js";import"./getEndpoint-ac94413e.js";import"./Link-9ad54ce1.js";import"./dayjs.min-59ef0ea6.js";import"./tiny-invariant-dd7d57d2.js";import"./inputBaseClasses-f1eaf41c.js";import"./Fade-5a6f6506.js";import"./_baseTimes-8cccc40f.js";import"./_baseRest-249bb166.js";import"./isArrayLikeObject-13b3ffa2.js";import"./hasIn-fb64c32e.js";import"./_Uint8Array-2748b5a2.js";import"./_isIndex-af14b756.js";import"./pick-e06fcf28.js";import"./_baseClone-f9fa137a.js";import"./sortBy-cb6dd856.js";import"./_baseIsEqual-44b1969b.js";import"./_setToArray-3d6ec6fd.js";import"./_baseFor-d254fa1e.js";import"./_isIterateeCall-10be50bc.js";import"./uniq-538169c7.js";import"./times-2a640a34.js";import"./SqlFunctions-7ffbd610.js";import"./factory-275e83af.js";import"./lodash.default-2841e216.js";import"./omitBy-f4c84392.js";import"./merge-89ed990d.js";import"./uniqueId-4d05949d.js";import"./cloneDeep-869a752e.js";import"./isNil-24de171f.js";import"./groupBy-bf3a2d38.js";import"./startCase-b39da44a.js";import"./toLower-271fcf43.js";import"./LoadingScreen-2548bf16.js";import"./LinearProgress-ddf61131.js";import"./Backdrop-1b2a4bbc.js";const Go={title:"Home Page/ThemesPlot",component:p},o={args:{onPointClick:m=>{console.log(m.event)},topBarPlot:{entityId:"syn21641485",xField:"totalCount",yField:"groupBy",groupField:"consortium",colors:{CSBC:"rgba(64,123,160, 1)","PS-ON":"rgba(91,176,181,1)",ICBP:"rgba(197, 191, 223, 1)",TEC:"rgba(156, 196, 233, 1)"},whereClause:"totalCount is not NULL"},sideBarPlot:{entityId:"syn21649281",xField:"totalCount",yField:"theme",groupField:"consortium",countLabel:"grants",plotStyle:{backgroundColor:"#f5f9fa"},colors:{CSBC:"#1c76af","PS-ON":"#5bb0b5",ICBP:"#9cc4e9",TEC:"#c5bfdf"}},dotPlot:{entityId:"syn21639584",xField:"totalCount",yField:"theme",groupField:"groupBy",infoField:"themeDescription",whereClause:"groupBy IN ('publications', 'tools', 'datasets')",markerSymbols:{tools:"y-down",datasets:"diamond-x",publications:"circle"},plotStyle:{markerLine:"rgba(0, 0, 0,1)",markerFill:"rgba(255, 255, 255,1)",markerSize:11,backgroundColor:"#f5f9fa"}}}};var t,r,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    onPointClick: e => {
+      console.log(e.event);
+    },
+    topBarPlot: {
+      entityId: 'syn21641485',
+      xField: 'totalCount',
+      yField: 'groupBy',
+      groupField: 'consortium',
+      colors: {
+        CSBC: 'rgba(64,123,160, 1)',
+        'PS-ON': 'rgba(91,176,181,1)',
+        ICBP: 'rgba(197, 191, 223, 1)',
+        TEC: 'rgba(156, 196, 233, 1)'
+      },
+      whereClause: 'totalCount is not NULL'
+    },
+    sideBarPlot: {
+      entityId: 'syn21649281',
+      xField: 'totalCount',
+      yField: 'theme',
+      groupField: 'consortium',
+      countLabel: 'grants',
+      plotStyle: {
+        backgroundColor: '#f5f9fa'
+      },
+      colors: {
+        CSBC: '#1c76af',
+        'PS-ON': '#5bb0b5',
+        ICBP: '#9cc4e9',
+        TEC: '#c5bfdf'
+      }
+    },
+    dotPlot: {
+      entityId: 'syn21639584',
+      xField: 'totalCount',
+      yField: 'theme',
+      groupField: 'groupBy',
+      infoField: 'themeDescription',
+      whereClause: "groupBy IN ('publications', 'tools', 'datasets')",
+      markerSymbols: {
+        tools: 'y-down',
+        datasets: 'diamond-x',
+        publications: 'circle'
+      },
+      plotStyle: {
+        markerLine: 'rgba(0, 0, 0,1)',
+        markerFill: 'rgba(255, 255, 255,1)',
+        markerSize: 11,
+        backgroundColor: '#f5f9fa'
+      }
+    }
+  }
+}`,...(i=(r=o.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const Jo=["ThemesPlotDemo"];export{o as ThemesPlotDemo,Jo as __namedExportsOrder,Go as default};
+//# sourceMappingURL=ThemesPlot.stories-7dc92fde.js.map
