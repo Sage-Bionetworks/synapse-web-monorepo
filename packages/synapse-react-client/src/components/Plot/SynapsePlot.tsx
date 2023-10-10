b6c32a704d35d3c3ca3b29f179272db357bcacf6
Jay Hodgson <jay.hodgson@sagebase.org>
2023-10-09 17:24:48 -0700

    PORTALS-2808: fix types and rewrite SynapsePlot so that it is no longer a class component

--- packages/synapse-react-client/src/components/Plot/SynapsePlot.tsx
+++ packages/synapse-react-client/src/components/Plot/SynapsePlot.tsx
@@ -1,135 +1,99 @@
-import Plotly from 'plotly.js-basic-dist'
+import Plotly, { AxisType, PlotType } from 'plotly.js-basic-dist'
 import React from 'react'
 import createPlotlyComponent from 'react-plotly.js/factory'
 import { SynapseConstants } from '../../utils'
-import { getFullQueryTableResults } from '../../synapse-client/SynapseClient'
-import {
-  QueryResultBundle,
-  QueryBundleRequest,
-} from '@sage-bionetworks/synapse-types'
+import { QueryBundleRequest } from '@sage-bionetworks/synapse-types'
 import { parseEntityIdFromSqlStatement } from '../../utils/functions/SqlFunctions'
-import { SynapseContext } from '../../utils/context/SynapseContext'
+import { useGetFullTableQueryResults } from '../../synapse-queries'
 const Plot = createPlotlyComponent(Plotly)
 
+export type SynapsePlotWidgetParams = {
+  query: string
+  title: string
+  xtitle: string
+  ytitle: string
+  type: string
+  xaxistype: string
+  showlegend: string
+  horizontal: string
+  barmode?: string
+}
 export type SynapsePlotProps = {
-  ownerId?: string
-  wikiId?: string
-  widgetparamsMapped?: any
+  widgetparamsMapped: SynapsePlotWidgetParams
 }
 
-type SynapsePlotState = {
-  isLoaded: boolean
-  queryData: QueryResultBundle
+const toBoolean = (v: string) => {
+  return v ? v.toLowerCase() == 'true' : false
+}
+export const SynapsePlot = (props: SynapsePlotProps) => {
+  const { query } = props.widgetparamsMapped
+  const queryRequest: QueryBundleRequest = {
+    concreteType: 'org.sagebionetworks.repo.model.table.QueryBundleRequest',
+    partMask: SynapseConstants.BUNDLE_MASK_QUERY_RESULTS,
+    entityId: parseEntityIdFromSqlStatement(query),
+    query: {
+      sql: query,
+    },
+  }
+  const { data: queryData, isLoading } =
+    useGetFullTableQueryResults(queryRequest)
+  if (isLoading || !queryData) {
+    return <></>
+  }
+
+  const { title, xtitle, ytitle, type, xaxistype, showlegend, barmode } =
+    props.widgetparamsMapped
+  const isShowLegend = toBoolean(showlegend)
+  const isHorizontal = toBoolean(props.widgetparamsMapped.horizontal)
+  const layout: Partial<Plotly.Layout> = {
+    showlegend: isShowLegend,
+    title,
+    barmode: barmode
+      ? (barmode.toLowerCase() as 'stack' | 'group' | 'overlay' | 'relative')
+      : undefined,
+  }
+  if (xtitle) {
+    layout.xaxis = {
+      title: xtitle,
+    }
+  }
+  if (xaxistype) {
+    layout.xaxis = {
+      ...layout.xaxis,
+      type: xaxistype.toLowerCase() as AxisType,
+    }
+  }
+  if (ytitle) {
+    layout.yaxis = {
+      title: ytitle,
+    }
+  }
+  // init plot_data
+  const plotData: Partial<Plotly.PlotData>[] = []
+  const orientation = isHorizontal ? 'h' : 'v'
+  const headers = queryData.queryResult?.queryResults.headers ?? []
+  for (let i = 0; i < headers.length - 1; i += 1) {
+    // make an entry for each set of data points
+    plotData[i] = {
+      orientation,
+      name: headers[i + 1].name,
+      type: type.toLowerCase() as PlotType,
+      x: [] as Plotly.Datum[],
+      y: [] as Plotly.Datum[],
+    }
+  }
+  // grab all the data
+  for (const row of queryData.queryResult?.queryResults.rows ?? []) {
+    for (let j = 1; j < row.values.length; j += 1) {
+      // create pairs of data
+      const rowValues = row.values
+      const xArray = plotData[j - 1]!.x as Plotly.Datum[]
+      const yArray = plotData[j - 1]!.y as Plotly.Datum[]
+      xArray.push(rowValues[0])
+      yArray.push(rowValues[j])
+    }
+  }
+  return <Plot layout={layout} data={plotData} />
 }
 
-export class SynapsePlot extends React.Component<
-  SynapsePlotProps,
-  SynapsePlotState
-> {
-  constructor(props: SynapsePlotProps) {
-    super(props)
-    this.state = {
-      isLoaded: false,
-      queryData: {} as QueryResultBundle,
-    }
-    this.fetchPlotlyData = this.fetchPlotlyData.bind(this)
-    this.showPlot = this.showPlot.bind(this)
-  }
-
-  static contextType = SynapseContext
-  declare context: NonNullable<React.ContextType<typeof SynapseContext>>
-
-  public componentDidMount() {
-    this.fetchPlotlyData()
-  }
-  /**
-   * Get data for plotly
-   *
-   * @returns data corresponding to plotly widget
-   */
-  public fetchPlotlyData() {
-    const { query } = this.props.widgetparamsMapped
-    const queryRequest: QueryBundleRequest = {
-      concreteType: 'org.sagebionetworks.repo.model.table.QueryBundleRequest',
-      partMask: SynapseConstants.BUNDLE_MASK_QUERY_RESULTS,
-      entityId: parseEntityIdFromSqlStatement(query),
-      query: {
-        sql: query,
-      },
-    }
-
-    getFullQueryTableResults(queryRequest, this.context.accessToken)
-      .then((data: QueryResultBundle) => {
-        this.setState({
-          isLoaded: true,
-          queryData: data,
-        })
-      })
-      .catch((err: any) => {
-        console.log('Error on full table query ', err)
-      })
-  }
-
-  public showPlot() {
-    if (!this.state.isLoaded) {
-      return
-    }
-    const { title, xtitle, ytitle, type, xaxistype, showlegend } =
-      this.props.widgetparamsMapped
-    const queryData = this.state
-    const isHorizontal = this.props.widgetparamsMapped.horizontal.toLowerCase()
-    const layout: any = {
-      showlegend,
-      title,
-    }
-    if (xtitle) {
-      layout.xaxis = {
-        title: xtitle,
-      }
-    }
-    if (xaxistype) {
-      layout.xaxis = {
-        ...layout.xaxis,
-        xaxistype: xaxistype.toLowerCase(),
-      }
-    }
-    if (ytitle) {
-      layout.yaxis = {
-        title: ytitle,
-      }
-    }
-    // init plot_data
-    const plotData: any = []
-    const orientation = isHorizontal ? 'v' : 'h'
-    const headers = queryData.queryData.queryResult?.queryResults.headers ?? []
-    for (let i = 0; i < headers.length - 1; i += 1) {
-      // make an entry for each set of data points
-      plotData[i] = {
-        orientation,
-        name: headers[i + 1].name,
-        type: type.toLowerCase(),
-        x: [],
-        y: [],
-      }
-    }
-    // grab all the data
-    for (const row of queryData.queryData.queryResult?.queryResults.rows ??
-      []) {
-      for (let j = 1; j < row.values.length; j += 1) {
-        // create pairs of data
-        const rowValues: any = row.values
-        plotData[j - 1].x.push(rowValues[0])
-        plotData[j - 1].y.push(rowValues[j])
-      }
-    }
-    return <Plot layout={layout} data={plotData} />
-  }
-
-  public render() {
-    if (!this.state.isLoaded) {
-      return null
-    }
-    return this.showPlot()
-  }
-}
 export default SynapsePlot
