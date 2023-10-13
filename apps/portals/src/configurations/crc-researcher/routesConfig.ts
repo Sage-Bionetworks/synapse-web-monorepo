a7d096cae4cd5788227f95be3dfb3f159b59ad7b
Jay Hodgson <jay.hodgson@sagebase.org>
2023-10-13 13:04:30 -0700

    fix types

--- apps/portals/src/configurations/crc-researcher/routesConfig.ts
+++ apps/portals/src/configurations/crc-researcher/routesConfig.ts
@@ -27,16 +27,16 @@ const routes: GenericRoute[] = [
         name: 'SynapsePlot',
         isOutsideContainer: false,
         props: {
-          widgetparamsMapped: {
+          synapsePlotWidgetParams: {
             query:
               'SELECT "date", CONTACT as "New accounts created", survey_1 as "Completed Survey 1", survey_2 as "Completed Survey 2", survey_3 as "Completed Survey 3", survey_4 as "Completed Survey 4" FROM syn22314856',
             title: 'New Participants Per Survey Per Day',
             xtitle: 'Date',
             ytitle: 'Count',
             type: 'scatter',
-            horizontal: 'true',
+            horizontal: true,
             // xaxistype:,
-            showlegend: 'true',
+            showlegend: true,
           },
         },
       },
