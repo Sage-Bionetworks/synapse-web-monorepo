76849fb2301a7c82f33e031474726526d0be6451
jay-hodgson <jay.hodgson@sagebase.org>
2023-10-19 17:13:31 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@78854d815022b01f2ced9bd8e0ddeb3a4926099a 🚀

---
+++ assets/researchProjectHandlers-039e1a94.js
@@ -0,0 +1,2 @@
+import{l as a}from"./index-15d66a4c.js";import{R,g as n}from"./SynapseClient-6827eecb.js";import{M as E}from"./MockResearchProject-810a3b51.js";function p(o){return[a.rest.post(`${o}${R}`,async(r,t,s)=>{const e=await r.json();return t(s.status(201),s.json(e))}),a.rest.get(`${o}${n(":id")}`,async(r,t,s)=>{const e={...E,accessRequirementId:r.params.id.toString()};return t(s.status(200),s.json(e))})]}export{p as g};
+//# sourceMappingURL=researchProjectHandlers-039e1a94.js.map
