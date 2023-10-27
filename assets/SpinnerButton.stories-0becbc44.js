de5cd66479df827736796580fbaf7748740f9af7
nickgros <nickgrosenbacher@gmail.com>
2023-10-27 18:51:52 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@d76097510d4440e7024b7369bb8c5f0966c1e2f4 🚀

---
+++ assets/SpinnerButton.stories-0becbc44.js
@@ -0,0 +1,13 @@
+import{j as t}from"./jsx-runtime-095bf462.js";import{S as i}from"./SpinnerButton-8a039098.js";import{S as s,a as c}from"./SynapseClient-9e187ec0.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./LoginAwareButton-e974f631.js";import"./react-router-dom-6af46c97.js";import"./inheritsLoose-c82a83d4.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./styled-9c91c4fe.js";import"./tiny-invariant-dd7d57d2.js";import"./SynapseConstants-c9342f94.js";import"./OrientationBanner-7886af1c.js";import"./FullWidthAlert-918940ef.js";import"./AlertTitle-af39c120.js";import"./createSvgIcon-fd8a55dc.js";import"./Paper-88da9df3.js";import"./IconButton-152addcf.js";import"./ButtonBase-7c3b1b89.js";import"./emotion-react.browser.esm-8f02f344.js";import"./assertThisInitialized-081f9914.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";import"./Typography-d16533eb.js";import"./extendSxProp-e5d44a75.js";import"./Stack-c9edc483.js";import"./Box-49321e59.js";import"./useTheme-68d5aa8d.js";import"./Grow-adae5ed8.js";import"./isHostComponent-fa76b8d9.js";import"./index-8ce4a492.js";import"./utils-87fd7144.js";import"./ClickAwayListener-36b79525.js";import"./Tooltip-39e0afa2.js";import"./Button-c45f00b4.js";import"./getEndpoint-ac94413e.js";import"./Link-fc5d7a73.js";import"./_getTag-39555acc.js";import"./isArray-5e3f9107.js";import"./StringUtils-2ea3ab4d.js";import"./dayjs.min-59ef0ea6.js";import"./index-58d3fd43.js";import"./CheckCircleTwoTone-784a445a.js";import"./inputBaseClasses-f05f2ac2.js";import"./Fade-26b5aaed.js";const et={title:"UI/SpinnerButton",component:i,argTypes:{isAuthenticated:{control:{type:"boolean"},defaultValue:!0},showSpinner:{control:{type:"boolean"},defaultValue:!1}},render:o=>{const a=o.isAuthenticated;return delete o.isAuthenticated,t(s,{children:e=>t(c,{synapseContext:{...e,accessToken:a?e.accessToken??"fake token":void 0},children:t("div",{children:t(i,{...o})})})})}},r={args:{isAuthenticated:!0,children:"Make Async call",showSpinner:!1,variant:"contained",color:"primary",onClick:()=>{alert("clicked")}}};var n,p,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: true,
+    children: 'Make Async call',
+    showSpinner: false,
+    variant: 'contained',
+    color: 'primary',
+    onClick: () => {
+      alert('clicked');
+    }
+  }
+}`,...(m=(p=r.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const it=["Demo"];export{r as Demo,it as __namedExportsOrder,et as default};
+//# sourceMappingURL=SpinnerButton.stories-0becbc44.js.map
--- assets/researchProjectHandlers-7df4474f.js
+++
@@ -1,2 +0,0 @@
-import{l as a}from"./index-2484f9c8.js";import{R as E,m as R}from"./SynapseClient-1c2fcc91.js";import{M as n}from"./MockResearchProject-95cb83f3.js";function i(o){return[a.rest.post(`${o}${E}`,async(r,t,s)=>{const e=await r.json();return t(s.status(201),s.json(e))}),a.rest.get(`${o}${R(":id")}`,async(r,t,s)=>{const e={...n,accessRequirementId:r.params.id.toString()};return t(s.status(200),s.json(e))})]}export{i as g};
-//# sourceMappingURL=researchProjectHandlers-7df4474f.js.map
