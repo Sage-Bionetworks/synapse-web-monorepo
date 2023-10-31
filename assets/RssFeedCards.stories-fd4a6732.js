609ed06e8323aa26f4809e5f3593f37fa4d5b1f7
jay-hodgson <jay.hodgson@sagebase.org>
2023-10-31 23:05:55 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@46659a2f8c746b7a74654e544013e6e59eef52d4 🚀

---
+++ assets/RssFeedCards.stories-fd4a6732.js
@@ -0,0 +1,14 @@
+import{R as a}from"./RssFeedCards-d85a8e31.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./url-20d5ecc4.js";import"./util-f846e4fa.js";import"./index-cc134824.js";import"./dayjs.min-59ef0ea6.js";import"./browser-9b5bb7e7.js";import"./NoData-59ea70fa.js";import"./LargeButton-4bb0cd55.js";import"./Button-8994ccb5.js";import"./styled-708eb06f.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./ButtonBase-e5aaf94c.js";import"./emotion-react.browser.esm-df0ef9f3.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useForkRef-f2aa3d66.js";import"./useIsFocusVisible-bbf13f29.js";const S={title:"Home Page/RssFeedCards",component:a},e={args:{url:"https://news.adknowledgeportal.org",itemsToShow:3,allowCategories:["Data Release","News","Webinar","rosMAP"],mailChimpListName:"AMP-AD quarterly newsletter",mailChimpUrl:"https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a",lockedColumn:{value:"MSBB"},viewAllNewsButtonText:"View All AD News"}};var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    url: 'https://news.adknowledgeportal.org',
+    itemsToShow: 3,
+    allowCategories: ['Data Release', 'News', 'Webinar', 'rosMAP'],
+    mailChimpListName: 'AMP-AD quarterly newsletter',
+    mailChimpUrl: 'https://sagebase.us7.list-manage.com/subscribe/post?u=b146de537186191a9d2110f3a&amp;id=96b614587a',
+    lockedColumn: {
+      value: 'MSBB'
+    },
+    viewAllNewsButtonText: 'View All AD News'
+  }
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const f=["Demo"];export{e as Demo,f as __namedExportsOrder,S as default};
+//# sourceMappingURL=RssFeedCards.stories-fd4a6732.js.map
---
+++ assets/ThemesPlot.stories-fa2ac068.js
@@ -0,0 +1,56 @@
+import{T as p}from"./ThemesPlot-df7f03ed.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./ElementWithTooltip-17dffe6c.js";import"./SynapseTableConstants-5f56c39f.js";import"./index-6a0b5248.js";import"./IconSvg-5d94ff35.js";import"./createSvgIcon-ad67928b.js";import"./styled-708eb06f.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./Tooltip-b7fc1fa3.js";import"./useTheme-8e3749c8.js";import"./Grow-2ef26a27.js";import"./isHostComponent-fa76b8d9.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-4b9b9c32.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-f2b81dde.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-ca5df95b.js";import"./GetAppTwoTone-d6d215ac.js";import"./InfoOutlined-f9189c3f.js";import"./CheckCircleTwoTone-9226ff35.js";import"./RegularExpressions-bc0adf55.js";import"./Icon-58c3c2fa.js";import"./SynapseConstants-4a3010cb.js";import"./OrientationBanner-a68ba799.js";import"./FullWidthAlert-92257a48.js";import"./AlertTitle-b21ffb10.js";import"./Paper-7e3a1b51.js";import"./IconButton-c6432c64.js";import"./ButtonBase-e5aaf94c.js";import"./emotion-react.browser.esm-df0ef9f3.js";import"./assertThisInitialized-081f9914.js";import"./Typography-661a732d.js";import"./extendSxProp-df3a54f3.js";import"./Stack-54f6c305.js";import"./Box-9f4bb844.js";import"./ClickAwayListener-36b79525.js";import"./Button-8994ccb5.js";import"./Dropdown-5dc5a7ec.js";import"./ThemeProvider-c7f6a893.js";import"./index-58d3fd43.js";import"./hasClass-ec9efd32.js";import"./createWithBsPrefix-90b90fab.js";import"./getEndpoint-ac94413e.js";import"./SynapseClient-13512ff2.js";import"./Link-f7a9268e.js";import"./_getTag-376e1527.js";import"./isArray-5e3f9107.js";import"./StringUtils-2ea3ab4d.js";import"./dayjs.min-59ef0ea6.js";import"./tiny-invariant-dd7d57d2.js";import"./inputBaseClasses-5ec058ed.js";import"./Fade-7b562d23.js";import"./SqlFunctions-da23a214.js";import"./factory-275e83af.js";import"./_baseClone-6c8475ae.js";import"./_Uint8Array-7452eb2c.js";import"./_baseTimes-8715be3e.js";import"./sortBy-fb74b0ce.js";import"./hasIn-f6dcb7c2.js";import"./isSymbol-7c514724.js";import"./toString-cc90cb98.js";import"./_getMatchData-4a84011d.js";import"./_cacheHas-be9101cd.js";import"./_setToArray-3d6ec6fd.js";import"./identity-46f208ab.js";import"./_baseFor-d254fa1e.js";import"./_baseRest-93480809.js";import"./_overRest-d604b584.js";import"./_isIterateeCall-db26cb66.js";import"./pick-3f1e07b9.js";import"./_baseSlice-cf92e063.js";import"./isPlainObject-37d87cd2.js";import"./uniq-58323293.js";import"./times-ac139058.js";import"./without-81e8db1a.js";import"./isArrayLikeObject-3672b360.js";import"./merge-ef9cbdc0.js";import"./uniqueId-4d05949d.js";import"./cloneDeep-ccda587a.js";import"./isNil-00e7296d.js";import"./LoadingScreen-7b803862.js";import"./Backdrop-e3dff1d0.js";import"./LinearProgress-9ffd4557.js";const Ao={title:"Home Page/ThemesPlot",component:p},o={args:{onPointClick:m=>{console.log(m.event)},topBarPlot:{entityId:"syn21641485",xField:"totalCount",yField:"groupBy",groupField:"consortium",colors:{CSBC:"rgba(64,123,160, 1)","PS-ON":"rgba(91,176,181,1)",ICBP:"rgba(197, 191, 223, 1)",TEC:"rgba(156, 196, 233, 1)"},whereClause:"totalCount is not NULL"},sideBarPlot:{entityId:"syn21649281",xField:"totalCount",yField:"theme",groupField:"consortium",countLabel:"grants",plotStyle:{backgroundColor:"#f5f9fa"},colors:{CSBC:"#1c76af","PS-ON":"#5bb0b5",ICBP:"#9cc4e9",TEC:"#c5bfdf"}},dotPlot:{entityId:"syn21639584",xField:"totalCount",yField:"theme",groupField:"groupBy",infoField:"themeDescription",whereClause:"groupBy IN ('publications', 'tools', 'datasets')",markerSymbols:{tools:"y-down",datasets:"diamond-x",publications:"circle"},plotStyle:{markerLine:"rgba(0, 0, 0,1)",markerFill:"rgba(255, 255, 255,1)",markerSize:11,backgroundColor:"#f5f9fa"}}}};var t,r,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    onPointClick: e => {
+      console.log(e.event);
+    },
+    topBarPlot: {
+      entityId: 'syn21641485',
+      xField: 'totalCount',
+      yField: 'groupBy',
+      groupField: 'consortium',
+      colors: {
+        CSBC: 'rgba(64,123,160, 1)',
+        'PS-ON': 'rgba(91,176,181,1)',
+        ICBP: 'rgba(197, 191, 223, 1)',
+        TEC: 'rgba(156, 196, 233, 1)'
+      },
+      whereClause: 'totalCount is not NULL'
+    },
+    sideBarPlot: {
+      entityId: 'syn21649281',
+      xField: 'totalCount',
+      yField: 'theme',
+      groupField: 'consortium',
+      countLabel: 'grants',
+      plotStyle: {
+        backgroundColor: '#f5f9fa'
+      },
+      colors: {
+        CSBC: '#1c76af',
+        'PS-ON': '#5bb0b5',
+        ICBP: '#9cc4e9',
+        TEC: '#c5bfdf'
+      }
+    },
+    dotPlot: {
+      entityId: 'syn21639584',
+      xField: 'totalCount',
+      yField: 'theme',
+      groupField: 'groupBy',
+      infoField: 'themeDescription',
+      whereClause: "groupBy IN ('publications', 'tools', 'datasets')",
+      markerSymbols: {
+        tools: 'y-down',
+        datasets: 'diamond-x',
+        publications: 'circle'
+      },
+      plotStyle: {
+        markerLine: 'rgba(0, 0, 0,1)',
+        markerFill: 'rgba(255, 255, 255,1)',
+        markerSize: 11,
+        backgroundColor: '#f5f9fa'
+      }
+    }
+  }
+}`,...(i=(r=o.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const Go=["ThemesPlotDemo"];export{o as ThemesPlotDemo,Go as __namedExportsOrder,Ao as default};
+//# sourceMappingURL=ThemesPlot.stories-fa2ac068.js.map
