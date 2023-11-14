e3d2b99bdd71e1ba874102db6b71821080a1abf7
jay-hodgson <jay.hodgson@sagebase.org>
2023-11-14 00:41:12 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@a75fa5539d34c5404a4b4cd49b263f48c10a1b75 🚀

---
+++ assets/IconList.stories-5ff5fa34.js
@@ -0,0 +1,121 @@
+import{I as E}from"./IconList-54c09c6c.js";import"./jsx-runtime-095bf462.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./IconSvg-a0643db9.js";import"./createSvgIcon-c69fbb93.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-4abd396b.js";import"./styled-17e9e47b.js";import"./Tooltip-9098414d.js";import"./useTheme-e16afdbe.js";import"./isHostComponent-fa76b8d9.js";import"./Grow-0db08d4d.js";import"./useForkRef-f2aa3d66.js";import"./index-8ce4a492.js";import"./utils-02ec0d60.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-aa95d3cf.js";import"./useIsFocusVisible-bbf13f29.js";import"./ErrorOutlined-3dd860d7.js";import"./GetAppTwoTone-355d174d.js";import"./InfoOutlined-61b6cf08.js";import"./CheckCircleTwoTone-8c636771.js";const H={title:"UI/IconList",component:E},n={args:{iconConfigs:{genomicVariants:{icon:"geneVariants"},geneExpression:{icon:"geneExpression"},image:{icon:"imaging"},drugCombinationScreen:{icon:"rat"},reload:{icon:"reload"},check:{icon:"check"},proteomics:{icon:"proteomics"},kinomics:{icon:"kinomics"}},iconNames:["reload","drugCombinationScreen","proteomics","check"]}},o={args:{iconConfigs:{genomicVariants:{icon:"geneVariants",sx:{color:"#ff0000"}},geneExpression:{icon:"geneExpression",sx:{color:"orange"}},image:{icon:"imaging",sx:{color:"yellow"}},drugCombinationScreen:{icon:"rat",sx:{color:"#00FF00"}},reload:{icon:"reload",sx:{color:"blue"}},check:{icon:"check",sx:{color:"#2E2B5F"}},kinomics:{icon:"kinomics",sx:{color:"#8B00FF"}}},iconNames:["image","genomicVariants","geneExpression","drugCombinationScreen","reload","check","kinomics"]}},e={args:{iconConfigs:{drugCombinationScreen:{icon:"rat"},geneExpression:{icon:"geneExpression"}},useTheme:!0,iconNames:["drugCombinationScreen","geneExpression"]}},r={args:{iconConfigs:{drugCombinationScreen:{icon:"rat"},geneVariants:{icon:"geneVariants"}},useTheme:!0,useBackground:!0,iconNames:["drugCombinationScreen","geneVariants"]}},i={args:{iconConfigs:{drugCombinationScreen:{icon:"rat",label:"I am a mouse!"},geneExpression:{icon:"geneExpression",label:":-D"}},iconNames:["drugCombinationScreen","geneExpression"]}};var s,c,a;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      genomicVariants: {
+        icon: 'geneVariants'
+      },
+      geneExpression: {
+        icon: 'geneExpression'
+      },
+      image: {
+        icon: 'imaging'
+      },
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      reload: {
+        icon: 'reload'
+      },
+      check: {
+        icon: 'check'
+      },
+      proteomics: {
+        icon: 'proteomics'
+      },
+      kinomics: {
+        icon: 'kinomics'
+      }
+    },
+    iconNames: ['reload', 'drugCombinationScreen', 'proteomics', 'check']
+  }
+}`,...(a=(c=n.parameters)==null?void 0:c.docs)==null?void 0:a.source}}};var t,m,g;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      genomicVariants: {
+        icon: 'geneVariants',
+        sx: {
+          color: '#ff0000'
+        }
+      },
+      geneExpression: {
+        icon: 'geneExpression',
+        sx: {
+          color: 'orange'
+        }
+      },
+      image: {
+        icon: 'imaging',
+        sx: {
+          color: 'yellow'
+        }
+      },
+      drugCombinationScreen: {
+        icon: 'rat',
+        sx: {
+          color: '#00FF00'
+        }
+      },
+      reload: {
+        icon: 'reload',
+        sx: {
+          color: 'blue'
+        }
+      },
+      check: {
+        icon: 'check',
+        sx: {
+          color: '#2E2B5F'
+        }
+      },
+      kinomics: {
+        icon: 'kinomics',
+        sx: {
+          color: '#8B00FF'
+        }
+      }
+    },
+    iconNames: ['image', 'genomicVariants', 'geneExpression', 'drugCombinationScreen', 'reload', 'check', 'kinomics']
+  }
+}`,...(g=(m=o.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var p,l,u;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      geneExpression: {
+        icon: 'geneExpression'
+      }
+    },
+    useTheme: true,
+    iconNames: ['drugCombinationScreen', 'geneExpression']
+  }
+}`,...(u=(l=e.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,C,x;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      geneVariants: {
+        icon: 'geneVariants'
+      }
+    },
+    useTheme: true,
+    useBackground: true,
+    iconNames: ['drugCombinationScreen', 'geneVariants']
+  }
+}`,...(x=(C=r.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var S,b,k;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat',
+        label: 'I am a mouse!'
+      },
+      geneExpression: {
+        icon: 'geneExpression',
+        label: ':-D'
+      }
+    },
+    iconNames: ['drugCombinationScreen', 'geneExpression']
+  }
+}`,...(k=(b=i.parameters)==null?void 0:b.docs)==null?void 0:k.source}}};const J=["NoColorSet","CustomColors","ThemeColor","ThemeColorWithCircleBackground","Tooltip"];export{o as CustomColors,n as NoColorSet,e as ThemeColor,r as ThemeColorWithCircleBackground,i as Tooltip,J as __namedExportsOrder,H as default};
+//# sourceMappingURL=IconList.stories-5ff5fa34.js.map
