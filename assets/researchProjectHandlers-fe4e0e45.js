2c3f266476144d2a6361a5250b6536864d443e3c
nickgros <nickgrosenbacher@gmail.com>
2023-12-01 21:34:44 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@9fbf49ea5ab29ff78bf19bf6d46e69c007dd4187 🚀

---
+++ assets/researchProjectHandlers-fe4e0e45.js
@@ -0,0 +1,2 @@
+import{l as a}from"./index-63ca33f0.js";import{R,g as n}from"./SynapseClient-1c857948.js";import{M as E}from"./MockResearchProject-f2f61a17.js";function p(o){return[a.rest.post(`${o}${R}`,async(r,t,s)=>{const e=await r.json();return t(s.status(201),s.json(e))}),a.rest.get(`${o}${n(":id")}`,async(r,t,s)=>{const e={...E,accessRequirementId:r.params.id.toString()};return t(s.status(200),s.json(e))})]}export{p as g};
+//# sourceMappingURL=researchProjectHandlers-fe4e0e45.js.map
