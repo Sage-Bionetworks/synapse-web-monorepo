de3534cfe7c8bb9a77fade6d31f9890a6eff31c9
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-12-07 12:18:33 -0500

    SWC-6612  - Remove unnecessary coercion in BooleanSchema  - Add tests for BooleanSchema

--- packages/synapse-react-client/src/components/TableColumnSchemaEditor/Validators/BooleanSchema.ts
+++ packages/synapse-react-client/src/components/TableColumnSchemaEditor/Validators/BooleanSchema.ts
@@ -5,6 +5,4 @@ const stringToBoolean = z.enum(['true', 'false']).transform(v => v === 'true')
 /**
  * Validates and converts a value to a valid Synapse BOOLEAN value
  */
-export const booleanSchema = z
-  .union([stringToBoolean, z.boolean()])
-  .pipe(z.coerce.boolean())
+export const booleanSchema = z.union([stringToBoolean, z.boolean()])
