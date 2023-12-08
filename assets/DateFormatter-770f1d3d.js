2fb7426cab493d4cbf3c51a33dffa1ee8801bc8c
jay-hodgson <jay.hodgson@sagebase.org>
2023-12-08 19:44:15 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@aa4bfbe36d7972f5c94eba1f5bf9e112ff3dff98 🚀

---
+++ assets/DateFormatter-770f1d3d.js
@@ -0,0 +1,2 @@
+import{n as m}from"./SynapseClient-2cd0e39d.js";import"./getEndpoint-ac94413e.js";import"./OrientationBanner-9afa28c1.js";import{d as e}from"./dayjs.min-59ef0ea6.js";import{u as r,t as a,a as i,l as s}from"./timezone-fd382bd1.js";e.extend(r);e.extend(a);e.extend(i);e.extend(s);function z(t,o){return o||(o="M/D/YYYY h:mm A",m.getUseUtcTimeFromCookie()&&(o+=" z")),m.getUseUtcTimeFromCookie()?e(t).tz("utc").format(o):e(t).tz(e.tz.guess()).format(o)}export{z as f};
+//# sourceMappingURL=DateFormatter-770f1d3d.js.map
