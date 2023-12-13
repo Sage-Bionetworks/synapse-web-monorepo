24a11e0589d5764d71fbb2908ba664daa02760d5
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2023-12-13 13:53:24 -0500

    SWC-6627 - Improvements for custom RadioGroup  - Allow passing a ReactNode label  - Allow disabling the group  - Make `id` optional, and use `useId` to generate a unique ID when one is not provided
    In SWC-6627 we will be reimplementing the ability to add evaluations to a SubmissionView. The existing UI uses a radio group. We will want to disable the radio group inputs while the data changes, e.g. loading new pages

--- packages/synapse-react-client/src/components/widgets/RadioGroup.test.tsx
+++ packages/synapse-react-client/src/components/widgets/RadioGroup.test.tsx
@@ -35,7 +35,7 @@ describe('basic function', () => {
     const radioOptions = screen.getAllByRole<HTMLInputElement>('radio')
     expect(radioOptions).toHaveLength(props.options.length)
     expect(radioGroup.className.includes('radioGroupClass')).toBe(true)
-    screen.getByLabelText(props.options[0].label)
+    screen.getByLabelText(props.options[0].label as string)
 
     expect(radioOptions[0]).not.toBeChecked()
     expect(radioOptions[1]).toBeChecked()
@@ -62,5 +62,16 @@ describe('basic function', () => {
     expect(radioOptions[0]).not.toBeChecked()
     expect(radioOptions[1]).not.toBeChecked()
     expect(radioOptions[2]).not.toBeChecked()
+  })
+
+  it('should disable the input values when disabled is true', () => {
+    init({ ...props, disabled: true })
+    screen.getByRole<HTMLDivElement>('radiogroup')
+    const radioOptions = screen.getAllByRole<HTMLInputElement>('radio')
+    expect(radioOptions).toHaveLength(props.options.length)
+
+    expect(radioOptions[0]).toBeDisabled()
+    expect(radioOptions[1]).toBeDisabled()
+    expect(radioOptions[2]).toBeDisabled()
   })
 })
--- packages/synapse-react-client/src/components/widgets/RadioGroup.tsx
+++ packages/synapse-react-client/src/components/widgets/RadioGroup.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
-import { uniqueId as _uniqueId } from 'lodash-es'
+import React, { useId } from 'react'
 
 export type RadioGroupProps<T extends string | boolean | number = string> = {
-  options: { label: string; value: T }[]
-  id: string
+  options: { label: React.ReactNode; value: T }[]
+  id?: string
   className?: string
   value?: T
   onChange: (value: T) => void
+  disabled?: boolean
 }
 
 export function RadioGroup<T extends string | boolean | number = string>(
@@ -17,15 +17,15 @@ export function RadioGroup<T extends string | boolean | number = string>(
     : `radiogroup`
 
   return (
-    <div className={className} role="radiogroup">
+    <div id={props.id} className={className} role="radiogroup">
       {props.options.map((option, index) => (
         <RadioOption<T>
           key={index.toString()}
-          groupId={props.id}
           label={option.label}
           value={option.value}
           currentValue={props.value}
           onChange={props.onChange}
+          disabled={props.disabled}
         />
       ))}
     </div>
@@ -33,18 +33,18 @@ export function RadioGroup<T extends string | boolean | number = string>(
 }
 
 export type RadioOptionProps<T extends string | boolean | number = string> = {
-  groupId: string
-  label: string
+  label: React.ReactNode
   value: T
   currentValue?: T
   style?: React.CSSProperties
   onChange: (value: T) => void
+  disabled?: boolean
 }
 
 export function RadioOption<T extends string | boolean | number = string>(
   props: RadioOptionProps<T>,
 ) {
-  const [uniqueId] = useState(_uniqueId('src-radio-'))
+  const uniqueId = useId()
   return (
     <div className={'radio'} onClick={() => props.onChange(props.value)}>
       <input
@@ -55,6 +55,7 @@ export function RadioOption<T extends string | boolean | number = string>(
         }}
         checked={props.currentValue === props.value}
         value={props.value.toString()}
+        disabled={props.disabled}
       />
       <label htmlFor={uniqueId} style={props.style}>
         {props.label}
