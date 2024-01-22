58eba19248d5f51611b56f17b8d7243fee48d4f4
Hallie Swan <26949006+hallieswan@users.noreply.github.com>
2024-01-22 15:11:14 -0800

    PORTALS-2898: bump playwright version to name blob report in playwright config

--- apps/portals/playwright.config.ts
+++ apps/portals/playwright.config.ts
@@ -36,7 +36,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['list'], ['blob']] : 'html',
+  reporter: process.env.CI
+    ? [['list'], ['blob', { fileName: `report-${process.env.PORTAL!}.zip` }]]
+    : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
