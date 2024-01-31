ab7ed7e6c9751777ec561d629c540f6a2acec31f
hallieswan <26949006+hallieswan@users.noreply.github.com>
2024-01-31 18:30:13 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@125b2c6f74b79889e951b92866403f88dffd1c5a 🚀

--- assets/mockAnnotationColumns-d9c0f167.js
+++
@@ -1,2 +0,0 @@
-import{C as e}from"./index-7db48dfb.js";const n={concreteType:"org.sagebionetworks.repo.model.table.ViewColumnModelResponse",results:[{id:"1235325",columnType:e.STRING,name:"columnFromAnnotations",maximumSize:10}],nextPageToken:null};export{n as M};
-//# sourceMappingURL=mockAnnotationColumns-d9c0f167.js.map
--- assets/researchProjectHandlers-9c6f3c7d.js
+++
@@ -1,2 +0,0 @@
-import{l as a}from"./index-609b69aa.js";import{R,t as n}from"./SynapseClient-776746cd.js";import{M as E}from"./MockResearchProject-20e47bfd.js";function p(o){return[a.rest.post(`${o}${R}`,async(t,r,s)=>{const e=await t.json();return r(s.status(201),s.json(e))}),a.rest.get(`${o}${n(":id")}`,async(t,r,s)=>{const e={...E,accessRequirementId:t.params.id.toString()};return r(s.status(200),s.json(e))})]}export{p as g};
-//# sourceMappingURL=researchProjectHandlers-9c6f3c7d.js.map
