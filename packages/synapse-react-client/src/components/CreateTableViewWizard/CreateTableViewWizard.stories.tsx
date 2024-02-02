780750d909cdfc238abd186aa2137c0bf76e024e
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-02-01 20:20:28 -0500

    SWC-6678 - Add onComplete story prop, figma design

--- packages/synapse-react-client/src/components/CreateTableViewWizard/CreateTableViewWizard.stories.tsx
+++ packages/synapse-react-client/src/components/CreateTableViewWizard/CreateTableViewWizard.stories.tsx
@@ -1,9 +1,16 @@
 import { Meta, StoryObj } from '@storybook/react'
 import CreateTableViewWizard from './CreateTableViewWizard'
+import { displayToast } from '../ToastMessage'
 
 const meta = {
   title: 'Synapse/Create Table Wizard',
   component: CreateTableViewWizard,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/vLusb6uSfhx45OyFx5IHwy/(XDM)-Extensible-Data-Management-Comps?type=design&node-id=7108-22337&mode=design&t=acwaLAqUDge5p8eQ-4',
+    },
+  },
   tags: ['autodocs'],
 } satisfies Meta
 
@@ -15,5 +22,8 @@ export const Demo: Story = {
   args: {
     open: true,
     parentId: 'syn23567475',
+    onComplete: newId => {
+      displayToast(`Successfully created ${newId}`)
+    },
   },
 }
