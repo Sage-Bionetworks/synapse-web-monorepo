a68d94553d84ec211c89614015d20f566048a787
Jay Hodgson <jay.hodgson@sagebase.org>
2024-02-21 17:46:31 -0800

    PORTALS-2934

--- apps/portals/src/portal-components/challengeportal/ChallengeDetailPageWrapper.tsx
+++ apps/portals/src/portal-components/challengeportal/ChallengeDetailPageWrapper.tsx
@@ -1,6 +1,10 @@
 import { Box } from '@mui/material'
 import * as React from 'react'
-import { AppUtils, ChallengeDetailPage } from 'synapse-react-client'
+import {
+  AppUtils,
+  ChallengeDetailPage,
+  SynapseQueries,
+} from 'synapse-react-client'
 
 /**
  * This is the Challenge Registration button.  This includes:
@@ -11,7 +15,23 @@ import { AppUtils, ChallengeDetailPage } from 'synapse-react-client'
  */
 const ChallengeDetailPageWrapper = () => {
   const projectId = AppUtils.useQuerySearchParam('id')
-  if (projectId) {
+  const { data, isLoading } = SynapseQueries.useGetEntityBundle(
+    projectId,
+    undefined,
+    {
+      includeAnnotations: true,
+    },
+    {
+      enabled: !!projectId,
+    },
+  )
+  if (isLoading) {
+    return <></>
+  }
+  if (
+    !!projectId &&
+    data?.annotations.annotations['Status'].value[0] == 'Active'
+  ) {
     return (
       <div className="container-fluid">
         <div className="row">
