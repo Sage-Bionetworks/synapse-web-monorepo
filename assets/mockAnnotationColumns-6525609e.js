c436fdbd215d6e20e51c49b896543d04a9a91c05
nickgros <nickgrosenbacher@gmail.com>
2024-02-27 18:21:29 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@b91fbbcaa106277ab419e12a0f8560ed92f4fbb1 🚀

--- assets/mockAnnotationColumns-6525609e.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-3a31b023.js";const n={concreteType:"org.sagebionetworks.repo.model.table.ViewColumnModelResponse",results:[{id:"1235325",columnType:e.STRING,name:"columnFromAnnotations",maximumSize:10}],nextPageToken:null};export{n as M};
