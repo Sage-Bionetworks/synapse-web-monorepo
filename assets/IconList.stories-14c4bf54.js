abd0a95a7937ab4bd9c91b9b2158fef20a981e21
nickgros <nickgrosenbacher@gmail.com>
2024-02-27 13:24:46 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@8a00b1e8e08070980cfb0639d2a6c1a5beec5d5f 🚀

---
+++ assets/IconList.stories-14c4bf54.js
@@ -0,0 +1,128 @@
+import{I as k}from"./IconList-f3e26e25.js";import"./jsx-runtime-9dc53467.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./IconSvg-a7217c47.js";import"./createSvgIcon-396e3e24.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./styled-3c6f4d83.js";import"./Tooltip-801f2a9c.js";import"./useTheme-e79ecbc0.js";import"./isHostComponent-fa76b8d9.js";import"./Grow-c431c788.js";import"./useForkRef-8feb2ebf.js";import"./index-d3ea75b5.js";import"./utils-6fa73630.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-e6fd87c7.js";import"./useIsFocusVisible-18cd238e.js";import"./ErrorOutlined-6f3ac166.js";import"./GetAppTwoTone-d672483d.js";import"./InfoOutlined-e9da64e0.js";import"./CheckCircleTwoTone-c37d81c7.js";import"./merge-75f6c925.js";import"./_Uint8Array-df44b265.js";import"./_Map-92f6da1c.js";import"./isArray-5e3f9107.js";import"./_baseTimes-8715be3e.js";import"./_initCloneObject-c3239c20.js";import"./_baseFor-d254fa1e.js";import"./isArrayLikeObject-8a0d3588.js";import"./isPlainObject-e233fb76.js";import"./_baseRest-c9d7baca.js";import"./identity-46f208ab.js";import"./_overRest-2155f832.js";import"./_isIterateeCall-9cddbe78.js";const en={title:"UI/IconList",component:k},n={args:{iconConfigs:{genomicVariants:{icon:"geneVariants"},geneExpression:{icon:"geneExpression"},image:{icon:"imaging"},drugCombinationScreen:{icon:"rat"},reload:{icon:"reload"},check:{icon:"check"},proteomics:{icon:"proteomics"},kinomics:{icon:"kinomics"}},iconNames:["reload","drugCombinationScreen","proteomics","check"]}},o={args:{iconConfigs:{genomicVariants:{icon:"geneVariants",sx:{color:"#ff0000"}},geneExpression:{icon:"geneExpression",sx:{color:"orange"}},image:{icon:"imaging",sx:{color:"yellow"}},drugCombinationScreen:{icon:"rat",sx:{color:"#00FF00"}},reload:{icon:"reload",sx:{color:"blue"}},check:{icon:"check",sx:{color:"#2E2B5F"}},kinomics:{icon:"kinomics",sx:{color:"#8B00FF"}}},iconNames:["image","genomicVariants","geneExpression","drugCombinationScreen","reload","check","kinomics"]}},e={args:{iconConfigs:{drugCombinationScreen:{icon:"rat"},geneExpression:{icon:"geneExpression"}},useTheme:!0,iconNames:["drugCombinationScreen","geneExpression"]}},r={args:{iconConfigs:{drugCombinationScreen:{icon:"rat"},geneVariants:{icon:"geneVariants"},other:{icon:"other"}},commonIconProps:{sx:{fontSize:"40px"}},useTheme:!0,useBackground:!0,iconNames:["drugCombinationScreen","geneVariants","other"]}},i={args:{iconConfigs:{drugCombinationScreen:{icon:"rat",label:"I am a mouse!"},geneExpression:{icon:"geneExpression",label:":-D"}},iconNames:["drugCombinationScreen","geneExpression"]}};var c,s,a;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      genomicVariants: {
+        icon: 'geneVariants'
+      },
+      geneExpression: {
+        icon: 'geneExpression'
+      },
+      image: {
+        icon: 'imaging'
+      },
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      reload: {
+        icon: 'reload'
+      },
+      check: {
+        icon: 'check'
+      },
+      proteomics: {
+        icon: 'proteomics'
+      },
+      kinomics: {
+        icon: 'kinomics'
+      }
+    },
+    iconNames: ['reload', 'drugCombinationScreen', 'proteomics', 'check']
+  }
+}`,...(a=(s=n.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,m,g;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      genomicVariants: {
+        icon: 'geneVariants',
+        sx: {
+          color: '#ff0000'
+        }
+      },
+      geneExpression: {
+        icon: 'geneExpression',
+        sx: {
+          color: 'orange'
+        }
+      },
+      image: {
+        icon: 'imaging',
+        sx: {
+          color: 'yellow'
+        }
+      },
+      drugCombinationScreen: {
+        icon: 'rat',
+        sx: {
+          color: '#00FF00'
+        }
+      },
+      reload: {
+        icon: 'reload',
+        sx: {
+          color: 'blue'
+        }
+      },
+      check: {
+        icon: 'check',
+        sx: {
+          color: '#2E2B5F'
+        }
+      },
+      kinomics: {
+        icon: 'kinomics',
+        sx: {
+          color: '#8B00FF'
+        }
+      }
+    },
+    iconNames: ['image', 'genomicVariants', 'geneExpression', 'drugCombinationScreen', 'reload', 'check', 'kinomics']
+  }
+}`,...(g=(m=o.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var p,l,u;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      geneExpression: {
+        icon: 'geneExpression'
+      }
+    },
+    useTheme: true,
+    iconNames: ['drugCombinationScreen', 'geneExpression']
+  }
+}`,...(u=(l=e.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,x,C;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat'
+      },
+      geneVariants: {
+        icon: 'geneVariants'
+      },
+      other: {
+        icon: 'other'
+      }
+    },
+    commonIconProps: ({
+      sx: {
+        fontSize: '40px'
+      }
+    } as IconListProps['commonIconProps']),
+    useTheme: true,
+    useBackground: true,
+    iconNames: ['drugCombinationScreen', 'geneVariants', 'other']
+  }
+}`,...(C=(x=r.parameters)==null?void 0:x.docs)==null?void 0:C.source}}};var S,h,b;i.parameters={...i.parameters,docs:{...(S=i.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    iconConfigs: {
+      drugCombinationScreen: {
+        icon: 'rat',
+        label: 'I am a mouse!'
+      },
+      geneExpression: {
+        icon: 'geneExpression',
+        label: ':-D'
+      }
+    },
+    iconNames: ['drugCombinationScreen', 'geneExpression']
+  }
+}`,...(b=(h=i.parameters)==null?void 0:h.docs)==null?void 0:b.source}}};const rn=["NoColorSet","CustomColors","ThemeColor","ThemeColorWithCircleBackground","Tooltip"];export{o as CustomColors,n as NoColorSet,e as ThemeColor,r as ThemeColorWithCircleBackground,i as Tooltip,rn as __namedExportsOrder,en as default};
--- assets/Topic.stories-910ae8c3.js
+++
@@ -1,9 +0,0 @@
-import{T as m}from"./Topic-75fe3b0f.js";import"./jsx-runtime-9dc53467.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./IconSvg-83f0daf8.js";import"./createSvgIcon-396e3e24.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./styled-3c6f4d83.js";import"./Tooltip-801f2a9c.js";import"./useTheme-e79ecbc0.js";import"./isHostComponent-fa76b8d9.js";import"./Grow-c431c788.js";import"./useForkRef-8feb2ebf.js";import"./index-d3ea75b5.js";import"./utils-6fa73630.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-e6fd87c7.js";import"./useIsFocusVisible-18cd238e.js";import"./ErrorOutlined-6f3ac166.js";import"./GetAppTwoTone-d672483d.js";import"./InfoOutlined-e9da64e0.js";import"./CheckCircleTwoTone-c37d81c7.js";import"./ConditionalWrapper-28a47e51.js";import"./Stack-4eda368b.js";import"./extendSxProp-1372051e.js";import"./Button-33299b2c.js";import"./ButtonBase-8587e55b.js";import"./emotion-react.browser.esm-5fddd3df.js";import"./assertThisInitialized-081f9914.js";import"./Skeleton-131bd473.js";import"./Link-6ae0d01e.js";import"./Typography-1d068b0b.js";const C={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!"}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  name: 'Topic',
-  args: {
-    isSubscribed: true,
-    isLoading: false,
-    icon: 'discussion',
-    name: 'Help me understand this project!'
-  }
-}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const G=["Demo"];export{o as Demo,G as __namedExportsOrder,C as default};
