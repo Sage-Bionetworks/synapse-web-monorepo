5c926f50f5665df03ffd61a52d988c3d420c1020
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-02-29 09:37:14 -0500

    make the color palette demo less overwhelming

--- packages/synapse-react-client/demo/containers/playground/ColorPaletteInspector.tsx
+++ packages/synapse-react-client/demo/containers/playground/ColorPaletteInspector.tsx
@@ -1,34 +1,37 @@
-import React, { useState } from 'react'
+import React from 'react'
 import {
   COLOR_PALETTE_EVEN,
   COLOR_PALETTE_ODD,
 } from '../../../src/utils/functions/colorPalette'
+import { Box } from '@mui/material'
+import tinycolor from 'tinycolor2'
 
-type DivColorInputProps = {
-  initialColor: string
+type DivColorTextProps = {
+  color: string
 }
 
-const DivWithColorInput: React.FunctionComponent<DivColorInputProps> = ({
-  initialColor,
+const DivWithColorText: React.FunctionComponent<DivColorTextProps> = ({
+  color,
 }) => {
-  const [color, setColor] = useState(initialColor)
-
   return (
     <div>
-      <input
-        value={color}
-        style={{ width: '135px' }}
-        onChange={event => setColor(event.target.value)}
-      />
-
-      <div
-        style={{
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        sx={{
           background: color,
-          width: '135px',
-          height: '135px',
-          borderRadius: '150px',
+          width: '100px',
+          height: '20px',
+          fontSize: '10px',
+          color: color,
+          '&:hover': {
+            color: tinycolor(color).isDark() ? 'white' : 'black',
+          },
         }}
-      ></div>
+      >
+        {color}
+      </Box>
     </div>
   )
 }
@@ -36,7 +39,7 @@ const DivWithColorInput: React.FunctionComponent<DivColorInputProps> = ({
 const ColorPaletteInspector: React.FunctionComponent = () => {
   return (
     <div>
-      <h2>Color Palette Odd</h2>
+      <h2>Odd Palette</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {COLOR_PALETTE_ODD.reduce(
           (rows: any, key, index) =>
@@ -47,20 +50,17 @@ const ColorPaletteInspector: React.FunctionComponent = () => {
           [],
         ).map((palette: string[], idx: number) => {
           return (
-            <div key={'odd' + idx} style={{ margin: '5px' }}>
-              <h4>Odd palette {idx}</h4>
+            <div key={'odd' + idx}>
+              <p>Odd {idx}</p>
               {palette.map(color => (
-                <DivWithColorInput
-                  key={color}
-                  initialColor={color}
-                ></DivWithColorInput>
+                <DivWithColorText key={color} color={color}></DivWithColorText>
               ))}
             </div>
           )
         })}
       </div>
       <hr></hr>
-      <h2>Color Palette Even</h2>
+      <h2>Even Palette</h2>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {COLOR_PALETTE_EVEN.reduce(
           (rows: any, key, index) =>
@@ -71,13 +71,10 @@ const ColorPaletteInspector: React.FunctionComponent = () => {
           [],
         ).map((palette: string[], idx: number) => {
           return (
-            <div key={'even' + idx} style={{ margin: '5px' }}>
-              <h4>Even palette {idx}</h4>
+            <div key={'even' + idx}>
+              <p>Even {idx}</p>
               {palette.map(color => (
-                <DivWithColorInput
-                  key={color}
-                  initialColor={color}
-                ></DivWithColorInput>
+                <DivWithColorText key={color} color={color}></DivWithColorText>
               ))}
             </div>
           )
