9e2fc06b84b3f07965432b42bb8a9abaaebf2b4e
hallieswan <26949006+hallieswan@users.noreply.github.com>
2024-03-06 00:17:35 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@f1862a79a493c2ac833e464c42ee19f3d21a789a 🚀

---
+++ assets/DropdownMenu.stories-bccbdef4.js
@@ -0,0 +1,28 @@
+import{j as p}from"./jsx-runtime-9dc53467.js";import{D as s}from"./DropdownMenu-48295de9.js";import{I as m}from"./IconSvg-dc6fa014.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./Button-065a21d4.js";import"./createTheme-d609db80.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./styled-bb145c31.js";import"./ButtonBase-0719e676.js";import"./emotion-react.browser.esm-ca9fe929.js";import"./TransitionGroupContext-e6fd87c7.js";import"./useForkRef-8feb2ebf.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./useIsFocusVisible-18cd238e.js";import"./Typography-f217f607.js";import"./extendSxProp-7fb4e09f.js";import"./Tooltip-248f4fe4.js";import"./useTheme-a28bb397.js";import"./isHostComponent-fa76b8d9.js";import"./Grow-2f9de6a4.js";import"./index-d3ea75b5.js";import"./utils-19a63403.js";import"./Fade-05777e49.js";import"./Paper-35c12a01.js";import"./MenuList-acf5f616.js";import"./index-c71daf5e.js";import"./getScrollbarSize-ac846fe6.js";import"./List-81115ae1.js";import"./MenuItem-9dd34ee5.js";import"./dividerClasses-ed167313.js";import"./ListItemText-766155b3.js";import"./Divider-f85fbd3b.js";import"./ClickAwayListener-3b20c94f.js";import"./createSvgIcon-b657d91b.js";import"./ErrorOutlined-5d3a6347.js";import"./GetAppTwoTone-6e3519f9.js";import"./InfoOutlined-aa78e498.js";import"./CheckCircleTwoTone-6570b572.js";const V={title:"UI/DropdownMenu",component:s},o=e=>()=>{console.log("Item clicked",e)},t={args:{dropdownButtonText:"Dropdown Menu Button",buttonProps:{endIcon:p(m,{icon:"verticalEllipsis",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}};var n,i,r;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    dropdownButtonText: 'Dropdown Menu Button',
+    buttonProps: {
+      endIcon: <IconSvg icon="verticalEllipsis" wrap={false} />
+    },
+    items: [[{
+      text: 'Do something cool',
+      onClick: onClickHandler(0)
+    }, {
+      text: 'This one has a tooltip',
+      tooltipText: 'Some more info',
+      onClick: onClickHandler(1)
+    }, {
+      text: 'This one is disabled',
+      disabled: true,
+      onClick: onClickHandler(3)
+    }, {
+      text: 'This one has both',
+      disabled: true,
+      tooltipText: "You can't do this for reasons",
+      onClick: onClickHandler(4)
+    }], [{
+      text: 'You can organize actions into groups',
+      onClick: onClickHandler(5)
+    }]]
+  }
+}`,...(r=(i=t.parameters)==null?void 0:i.docs)==null?void 0:r.source}}};const W=["Demo"];export{t as Demo,W as __namedExportsOrder,V as default};
