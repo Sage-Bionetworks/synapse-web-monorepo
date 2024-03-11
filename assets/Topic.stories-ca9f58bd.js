2fcdab66e784055c6a5980be205f1d5cd1415b75
nickgros <nickgrosenbacher@gmail.com>
2024-03-11 19:02:43 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@6f43c8e7db2fa4d32db868892adfe5e3821d7526 🚀

--- assets/Topic.stories-ca9f58bd.js
+++
@@ -1,9 +0,0 @@
-import{T as m}from"./Topic-23a799e2.js";import"./jsx-runtime-9dc53467.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./IconSvg-dc6fa014.js";import"./createSvgIcon-b657d91b.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-55b21f7f.js";import"./createTheme-d609db80.js";import"./styled-bb145c31.js";import"./Tooltip-248f4fe4.js";import"./useTheme-a28bb397.js";import"./isHostComponent-fa76b8d9.js";import"./Grow-2f9de6a4.js";import"./useForkRef-8feb2ebf.js";import"./index-d3ea75b5.js";import"./utils-19a63403.js";import"./inheritsLoose-c82a83d4.js";import"./TransitionGroupContext-e6fd87c7.js";import"./useIsFocusVisible-18cd238e.js";import"./ErrorOutlined-5d3a6347.js";import"./GetAppTwoTone-6e3519f9.js";import"./InfoOutlined-aa78e498.js";import"./CheckCircleTwoTone-6570b572.js";import"./ConditionalWrapper-28a47e51.js";import"./Stack-9fb6382b.js";import"./extendSxProp-7fb4e09f.js";import"./Button-065a21d4.js";import"./ButtonBase-0719e676.js";import"./emotion-react.browser.esm-ca9fe929.js";import"./assertThisInitialized-081f9914.js";import"./Skeleton-2007774f.js";import"./Link-5cdd275a.js";import"./Typography-f217f607.js";const G={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!"}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  name: 'Topic',
-  args: {
-    isSubscribed: true,
-    isLoading: false,
-    icon: 'discussion',
-    name: 'Help me understand this project!'
-  }
-}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const I=["Demo"];export{o as Demo,I as __namedExportsOrder,G as default};
