fc21dc995111efcdf45523e81def04016cf1b975
jay-hodgson <jay.hodgson@sagebase.org>
2024-03-13 21:56:37 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@8946504b9f1a242eadaf62d601d9906a1edd4472 🚀

---
+++ assets/useGetEntityChallenge-aop62Zku.js
@@ -0,0 +1,1 @@
+import{h as o,l as s,m as y}from"./ApplicationSessionManager-CoKgEkVZ.js";import"./OrientationBanner-DFzKDcoJ.js";import"./getEndpoint-CjoHA800.js";import"./index-CBqU2yxZ.js";import"./jsx-runtime-CKrituN3.js";function m(e,t){const{accessToken:n,keyFactory:r}=o();return s({...t,queryKey:r.getEntityChallengeQueryKey(e),queryFn:()=>y.getEntityChallenge(e,n)})}export{m as u};
