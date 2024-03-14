858c7fca6a5bc47c6256f9b7e7e3a93381501393
jay-hodgson <jay.hodgson@sagebase.org>
2024-03-14 23:19:50 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@ef0543ec1c28e209001f4d249dd675ff033ab4ca 🚀

---
+++ assets/ComplexMenu.stories-Drwo1ppB.js
@@ -0,0 +1,68 @@
+import{j as t}from"./jsx-runtime-CKrituN3.js";import{I as i}from"./IconSvg-sfHaiTJJ.js";import{C as a}from"./ComplexMenu-DcrJg818.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./createSvgIcon-ClnqzT54.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-BPGUNDjC.js";import"./createTheme-2XtGYoE4.js";import"./index-DMoHWj5j.js";import"./styled-CPWjInoX.js";import"./Tooltip-XprEAl00.js";import"./index-CJ-wfEIf.js";import"./utils-SU9mu0D7.js";import"./inheritsLoose-B7h9gheN.js";import"./index-BtM5VmRH.js";import"./TransitionGroupContext-B1FFUpIO.js";import"./useForkRef-BSLA1zlB.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-DHJeVGyA.js";import"./mergeSlotProps-ih6DGlm4.js";import"./isHostComponent-DVu5iVWx.js";import"./useIsFocusVisible-Chjat8DO.js";import"./ErrorOutlined-BhDYotgP.js";import"./GetAppTwoTone-DGNvw6OX.js";import"./InfoOutlined-G1tbAqgK.js";import"./CheckCircleTwoTone-B5Iaww82.js";import"./DropdownMenu-C942pxi2.js";import"./Button-BOCcoe4v.js";import"./ButtonBase-xB_9igmm.js";import"./assertThisInitialized-B9jnkVVz.js";import"./Typography-BhttL55U.js";import"./Fade--kK2ZsE4.js";import"./Paper-DYLu205z.js";import"./MenuList-CiRE7Hfk.js";import"./getScrollbarSize-Bqq2hMjQ.js";import"./List-n4ZFYRWO.js";import"./MenuItem-Bcq3Naps.js";import"./dividerClasses-UDilKsZe.js";import"./ListItemText-DG4Gik5b.js";import"./Divider-DDkVRX30.js";import"./ClickAwayListener-CVXPLwF5.js";import"./Box-qzYP1vnN.js";import"./IconButton-DUzhqGqo.js";const Z={title:"UI/ComplexMenu",component:a},o=s=>()=>{console.log("Item clicked",s)},n={args:{iconButtons:[{icon:"edit",tooltipText:"This is an icon button",onClick:o("edit icon")},{icon:"label",tooltipText:"View annotations",onClick:o("label icon")},{icon:"createVersion",tooltipText:"Create new version",onClick:o("createVersion icon")}],dropdownMenus:[{dropdownButtonText:"You can have...",buttonProps:{endIcon:t.jsx(i,{icon:"upload",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]},{dropdownButtonText:"...multiple dropdown menus!",buttonProps:{endIcon:t.jsx(i,{icon:"download",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}]}};var e,r,l;n.parameters={...n.parameters,docs:{...(e=n.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    iconButtons: [{
+      icon: 'edit',
+      tooltipText: 'This is an icon button',
+      onClick: onClickHandler('edit icon')
+    }, {
+      icon: 'label',
+      tooltipText: 'View annotations',
+      onClick: onClickHandler('label icon')
+    }, {
+      icon: 'createVersion',
+      tooltipText: 'Create new version',
+      onClick: onClickHandler('createVersion icon')
+    }],
+    dropdownMenus: [{
+      dropdownButtonText: 'You can have...',
+      buttonProps: {
+        endIcon: <IconSvg icon="upload" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }, {
+      dropdownButtonText: '...multiple dropdown menus!',
+      buttonProps: {
+        endIcon: <IconSvg icon="download" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }]
+  }
+}`,...(l=(r=n.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const $=["Demo"];export{n as Demo,$ as __namedExportsOrder,Z as default};
