3aec57b35efcb1611deb24ea24ab601152c5585e
jay-hodgson <jay.hodgson@sagebase.org>
2024-04-17 17:45:23 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@d6a430cc009ae3ab010707068d48e38b5ebef02d 🚀

---
+++ assets/OrientationBanner.stories-C1bGRXNs.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-Du8NFWEI.js";import{O as o,g as s}from"./OrientationBanner-tb7WDFcR.js";import"./index-Dl6G-zuu.js";import"./FullWidthAlert-xeBFwHL5.js";import"./Alert-HJYNNEvY.js";import"./createTheme-CwlmBDro.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-Bpxk95mn.js";import"./index-DMoHWj5j.js";import"./styled-OgyJf9MH.js";import"./mergeSlotProps-DHTQkAbv.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./createSvgIcon-BYNyJbGL.js";import"./Paper-BWoP1bRz.js";import"./IconButton-CgTC7RgJ.js";import"./ButtonBase-5SeL_6IU.js";import"./TransitionGroupContext-DGN18uA0.js";import"./assertThisInitialized-B9jnkVVz.js";import"./inheritsLoose-B7h9gheN.js";import"./useIsFocusVisible-DHYsnH_Q.js";import"./Stack-CkbmjR2j.js";import"./Box-DRdN2jdb.js";import"./AlertTitle-DKiwjjrh.js";import"./Typography-B88_J_TK.js";import"./utils-C1pLSGi5.js";import"./index-D1_ZHIBm.js";import"./Grow-DYov3NPi.js";import"./ClickAwayListener-BnMSRy2k.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-CYVQ1vhC.js";import"./index-aEXBT5Ni.js";import"./Button-mb55Lwfk.js";const U={title:"UI/OrientationBanner",component:o,render:e=>(localStorage.removeItem(s(e.name)),n.jsx(o,{...e}))},t={args:{name:"Teams",title:"Getting Started With Your Teams",text:"Teams allow you to easily manage groups of users to control access to projects, communicate with colleagues, and participate in challenges.",primaryButtonConfig:{text:"Search Teams",href:"https://www.synapse.org/#!TeamSearch:"},secondaryButtonConfig:{text:"Learn More About Teams",href:"https://help.synapse.org/docs/Teams.1985446029.html"}}};var r,a,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: 'Teams',
+    title: 'Getting Started With Your Teams',
+    text: 'Teams allow you to easily manage groups of users to control access to projects, communicate with colleagues, and participate in challenges.',
+    primaryButtonConfig: {
+      text: 'Search Teams',
+      href: 'https://www.synapse.org/#!TeamSearch:'
+    },
+    secondaryButtonConfig: {
+      text: 'Learn More About Teams',
+      href: 'https://help.synapse.org/docs/Teams.1985446029.html'
+    }
+  }
+}`,...(m=(a=t.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const k=["Demo"];export{t as Demo,k as __namedExportsOrder,U as default};
