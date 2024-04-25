5de48df3d02b555d51340f66460cc07e6a2ffc11
Jay Hodgson <jay.hodgson@sagebase.org>
2024-04-25 14:32:34 -0700

    Update apps/portals/src/configurations/elportal/synapseConfigs/handleUpsetPlotClick.tsx
    Co-authored-by: hallieswan <26949006+hallieswan@users.noreply.github.com>

--- apps/portals/src/configurations/elportal/synapseConfigs/handleUpsetPlotClick.tsx
+++ apps/portals/src/configurations/elportal/synapseConfigs/handleUpsetPlotClick.tsx
@@ -3,19 +3,9 @@ import { cohortBuilderFilesSql } from '../resources'
 
 export const handleUpsetPlotClick = (selection: any) => {
   const { name } = selection
-  const query: Query = {
-    sql: cohortBuilderFilesSql,
-    selectedFacets: [
-      {
-        concreteType:
-          'org.sagebionetworks.repo.model.table.FacetColumnValuesRequest',
-        columnName: 'Assays',
-        facetValues: [name],
-      },
-    ],
-    // additionalFilters: [filter], //technically,
-  }
-  window.location.assign(
-    `/Explore/Data%20by%20Files?QueryWrapper0=${JSON.stringify(query)}`,
-  )
+  const url = generateEncodedPathAndQueryForSelectedFacetURL(
+    '/Explore/Data%20by%20Files',
+    cohortBuilderFilesSql,
+    [{facet: 'Assays', facetValue: name}]
+  window.location.assign(url)
 }
