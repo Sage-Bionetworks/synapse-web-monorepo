9b53598abbb4d63befba3b81ac7b83c69db8daff
jay-hodgson <jay.hodgson@sagebase.org>
2024-05-06 20:00:20 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@16cef8b98fc80bd64c58cab2eb6bbcf432dc4769 🚀

---
+++ assets/mockAnnotationColumns-emAFBLO9.js
@@ -0,0 +1,1 @@
+import{C as e}from"./index-Pxtlv3yl.js";const n={concreteType:"org.sagebionetworks.repo.model.table.ViewColumnModelResponse",results:[{id:"1235325",columnType:e.STRING,name:"columnFromAnnotations",maximumSize:10}],nextPageToken:null};export{n as M};
