4087e89a2f2f77207559c00e04162f5b3ae0c3b4
nickgros <nickgrosenbacher@gmail.com>
2024-06-13 18:29:50 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@889f4f215754c7e1e083b1c23929c2ba625dd251 🚀

--- assets/useGetEntityChallenge-BA2fGzwr.js
+++
@@ -1,1 +0,0 @@
-import{k as o,o as s,p as y}from"./SynapseClient-BR_yfqmX.js";import"./OrientationBanner-Dih8CVog.js";import"./getEndpoint-CjoHA800.js";import"./index-Dl6G-zuu.js";import"./jsx-runtime-Du8NFWEI.js";function m(e,t){const{accessToken:n,keyFactory:r}=o();return s({...t,queryKey:r.getEntityChallengeQueryKey(e),queryFn:()=>y.getEntityChallenge(e,n)})}export{m as u};
