1660e0b84bce223485075f5bed5e65c79f89c974
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-06-26 09:13:21 -0400

    Code review: add comments to individual portal HTML/Vite config to guide dev towards making changes across all portals

--- apps/portals/elportal/vite.config.ts
+++ apps/portals/elportal/vite.config.ts
@@ -4,9 +4,11 @@ import { resolve } from 'path'
 
 const projectRootDir = resolve(__dirname)
 
+// Unless absolutely necessary, all changes should go into the shared vite configuration, not the individual portal configuration
 export default mergeConfig(portalsViteConfig, {
   // TODO: PORTALS-3159
   // Until we create a build/exports strategy for synapse-portal-framework, we have to create an alias so Vite can resolve the module
+  // This must be configured for each file because it depends on the projectRootDir
   resolve: {
     alias: [
       {
