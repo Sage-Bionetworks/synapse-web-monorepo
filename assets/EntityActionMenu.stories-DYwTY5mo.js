eece4fc7a11b7cca8e23ce269c408a79bbceb16d
jay-hodgson <jay.hodgson@sagebase.org>
2024-07-19 17:26:00 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@34d314f4efbb6dd1cbdc4426d3107625b7e35d34 🚀

---
+++ assets/EntityActionMenu.stories-DYwTY5mo.js
@@ -0,0 +1,175 @@
+import{E}from"./EntityActionMenu-DEzNaf67.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./ComplexMenu-BQezg7SK.js";import"./DropdownMenu-5Dh28S4G.js";import"./IconSvg-BAp0YZOK.js";import"./createSvgIcon-gkOuU5-G.js";import"./createTheme-DFSTUSmt.js";import"./index-GEGPABih.js";import"./styled-D9wW3ABP.js";import"./Tooltip-Drr3vXNI.js";import"./index-hJhP8EJR.js";import"./utils-Bdp_oNP4.js";import"./TransitionGroupContext-B611AcNu.js";import"./useForkRef-CEBgoE3Z.js";import"./index-B6qzg4VC.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-lFU1cKdF.js";import"./mergeSlotProps-deWJORir.js";import"./isHostComponent-DVu5iVWx.js";import"./useIsFocusVisible-Cwvw9TRl.js";import"./ErrorOutlined-CnICwQW0.js";import"./GetAppTwoTone-CEgy4oXX.js";import"./InfoOutlined-DJDm_gVQ.js";import"./CheckCircleTwoTone-U_UPMvHV.js";import"./Button-CLkrjdQe.js";import"./ButtonBase-BgDXtZj-.js";import"./Typography-DgBbIcOX.js";import"./Fade-Bhxuafx5.js";import"./Paper-6hBhi2Vr.js";import"./MenuList-CWcqbzO9.js";import"./getScrollbarSize-Bqq2hMjQ.js";import"./List-JFp8Imct.js";import"./MenuItem-Dw5ZhG7D.js";import"./dividerClasses-B5r-ooM0.js";import"./ListItemIcon-Cm7hmj2X.js";import"./ListItemText-F60cWPc1.js";import"./Divider-DJ8WkHe7.js";import"./ClickAwayListener-5odGQiyi.js";import"./IconSvgButton-C2ZLPwfr.js";import"./IconButton-CLoAcDX9.js";import"./Box-DRYT9rh3.js";const $={title:"Synapse/EntityPage/ActionMenu",component:E},n=T=>()=>{console.log("Action clicked: "+T)},i={args:{actionConfiguration:{ADD_TO_DOWNLOAD_CART:{text:"Add to Download Cart",onClick:n("ADD_TO_DOWNLOAD_CART"),disabled:!0,tooltipText:"You must have some permission to download this thing.",visible:!0},VIEW_SHARING_SETTINGS:{text:"View sharing settings",onClick:n("VIEW_SHARING_SETTINGS"),disabled:!1,visible:!0},MOVE_ENTITY:{text:"Move File",onClick:n("MOVE_ENTITY"),disabled:!1,visible:!0},DELETE_ENTITY:{text:"Delete File",onClick:n("DELETE_ENTITY"),disabled:!1,visible:!0},UPLOAD_FILE:{text:"Upload a file",onClick:n("UPLOAD_FILE"),disabled:!1,visible:!0},EDIT_FILE_METADATA:{text:"Edit some metadata",onClick:n("EDIT_FILE_METADATA"),disabled:!1,visible:!0},REPORT_VIOLATION:{text:"Report Violation",onClick:n("REPORT_VIOLATION"),disabled:!1,visible:!0}},menuConfiguration:{DOWNLOAD:{visible:!0},PRIMARY:{visible:!0}},layout:{buttonActions:[{action:"UPLOAD_FILE",icon:"upload"},{action:"EDIT_FILE_METADATA",icon:"edit"}],downloadMenuActions:[[{action:"ADD_TO_DOWNLOAD_CART"}]],primaryMenuActions:[[{action:"VIEW_SHARING_SETTINGS"}],[{action:"MOVE_ENTITY"},{action:"DELETE_ENTITY",textSx:{color:"error.main"}}],[{action:"REPORT_VIOLATION",icon:"flag",iconSx:{color:"error.main"}}]],primaryMenuText:"Tools Menu Text is Configurable",primaryMenuEndIcon:"verticalEllipsis"}}},e={args:{actionConfiguration:{ADD_TO_DOWNLOAD_CART:{text:"Add to Download Cart",onClick:n("ADD_TO_DOWNLOAD_CART"),disabled:!0,tooltipText:"You must have some permission to download this thing.",visible:!0},VIEW_SHARING_SETTINGS:{text:"View sharing settings",onClick:n("VIEW_SHARING_SETTINGS"),disabled:!1,visible:!0},MOVE_ENTITY:{text:"Move File",onClick:n("MOVE_ENTITY"),disabled:!1,visible:!0},DELETE_ENTITY:{text:"Delete File",onClick:n("DELETE_ENTITY"),disabled:!1,visible:!0},UPLOAD_FILE:{text:"Upload a file",onClick:n("UPLOAD_FILE"),disabled:!1,visible:!0},EDIT_FILE_METADATA:{text:"Edit some metadata",onClick:n("EDIT_FILE_METADATA"),disabled:!1,visible:!0},REPORT_VIOLATION:{text:"Report Violation",onClick:n("REPORT_VIOLATION"),disabled:!1,visible:!0}},menuConfiguration:{DOWNLOAD:{visible:!0,disabled:!1,tooltipText:"All download actions are blocked for some reason."},PRIMARY:{visible:!0}},layout:{buttonActions:[{action:"UPLOAD_FILE",icon:"upload"},{action:"EDIT_FILE_METADATA",icon:"edit"}],downloadMenuActions:[[{action:"ADD_TO_DOWNLOAD_CART"}]],primaryMenuActions:[[{action:"VIEW_SHARING_SETTINGS"}],[{action:"MOVE_ENTITY"},{action:"DELETE_ENTITY",textSx:{color:"error.main"}}],[{action:"REPORT_VIOLATION",icon:"flag",iconSx:{color:"error.main"}}]],primaryMenuText:"Tools Menu Text is Configurable",primaryMenuEndIcon:"verticalEllipsis"}}};var o,t,l;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    actionConfiguration: {
+      ['ADD_TO_DOWNLOAD_CART']: {
+        text: 'Add to Download Cart',
+        onClick: onClickHandler('ADD_TO_DOWNLOAD_CART'),
+        disabled: true,
+        tooltipText: 'You must have some permission to download this thing.',
+        visible: true
+      },
+      ['VIEW_SHARING_SETTINGS']: {
+        text: 'View sharing settings',
+        onClick: onClickHandler('VIEW_SHARING_SETTINGS'),
+        disabled: false,
+        visible: true
+      },
+      ['MOVE_ENTITY']: {
+        text: 'Move File',
+        onClick: onClickHandler('MOVE_ENTITY'),
+        disabled: false,
+        visible: true
+      },
+      ['DELETE_ENTITY']: {
+        text: 'Delete File',
+        onClick: onClickHandler('DELETE_ENTITY'),
+        disabled: false,
+        visible: true
+      },
+      ['UPLOAD_FILE']: {
+        text: 'Upload a file',
+        onClick: onClickHandler('UPLOAD_FILE'),
+        disabled: false,
+        visible: true
+      },
+      ['EDIT_FILE_METADATA']: {
+        text: 'Edit some metadata',
+        onClick: onClickHandler('EDIT_FILE_METADATA'),
+        disabled: false,
+        visible: true
+      },
+      ['REPORT_VIOLATION']: {
+        text: 'Report Violation',
+        onClick: onClickHandler('REPORT_VIOLATION'),
+        disabled: false,
+        visible: true
+      }
+    },
+    menuConfiguration: {
+      DOWNLOAD: {
+        visible: true
+      },
+      PRIMARY: {
+        visible: true
+      }
+    },
+    layout: {
+      buttonActions: [{
+        action: 'UPLOAD_FILE',
+        icon: 'upload'
+      }, {
+        action: 'EDIT_FILE_METADATA',
+        icon: 'edit'
+      }],
+      downloadMenuActions: [[{
+        action: 'ADD_TO_DOWNLOAD_CART'
+      }]],
+      primaryMenuActions: [[{
+        action: 'VIEW_SHARING_SETTINGS'
+      }], [{
+        action: 'MOVE_ENTITY'
+      }, {
+        action: 'DELETE_ENTITY',
+        textSx: {
+          color: 'error.main'
+        }
+      }], [{
+        action: 'REPORT_VIOLATION',
+        icon: 'flag',
+        iconSx: {
+          color: 'error.main'
+        }
+      }]],
+      primaryMenuText: 'Tools Menu Text is Configurable',
+      primaryMenuEndIcon: 'verticalEllipsis'
+    }
+  }
+}`,...(l=(t=i.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};var a,r,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    actionConfiguration: {
+      ['ADD_TO_DOWNLOAD_CART']: {
+        text: 'Add to Download Cart',
+        onClick: onClickHandler('ADD_TO_DOWNLOAD_CART'),
+        disabled: true,
+        tooltipText: 'You must have some permission to download this thing.',
+        visible: true
+      },
+      ['VIEW_SHARING_SETTINGS']: {
+        text: 'View sharing settings',
+        onClick: onClickHandler('VIEW_SHARING_SETTINGS'),
+        disabled: false,
+        visible: true
+      },
+      ['MOVE_ENTITY']: {
+        text: 'Move File',
+        onClick: onClickHandler('MOVE_ENTITY'),
+        disabled: false,
+        visible: true
+      },
+      ['DELETE_ENTITY']: {
+        text: 'Delete File',
+        onClick: onClickHandler('DELETE_ENTITY'),
+        disabled: false,
+        visible: true
+      },
+      ['UPLOAD_FILE']: {
+        text: 'Upload a file',
+        onClick: onClickHandler('UPLOAD_FILE'),
+        disabled: false,
+        visible: true
+      },
+      ['EDIT_FILE_METADATA']: {
+        text: 'Edit some metadata',
+        onClick: onClickHandler('EDIT_FILE_METADATA'),
+        disabled: false,
+        visible: true
+      },
+      ['REPORT_VIOLATION']: {
+        text: 'Report Violation',
+        onClick: onClickHandler('REPORT_VIOLATION'),
+        disabled: false,
+        visible: true
+      }
+    },
+    menuConfiguration: {
+      DOWNLOAD: {
+        visible: true,
+        disabled: false,
+        tooltipText: 'All download actions are blocked for some reason.'
+      },
+      PRIMARY: {
+        visible: true
+      }
+    },
+    layout: {
+      buttonActions: [{
+        action: 'UPLOAD_FILE',
+        icon: 'upload'
+      }, {
+        action: 'EDIT_FILE_METADATA',
+        icon: 'edit'
+      }],
+      downloadMenuActions: [[{
+        action: 'ADD_TO_DOWNLOAD_CART'
+      }]],
+      primaryMenuActions: [[{
+        action: 'VIEW_SHARING_SETTINGS'
+      }], [{
+        action: 'MOVE_ENTITY'
+      }, {
+        action: 'DELETE_ENTITY',
+        textSx: {
+          color: 'error.main'
+        }
+      }], [{
+        action: 'REPORT_VIOLATION',
+        icon: 'flag',
+        iconSx: {
+          color: 'error.main'
+        }
+      }]],
+      primaryMenuText: 'Tools Menu Text is Configurable',
+      primaryMenuEndIcon: 'verticalEllipsis'
+    }
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const nn=["Demo","DisabledDownloadMenu"];export{i as Demo,e as DisabledDownloadMenu,nn as __namedExportsOrder,$ as default};
