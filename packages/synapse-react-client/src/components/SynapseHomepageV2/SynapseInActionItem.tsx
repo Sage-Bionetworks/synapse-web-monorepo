a1725f173ade9e2299d30d2363f39478231650d4
jay-hodgson <jay.hodgson@sagebase.org>
2024-07-20 17:29:12 -0700

    rework layout to have all desktop images loaded (for each synapse in action item)

--- packages/synapse-react-client/src/components/SynapseHomepageV2/SynapseInActionItem.tsx
+++ packages/synapse-react-client/src/components/SynapseHomepageV2/SynapseInActionItem.tsx
@@ -7,10 +7,9 @@ import {
   useMediaQuery,
   SxProps,
 } from '@mui/material'
-import React, { useEffect } from 'react'
+import React from 'react'
 import ImageFromSynapseTable from '../ImageFromSynapseTable'
 import { EastTwoTone } from '@mui/icons-material'
-import { useInView } from 'react-intersection-observer'
 import { darkTextColor, homepageBodyText } from './SynapseHomepageV2'
 
 export type SynapseInActionItemProps = {
@@ -23,7 +22,8 @@ export type SynapseInActionItemProps = {
   link: string
   primaryColor: string
   secondaryColor: string
-  onInView: () => void
+  imageFileHandleId: string
+  mobileImageFileHandleId: string
 }
 
 const mobileViewSxProps: SxProps = {
@@ -43,74 +43,110 @@ export const SynapseInActionItem: React.FunctionComponent<
   tags,
   logoFileHandleId,
   link,
-  onInView,
+  imageFileHandleId,
+  mobileImageFileHandleId,
 }) => {
-  const { ref, inView } = useInView({ threshold: 0.7 }) //do not report this is in view until the description is almost entirely shown (70%)
-  useEffect(() => {
-    if (inView) {
-      onInView()
-    }
-  }, [inView])
   const theme = useTheme()
   const isMobileView = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <Box
       sx={{
         p: '15px',
+        display: 'grid',
+        gridTemplateColumns: {
+          xs: 'auto 0px',
+          md: '450px auto',
+        },
       }}
     >
-      <Box sx={isMobileView ? mobileViewSxProps : undefined}>
-        <Box>
-          {tags &&
-            tags.map(tag => {
-              return (
-                <Chip
-                  key={tag}
-                  sx={{
-                    mr: '5px',
-                    color: 'secondary.600',
-                    backgroundColor: 'secondary.100',
-                  }}
-                  label={tag}
-                />
-              )
-            })}
+      <Box
+        sx={{
+          ml: {
+            xs: '5px',
+            md: '20px',
+          },
+        }}
+      >
+        {/* mobile image */}
+        <Box
+          sx={{
+            display: { xs: 'flex', md: 'none' },
+            justifyContent: 'center',
+            width: '100%',
+            mb: '10px',
+            img: { width: '320px' },
+          }}
+        >
+          <ImageFromSynapseTable
+            tableId={tableId}
+            fileHandleId={mobileImageFileHandleId}
+          />
         </Box>
-        <Typography
-          variant="body1"
-          sx={{
-            fontWeight: 400,
-            fontSize: '36px',
-            lineHeight: '46px',
-            mt: '32px',
-            color: darkTextColor,
-          }}
-        >
-          {title}
-        </Typography>
-        <Typography
-          variant="body1"
-          sx={{
-            ...homepageBodyText,
-            mt: '32px',
-            mb: '32px',
-          }}
-          ref={ref}
-        >
-          {description}
-        </Typography>
+        <Box sx={isMobileView ? mobileViewSxProps : undefined}>
+          <Box>
+            {tags &&
+              tags.map(tag => {
+                return (
+                  <Chip
+                    key={tag}
+                    sx={{
+                      mr: '5px',
+                      color: 'secondary.600',
+                      backgroundColor: 'secondary.100',
+                    }}
+                    label={tag}
+                  />
+                )
+              })}
+          </Box>
+          <Typography
+            variant="body1"
+            sx={{
+              fontWeight: 400,
+              fontSize: '36px',
+              lineHeight: '46px',
+              mt: '32px',
+              color: darkTextColor,
+            }}
+          >
+            {title}
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{
+              ...homepageBodyText,
+              mt: '32px',
+              mb: '32px',
+            }}
+          >
+            {description}
+          </Typography>
+          <ImageFromSynapseTable
+            tableId={tableId}
+            fileHandleId={logoFileHandleId}
+            alt={`${friendlyName} logo`}
+            style={{ height: '40px' }}
+          />
+          <Box sx={{ mt: '32px' }}>
+            <Link href={link} target="_blank">
+              View {friendlyName.endsWith('Portal') ? 'the' : ''} {friendlyName}{' '}
+              <EastTwoTone sx={{ mb: '-8px', ml: '6px' }} />
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+      {/* desktop image */}
+      <Box
+        sx={{
+          display: { xs: 'none', md: 'block' },
+          justifySelf: 'end',
+          mt: '-100px',
+        }}
+      >
         <ImageFromSynapseTable
           tableId={tableId}
-          fileHandleId={logoFileHandleId}
-          alt={`${friendlyName} logo`}
-          style={{ height: '40px' }}
+          fileHandleId={imageFileHandleId}
         />
-        <Box sx={{ mt: '32px' }}>
-          <Link href={link} target="_blank">
-            View {friendlyName.endsWith('Portal') ? 'the' : ''} {friendlyName}{' '}
-            <EastTwoTone sx={{ mb: '-8px', ml: '6px' }} />
-          </Link>
-        </Box>
       </Box>
     </Box>
   )
