408b105f26a88dca94fd7e0adfbb963470a793d5
jay-hodgson <jay.hodgson@sagebase.org>
2024-07-31 21:28:52 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@8bbdf9f5b880557add04fd6b55af0e2c97e4ef19 🚀

---
+++ assets/OrientationBanner.stories-CYlhHdKU.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-Du8NFWEI.js";import{O as o,g as s}from"./OrientationBanner-DV6G-OQI.js";import"./index-Dl6G-zuu.js";import"./FullWidthAlert-C1X-IDYx.js";import"./Alert-C2wXqEQ3.js";import"./createTheme-Dtzk88yv.js";import"./index-GEGPABih.js";import"./styled-BEXTB4Ho.js";import"./mergeSlotProps-Dv5GaMzt.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./createSvgIcon-eMxRw33w.js";import"./Paper-B2c6t1gu.js";import"./IconButton-cf2NThiY.js";import"./ButtonBase-CI9J1-uo.js";import"./TransitionGroupContext-B611AcNu.js";import"./useIsFocusVisible-Cwvw9TRl.js";import"./Stack-DkXTQwBs.js";import"./Box-BlHPf8tq.js";import"./AlertTitle-CmC2H_AO.js";import"./Typography-qpntpuFp.js";import"./utils-C59yjAo_.js";import"./index-B6qzg4VC.js";import"./Grow-CspkvTld.js";import"./ClickAwayListener-5odGQiyi.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-BZeYxe44.js";import"./index-Cvyar9fI.js";import"./Button-jcEr4EiK.js";const v={title:"UI/OrientationBanner",component:o,render:e=>(localStorage.removeItem(s(e.name)),n.jsx(o,{...e}))},t={args:{name:"Teams",title:"Getting Started With Your Teams",text:"Teams allow you to easily manage groups of users to control access to projects, communicate with colleagues, and participate in challenges.",primaryButtonConfig:{text:"Search Teams",href:"https://www.synapse.org/TeamSearch:"},secondaryButtonConfig:{text:"Learn More About Teams",href:"https://help.synapse.org/docs/Teams.1985446029.html"}}};var r,a,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    name: 'Teams',
+    title: 'Getting Started With Your Teams',
+    text: 'Teams allow you to easily manage groups of users to control access to projects, communicate with colleagues, and participate in challenges.',
+    primaryButtonConfig: {
+      text: 'Search Teams',
+      href: 'https://www.synapse.org/TeamSearch:'
+    },
+    secondaryButtonConfig: {
+      text: 'Learn More About Teams',
+      href: 'https://help.synapse.org/docs/Teams.1985446029.html'
+    }
+  }
+}`,...(m=(a=t.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const K=["Demo"];export{t as Demo,K as __namedExportsOrder,v as default};
---
+++ assets/Topic.stories-LwwVB6Y5.js
@@ -0,0 +1,10 @@
+import{T as m}from"./Topic-CNTJ_p5q.js";import{f as p}from"./index-CVAUwOAi.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./IconSvg-CRqkcbEG.js";import"./createSvgIcon-eMxRw33w.js";import"./createTheme-Dtzk88yv.js";import"./index-GEGPABih.js";import"./styled-BEXTB4Ho.js";import"./Tooltip-BZeYxe44.js";import"./index-Cvyar9fI.js";import"./utils-C59yjAo_.js";import"./TransitionGroupContext-B611AcNu.js";import"./useForkRef-CEBgoE3Z.js";import"./index-B6qzg4VC.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-CspkvTld.js";import"./mergeSlotProps-Dv5GaMzt.js";import"./isHostComponent-DVu5iVWx.js";import"./useIsFocusVisible-Cwvw9TRl.js";import"./ErrorOutlined-Bqgz-1Qz.js";import"./GetAppTwoTone-EWgzB7jO.js";import"./InfoOutlined-erQdz4z_.js";import"./CheckCircleTwoTone-BB9W7Hyp.js";import"./ConditionalWrapper-Be926PbJ.js";import"./Stack-DkXTQwBs.js";import"./Button-jcEr4EiK.js";import"./ButtonBase-CI9J1-uo.js";import"./Skeleton-BnDUWcOg.js";import"./colorManipulator-D5JWZs3m.js";import"./Link-DhsldE67.js";import"./Typography-qpntpuFp.js";import"./index-D0JCpg9J.js";const I={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!",onToggleSubscribe:p()}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Topic',
+  args: {
+    isSubscribed: true,
+    isLoading: false,
+    icon: 'discussion',
+    name: 'Help me understand this project!',
+    onToggleSubscribe: fn()
+  }
+}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const J=["Demo"];export{o as Demo,J as __namedExportsOrder,I as default};
