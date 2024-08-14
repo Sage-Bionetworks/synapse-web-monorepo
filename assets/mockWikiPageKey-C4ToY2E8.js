c86a38d6f7d141c9025bf8f991af942385754fee
nickgros <nickgrosenbacher@gmail.com>
2024-08-14 20:42:40 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@23a873f313b0b861edbc70619ed7b1bdf2126a0f 🚀

---
+++ assets/mockWikiPageKey-C4ToY2E8.js
@@ -0,0 +1,1 @@
+import{O as e}from"./index-CcBaOCma.js";import{M as o}from"./mockFileEntity-BUy6HkxR.js";import{d as t,e as a}from"./mockWiki-CrgHzIE3.js";import{g as m}from"./mockAccessRequirements-BkjIFk_P.js";const i={ownerObjectType:e.ENTITY,ownerObjectId:o,wikiPageId:t.id},s={...i,wikiPageId:a.id},y=[...m,i,s];export{s as a,y as b,i as m};
