ef33eb3d9feb19559e4e4d703ee1d0ae98e31599
jay-hodgson <jay.hodgson@sagebase.org>
2024-09-03 13:09:13 -0700

    NFINT-816: general dynamic form based on json schema, ui json schema, and endpoint to send form submission data

---
+++ packages/synapse-react-client/src/components/DynamicForm/DynamicForm.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+import DynamicForm, { DynamicFormProps } from './DynamicForm'
+
+const meta = {
+  title: 'UI/DynamicForm',
+  component: DynamicForm,
+} satisfies Meta<DynamicFormProps>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const NFDataSubmissionForm: Story = {
+  args: {
+    schemaUrl: '',
+    uiSchemaUrl: '',
+    postUrl:
+      'https://invalidpost-address.fdsjklafjsdkljdklsaf.org/fdsklajfdklsa',
+    mutateFormDataBeforePost: formData => {
+      return {
+        event_type: 'submission',
+        client_payload: {
+          formData: formData,
+        },
+      }
+    },
+  },
+}
---
+++ packages/synapse-react-client/src/components/DynamicForm/DynamicForm.tsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react'
+import Form from '@rjsf/mui'
+import validator from '@rjsf/validator-ajv8'
+import { displayToast } from '../ToastMessage'
+import { Box, Typography } from '@mui/material'
+import { SkeletonParagraph } from '../Skeleton'
+
+export type DynamicFormProps = {
+  schemaUrl: string
+  uiSchemaUrl: string
+  postUrl: string
+  mutateFormDataBeforePost?: (formData: any) => any // allow caller to embed formdata into custom request body object
+}
+
+function DynamicForm(props: DynamicFormProps) {
+  const { schemaUrl, uiSchemaUrl, postUrl, mutateFormDataBeforePost } = props
+  const [schema, setSchema] = useState(null)
+  const [uiSchema, setUiSchema] = useState(null)
+  const [formData, setFormData] = useState({})
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  useEffect(() => {
+    // Fetch the form schema
+    const fetchSchema = async () => {
+      try {
+        const schemaResponse = await fetch(schemaUrl)
+        const schemaData = await schemaResponse.json()
+        setSchema(schemaData)
+      } catch (error) {
+        displayToast(`Error fetching Schema: ${error}`, 'danger')
+      }
+    }
+
+    // Fetch the UI schema
+    const fetchUiSchema = async () => {
+      try {
+        const uiSchemaResponse = await fetch(uiSchemaUrl)
+        const uiSchemaData = await uiSchemaResponse.json()
+        setUiSchema(uiSchemaData)
+      } catch (error) {
+        displayToast(`Error fetching UI Schema: ${error}`, 'danger')
+      }
+    }
+
+    fetchSchema()
+    fetchUiSchema()
+  }, [schemaUrl, uiSchemaUrl])
+
+  const handleSubmit = async (formData: any) => {
+    setIsSubmitting(true)
+    const formDataToSubmit = mutateFormDataBeforePost
+      ? mutateFormDataBeforePost(formData)
+      : formData
+    try {
+      const response = await fetch(postUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formDataToSubmit),
+      })
+
+      if (response.ok) {
+        displayToast('Form submitted successfully!', 'success')
+      } else {
+        displayToast(`Form submission failed: ${response.statusText}`, 'danger')
+      }
+    } catch (error) {
+      displayToast(`Form submission failed: ${error}`, 'danger')
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  if (!schema || !uiSchema) {
+    return (
+      <Box>
+        <Typography variant="body1">Loading form...</Typography>
+        <SkeletonParagraph numRows={6} />
+      </Box>
+    )
+  }
+
+  return (
+    <Form
+      schema={schema}
+      uiSchema={uiSchema}
+      formData={formData}
+      validator={validator}
+      onChange={({ formData }) => setFormData(formData)}
+      onSubmit={({ formData }) => handleSubmit(formData)}
+      disabled={isSubmitting}
+    />
+  )
+}
+
+export default DynamicForm
