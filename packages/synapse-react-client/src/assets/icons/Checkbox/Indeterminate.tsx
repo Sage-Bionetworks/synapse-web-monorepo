e57f29bc37577553341f8235abae1ae08e8af54d
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-09-04 15:07:28 -0400

    Split SvgIcon and svg

--- packages/synapse-react-client/src/assets/icons/Checkbox/Indeterminate.tsx
+++ packages/synapse-react-client/src/assets/icons/Checkbox/Indeterminate.tsx
@@ -1,28 +1,8 @@
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon'
 import React from 'react'
+import { ReactComponent as IndeterminateIcon } from './Indeterminate.svg'
 
-const Indeterminate = (props: SvgIconProps) => {
-  return (
-    <SvgIcon {...props} inheritViewBox>
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          x="0.5"
-          y="0.5"
-          width="11"
-          height="11"
-          fill="currentColor"
-          stroke="currentColor"
-        />
-        <path d="M 9.5 6.5 H 2.5 V 5.5 H 9.5 V 6.5 Z" fill="white" />
-      </svg>
-    </SvgIcon>
-  )
+function Indeterminate(props: SvgIconProps) {
+  return <SvgIcon {...props} component={IndeterminateIcon} inheritViewBox />
 }
-
 export default Indeterminate
--- packages/synapse-react-client/src/assets/icons/Checkbox/Unchecked.tsx
+++ packages/synapse-react-client/src/assets/icons/Checkbox/Unchecked.tsx
@@ -1,27 +1,8 @@
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon'
 import React from 'react'
+import { ReactComponent as UncheckedIcon } from './Unchecked.svg'
 
-const Checked = (props: SvgIconProps) => {
-  return (
-    <SvgIcon {...props} inheritViewBox>
-      <svg
-        width="12"
-        height="12"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          x="0.5"
-          y="0.5"
-          width="11"
-          height="11"
-          fill="white"
-          stroke="currentColor"
-        />
-      </svg>
-    </SvgIcon>
-  )
+function Unchecked(props: SvgIconProps) {
+  return <SvgIcon {...props} component={UncheckedIcon} inheritViewBox />
 }
-
-export default Checked
+export default Unchecked
