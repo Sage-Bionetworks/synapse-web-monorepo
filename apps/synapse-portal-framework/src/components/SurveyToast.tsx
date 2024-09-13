388da4ecedc9dd7612f573add88f6042331d8b35
jay-hodgson <jay.hodgson@sagebase.org>
2024-09-12 21:03:58 -0700

    PORTALS-3243, PORTALS-3244

---
+++ apps/synapse-portal-framework/src/components/SurveyToast.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { SynapseComponents, SynapseHookUtils } from 'synapse-react-client'
+
+export type SurveyToastProps = {
+  localStorageKey: string
+  description: string
+  surveyURL: string
+  title?: string
+  surveyButtonText?: string
+}
+const SurveyToast = (props: SurveyToastProps) => {
+  const {
+    localStorageKey,
+    title = '',
+    description,
+    surveyURL,
+    surveyButtonText = 'Take The Survey',
+  } = props
+  const [cookiePreferences] = SynapseHookUtils.useCookiePreferences()
+  const [showBanner, setShowBanner] = React.useState(
+    localStorage.getItem(localStorageKey) === null,
+  )
+  return !showBanner ? (
+    <></>
+  ) : (
+    <SynapseComponents.FullWidthAlert
+      isGlobal={true}
+      onClose={() => {
+        if (cookiePreferences.functionalAllowed) {
+          localStorage.setItem(localStorageKey, 'true')
+        }
+        setShowBanner(false)
+      }}
+      variant={'info'}
+      show={true}
+      description={description}
+      title={title}
+      primaryButtonConfig={{
+        text: surveyButtonText,
+        href: surveyURL,
+      }}
+    />
+  )
+}
+
+export default SurveyToast
