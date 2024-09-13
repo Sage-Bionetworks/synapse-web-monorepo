f439dd8d3e785817638798e64088c3cc27b8ade0
jay-hodgson <jay.hodgson@sagebase.org>
2024-09-13 14:48:37 -0700

    update e2e tests for genie and adkp

--- apps/portals-e2e/src/configs/bannersConfig.ts
+++ apps/portals-e2e/src/configs/bannersConfig.ts
@@ -7,7 +7,7 @@ type BannerConfig = Record<Portal, BannerType[]>
 // lists the full width alerts displayed on each Portal,
 // ...in the order in which the banners should be dismissed
 const bannerConfig: BannerConfig = {
-  adknowledgeportal: ['cookies'],
+  adknowledgeportal: ['cookies', 'survey'],
   arkportal: ['cookies'],
   bsmn: ['cookies'],
   cancercomplexity: ['cookies'],
@@ -17,7 +17,7 @@ const bannerConfig: BannerConfig = {
   nf: ['cookies', 'survey'],
   psychencode: ['cookies'],
   stopadportal: ['cookies'],
-  genie: ['cookies'],
+  genie: ['cookies', 'survey'],
 }
 
 export default bannerConfig
