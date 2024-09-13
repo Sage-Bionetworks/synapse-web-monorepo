9a4581a8254f66cb706611131d6e83121a2595bb
jay-hodgson <jay.hodgson@sagebase.org>
2024-09-13 12:52:43 -0700

    attempt to fix typecheck

--- packages/synapse-react-client/src/components/SynapseForm/SynapseFormCheckboxWidget.tsx
+++ packages/synapse-react-client/src/components/SynapseForm/SynapseFormCheckboxWidget.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { Widgets } from '@rjsf/mui'
-import {
-  FormContextType,
-  RJSFSchema,
-  StrictRJSFSchema,
-  WidgetProps,
-} from '@rjsf/utils'
+import { RJSFSchema, WidgetProps } from '@rjsf/utils'
 
 // See PORTALS-3246
 export default function SynapseFormCheckboxWidget<
   T = any,
-  S extends StrictRJSFSchema = RJSFSchema,
-  F extends FormContextType = any,
->(props: WidgetProps<T, S, F>) {
+  S = RJSFSchema,
+  F = any,
+>(props: WidgetProps) {
   return <Widgets.CheckboxWidget {...props} hideLabel />
 }
