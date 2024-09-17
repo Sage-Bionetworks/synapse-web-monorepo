a0909a0caf92bcc2674ef6c09bc137400d1d9158
Jay Hodgson <jay.hodgson@sagebase.org>
2024-09-17 09:19:27 -0700

    Update packages/synapse-react-client/src/components/error/ErrorPage.tsx
    Co-authored-by: Nick Grosenbacher <nickgrosenbacher@gmail.com>

--- packages/synapse-react-client/src/components/error/ErrorPage.tsx
+++ packages/synapse-react-client/src/components/error/ErrorPage.tsx
@@ -148,7 +148,6 @@ const ErrorPage: React.FunctionComponent<ErrorPageProps> = props => {
     () => setSendMessageToAdminDialogOpen(false),
     [],
   )
-  console.log('Entity ID', entityId)
   return (
     <>
       <Box
