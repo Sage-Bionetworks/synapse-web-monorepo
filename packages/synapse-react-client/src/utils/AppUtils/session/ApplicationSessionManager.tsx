2cc47f3af7f808eb2395de315d065cb5845e9e28
jay-hodgson <jay.hodgson@sagebase.org>
2024-09-17 13:27:57 -0700

    PORTALS-3249

--- packages/synapse-react-client/src/utils/AppUtils/session/ApplicationSessionManager.tsx
+++ packages/synapse-react-client/src/utils/AppUtils/session/ApplicationSessionManager.tsx
@@ -148,6 +148,11 @@ export function ApplicationSessionManager(
     } else {
       refreshSession()
     }
+    // PORTALS-Set up an interval to call refreshSession every 60 seconds (60000 milliseconds)
+    const intervalId = setInterval(refreshSession, 60000)
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(intervalId)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
