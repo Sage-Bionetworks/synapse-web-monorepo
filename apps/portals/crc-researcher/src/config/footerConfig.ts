714ab9c9489d98a27a834b0cff05a7abe4afc643
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-10-02 13:27:08 -0400

    delete crc-researcher portal

--- apps/portals/crc-researcher/src/config/footerConfig.ts
+++
@@ -1,7 +0,0 @@
-import { FooterConfig } from '@sage-bionetworks/synapse-portal-framework/types/portal-config'
-
-const footer: FooterConfig = {
-  contactUs: 'mailto:synapseinfo@sagebionetworks.org',
-}
-
-export default footer
