ed75238334cf38da80415158e22800d7465b37ad
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-10-04 14:23:58 -0400

    Put webhooks under feature flag. Add footer to accounts site to allow toggling experimental mode

---
+++ apps/SageAccountWeb/src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import { ExperimentalMode } from 'synapse-react-client'
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <Box
+        sx={{
+          backgroundColor: '#76919e',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          p: { xs: '20px 40px', md: '40px 80px' },
+          color: 'white',
+        }}
+      >
+        <Typography variant="body1">Sage Bionetworks</Typography>
+        <ExperimentalMode />
+      </Box>
+    </footer>
+  )
+}
+
+export default React.memo(Footer)
