0b814597c32b4d968576c6ad23804ca60ef8416a
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-10-10 10:42:03 -0700

    synapse-oauth-signin migrated

--- apps/synapse-oauth-signin/src/App.tsx
+++ apps/synapse-oauth-signin/src/App.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
+import { createTheme, StyledEngineProvider } from '@mui/material/styles'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 import {
-  SynapseClient,
   defaultQueryClientConfig,
-  SynapseTheme,
+  SynapseClient,
   SynapseErrorBoundary,
+  SynapseTheme,
+  useSynapseContext,
 } from 'synapse-react-client'
 import './App.scss'
 import AppInitializer from './AppInitializer'
-import { OAuth2Form } from './OAuth2Form'
 import { getURLParam } from './URLUtils'
 import Versions from './Versions'
 import generalTheme from './style/theme'
-import { createTheme, StyledEngineProvider } from '@mui/material/styles'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from '@mui/material'
 
 const defaultQueryClient = new QueryClient(defaultQueryClientConfig)
 const theme = createTheme(SynapseTheme.mergeTheme(generalTheme))
@@ -24,58 +24,42 @@ type AppProps = {
   queryClient?: QueryClient
 }
 
+export function LogoutComponent() {
+  const { accessToken } = useSynapseContext()
+  useEffect(() => {
+    SynapseClient.signOut()
+  }, [])
+  return <p style={{ margin: 10 }}>{accessToken ? 'Logging' : 'Logged'} out</p>
+}
+
+export function LoginComponent() {
+  const { accessToken } = useSynapseContext()
+  useEffect(() => {
+    const code = getURLParam('code')
+    // look for the code from the params
+    SynapseClient.setAccessTokenCookie(code)
+  }, [])
+
+  return <p style={{ margin: 10 }}>{accessToken ? 'Logged' : 'Logging'} in</p>
+}
+
 function App(props: AppProps) {
   const { queryClient = defaultQueryClient } = props
-  const [isLoggedOut, setIsLoggedOut] = useState(false)
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <StyledEngineProvider injectFirst>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider theme={theme}>
-              <AppInitializer>
-                <SynapseErrorBoundary>
-                  <Switch>
-                    <Route exact path="/" render={() => <OAuth2Form />} />
-                    <Route
-                      exact
-                      path="/logout"
-                      render={() => {
-                        SynapseClient.signOut().then(() => {
-                          setIsLoggedOut(true)
-                        })
-                        return (
-                          <p style={{ margin: 10 }}>
-                            {isLoggedOut ? 'Logged' : 'Logging'} out
-                          </p>
-                        )
-                      }}
-                    />
-                    <Route
-                      exact
-                      path="/login"
-                      render={() => {
-                        // look for the code from the params
-                        const code = getURLParam('code')
-                        SynapseClient.setAccessTokenCookie(code).then(() => {
-                          setIsLoggedOut(false)
-                        })
-                        return (
-                          <p style={{ margin: 10 }}>
-                            {isLoggedOut ? 'Logging' : 'Logged'} in
-                          </p>
-                        )
-                      }}
-                    />
-                  </Switch>
-                </SynapseErrorBoundary>
-              </AppInitializer>
-              <Versions />
-            </ThemeProvider>
-          </QueryClientProvider>
-        </StyledEngineProvider>
-      </BrowserRouter>
+      <StyledEngineProvider injectFirst>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <AppInitializer>
+              <SynapseErrorBoundary>
+                <Outlet />
+              </SynapseErrorBoundary>
+            </AppInitializer>
+            <Versions />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </StyledEngineProvider>
     </div>
   )
 }
