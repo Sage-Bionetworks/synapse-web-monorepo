076101176889463188f21ece93f30dab04299124
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2024-10-15 11:26:14 -0400

    PORTALS-3195 - SageAccountWeb completely migrated

--- apps/SageAccountWeb/src/App.tsx
+++ apps/SageAccountWeb/src/App.tsx
@@ -1,158 +1,129 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {
+  CookiesNotification,
+  SynapseContextConsumer,
+  SynapseContextType,
+  useSynapseContext,
+} from 'synapse-react-client'
+import './App.scss'
 import { AppContextConsumer } from './AppContext'
-import Footer from './components/Footer'
-import { SageResourcesPage } from './components/SageResourcesPage'
 import { AccountCreatedPage } from './components/AccountCreatedPage'
 import { AccountSettings } from './components/AccountSettings'
 import { CertificationQuiz } from './components/CertificationQuiz'
+import { ChangePasswordPage } from './components/ChangePasswordPage'
 import { CurrentAffiliationPage } from './components/CurrentAffiliationPage'
+import Footer from './components/Footer'
 import { JoinTeamPage } from './components/JoinTeamPage'
+import LogoutPage from './components/LogoutPage'
+import { OAuthClientManagementPage } from './components/OAuthClientManagementPage'
+import { PersonalAccessTokensPage } from './components/PersonalAccessTokensPage'
 import { ProfileValidation } from './components/ProfileValidation/ProfileValidation'
 import { RegisterAccount1 } from './components/RegisterAccount1'
 import { RegisterAccount2 } from './components/RegisterAccount2'
 import { ResetPassword } from './components/ResetPassword'
+import { SageResourcesPage } from './components/SageResourcesPage'
 import { TermsOfUsePage } from './components/TermsOfUsePage'
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-import {
-  CookiesNotification,
-  SynapseClient,
-  SynapseContextConsumer,
-  SynapseContextType,
-} from 'synapse-react-client'
-import { WebhookManagementPage } from './components/WebhooksManagementPage'
-import { getSearchParam } from './URLUtils'
-import './App.scss'
-import LoginPage from './LoginPage'
-import TwoFactorAuthEnrollmentPage from './components/TwoFactorAuth/TwoFactorAuthEnrollmentPage'
-import TwoFactorAuthBackupCodesPage from './components/TwoFactorAuth/TwoFactorAuthBackupCodesPage'
-import { PersonalAccessTokensPage } from './components/PersonalAccessTokensPage'
-import { OAuthClientManagementPage } from './components/OAuthClientManagementPage'
 import { ResetTwoFactorAuth } from './components/TwoFactorAuth/ResetTwoFactorAuth'
+import TwoFactorAuthBackupCodesPage from './components/TwoFactorAuth/TwoFactorAuthBackupCodesPage'
+import TwoFactorAuthEnrollmentPage from './components/TwoFactorAuth/TwoFactorAuthEnrollmentPage'
+import { WebhookManagementPage } from './components/WebhooksManagementPage'
 import { RESET_2FA_ROUTE } from './Constants'
-import { ChangePasswordPage } from './components/ChangePasswordPage'
+import LoginPage from './LoginPage'
+import { getSearchParam } from './URLUtils'
+
+function AuthenticatedRoutes() {
+  const { accessToken } = useSynapseContext()
+  const isAuthenticated = Boolean(accessToken)
+
+  if (!isAuthenticated) {
+    return <LoginPage />
+  }
+  return (
+    <Routes>
+      <Route path={'validate'} element={<ProfileValidation />} />
+      <Route path={'signTermsOfUse'} element={<TermsOfUsePage />} />
+      <Route path={'myaccount'} element={<AccountSettings />} />
+      <Route path={'currentaffiliation'} element={<CurrentAffiliationPage />} />
+      <Route path={'accountcreated'} element={<AccountCreatedPage />} />
+      <Route path={'certificationquiz'} element={<CertificationQuiz />} />
+      <Route path={'2fa/enroll'} element={<TwoFactorAuthEnrollmentPage />} />
+      <Route
+        path={'2fa/generatecodes'}
+        element={<TwoFactorAuthBackupCodesPage />}
+      />
+      <Route
+        path={'personalaccesstokens'}
+        element={<PersonalAccessTokensPage />}
+      />
+      <Route
+        path={'oauthclientmanagement'}
+        element={<OAuthClientManagementPage />}
+      />
+      <Route path={'webhook'} element={<WebhookManagementPage />} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <CookiesNotification />
-      <Switch>
-        <Route exact path="/">
-          <SynapseContextConsumer>
-            {(ctx?: SynapseContextType) => {
-              if (!ctx?.accessToken) {
-                return <LoginPage returnToUrl={'/'} />
-              } else {
-                return (
-                  <AppContextConsumer>
-                    {appContext => {
-                      const isCodeSearchParam =
-                        getSearchParam('code') !== undefined
-                      const isProviderSearchParam =
-                        getSearchParam('provider') !== undefined
-                      const isInSSOFlow =
-                        isCodeSearchParam && isProviderSearchParam
-                      return (
-                        <>
-                          {appContext?.redirectURL &&
-                            !isInSSOFlow &&
-                            window.location.replace(appContext?.redirectURL)}
-                        </>
-                      )
-                    }}
-                  </AppContextConsumer>
-                )
-              }
-            }}
-          </SynapseContextConsumer>
-        </Route>
+      <Routes>
         <Route
-          exact
-          path="/logout"
-          render={props => {
-            SynapseClient.signOut().then(() => {
-              window.history.replaceState(null, '', '/authenticated/myaccount')
-            })
-            return <></>
-          }}
+          path=""
+          element={
+            <SynapseContextConsumer>
+              {(ctx?: SynapseContextType) => {
+                if (!ctx?.accessToken) {
+                  return <LoginPage returnToUrl={'/'} />
+                } else {
+                  return (
+                    <AppContextConsumer>
+                      {appContext => {
+                        const isCodeSearchParam =
+                          getSearchParam('code') !== undefined
+                        const isProviderSearchParam =
+                          getSearchParam('provider') !== undefined
+                        const isInSSOFlow =
+                          isCodeSearchParam && isProviderSearchParam
+                        return (
+                          <>
+                            {appContext?.redirectURL &&
+                              !isInSSOFlow &&
+                              window.location.replace(appContext?.redirectURL)}
+                          </>
+                        )
+                      }}
+                    </AppContextConsumer>
+                  )
+                }
+              }}
+            </SynapseContextConsumer>
+          }
         />
-        <Route exact path="/register1" component={RegisterAccount1} />
-        <Route exact path="/register2" component={RegisterAccount2} />
-        <Route exact path="/jointeam" component={JoinTeamPage} />
-        <Route exact path="/changePassword" component={ChangePasswordPage} />
-        <Route exact path="/sageresources" component={SageResourcesPage} />
-        <Route exact path="/resetPassword">
-          <ResetPassword returnToUrl="/authenticated/myaccount" />
-        </Route>
-        <Route exact path={RESET_2FA_ROUTE}>
-          <ResetTwoFactorAuth />
-        </Route>
-        <SynapseContextConsumer>
-          {(ctx?: SynapseContextType) => {
-            const isAuthenticated = !!ctx?.accessToken
-            return (
-              <>
-                {/* If not signed in and in the "/authenticated" path, show the login page */}
-                {!isAuthenticated && (
-                  <Route path="/authenticated" exact={false}>
-                    <LoginPage />
-                  </Route>
-                )}
-                {isAuthenticated && (
-                  <>
-                    <Route path={'/authenticated/validate'} exact>
-                      <ProfileValidation />
-                    </Route>
-                    <Route path={'/authenticated/signTermsOfUse'} exact>
-                      <TermsOfUsePage />
-                    </Route>
-                    <Route path={'/authenticated/myaccount'} exact>
-                      <AccountSettings />
-                    </Route>
-                    <Route path={'/authenticated/currentaffiliation'} exact>
-                      <CurrentAffiliationPage />
-                    </Route>
-                    <Route path={'/authenticated/accountcreated'} exact>
-                      <AccountCreatedPage />
-                    </Route>
-                    <Route path={'/authenticated/certificationquiz'} exact>
-                      <CertificationQuiz />
-                    </Route>
-                    <Route
-                      path={'/authenticated/2fa/enroll'}
-                      exact
-                      render={() => <TwoFactorAuthEnrollmentPage />}
-                    />
-                    <Route
-                      path={'/authenticated/2fa/generatecodes'}
-                      exact
-                      render={() => <TwoFactorAuthBackupCodesPage />}
-                    />
-                    <Route
-                      path={'/authenticated/personalaccesstokens'}
-                      exact
-                      render={() => <PersonalAccessTokensPage />}
-                    />
-                    <Route
-                      path={'/authenticated/oauthclientmanagement'}
-                      exact
-                      render={() => <OAuthClientManagementPage />}
-                    />
-                    <Route
-                      path={'/authenticated/webhook'}
-                      exact
-                      render={() => <WebhookManagementPage />}
-                    />
-                    <Footer />
-                  </>
-                )}
-              </>
-            )
-          }}
-        </SynapseContextConsumer>
-        <Route exact={true} path="/login">
-          <LoginPage returnToUrl={'/'} />
-        </Route>
-      </Switch>
+        <Route path="logout" element={<LogoutPage />} />
+        <Route path="register1" element={<RegisterAccount1 />} />
+        <Route path="register2" element={<RegisterAccount2 />} />
+        <Route path="jointeam" element={<JoinTeamPage />} />
+        <Route path="changePassword" element={<ChangePasswordPage />} />
+        <Route path="sageresources" element={<SageResourcesPage />} />
+        <Route
+          path="resetPassword"
+          element={<ResetPassword returnToUrl="/authenticated/myaccount" />}
+        />
+        <Route path={RESET_2FA_ROUTE} element={<ResetTwoFactorAuth />} />
+        <Route
+          path="authenticated/*"
+          element={
+            <>
+              <AuthenticatedRoutes />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="login" element={<LoginPage returnToUrl={'/'} />} />
+      </Routes>
     </div>
   )
 }
