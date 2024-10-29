0443a1584962b82b41d7366e705dab0ec917077b
Kiana McCullough <kianamccu@gmail.com>
2024-10-29 11:30:41 -0700

    unused variable

--- packages/synapse-react-client/src/components/CardContainer/CardContainer.tsx
+++ packages/synapse-react-client/src/components/CardContainer/CardContainer.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import useGetInfoFromIds from '../../utils/hooks/useGetInfoFromIds'
 import {
   DATASET,
