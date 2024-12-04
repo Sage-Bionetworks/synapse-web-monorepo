71e3a0bc13e04c7a10c184c9614c78f0df1a8726
lakikowolfe <loren.wolfe@sagebase.org>
2024-12-04 12:23:15 -0800

    tweak styling to match figma design

--- apps/synapse-portal-framework/src/components/elportal/ELGettingStarted.tsx
+++ apps/synapse-portal-framework/src/components/elportal/ELGettingStarted.tsx
@@ -1,9 +1,11 @@
 import { Box, Link, Button, Typography, darken } from '@mui/material'
 import React from 'react'
-// TODO import assets
 import exploreIcon from './assets/explore_icon.png'
 import uncoverIcon from './assets/uncover_icon.png'
 import accessIcon from './assets/access_icon.png'
+
+// TODO Fix vertical column space (getting started section is slightly bigger)
+// TODO Set padding for mobile and desktop
 
 const ELGettingStarted: React.FC = () => {
   return (
@@ -13,11 +15,10 @@ const ELGettingStarted: React.FC = () => {
         display: 'grid',
         gridTemplateColumns: '25% 25% 25% 25%',
         position: 'relative',
-        // TODO: Add padding for both desktop and mobile
         pt: '80px',
         pb: '80px',
-        pl: '50px',
-        pr: 3,
+        pl: '80px',
+        pr: '80px',
       }}
     >
       <Box
@@ -25,27 +26,37 @@ const ELGettingStarted: React.FC = () => {
           flex: '1 1 auto',
           width: '100%',
           height: '100%',
+          borderTop: '3px solid #ffffff88',
+          pt: '20px',
+          pb: '20px',
         }}
       >
         <Typography
           variant="headline1"
-          style={{ color: 'white' }}
+          style={{
+            color: 'white',
+            fontFamily: "'Merriweather', serif",
+            fontWeight: 400,
+            fontSize: '24px',
+          }}
           sx={{
-            pt: 2,
-            pb: 2,
-            width: 'max-content',
-            borderTop: '3px solid #ffffff88',
+            width: '100%',
           }}
         >
           Getting Started
         </Typography>
         <Typography
           variant="body1"
-          style={{ color: 'white', fontStyle: 'italic' }}
+          style={{
+            color: 'white',
+            fontStyle: 'italic',
+            fontSize: '13px',
+            lineHeight: '20px',
+          }}
           sx={{
-            mb: 1,
-            pb: 1,
             maxWidth: '100%',
+            mt: '16px',
+            mb: '16px',
           }}
         >
           We provide all the help you need for navigating the portal and
@@ -54,9 +65,14 @@ const ELGettingStarted: React.FC = () => {
         <Button
           variant="contained"
           // TODO: href="<LINK TO HELP SECTION>"
+          style={{
+            fontSize: '14px',
+            fontWeight: 500,
+          }}
           sx={{
             backgroundColor: '#39AC97',
             border: '1px solid white',
+            padding: '6px 24px',
             boxShadow: 'none',
             '&:hover': {
               backgroundColor: darken('#5BA998', 0.05),
@@ -106,6 +122,7 @@ export function IconSquare({ iconUrl, headline, description }) {
           flex: '0 0 auto',
           width: '50%',
           height: '50%',
+          mb: '10px',
           backgroundImage: `url(${iconUrl})`,
           backgroundRepeat: 'no-repeat',
           backgroundSize: 'contain',
@@ -113,11 +130,13 @@ export function IconSquare({ iconUrl, headline, description }) {
       ></Box>
       <Typography
         variant="headline3"
-        style={{ color: 'white', fontStyle: 'italic' }}
+        style={{
+          color: 'white',
+          fontFamily: "'Merriweather', serif",
+          fontWeight: 400,
+        }}
         sx={{
-          mb: 1,
-          pt: 1,
-          pb: 1,
+          mb: '10px',
           maxWidth: '100%',
         }}
       >
@@ -125,11 +144,14 @@ export function IconSquare({ iconUrl, headline, description }) {
       </Typography>
       <Typography
         variant="body1"
-        // TODO Text is running off of component
-        style={{ color: 'white', fontStyle: 'italic' }}
+        style={{
+          color: 'white',
+          fontFamily: "'DM Sans', sans-serif",
+          fontSize: '13px',
+          lineHeight: '15px',
+          fontWeight: 400,
+        }}
         sx={{
-          mb: 1,
-          pb: 1,
           maxWidth: '100%',
         }}
       >
