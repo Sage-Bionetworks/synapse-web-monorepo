fbc0192e234a76c0ccd211e73a150667de3c8cd3
Kiana McCullough <kianamccu@gmail.com>
2024-12-11 13:06:06 -0800

    fix typecheck error

--- packages/synapse-react-client/src/components/PortalFeatureHighlights/PortalFeatureHighlights.tsx
+++ packages/synapse-react-client/src/components/PortalFeatureHighlights/PortalFeatureHighlights.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box, Button, CardMedia, Grid, Stack } from '@mui/material'
+import { Typography, Button, CardMedia, Grid, Stack } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
