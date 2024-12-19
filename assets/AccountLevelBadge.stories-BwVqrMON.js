ccc433d48b6fdd7da338a2c4823dbdbda016abec
xschildw <x.schildwachter@sagebase.org>
2024-12-19 18:31:27 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@0169fdbb6f0bfb97c5563617418c4b0bc7d4ea8c 🚀

---
+++ assets/AccountLevelBadge.stories-BwVqrMON.js
@@ -0,0 +1,14 @@
+import{A as s}from"./AccountLevelBadge-uV4xsVHj.js";import{d as c}from"./ToastMessage-BzITCHuf.js";import"./jsx-runtime-DoxjgJx5.js";import"./index-Cu9bd8lq.js";import"./Card-C3hcFJq7.js";import"./createTheme-BIBP8v0l.js";import"./index-GEGPABih.js";import"./extends-CF3RwP-h.js";import"./styled-DKRXvDfM.js";import"./Paper-BxvgHMc4.js";import"./Box-Bt_N9PQN.js";import"./Tooltip-CTVx2uGI.js";import"./index-Bn-p0IzE.js";import"./utils-B6ltzQPV.js";import"./TransitionGroupContext-C4KIfRep.js";import"./useForkRef-Bm_QQ74L.js";import"./index-zcEXKDIA.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-Bb1htYw2.js";import"./mergeSlotProps-D9SyexBH.js";import"./isHostComponent-DVu5iVWx.js";import"./useControlled-JEQ1v-P8.js";import"./useId-g68bhPgm.js";import"./useIsFocusVisible-Lp1_y5xp.js";import"./Typography-D6GCV-CB.js";import"./Link-DPsc3TBb.js";import"./Button-j3jTttn9.js";import"./ButtonBase-DSnjMKVu.js";import"./FullWidthAlert-BvVq9GNb.js";import"./spreadSx-CwcO6WA9.js";import"./Alert-B6PGsAkP.js";import"./createSvgIcon-Dn0UJ7ef.js";import"./IconButton-DhHKwBf8.js";import"./Stack-BbdlCzXz.js";import"./AlertTitle-CCxKidd9.js";import"./ClickAwayListener-Ba-YFlDA.js";import"./CSSTransition-Brl35HA-.js";import"./hasClass-D5ZjVvBY.js";import"./uniqueId-CSw6ftlJ.js";import"./toString-CYyvKWOl.js";import"./isArray-ggc3KxVp.js";import"./isSymbol-BzKS7Qf1.js";const Z={title:"UI/AccountLevelBadge",component:s},r={args:{badgeType:"certified"}},t={args:{badgeType:"certified",buttonProps:{variant:"outlined",children:"Revoke Certification",onClick:()=>c("Revoke Certification Clicked","success")}}};var o,i,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    badgeType: 'certified'
+  }
+}`,...(e=(i=r.parameters)==null?void 0:i.docs)==null?void 0:e.source}}};var p,m,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    badgeType: 'certified',
+    buttonProps: {
+      variant: 'outlined',
+      children: 'Revoke Certification',
+      onClick: () => displayToast('Revoke Certification Clicked', 'success')
+    }
+  }
+}`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};const $=["Certified","CertifiedRevokable"];export{r as Certified,t as CertifiedRevokable,$ as __namedExportsOrder,Z as default};
