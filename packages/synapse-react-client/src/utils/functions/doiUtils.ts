6b3e814f05cc09203bab60591b0cbddcee770fb1
Kiana McCullough <kianamccu@gmail.com>
2024-12-20 13:14:27 -0800

    fct format

--- packages/synapse-react-client/src/utils/functions/doiUtils.ts
+++ packages/synapse-react-client/src/utils/functions/doiUtils.ts
@@ -1,6 +1,6 @@
 import { DOI_REGEX } from './RegularExpressions'
 
-export const convertDoiToLink = (doi: string) => {
+export function convertDoiToLink(doi: string) {
   doi = doi.trim()
   if (DOI_REGEX.test(doi)) {
     return `https://dx.doi.org/${doi}`
