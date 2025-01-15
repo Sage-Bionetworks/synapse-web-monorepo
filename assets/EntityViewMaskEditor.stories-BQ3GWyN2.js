1af8c34509e9bfba0da8075dd53d1a0cd75b86de
jay-hodgson <jay.hodgson@sagebase.org>
2025-01-15 22:07:25 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@b37d1df9f0c16e1e4f374783b7e1342b024c60ec 🚀

---
+++ assets/EntityViewMaskEditor.stories-BQ3GWyN2.js
@@ -0,0 +1,9 @@
+import{j as t}from"./jsx-runtime-Du8NFWEI.js";import{r as T}from"./index-Dl6G-zuu.js";import{E as a}from"./EntityViewMaskEditor-D3MjTnif.js";import{i as _,m as l}from"./VerificationSubmission-CjmU3lv6.js";import{P as I}from"./Paper-BrACm7G9.js";import"./Typography-DrqHmlDD.js";import"./createTheme-C4MKIpuQ.js";import"./index-GEGPABih.js";import"./styled-BwKg_4wT.js";import"./FormGroup-DKsWbUcB.js";import"./useFormControl-CQLCXd-P.js";import"./FormControlLabel-CgCL0_qx.js";import"./ButtonBase-6jsyScMO.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useForkRef-CEBgoE3Z.js";import"./useIsFocusVisible-ByxglBfx.js";import"./useControlled-1Y2rT-1r.js";import"./Stack-DTWuWz9s.js";import"./Checkbox-CnAHeP9R.js";import"./createSvgIcon-CLD93Ce9.js";import"./Alert-CduaGk2B.js";import"./mergeSlotProps-BB45Ai_O.js";import"./isHostComponent-DVu5iVWx.js";import"./IconButton-BIc9jQ57.js";const b={title:"Synapse/EntityView/Mask Editor",component:a,decorators:[e=>t.jsx(I,{sx:{mx:"auto",p:4,maxWidth:"720px"},children:t.jsx(e,{})})],render:function(s){const[u,d]=T.useState(s.value);return t.jsx(a,{...s,value:u,onChange:d})}},r={args:{value:_}},o={args:{value:_|l}};var m,i,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    value: ENTITY_VIEW_TYPE_MASK_FILE
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var n,E,c;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    value: ENTITY_VIEW_TYPE_MASK_FILE | ENTITY_VIEW_TYPE_MASK_DOCKER
+  }
+}`,...(c=(E=o.parameters)==null?void 0:E.docs)==null?void 0:c.source}}};const q=["FileView","CustomScope"];export{o as CustomScope,r as FileView,q as __namedExportsOrder,b as default};
---
+++ assets/PortalFeatureHighlights.stories-Dfu-nF4u.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{P as i}from"./PortalFeatureHighlights-BjtiEZdl.js";import{M as a}from"./index-CwhkrBS5.js";import"./index-Dl6G-zuu.js";import"./index-C_6iVBSW.js";import"./index-pgNzZV2V.js";import"./index-DJkgPHwn.js";import"./Box-CaFleW7-.js";import"./createTheme-C4MKIpuQ.js";import"./index-GEGPABih.js";import"./Fade-Bx5Dq3o-.js";import"./utils-BNvIpMjh.js";import"./styled-BwKg_4wT.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useForkRef-CEBgoE3Z.js";import"./Slide-T_Ccmief.js";import"./ownerWindow-BN2rbQ_G.js";import"./ownerDocument-DW-IO8s5.js";import"./CardMedia-BuF7IbSb.js";import"./Stack-DTWuWz9s.js";import"./Typography-DrqHmlDD.js";import"./Button-DtfaTZY_.js";import"./ButtonBase-6jsyScMO.js";import"./useIsFocusVisible-ByxglBfx.js";const v={title:"Home Page/PortalFeatureHighlights",component:i,parameters:{chromatic:{viewports:[600,1200]}}},t={render:p=>r.jsx(a,{children:r.jsx(i,{...p})}),args:{title:"Section Title",summaryText:"Section summary text.",buttonText:"Section button",image:"https://link-to-image.png",link:"https://example.com"}};var o,e,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <MemoryRouter>
+      <PortalFeatureHighlights {...args} />
+    </MemoryRouter>,
+  args: {
+    title: 'Section Title',
+    summaryText: 'Section summary text.',
+    buttonText: 'Section button',
+    image: 'https://link-to-image.png',
+    link: 'https://example.com'
+  }
+}`,...(m=(e=t.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const w=["Demo"];export{t as Demo,w as __namedExportsOrder,v as default};
