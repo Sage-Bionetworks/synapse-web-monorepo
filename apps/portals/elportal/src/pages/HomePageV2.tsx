645026ace27667698e983de1f4ec16f3c7acaf54
jay-hodgson <jay.hodgson@sagebase.org>
2025-01-17 09:59:49 -0800

    PORTALS-3310: in progress

--- apps/portals/elportal/src/pages/HomePageV2.tsx
+++ apps/portals/elportal/src/pages/HomePageV2.tsx
@@ -11,11 +11,13 @@ import {
   topPublicationsSql,
   whatWeDoSql,
   featuredResearchSql,
+  dataSql,
 } from '../config/resources'
 import { Link, Typography } from '@mui/material'
 import analyzetheclouds from '../assets/analyzetheclouds.png'
 import computationaltools from '../assets/computationaltools.png'
 import { Box } from '@mui/material'
+import { FeaturedDataTabs } from 'synapse-react-client'
 
 export default function HomePage() {
   const styledPortalFeatureHighlightsSummaryText = (
@@ -51,26 +53,6 @@ export default function HomePage() {
         summaryText="We provide multi-omic datasets, software tools, and publications that empower researchers to discover the latest health-promoting therapeutics."
       />
       <FeaturedResearch sql={featuredResearchSql} />
-      {/* Commented out for release (see EC-485) */}
-      {/*<div className={'home-bg-dark'}>*/}
-      {/*<SectionLayout*/}
-      {/*  title="Exploring the Data"*/}
-      {/*  centerTitle*/}
-      {/*  ContainerProps={{ className: 'home-spacer' }}*/}
-      {/*>*/}
-      {/* Commented out for release (see EC-485) */}
-      {/*  <UpsetPlot*/}
-      {/*    sql={upsetPlotSql}*/}
-      {/*    rgbIndex={0}*/}
-      {/*    maxBarCount={20}*/}
-      {/*    setName="# Files per assay"*/}
-      {/*    combinationName="# individuals"*/}
-      {/*    onClick={handleUpsetPlotClick}*/}
-      {/*    // summaryLinkText='Explore All Data'*/}
-      {/*    // summaryLink='/Explore/Data'*/}
-      {/*  />*/}
-      {/*</SectionLayout>*/}
-      {/*</div>*/}
       <Box sx={{ backgroundColor: 'grey.100' }}>
         <RecentPublicationsGrid
           sql={topPublicationsSql}
@@ -86,6 +68,83 @@ export default function HomePage() {
         }}
       >
         <ELGettingStarted />
+
+        <div className={'home-bg-dark'}>
+          <SectionLayout ContainerProps={{ className: 'home-spacer' }}>
+            <FeaturedDataTabs
+              sql={dataSql}
+              rgbIndex={3}
+              configs={[
+                {
+                  title: 'Human Studies',
+                  icon: 'PERSON',
+                  explorePagePath: '/Explore/Studies',
+                  exploreObjectType: 'Studies',
+                  plotsConfig: {
+                    configs: [
+                      {
+                        title:
+                          'The Mendelian randomization of human longevity using genetically-predicted exposures from the GWAS catalog study',
+                        description:
+                          'This study provides analysis results of a two Sample Mendelian Randomization used to analyze the relationship between significantly associated GWAS traits and five distinct definitions of longevity.',
+                        facetsToPlot: ['dataType'],
+                        selectFacetColumnName: 'Study',
+                        selectFacetColumnValue: 'LLFS',
+                        detailsPagePath:
+                          '/Explore/Studies/DetailsPage?studyKey=LLFS',
+                        unitDescription: 'Files',
+                      },
+                      {
+                        title:
+                          'The Characterization of gene associations with aging-related traits with a genetically-predicted transcriptome-wide association study',
+                        description:
+                          'This study provides analyses of candidate genes and the association of Longevity-Associated Variants (LAVs) with aging-related traits and diseases.',
+                        facetsToPlot: ['dataType'],
+                        selectFacetColumnName: 'Study',
+                        selectFacetColumnValue: 'ADAMTS7',
+                        detailsPagePath:
+                          '/Explore/Studies/DetailsPage?studyKey=ADAMTS7',
+                        unitDescription: 'Files',
+                      },
+                    ],
+                  },
+                },
+                {
+                  title: 'Translation Studies',
+                  icon: 'MOUSE',
+                  explorePagePath: '/Explore/Studies',
+                  exploreObjectType: 'Studies',
+                  plotsConfig: {
+                    configs: [
+                      {
+                        title: 'MRGWAS',
+                        description:
+                          'The Mendelian randomization of human longevity using genetically-predicted exposures from the GWAS catalog (MRGWAS) study provides analysis results of a two Sample Mendelian Randomization used to analyze the relationship between significantly associated GWAS  traits and five distinct definitions of longevity.',
+                        facetsToPlot: ['dataType'],
+                        selectFacetColumnName: 'Study',
+                        selectFacetColumnValue: 'MRGWAS',
+                        detailsPagePath:
+                          '/Explore/Studies/DetailsPage?studyKey=MRGWAS',
+                        unitDescription: 'Files',
+                      },
+                      {
+                        title: 'Aging-PheWAS',
+                        description:
+                          'This study is a collection of genetically-predicted tissue-specific gene expression associations with a collection of aging-related traits and outcomes.',
+                        facetsToPlot: ['dataType'],
+                        selectFacetColumnName: 'Study',
+                        selectFacetColumnValue: 'Aging-PheWAS',
+                        detailsPagePath:
+                          '/Explore/Studies/DetailsPage?studyKey=Aging-PheWAS',
+                        unitDescription: 'Files',
+                      },
+                    ],
+                  },
+                },
+              ]}
+            />
+          </SectionLayout>
+        </div>
         <ELContributeYourData />
         <PortalFeatureHighlights
           image={analyzetheclouds}
