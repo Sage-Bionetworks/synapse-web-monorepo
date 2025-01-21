b178b605c50a966a2540438605718cf6a4a21a1f
nickgros <nickgrosenbacher@gmail.com>
2025-01-21 17:54:28 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@c53b43429ae4332030ad9b271f55124d17e00f3c 🚀

---
+++ assets/PortalFeatureHighlights.stories-C_HBF0PD.js
@@ -0,0 +1,12 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{M as a}from"./chunk-K6AXKMTT-B5GX6HSq.js";import{P as i}from"./PortalFeatureHighlights-JyhpSCst.js";import"./index-Dl6G-zuu.js";import"./index-GEGPABih.js";import"./index-C_6iVBSW.js";import"./Box-CaFleW7-.js";import"./createTheme-C4MKIpuQ.js";import"./Fade-BRP5-t6h.js";import"./utils-DDqOmLXv.js";import"./styled-BwKg_4wT.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useForkRef-CEBgoE3Z.js";import"./index-BIzb42Jq.js";import"./Slide-DDx5_dIA.js";import"./ownerWindow-BN2rbQ_G.js";import"./ownerDocument-DW-IO8s5.js";import"./CardMedia-BuF7IbSb.js";import"./Stack-DTWuWz9s.js";import"./Typography-DrqHmlDD.js";import"./Button-DtfaTZY_.js";import"./ButtonBase-6jsyScMO.js";import"./useIsFocusVisible-ByxglBfx.js";const _={title:"Home Page/PortalFeatureHighlights",component:i,parameters:{chromatic:{viewports:[600,1200]}}},t={render:p=>r.jsx(a,{children:r.jsx(i,{...p})}),args:{title:"Section Title",summaryText:"Section summary text.",buttonText:"Section button",image:"https://link-to-image.png",link:"https://example.com"}};var o,e,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: args => <MemoryRouter>
+      <PortalFeatureHighlights {...args} />
+    </MemoryRouter>,
+  args: {
+    title: 'Section Title',
+    summaryText: 'Section summary text.',
+    buttonText: 'Section button',
+    image: 'https://link-to-image.png',
+    link: 'https://example.com'
+  }
+}`,...(m=(e=t.parameters)==null?void 0:e.docs)==null?void 0:m.source}}};const v=["Demo"];export{t as Demo,v as __namedExportsOrder,_ as default};
