f768baae05218838986c798d94b613a0e31239d2
jay-hodgson <jay.hodgson@sagebase.org>
2025-01-27 15:02:51 -0800

    ADM-4286: apply AND (intersection) filters on click of UpsetPlot, and filter out rows that have null individualID

--- apps/portals/elportal/src/config/synapseConfigs/handleUpsetPlotClick.tsx
+++ apps/portals/elportal/src/config/synapseConfigs/handleUpsetPlotClick.tsx
@@ -1,18 +1,28 @@
-import { Query, FacetColumnRequest } from '@sage-bionetworks/synapse-types'
+import {
+  Query,
+  ColumnMultiValueFunctionQueryFilter,
+  ColumnMultiValueFunction,
+  COLUMN_MULTI_VALUE_FUNCTION_QUERY_FILTER_CONCRETE_TYPE_VALUE,
+} from '@sage-bionetworks/synapse-types'
 import { cohortBuilderFilesSql } from '../resources'
 
 export const handleUpsetPlotClick = (selection: any) => {
   //  We should use generateEncodedQueryForSelectedFacetURL, but this single facet may have multiple values (union from UpsetPlot)
   const sets = selection.sets
-  const facetColumnRequest: FacetColumnRequest = {
-    concreteType:
-      'org.sagebionetworks.repo.model.table.FacetColumnValuesRequest',
-    columnName: 'Assays',
-    facetValues: [...sets.values()].map((v: any) => v.name) as string[],
-  }
+  const columnValues = [...sets.values()].map((v: any) => v.name) as string[]
+
   const query: Query = {
     sql: cohortBuilderFilesSql,
-    selectedFacets: [facetColumnRequest],
+    additionalFilters: columnValues.map(value => {
+      const filter: ColumnMultiValueFunctionQueryFilter = {
+        concreteType:
+          COLUMN_MULTI_VALUE_FUNCTION_QUERY_FILTER_CONCRETE_TYPE_VALUE,
+        columnName: 'Assays',
+        function: ColumnMultiValueFunction.HAS,
+        values: [value],
+      }
+      return filter
+    }),
   }
   const url = `/Explore/Data?QueryWrapper0=${JSON.stringify(query)}`
   window.location.assign(url)
