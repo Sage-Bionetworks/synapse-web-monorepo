8c8871009f945deda24e0457bddcf154dca13c26
jay-hodgson <jay.hodgson@sagebase.org>
2025-01-28 14:03:27 -0800

    pick prop type instead of duplicating

--- apps/portals/elportal/src/config/synapseConfigs/handleUpsetPlotClick.tsx
+++ apps/portals/elportal/src/config/synapseConfigs/handleUpsetPlotClick.tsx
@@ -5,25 +5,28 @@ import {
   COLUMN_MULTI_VALUE_FUNCTION_QUERY_FILTER_CONCRETE_TYPE_VALUE,
 } from '@sage-bionetworks/synapse-types'
 import { cohortBuilderSql } from '../resources'
-import { ISetCombination, ISetLike, ISet } from '@upsetjs/react'
+import { ISetCombination, ISet, UpSetSelectionProps } from '@upsetjs/react'
 
-export const handleUpsetPlotClick = (selection: ISetLike<string> | null) => {
-  //  Gather all values (intersection from UpsetPlot), and create an additional filter for each value
-  const clickedSets = (selection as ISetCombination)?.sets.values()
-  const columnValues = clickedSets.map((v: ISet<any>) => v.name).toArray()
-  const query: Query = {
-    sql: cohortBuilderSql,
-    additionalFilters: columnValues.map(value => {
-      const filter: ColumnMultiValueFunctionQueryFilter = {
-        concreteType:
-          COLUMN_MULTI_VALUE_FUNCTION_QUERY_FILTER_CONCRETE_TYPE_VALUE,
-        columnName: 'Assays',
-        function: ColumnMultiValueFunction.HAS,
-        values: [value],
-      }
-      return filter
-    }),
+export const handleUpsetPlotClick: UpSetSelectionProps['onClick'] =
+  selection => {
+    //  Gather all values (intersection from UpsetPlot), and create an additional filter for each value
+    const clickedSets = (selection as ISetCombination)?.sets.values()
+    const columnValues = clickedSets.map((v: ISet<any>) => v.name).toArray()
+    const query: Query = {
+      sql: cohortBuilderSql,
+      additionalFilters: columnValues.map(value => {
+        const filter: ColumnMultiValueFunctionQueryFilter = {
+          concreteType:
+            COLUMN_MULTI_VALUE_FUNCTION_QUERY_FILTER_CONCRETE_TYPE_VALUE,
+          columnName: 'Assays',
+          function: ColumnMultiValueFunction.HAS,
+          values: [value],
+        }
+        return filter
+      }),
+    }
+    const url = `/Explore/Cohort%20Builder?QueryWrapper0=${JSON.stringify(
+      query,
+    )}`
+    window.location.assign(url)
   }
-  const url = `/Explore/Cohort%20Builder?QueryWrapper0=${JSON.stringify(query)}`
-  window.location.assign(url)
-}
