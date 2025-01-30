3e9b026b93d8510f9839b6b0302d36b2420af9c1
afwillia <anthony.williams@sagebase.org>
2025-01-30 10:17:34 -0800

    WIP: create a mobile drop down menu

--- apps/SageAccountWeb/src/components/AccountSettingsTopBar.tsx
+++ apps/SageAccountWeb/src/components/AccountSettingsTopBar.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BadgeOutlined } from '@mui/icons-material'
 import { Box, SxProps, Typography } from '@mui/material'
 import { useSourceApp } from './useSourceApp'
@@ -6,7 +7,11 @@ import {
   SynapseClient,
   useApplicationSessionContext,
 } from 'synapse-react-client'
-import { Link } from '@mui/material'
+//import { Link } from '@mui/material'
+import { IconButton } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 
 function AccountSettingsTopBar() {
   const sourceApp = useSourceApp()
@@ -18,6 +23,11 @@ function AccountSettingsTopBar() {
   }
   const appContext = useAppContext()
   const { refreshSession } = useApplicationSessionContext()
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
   return (
     <Box
@@ -27,6 +37,7 @@ function AccountSettingsTopBar() {
         alignItems: 'center',
         justifyContent: 'space-between',
       }}
+      onClick={event => setAnchorEl(event.currentTarget)}
     >
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <BadgeOutlined sx={iconSx} />
@@ -35,21 +46,45 @@ function AccountSettingsTopBar() {
         </Typography>
         <Box
           className="AccountSettingsSourceAppLogo"
-          sx={{ marginLeft: '30px' }}
+          sx={{ marginLeft: '30px', display: { xs: 'none', md: 'block' } }}
         >
           <a href={appContext?.redirectURL}>{sourceApp?.logo}</a>
         </Box>
       </Box>
-      <Link
-        sx={{ marginRight: '15px' }}
-        onClick={() => {
-          SynapseClient.signOut().then(() => {
-            refreshSession()
-          })
-        }}
-      >
-        Sign out
-      </Link>
+      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          id="menu-appbar"
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              SynapseClient.signOut().then(() => {
+                refreshSession()
+              })
+            }}
+          >
+            Sign out
+          </MenuItem>
+        </Menu>
+      </Box>
     </Box>
   )
 }
