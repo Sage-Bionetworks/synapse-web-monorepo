b73795e2c41c8701c814adafced21b33c4f2f7b1
jay-hodgson <jay.hodgson@sagebase.org>
2025-02-05 11:07:29 -0800

    from code review

--- apps/portals/cancercomplexity/src/pages/CCKPSearchPage.tsx
+++ apps/portals/cancercomplexity/src/pages/CCKPSearchPage.tsx
@@ -99,9 +99,11 @@ export function CCKPSearchPage(props: CCKPSearchPageProps) {
       // PORTALS-3382: If no tab is selected and all counts have been set, then redirect to the item with the highest count
       const allCountsSet = searchPageTabs.every(tab => tab.count !== undefined)
       if (selectedTabIndex == undefined && allCountsSet) {
-        // navigate to the tab that has the highest count
-        const maxCountTab = searchPageTabs.reduce((max, tab) =>
-          max === null || tab.count! > max.count! ? tab : max,
+        // Navigate to the tab that has the highest count.
+        // Explicitly initialize the accumulator ("max") to the first element (searchPageTabs[0]). "max" will never be null
+        const maxCountTab = searchPageTabs.reduce(
+          (max, tab) => (tab.count! > max.count! ? tab : max),
+          searchPageTabs[0],
         )
         navigate({
           pathname: `/Search/${maxCountTab.path}`,
