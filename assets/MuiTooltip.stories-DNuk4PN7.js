fb5ccc1252845e05ffa9e8fa17cd9baa8b01af28
nickgros <nickgrosenbacher@gmail.com>
2025-02-07 21:32:50 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@f91979b0cf631b1c649209a6e3b1fcaf42437d56 🚀

---
+++ assets/MuiTooltip.stories-DNuk4PN7.js
@@ -0,0 +1,10 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{T as a}from"./Tooltip-D8c5U2qR.js";import{w as m,u as l}from"./index-SqWy7oMY.js";import{I as c}from"./InfoTwoTone-D-FIW5gs.js";import{L as d}from"./Link-DxNs_73S.js";import"./index-Dl6G-zuu.js";import"./createTheme-C4MKIpuQ.js";import"./index-GEGPABih.js";import"./index-CarQ_cRE.js";import"./styled-BwKg_4wT.js";import"./utils-DDqOmLXv.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useForkRef-CEBgoE3Z.js";import"./index-BIzb42Jq.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-30Ov46SB.js";import"./mergeSlotProps-BB45Ai_O.js";import"./isHostComponent-DVu5iVWx.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-ByxglBfx.js";import"./createSvgIcon-CLD93Ce9.js";import"./Typography-DrqHmlDD.js";const e=o=>r.jsx(a,{...o});try{e.displayName="Tooltip",e.__docgenInfo={description:"",displayName:"Tooltip",props:{ref:{defaultValue:null,description:"",name:"ref",required:!1,type:{name:"Ref<unknown>"}}}}}catch{}const q={title:"UI/MUI/Tooltip",component:e,parameters:{design:{type:"figma",url:"https://www.figma.com/file/0oPm5lLSUva8kyfVNMS6FA/Sage-Style-%26-Component-Library?node-id=187%3A6615"}},play:async({canvasElement:o})=>{const s=m(o).getByTestId("tooltipAnchor");await l.hover(s)}},t={name:"Tooltip",args:{children:r.jsx(c,{"data-testid":"tooltipAnchor"}),title:r.jsxs("p",{children:["This is some text, and"," ",r.jsx(d,{href:"https://synapse.org/",children:"here is a link"}),"."]})}};var i,n,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  name: 'Tooltip',
+  args: {
+    children: <InfoTwoTone data-testid={'tooltipAnchor'} />,
+    title: <p>
+        This is some text, and{' '}
+        <Link href={'https://synapse.org/'}>here is a link</Link>.
+      </p>
+  }
+}`,...(p=(n=t.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const B=["Demo"];export{t as Demo,B as __namedExportsOrder,q as default};
