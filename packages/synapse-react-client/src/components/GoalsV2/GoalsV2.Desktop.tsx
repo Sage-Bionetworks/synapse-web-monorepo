09d058fead24403955d6bdb9f8ea425a25f68398
Kiana McCullough <kianamccu@gmail.com>
2025-02-10 11:27:01 -0800

    typography tweak

--- packages/synapse-react-client/src/components/GoalsV2/GoalsV2.Desktop.tsx
+++ packages/synapse-react-client/src/components/GoalsV2/GoalsV2.Desktop.tsx
@@ -35,7 +35,10 @@ export default function GoalsV2Desktop({
             justifyContent: 'center',
           }}
         >
-          <Typography variant="h6" component="strong" sx={{ marginRight: 1 }}>
+          <Typography
+            variant="h6"
+            sx={{ marginRight: 1, fontSize: '16px', fontWeight: 900 }}
+          >
             {countSql && (
               <QueryCount parens={false} query={{ sql: countSql }} />
             )}
