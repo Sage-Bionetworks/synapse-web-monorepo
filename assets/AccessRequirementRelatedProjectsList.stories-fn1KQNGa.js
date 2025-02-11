c20b634e193694c26e84fc8e284f8088fec1647b
afwillia <anthony.williams@sagebase.org>
2025-02-11 20:23:00 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@2e1083b07ee523ddd476f3d0a7a25bcf1543858d 🚀

---
+++ assets/AccessRequirementRelatedProjectsList.stories-fn1KQNGa.js
@@ -0,0 +1,35 @@
+import{A as j}from"./AccessRequirementRelatedProjectsList-zDmiWcI2.js";import{M as P,b as I}from"./useFiles-DgS9yAar.js";import{l as g}from"./index-D3psT3K5.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./VerificationSubmission-DL9jxYsQ.js";import"./StringUtils-By8SXO8c.js";import"./OrientationBanner-D9CLn5zV.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-C4tN5ngN.js";import"./Alert-CduaGk2B.js";import"./createTheme-C4MKIpuQ.js";import"./index-GEGPABih.js";import"./styled-BwKg_4wT.js";import"./mergeSlotProps-BB45Ai_O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./createSvgIcon-CLD93Ce9.js";import"./Paper-BrACm7G9.js";import"./IconButton-BIc9jQ57.js";import"./ButtonBase-6jsyScMO.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useIsFocusVisible-ByxglBfx.js";import"./Stack-DTWuWz9s.js";import"./Box-CaFleW7-.js";import"./AlertTitle-G3es5_Tb.js";import"./Typography-DrqHmlDD.js";import"./utils-DDqOmLXv.js";import"./index-BIzb42Jq.js";import"./Grow-30Ov46SB.js";import"./ClickAwayListener-D7YV5tOA.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-D8c5U2qR.js";import"./index-CarQ_cRE.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-DtfaTZY_.js";import"./useAccessRequirements-DJw-_Pns.js";import"./useQueries-CrJ5bZfl.js";import"./useQuery-6SsvXKRa.js";import"./utils-Bx1BRerw.js";import"./useMutation-DjwfMZ6R.js";import"./useInfiniteQuery-CjTuFu_p.js";import"./QueryFilterUtils-D6d8f_ZF.js";import"./SynapseConstants-ByimdbyM.js";import"./_baseTimes-36S_kd0L.js";import"./isNumber-BM-UBS4N.js";import"./isArray-ggc3KxVp.js";import"./useMediaQuery-DfCTVEwh.js";import"./sortBy-CU7u0WWg.js";import"./hasIn-Dnub9brO.js";import"./cloneDeep-BzxzZd4X.js";import"./_initCloneObject-CKp18hZk.js";import"./isSymbol-BzKS7Qf1.js";import"./toString-CYyvKWOl.js";import"./_baseIteratee-BFw4wzxm.js";import"./identity-DKeuBCMA.js";import"./merge-DLQdEICg.js";import"./EntityLink-DVEGXBrd.js";import"./useEntity--O3QjCXV.js";import"./omitBy-C3ODf4vb.js";import"./_baseIndexOf-DlmoT9Yo.js";import"./toInteger-czOlq-ma.js";import"./pick-Bj2GTyCf.js";import"./_baseSlice-F8doVSIJ.js";import"./InfiniteQueryUtils-CKlRW-xB.js";import"./EntityIcon-ggZH6RW_.js";import"./IconSvg-M9AhD_N5.js";import"./LayersTwoTone-Be5uNxsz.js";import"./ErrorOutlined-qWluJVEZ.js";import"./GetAppTwoTone-BARpdX8y.js";import"./InfoOutlined-CgkZO6RQ.js";import"./DeleteTwoTone-DDTGJUFj.js";import"./CheckCircleTwoTone-DTl0gYtq.js";import"./ErrorChip-BWfydRzz.js";import"./Chip-ByHPzk0X.js";import"./Avatar-BXRn3zVs.js";import"./Link-DxNs_73S.js";import"./Skeleton-CW6YXi1_.js";import"./pluralize-CVwSCrA2.js";import"./_commonjs-dynamic-modules-TDtrdbi3.js";import"./List-nAK0nmZc.js";import"./ListItem-Vlbnwq9f.js";import"./listItemButtonClasses-BmJDwxiU.js";import"./isMuiElement-DAcuSkv2.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-D-FIW5gs.js";import"./calculateFriendlyFileSize-B85TBsSb.js";import"./inputBaseClasses-u7jm9M-e.js";import"./Fade-BRP5-t6h.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-B5GX6HSq.js";import"./util-BbuHFHEZ.js";const it={title:"Governance/AccessRequirementRelatedProjectsList",component:j},r={args:{accessRequirementId:9603055}},t={args:{accessRequirementId:9605913}},e={args:{accessRequirementId:1234567},parameters:{stack:"mock",msw:{handlers:[g.rest.post(`${P}${I}`,async(A,u,o)=>{const l={results:[{id:"1234567",type:"org.sagebionetworks.repo.model.ManagedACTAccessRequirement",createdOn:"2017-08-23T18:48:20.892Z",modifiedOn:"2023-12-14T00:43:41.315Z",name:"Team AR without Related Projects",version:"1",relatedProjectIds:[],reviewerIds:[]}]};return u(o.status(200),o.json(l))})]}}};var m,i,s;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 9603055
+  }
+}`,...(s=(i=r.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var p,a,n;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 9605913
+  }
+}`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var c,d,R;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 1234567
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: [
+      // searchAccessRequirements
+      rest.post(\`\${MOCK_REPO_ORIGIN}\${ACCESS_REQUIREMENT_SEARCH}\`, async (req, res, ctx) => {
+        const zeroRelatedProjects = {
+          results: [{
+            id: '1234567',
+            type: 'org.sagebionetworks.repo.model.ManagedACTAccessRequirement',
+            createdOn: '2017-08-23T18:48:20.892Z',
+            modifiedOn: '2023-12-14T00:43:41.315Z',
+            name: 'Team AR without Related Projects',
+            version: '1',
+            relatedProjectIds: [],
+            reviewerIds: []
+          }]
+        };
+        return res(ctx.status(200), ctx.json(zeroRelatedProjects));
+      })]
+    }
+  }
+}`,...(R=(d=e.parameters)==null?void 0:d.docs)==null?void 0:R.source}}};const st=["ManyProjects","OneProject","ZeroProjectsMock"];export{r as ManyProjects,t as OneProject,e as ZeroProjectsMock,st as __namedExportsOrder,it as default};
