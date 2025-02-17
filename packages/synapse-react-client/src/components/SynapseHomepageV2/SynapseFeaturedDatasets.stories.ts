483029c294a7020c866ecda32530af1a6c917f58
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-02-17 15:44:27 -0500

    SWC-7247
    - SynapseFeaturedDatasetItem: a Link can override ID
    - Use Card instead of Paper (closer to design, looks better)

---
+++ packages/synapse-react-client/src/components/SynapseHomepageV2/SynapseFeaturedDatasets.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SynapseFeaturedDatasets } from './SynapseFeaturedDatasets'
+import { featuredDatasetsTable } from './SynapseHomepageV2'
+
+const meta = {
+  title: 'Synapse/HomePage/FeaturedDatasets',
+  component: SynapseFeaturedDatasets,
+  parameters: {
+    chromatic: { viewports: [600, 1200] },
+  },
+} satisfies Meta
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Demo: Story = {
+  name: 'Synapse Featured Datasets',
+  args: {
+    sourceTable: featuredDatasetsTable,
+  },
+}
