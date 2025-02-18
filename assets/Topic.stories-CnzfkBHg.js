d10d827c05978e28e9052c31124155c38950435a
nickgros <nickgrosenbacher@gmail.com>
2025-02-18 18:37:43 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@a3ddd78ec9b6ddeaced8a7580df9059a656563fb 🚀

---
+++ assets/Topic.stories-CnzfkBHg.js
@@ -0,0 +1,10 @@
+import{T as m}from"./Topic-GSjOQAj1.js";import{f as p}from"./index-SqWy7oMY.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./IconSvg-eEMewTcf.js";import"./createSvgIcon-CLD93Ce9.js";import"./createTheme-C4MKIpuQ.js";import"./index-GEGPABih.js";import"./styled-BwKg_4wT.js";import"./spreadSx-CwcO6WA9.js";import"./Tooltip-D8c5U2qR.js";import"./index-CarQ_cRE.js";import"./utils-DDqOmLXv.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useForkRef-CEBgoE3Z.js";import"./index-BIzb42Jq.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-30Ov46SB.js";import"./mergeSlotProps-BB45Ai_O.js";import"./isHostComponent-DVu5iVWx.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-ByxglBfx.js";import"./LayersTwoTone-Be5uNxsz.js";import"./ErrorOutlined-qWluJVEZ.js";import"./GetAppTwoTone-BARpdX8y.js";import"./InfoOutlined-CgkZO6RQ.js";import"./DeleteTwoTone-DDTGJUFj.js";import"./CheckCircleTwoTone-DTl0gYtq.js";import"./ConditionalWrapper-Be926PbJ.js";import"./Stack-DTWuWz9s.js";import"./Button-DtfaTZY_.js";import"./ButtonBase-6jsyScMO.js";import"./Skeleton-CW6YXi1_.js";import"./Link-DxNs_73S.js";import"./Typography-DrqHmlDD.js";const M={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!",onToggleSubscribe:p()}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Topic',
+  args: {
+    isSubscribed: true,
+    isLoading: false,
+    icon: 'discussion',
+    name: 'Help me understand this project!',
+    onToggleSubscribe: fn()
+  }
+}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const N=["Demo"];export{o as Demo,N as __namedExportsOrder,M as default};
