3c21e946c30c0bec14ac46a1dc64a308d0e6a6b1
Sigfried Gold <sigfried@sigfried.org>
2025-03-03 15:05:23 -0500

    Putting back commented-out Icon code
    Had commented out because they weren't displaying at a reasonable size,
    but when I got AI for stacking based on metadata height, it must have
    deleted it.

--- packages/synapse-react-client/src/components/HeaderCard/HeaderCardV2.tsx
+++ packages/synapse-react-client/src/components/HeaderCard/HeaderCardV2.tsx
@@ -244,6 +244,21 @@ function HeaderCardV2({
     >
       <Box sx={{ position: 'relative', zIndex: 1, p: 3 }}>
         <Grid container spacing={3}>
+          {/* Icon Column */}
+          {icon && (
+            <Grid item xs={12} md="auto">
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'flex-start',
+                  justifyContent: { xs: 'center', md: 'flex-start' },
+                }}
+              >
+                {icon}
+              </Box>
+            </Grid>
+          )}
+
           {/* Main Content Grid */}
           <Grid item xs={12} md={useStackedLayout ? 12 : 8}>
             <Stack spacing={2} ref={descriptionRef}>
