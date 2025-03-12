920a800825befb12757564b5d8269cd6b6e0b75f
afwillia <anthony.williams@sagebase.org>
2025-03-12 14:44:38 -0700

    Create unit tests for AddToDownloadCart

---
+++ packages/synapse-react-client/src/components/AddToDownloadCart/AddToDownloadCart.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { AddToDownloadCart, AddToDownloadCartProps } from './AddToDownloadCart'
+import { createWrapper } from '../../testutils/TestingLibraryUtils'
+
+jest.mock('../download_list/TableQueryDownloadConfirmation', () => ({
+  TableQueryDownloadConfirmation: () => (
+    <div>TableQueryDownloadConfirmation</div>
+  ),
+}))
+jest.mock('../download_list/FolderDownloadConfirmation', () => ({
+  FolderDownloadConfirmation: () => <div>FolderDownloadConfirmation</div>,
+}))
+
+describe('AddToDownloadCart', () => {
+  const props: AddToDownloadCartProps = {
+    folderId: 'syn7248585',
+  }
+
+  it('renders the button', () => {
+    render(<AddToDownloadCart {...props} />, { wrapper: createWrapper() })
+    fireEvent.click(screen.getByText(/add to download cart/i))
+    expect(
+      screen.getByRole('button', { name: /add to download cart/i }),
+    ).toBeInTheDocument()
+  })
+
+  it('renders TableQueryDownloadConfirmation after button click', async () => {
+    render(<AddToDownloadCart {...props} />, { wrapper: createWrapper() })
+    fireEvent.click(screen.getByText(/add to download cart/i))
+    await waitFor(() => {
+      expect(
+        screen.getByText('TableQueryDownloadConfirmation'),
+      ).toBeInTheDocument()
+    })
+  })
+})
