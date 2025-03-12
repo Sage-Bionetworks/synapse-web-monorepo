2f8129d6db3e28569a003f2550936d7009d593b9
afwillia <anthony.williams@sagebase.org>
2025-03-12 14:46:32 -0700

    Fix import case for Dataset_V2 component in row_renderers

--- packages/synapse-react-client/src/components/row_renderers/index.tsx
+++ packages/synapse-react-client/src/components/row_renderers/index.tsx
@@ -1,5 +1,5 @@
 import Dataset from './Dataset'
 import Funder from './Funder'
-import Dataset_V2 from './Dataset_v2'
+import Dataset_V2 from './Dataset_V2'
 
 export { Dataset, Funder, Dataset_V2 }
