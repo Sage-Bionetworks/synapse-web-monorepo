10575aa1c96e57725c79aad1400f71a36e9e4713
jay-hodgson <jay.hodgson@sagebase.org>
2025-03-24 13:24:47 -0700

    PORTALS-3482: update featured data tabs to point to standards registry data

--- apps/portals/standards/src/pages/HomePage.tsx
+++ apps/portals/standards/src/pages/HomePage.tsx
@@ -1,64 +1,37 @@
 import { SectionLayout } from '@sage-bionetworks/synapse-portal-framework/components/SectionLayout'
 import StandardsHeader from '@sage-bionetworks/synapse-portal-framework/components/standards/StandardsHeader'
 import { dataSql } from '../config/resources'
-import { FeaturedDataTabs, Goals, SynapseConstants } from 'synapse-react-client'
+import { FeaturedDataTabs, SynapseConstants } from 'synapse-react-client'
 
 //TODO
 export default function HomePage() {
   return (
     <>
       <StandardsHeader dataSql={dataSql} />
-      <SectionLayout ContainerProps={{ className: 'home-spacer' }}>
+      {/* <SectionLayout ContainerProps={{ className: 'home-spacer' }}>
         <Goals entityId={'syn23518009'} />
-      </SectionLayout>
+      </SectionLayout> */}
       <div className={'home-bg-dark'}>
         <SectionLayout
-          title={'Collections'}
+          title={'Exploring the Standards'}
           centerTitle
           ContainerProps={{ className: 'home-spacer' }}
         >
           <FeaturedDataTabs
-            sql="SELECT * FROM syn21994974"
+            sql={dataSql}
             rgbIndex={3}
             configs={[
               {
-                title: 'Studies',
-                icon: SynapseConstants.FILE,
-                explorePagePath: '/Explore/Collections',
-                exploreObjectType: 'Collections',
+                title: '',
+                icon: 'chart2',
+                explorePagePath: '/Explore',
+                exploreObjectType: 'Standards',
                 plotsConfig: {
-                  sql: 'SELECT * FROM syn21994974 WHERE ( ( "collectionType" = \'Validation Study\' OR "collectionType" = \'Interventional Study\' OR "collectionType" = \'Observational Study\' ) )',
+                  sql: dataSql,
                   configs: [
                     {
-                      facetsToPlot: [
-                        'diagnosis',
-                        'dataCollectionMethod',
-                        'digitalAssessmentCategory',
-                        'sensorType',
-                        'devicePlatform',
-                      ],
-                      unitDescription: 'Collections',
-                    },
-                  ],
-                },
-              },
-              {
-                title: 'Analysis',
-                icon: SynapseConstants.CHART2,
-                explorePagePath: '/Explore/Collections',
-                exploreObjectType: 'Collections',
-                plotsConfig: {
-                  sql: 'SELECT * FROM syn21994974 WHERE ( ( "collectionType" = \'Analysis\' OR "collectionType" = \'Challenge\' ) )',
-                  configs: [
-                    {
-                      facetsToPlot: [
-                        'diagnosis',
-                        'dataCollectionMethod',
-                        'digitalAssessmentCategory',
-                        'sensorType',
-                        'devicePlatform',
-                      ],
-                      unitDescription: 'Collections',
+                      facetsToPlot: ['Topic', 'Org_Name'],
+                      unitDescription: 'standard',
                     },
                   ],
                 },
