4824a0d46286a287762fe284a452e4876af6c298
kianamcc <42532725+kianamcc@users.noreply.github.com>
2025-03-28 20:12:48 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@cea3ca6a4c68ca914f95f3e91c0c6586ba64bec9 🚀

---
+++ assets/SharePageLinkButton.stories-ChrLqb6u.js
@@ -0,0 +1,16 @@
+import{S as p}from"./SharePageLinkButton-COGvptVW.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./useMutation-DjwfMZ6R.js";import"./utils-Bx1BRerw.js";import"./ToastMessage-Cdc-RPF1.js";import"./FullWidthAlert-B22pQuiY.js";import"./spreadSx-CwcO6WA9.js";import"./Alert-Dcz6JZ5x.js";import"./createTheme-BAeQEIuu.js";import"./index-GEGPABih.js";import"./styled-CVek1lbE.js";import"./mergeSlotProps-D1doMhlu.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./createSvgIcon-DGYn73cf.js";import"./Paper-BMFCclTH.js";import"./IconButton-B2vXFIju.js";import"./ButtonBase-CJOJSfnP.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useIsFocusVisible-ByxglBfx.js";import"./Stack-Cg5HdmvL.js";import"./Box-DN73Q90z.js";import"./AlertTitle-D21D8EIu.js";import"./Typography-Cqa7rFgc.js";import"./useTheme-DGffmoS1.js";import"./Grow-DPWvTaVu.js";import"./index-BIzb42Jq.js";import"./utils-Cn9x6_1Z.js";import"./ClickAwayListener-D7YV5tOA.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-B_hdhCmy.js";import"./index-OQIcOgx6.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-jaxqGIs_.js";import"./CSSTransition-DjXnLGfQ.js";import"./uniqueId-CNPmaLAy.js";import"./toString-C-tSuG3i.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./isSymbol-VqtJwUUo.js";import"./IconSvg-DiFKwx5l.js";import"./LayersTwoTone-hfVo2OcY.js";import"./ErrorOutlined-Cv1ApGtt.js";import"./GetAppTwoTone-2-W2Mxbb.js";import"./InfoOutlined-BOPmYxHD.js";import"./DeleteTwoTone-BaSxwZ9v.js";import"./CheckCircleTwoTone-ChmDaZaB.js";const tt={title:"UI/SharePageLinkButton",component:p},t={args:{buttonProps:{sx:{position:"fixed",right:"20px"}}},parameters:{stack:"mock",msw:{handlers:[]}}};var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    buttonProps: {
+      sx: {
+        position: 'fixed',
+        right: '20px'
+      }
+    }
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: []
+    }
+  }
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const rt=["SharePageLinkButtonStory"];export{t as SharePageLinkButtonStory,rt as __namedExportsOrder,tt as default};
