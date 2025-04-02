2faf5aed7692db3e1121bf3a31dd46d5b007525b
jay-hodgson <jay.hodgson@sagebase.org>
2025-04-02 10:57:25 -0700

    PORTALS-3515: no-op change to cause ARK staging portal build/deploy action

--- apps/portals/arkportal/src/config/routesConfig.tsx
+++ apps/portals/arkportal/src/config/routesConfig.tsx
@@ -16,7 +16,7 @@ const routes: RouteObject[] = [
     path: '/',
     element: (
       <App>
-        {/* PORTALS-3508, and PORTALS-3515 */}
+        {/* PORTALS-3508 and PORTALS-3515 */}
         <RepositoryUnderReviewAlert portalSpecificDisclaimer="This repository is developed by Sage Bionetworks to host and share resources related to arthritis and autoimmune and related diseases, and remains fully operational. We continue to maintain and accept related data and resources throughout this review process." />
       </App>
     ),
