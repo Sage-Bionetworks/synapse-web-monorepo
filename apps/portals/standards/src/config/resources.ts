2c51dc54441701c2c7c411ac55463b63c8b9bcec
Sigfried Gold <sigfried@sigfried.org>
2025-04-04 13:07:06 -0400

    Fixes https://github.com/bridge2ai/b2ai-standards-registry/issues/251
    Acronym column should link to the standard

--- apps/portals/standards/src/config/resources.ts
+++ apps/portals/standards/src/config/resources.ts
@@ -1,2 +1,25 @@
-// export const dataSql = `SELECT * FROM syn64960277`  // RegistryTable
-export const dataSql = `SELECT * FROM syn65676531` // DST_denormalized
+const DST_TABLE_ID = 'syn65676531'
+// can replace with specific version if wanted
+
+// for the Explore page table:
+export const dataSql = `
+    SELECT
+        concat('[', acronym, '](/Explore/Standard/DetailsPage?id=', id, ')') as acronym,
+            name, category, collections, topic as topics, relevantOrgAcronym as organizations, isOpen, registration FROM ${DST_TABLE_ID}
+`
+// concat('/Explore/Standard/DetailsPage?id=', id) as link, acronym,
+
+// for details page:
+export const standardsDetailsPageSQL = `
+    SELECT  acronym,
+            name as standardName,
+            description,
+            category,
+            collections,
+            topic as topics,
+            relevantOrgAcronym as orranizations,
+            responsibleOrgName as SDO,
+            isOpen,
+            registration
+    FROM ${DST_TABLE_ID}
+`
--- apps/portals/standards/src/config/synapseConfigs/data.ts
+++ apps/portals/standards/src/config/synapseConfigs/data.ts
@@ -9,13 +9,24 @@ import { dataSql } from '../resources'
 
 const dataRgbIndex = 0
 export const dataColumnLinks: LabelLinkConfig = [
-  {
+  /* {
     isMarkdown: false,
     baseURL: 'Explore/Standard/DetailsPage',
-    // URLColumnName: 'name',
-    // matchColumnName: 'name',
-    URLColumnName: 'id',
     matchColumnName: 'id',
+    URLColumnName: 'id',
+  },
+  {
+    isMarkdown: false,
+    matchColumnName: 'acronym',
+    linkColumnName: 'link',
+  },
+  */
+  {
+    isMarkdown: true,
+    // the column whose value will be used for the markdown
+    matchColumnName: 'acronym',
+    // If set, also show a tooltip
+    // tooltipText?: string
   },
 ]
 
@@ -32,8 +43,7 @@ export const dataQueryWrapperPlotNavProps: QueryWrapperPlotNavProps = {
   },
 
   facetsToPlot: [
-    // 'Data_Topic',  // what it's called in DataRegistry
-    'topic', // what it's called in DST_denormalized
+    'topic',
     // 'Organizations',
     'relevantOrgName',
   ],
