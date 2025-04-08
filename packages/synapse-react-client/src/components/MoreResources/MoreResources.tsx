76e242760cfe4361ea62a735e6ea84c6abe82f0e
“kianamcc” <user.email=kianamccu@gmail.com>
2025-04-08 12:24:07 -0700

    more resources wip again

--- packages/synapse-react-client/src/components/MoreResources/MoreResources.tsx
+++ packages/synapse-react-client/src/components/MoreResources/MoreResources.tsx
@@ -1,25 +1,80 @@
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Link, Stack, Typography, useTheme } from '@mui/material'
 
 const MoreResources = () => {
+  const theme = useTheme()
   const resources = [
-    { title: 'title 1', description: 'desc 1', link: 'link 1' },
-    { title: 'title 2', description: 'desc 2', link: 'link 2' },
-    { title: 'title 3', description: 'desc 3', link: 'link 3' },
+    {
+      title: 'For Researchers',
+      description:
+        'Tutorials about using this portal, as well as links to external partner resources, and related ALS knowledge portals.',
+      link: 'View resources',
+    },
+    {
+      title: 'For Data Contributors',
+      description:
+        'Visit this page for instructions on how to contribute data to this portal.',
+      link: 'View resources',
+    },
+    {
+      title: 'For Persons with Lived Experience',
+      description:
+        'Visit this page for links to external resources with information about clinical trials, ongoing research, and community.',
+      link: 'View resources',
+    },
   ]
   return (
-    <Box sx={{ padding: { xs: '40px', lg: '80px' } }}>
+    <Box
+      sx={{
+        padding: { xs: '40px', lg: '60px 80px 80px 80px' },
+        background: `linear-gradient(0deg, rgba(255, 255, 255, 0.90) 0%, rgba(255, 255, 255, 0.90) 100%), ${theme.palette.primary.main}`,
+      }}
+    >
       <Typography
-        sx={{ fontSize: '24px', fontWeight: 600, lineHeight: 'normal' }}
+        sx={{
+          fontSize: '24px',
+          fontWeight: 600,
+          lineHeight: 'normal',
+          marginBottom: '40px',
+          textAlign: 'center',
+        }}
       >
         More Resources
       </Typography>
       <Box sx={{ display: 'flex', gap: '16px', width: '100%' }}>
         {resources.map(resource => (
-          <Box sx={{ background: 'grey', flex: '1 0 0', padding: '24px' }}>
-            <Typography>{resource.title}</Typography>
-            <Typography>{resource.description}</Typography>
-            <Link>{resource.link}</Link>
-          </Box>
+          <Stack
+            sx={{
+              gap: '16px',
+              background: '#FFF',
+              border: '1px solid',
+              borderColor: 'grey.300',
+              flex: '1 0 0',
+              padding: '24px',
+              boxShadow:
+                '0px 1px 1px 0px rgba(0, 0, 0, 0.03), 0px 3px 3px 0px rgba(0, 0, 0, 0.03), 0px 9px 9px 0px rgba(0, 0, 0, 0.03)',
+            }}
+          >
+            <Typography
+              sx={{ fontSize: '18px', fontWeight: 700, lineHeight: 'normal' }}
+            >
+              {resource.title}
+            </Typography>
+            <Typography sx={{ fontSize: '14px', lineHeight: 'normal' }}>
+              {resource.description}
+            </Typography>
+            <Link sx={{ textDecoration: 'none', marginTop: 'auto' }}>
+              <Typography
+                sx={{
+                  fontSize: '14px',
+                  fontWeight: 700,
+                  lineHeight: '150%',
+                  color: 'primary.main',
+                }}
+              >
+                {resource.link}
+              </Typography>
+            </Link>
+          </Stack>
         ))}
       </Box>
     </Box>
