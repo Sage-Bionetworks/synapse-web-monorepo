89ad798f739216235ec2748758731481de601088
kianamcc <42532725+kianamcc@users.noreply.github.com>
2025-04-08 18:19:47 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@c0857b5d023d2f69ecefde65f8775a0d59faa876 🚀

---
+++ assets/ErrorChip.stories-CkjrdBHr.js
@@ -0,0 +1,19 @@
+import{E as h}from"./ErrorChip-CqspfFeL.js";import{U as i}from"./useFiles-j39sV9Ke.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./Tooltip-BN1O7Byi.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./index-pKvUPVqT.js";import"./styled-GqwmQbHR.js";import"./useTheme-C2nLQS7p.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-DdGCipi8.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./index-BIzb42Jq.js";import"./utils-BUDe8ykH.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-ByxglBfx.js";import"./Chip-vbsCVde0.js";import"./createSvgIcon-IuIKk-AJ.js";import"./ButtonBase-CWW9SwzN.js";import"./Avatar-pzUOjEvu.js";import"./Alert-NNHAfyKq.js";import"./Paper-Dap2c6xL.js";import"./IconButton-BUqwv789.js";import"./ErrorOutlined-BKx_L9lY.js";import"./VerificationSubmission-CtqFoX_D.js";import"./SynapseConstants-B9NyzZbd.js";import"./OrientationBanner-1gOdpYHH.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-Dd_L0lmf.js";import"./Stack-CMgji9n-.js";import"./Box-sTghsUmj.js";import"./AlertTitle-CogUI_yl.js";import"./Typography-u5-eOh5K.js";import"./ClickAwayListener-D7YV5tOA.js";import"./Button-JnyLG7Y6.js";import"./StringUtils-BUxkeJpG.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-Cctz1ZmX.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./calculateFriendlyFileSize-BBeMI-i_.js";import"./Skeleton-h9JRD3HJ.js";import"./inputBaseClasses-CTPjn8Z_.js";import"./Fade-C_8BNI_I.js";import"./utils-Bx1BRerw.js";import"./useQuery-BiN9FESP.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-CT__oUM4.js";import"./Link-kli9SxON.js";import"./Collapse-Cd9-krBm.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-B5GX6HSq.js";import"./merge-6t4vjKrJ.js";import"./_isIterateeCall-FuixZKJ7.js";const Fr={title:"Components/ErrorChip",component:h,argTypes:{isAuthenticated:{control:{type:"boolean"}}},args:{isAuthenticated:!0}},g=new i(401,"You must be signed in to access this resource.",""),r={args:{chipText:"syn1234567",error:g,isAuthenticated:!1}},A=new i(403,"You do not have READ access on this entity.",""),t={args:{chipText:"syn1234567",error:A,isAuthenticated:!0}},E=new i(404,"The entity does not exist.",""),o={args:{chipText:"syn1234567",error:E,isAuthenticated:!0}};var e,p,s;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    chipText: 'syn1234567',
+    error: NotSignedInError,
+    isAuthenticated: false
+  }
+}`,...(s=(p=r.parameters)==null?void 0:p.docs)==null?void 0:s.source}}};var m,n,a;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    chipText: 'syn1234567',
+    error: NoAccessError,
+    isAuthenticated: true
+  }
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var c,d,u;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    chipText: 'syn1234567',
+    error: NotFoundError,
+    isAuthenticated: true
+  }
+}`,...(u=(d=o.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};const Ir=["NotSignedIn","NoAccess","NotFound"];export{t as NoAccess,o as NotFound,r as NotSignedIn,Ir as __namedExportsOrder,Fr as default};
---
+++ assets/Topic.stories-C3AysbTS.js
@@ -0,0 +1,10 @@
+import{T as m}from"./Topic-Bk-6qWJ5.js";import{f as p}from"./index-SqWy7oMY.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./ConditionalWrapper-Be926PbJ.js";import"./IconSvg-BlV_UYWI.js";import"./createSvgIcon-IuIKk-AJ.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./spreadSx-CwcO6WA9.js";import"./useTheme-C2nLQS7p.js";import"./Tooltip-BN1O7Byi.js";import"./index-pKvUPVqT.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-DdGCipi8.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./index-BIzb42Jq.js";import"./utils-BUDe8ykH.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-ByxglBfx.js";import"./LayersTwoTone-fKfzxiPS.js";import"./ErrorOutlined-BKx_L9lY.js";import"./GetAppTwoTone-CD3TgP2w.js";import"./InfoOutlined-DpeT4h8X.js";import"./DeleteTwoTone-XAGzVgvj.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./Stack-CMgji9n-.js";import"./Button-JnyLG7Y6.js";import"./ButtonBase-CWW9SwzN.js";import"./Skeleton-h9JRD3HJ.js";import"./Link-kli9SxON.js";import"./Typography-u5-eOh5K.js";const N={title:"Synapse/Following/Topic",component:m,tags:["autodocs"]},o={name:"Topic",args:{isSubscribed:!0,isLoading:!1,icon:"discussion",name:"Help me understand this project!",onToggleSubscribe:p()}};var r,i,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  name: 'Topic',
+  args: {
+    isSubscribed: true,
+    isLoading: false,
+    icon: 'discussion',
+    name: 'Help me understand this project!',
+    onToggleSubscribe: fn()
+  }
+}`,...(t=(i=o.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const P=["Demo"];export{o as Demo,P as __namedExportsOrder,N as default};
