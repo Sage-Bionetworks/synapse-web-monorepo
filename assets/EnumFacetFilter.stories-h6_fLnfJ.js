fb2d7949da672e4b8ad6aa79d0ffc3604e4884ae
jay-hodgson <jay.hodgson@sagebase.org>
2025-04-10 17:14:35 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@d362fec3b04a12b80bb1c0d2d452c0c964697b8f 🚀

---
+++ assets/EnumFacetFilter.stories-h6_fLnfJ.js
@@ -0,0 +1,67 @@
+import{j as m}from"./jsx-runtime-Du8NFWEI.js";import{V as N,F as E}from"./SynapseConstants-CMdv2tqR.js";import{f as a}from"./index-SqWy7oMY.js";import{E as b}from"./EnumFacetFilterSkeleton-BBSFmoP_.js";import{E as M}from"./EnumFacetFilterUI-xHME7j00.js";import{P as L}from"./Paper-Dap2c6xL.js";import{c as t}from"./cloneDeep-CT__oUM4.js";import"./index-Dl6G-zuu.js";import"./OrientationBanner-N5yxtprX.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-CTZAZdOF.js";import"./Alert-NNHAfyKq.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-CEBgoE3Z.js";import"./createSvgIcon-IuIKk-AJ.js";import"./IconButton-BUqwv789.js";import"./ButtonBase-CWW9SwzN.js";import"./TransitionGroupContext-TdpM2qIg.js";import"./useIsFocusVisible-ByxglBfx.js";import"./Stack-CMgji9n-.js";import"./Box-sTghsUmj.js";import"./AlertTitle-CogUI_yl.js";import"./Typography-u5-eOh5K.js";import"./useTheme-C2nLQS7p.js";import"./Grow-DdGCipi8.js";import"./index-BIzb42Jq.js";import"./utils-BUDe8ykH.js";import"./ClickAwayListener-D7YV5tOA.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-BN1O7Byi.js";import"./index-pKvUPVqT.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-JnyLG7Y6.js";import"./Skeleton-h9JRD3HJ.js";import"./IconSvg-vbjE1Wpb.js";import"./LayersTwoTone-fKfzxiPS.js";import"./ErrorOutlined-BKx_L9lY.js";import"./GetAppTwoTone-CD3TgP2w.js";import"./InfoOutlined-DpeT4h8X.js";import"./DeleteTwoTone-XAGzVgvj.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./FormControlLabel-3d74PZ9S.js";import"./useFormControl-CQLCXd-P.js";import"./Checkbox-S_rxB3Vg.js";import"./Radio-BoruoOY9.js";import"./createChainedFunction-BO_9K8Jh.js";import"./TextField-DFnzd7X6.js";import"./InputLabel-BUvLjrbW.js";import"./inputBaseClasses-CTPjn8Z_.js";import"./ownerWindow-BN2rbQ_G.js";import"./isMuiElement-DAcuSkv2.js";import"./Menu-DMvSCpxN.js";import"./Popover-CAXmv72_.js";import"./Modal-huZDyz-p.js";import"./Backdrop-BwhHnj0X.js";import"./Fade-C_8BNI_I.js";import"./getScrollbarSize-Bqq2hMjQ.js";import"./MenuList-GXSeXcBC.js";import"./List-DXimupJH.js";import"./Input-l5O8toY5.js";import"./MenuItem-gDt7UEKw.js";import"./dividerClasses-C4eIAzo0.js";import"./Collapse-Cd9-krBm.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";const et={title:"Explore/Components/Facets/EnumFacetFilter",component:M,decorators:[e=>m.jsx(L,{sx:{p:4,maxWidth:"400px"},children:m.jsx(e,{})})],args:{onAddValueToSelection:a(),onRemoveValueFromSelection:a(),onRemoveAllFacetSelections:a(),onHoverOverValue:a()}},o=[{value:N,displayText:E,count:150,isSelected:!1},{value:"foo",displayText:"foo",count:100,isSelected:!1},{value:"bar",displayText:"bar",count:50,isSelected:!1},{value:"baz",displayText:"baz",count:25,isSelected:!1},{value:"veryLongValue",displayText:"Very_long-text_that-will_overflow_the_sidebar_if_not-handled-correctly",count:525600,isSelected:!1},{value:"veryLongValue2",displayText:"string_with_underscores_that_will_overflow_if_not_handled_correctly",count:42,isSelected:!1},{value:"veryLongValue3",displayText:"a-long-string-with-hyphens-that-will-overflow-if-not-handled-correctly",count:75,isSelected:!1},{value:"value1",displayText:"value1",count:10,isSelected:!1},{value:"value2",displayText:"value2",count:9,isSelected:!1},{value:"value3",displayText:"value3",count:8,isSelected:!1},{value:"value4",displayText:"value4",count:7,isSelected:!1},{value:"value5",displayText:"value5",count:6,isSelected:!1},{value:"value6",displayText:"value6",count:5,isSelected:!1},{value:"value7",displayText:"value7",count:4,isSelected:!1},{value:"value8",displayText:"value8",count:3,isSelected:!1}],l={args:{filterIsActive:!1,facetTitle:"Column Named Foo",facetValues:t(o).map(e=>({...e,isSelected:!1})),containerAs:"Collapsible",dropdownType:"Icon",canMultiSelect:!0}},r={args:{filterIsActive:!0,facetValues:t(o).map((e,p)=>({...e,isSelected:p<3})),facetTitle:"Column Named Foo",containerAs:"Collapsible",dropdownType:"Icon",canMultiSelect:!0}},n={args:{filterIsActive:!1,facetValues:t(o).map(e=>({...e,isSelected:!1})),facetTitle:"Column Named Foo",containerAs:"Dropdown",dropdownType:"Icon",canMultiSelect:!0}},s={args:{filterIsActive:!1,facetValues:t(o).map(e=>({...e,isSelected:!1})),facetTitle:"Column Named Foo",containerAs:"Dropdown",dropdownType:"SelectBox",canMultiSelect:!0}},i={args:{filterIsActive:!0,facetTitle:"Column Named Foo",facetValues:t(o).map((e,p)=>({...e,isSelected:p==2})),containerAs:"Collapsible",dropdownType:"Icon",canMultiSelect:!1}},c={render:e=>m.jsx(b,{...e}),args:{containerAs:"Collapsible",dropdownType:"Icon"}};var u,d,f;l.parameters={...l.parameters,docs:{...(u=l.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    filterIsActive: false,
+    facetTitle: 'Column Named Foo',
+    facetValues: cloneDeep(facetValues).map(facetValue => ({
+      ...facetValue,
+      isSelected: false
+    })),
+    containerAs: 'Collapsible',
+    dropdownType: 'Icon',
+    canMultiSelect: true
+  }
+}`,...(f=(d=l.parameters)==null?void 0:d.docs)==null?void 0:f.source}}};var S,v,T;r.parameters={...r.parameters,docs:{...(S=r.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    filterIsActive: true,
+    facetValues: cloneDeep(facetValues).map((facetValue, i) => ({
+      ...facetValue,
+      isSelected: i < 3
+    })),
+    facetTitle: 'Column Named Foo',
+    containerAs: 'Collapsible',
+    dropdownType: 'Icon',
+    canMultiSelect: true
+  }
+}`,...(T=(v=r.parameters)==null?void 0:v.docs)==null?void 0:T.source}}};var V,y,g;n.parameters={...n.parameters,docs:{...(V=n.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  args: {
+    filterIsActive: false,
+    facetValues: cloneDeep(facetValues).map(facetValue => ({
+      ...facetValue,
+      isSelected: false
+    })),
+    facetTitle: 'Column Named Foo',
+    containerAs: 'Dropdown',
+    dropdownType: 'Icon',
+    canMultiSelect: true
+  }
+}`,...(g=(y=n.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};var x,w,A;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    filterIsActive: false,
+    facetValues: cloneDeep(facetValues).map(facetValue => ({
+      ...facetValue,
+      isSelected: false
+    })),
+    facetTitle: 'Column Named Foo',
+    containerAs: 'Dropdown',
+    dropdownType: 'SelectBox',
+    canMultiSelect: true
+  }
+}`,...(A=(w=s.parameters)==null?void 0:w.docs)==null?void 0:A.source}}};var I,F,_;i.parameters={...i.parameters,docs:{...(I=i.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    filterIsActive: true,
+    facetTitle: 'Column Named Foo',
+    facetValues: cloneDeep(facetValues).map((facetValue, i) => ({
+      ...facetValue,
+      isSelected: i == 2
+    })),
+    containerAs: 'Collapsible',
+    dropdownType: 'Icon',
+    canMultiSelect: false
+  }
+}`,...(_=(F=i.parameters)==null?void 0:F.docs)==null?void 0:_.source}}};var C,h,D;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: args => <EnumFacetFilterSkeleton {...args} />,
+  args: {
+    containerAs: 'Collapsible',
+    dropdownType: 'Icon'
+  }
+}`,...(D=(h=c.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};const tt=["PanelNoneSelected","PanelWithSelections","IconDropdown","MenuDropdown","SingleSelectRadioButtons","Skeleton"];export{n as IconDropdown,s as MenuDropdown,l as PanelNoneSelected,r as PanelWithSelections,i as SingleSelectRadioButtons,c as Skeleton,tt as __namedExportsOrder,et as default};
