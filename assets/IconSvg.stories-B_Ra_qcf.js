806fd91ac3d0ac7ca346513d08abdc145ce28669
lakikowolfe <loren.wolfe@sagebase.org>
2025-04-11 19:00:13 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@d4697ceb0dba1c753e0d859e1440726da5a59989 🚀

---
+++ assets/IconSvg.stories-B_Ra_qcf.js
@@ -0,0 +1,8 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{a as s,I as a}from"./IconSvg-DDKWfwZf.js";import"./index-Dl6G-zuu.js";import"./createSvgIcon-IuIKk-AJ.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./spreadSx-CwcO6WA9.js";import"./useTheme-C2nLQS7p.js";import"./Tooltip-DghXNkD2.js";import"./index-pKvUPVqT.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-BjId_ape.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./index-BIzb42Jq.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./utils-nOa-7XVu.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useTimeout-DgmtfV4_.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./LayersTwoTone-fKfzxiPS.js";import"./ErrorOutlined-BKx_L9lY.js";import"./GetAppTwoTone-CD3TgP2w.js";import"./InfoOutlined-DpeT4h8X.js";import"./DeleteTwoTone-XAGzVgvj.js";import"./CheckCircleTwoTone-CDfc8b31.js";const G={title:"UI/IconSvg",render:e=>r.jsx("div",{style:{display:"flex",flexWrap:"wrap",flexDirection:"row"},children:[...s].sort().map(t=>r.jsxs("div",{style:{margin:"10px",textAlign:"center"},children:[r.jsx("div",{children:r.jsx(a,{...e,icon:t,label:t})}),r.jsx("div",{style:{fontSize:"10px"},children:t})]},t))})},o={args:{sx:{color:"primary.main"},fontSize:"large"}};var i,m,p;o.parameters={...o.parameters,docs:{...(i=o.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    sx: {
+      color: 'primary.main'
+    },
+    fontSize: 'large'
+  }
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const H=["Icon"];export{o as Icon,H as __namedExportsOrder,G as default};
