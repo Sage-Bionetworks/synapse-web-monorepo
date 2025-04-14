fba50dc173ab5f0cf92cfbd33f9003a34ebb36dd
nickgros <nickgrosenbacher@gmail.com>
2025-04-14 16:36:44 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@4e612a790d80b62e238857ab0c38c9d71791b72f 🚀

---
+++ assets/UserCard.stories-86qLOL2s.js
@@ -0,0 +1,45 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{q as L}from"./useFiles-BFKs8i28.js";import"./VerificationSubmission-CWAFIXEx.js";import"./StringUtils-DBSsSsIU.js";import{k as z,l as D,M as k,m as b,i as h,j}from"./SynapseConstants-WPaa3dXz.js";import"./index-Dl6G-zuu.js";import"./OrientationBanner-C-7EA0fi.js";import{a as c}from"./UserBadge-dZ5OtnJP.js";import{B as v}from"./Box-sTghsUmj.js";import"./createSvgIcon-IuIKk-AJ.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-Cctz1ZmX.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./calculateFriendlyFileSize-BBeMI-i_.js";import"./Skeleton-h9JRD3HJ.js";import"./inputBaseClasses-CTPjn8Z_.js";import"./Fade-Cxdmh2eZ.js";import"./useTheme-C2nLQS7p.js";import"./utils-nOa-7XVu.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./index-BIzb42Jq.js";import"./useForkRef-BDoLG09A.js";import"./index-pKvUPVqT.js";import"./utils-Bx1BRerw.js";import"./FullWidthAlert-BW-IV7Y-.js";import"./spreadSx-CwcO6WA9.js";import"./Alert-By3H5ehm.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./Paper-Dap2c6xL.js";import"./IconButton-xnDYVMXw.js";import"./ButtonBase-BN-dALdo.js";import"./useTimeout-DgmtfV4_.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-CMgji9n-.js";import"./AlertTitle-C14X1--E.js";import"./Typography-u5-eOh5K.js";import"./Grow-BjId_ape.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-DghXNkD2.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-mTo7xxsU.js";import"./useQuery-BiN9FESP.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-CT__oUM4.js";import"./Link-CWXcK22Y.js";import"./Collapse-CmkIepef.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-B5GX6HSq.js";import"./merge-6t4vjKrJ.js";import"./_isIterateeCall-FuixZKJ7.js";import"./index-CZCSLsz_.js";import"./Popover-dS6E1zFr.js";import"./Modal-BtUDilAu.js";import"./Backdrop-80n8Lf7R.js";import"./getScrollbarSize-Bqq2hMjQ.js";import"./ownerWindow-BN2rbQ_G.js";import"./createChainedFunction-BO_9K8Jh.js";import"./IconSvg-CDfFVDM_.js";import"./LayersTwoTone-fKfzxiPS.js";import"./ErrorOutlined-BKx_L9lY.js";import"./GetAppTwoTone-CD3TgP2w.js";import"./InfoOutlined-DpeT4h8X.js";import"./DeleteTwoTone-XAGzVgvj.js";import"./SkeletonTable-B9cByFTb.js";import"./times-DslHYRns.js";import"./toInteger-2vkq8YOc.js";import"./isSymbol-VqtJwUUo.js";import"./ToastMessage-BXb1aV_x.js";import"./CSSTransition-D3zyGi8F.js";import"./uniqueId-CNPmaLAy.js";import"./toString-C-tSuG3i.js";import"./Menu-B1TgkTrV.js";import"./MenuList-BC6o5nhv.js";import"./List-DXimupJH.js";import"./Divider-BaVRBdbx.js";import"./dividerClasses-C4eIAzo0.js";import"./MenuItem-CWmy7e8c.js";import"./Card-CnbpaPrh.js";import"./Avatar-C5D2-eAC.js";const So={title:"UI/UserCard",component:c,render:function(n){const{data:a}=L();let p=a==null?void 0:a.ownerId;return p===j.toString()&&(p=void 0),r.jsxs(r.Fragment,{children:[r.jsx("p",{children:"If you are logged in, your avatar or card will appear. If you are not logged in, enter an ownerId (e.g. 273960) or alias (e.g. brucehoff) below."}),r.jsx(c,{ownerId:n.ownerId??p,...n})]})}},o={args:{size:z,avatarSize:"SMALL"}},t={args:{size:D}},i={args:{size:k}},e={decorators:[s=>r.jsx(v,{maxWidth:"450px",children:r.jsx(s,{})})],args:{size:k,menuActions:[{field:"Action 1",callback:()=>{console.log("Action 1 clicked")}},{field:b},{field:"Action 2",callback:()=>{console.log("Action 2 clicked")}},{field:"Action 3",callback:()=>{console.log("Action 3 clicked")}}]}},m={args:{size:h}};var d,l,A;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: AVATAR,
+    avatarSize: 'SMALL'
+  }
+}`,...(A=(l=o.parameters)==null?void 0:l.docs)==null?void 0:A.source}}};var u,g,S;t.parameters={...t.parameters,docs:{...(u=t.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: SMALL_USER_CARD
+  }
+}`,...(S=(g=t.parameters)==null?void 0:g.docs)==null?void 0:S.source}}};var R,U,f;i.parameters={...i.parameters,docs:{...(R=i.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    size: MEDIUM_USER_CARD
+  }
+}`,...(f=(U=i.parameters)==null?void 0:U.docs)==null?void 0:f.source}}};var C,E,_;e.parameters={...e.parameters,docs:{...(C=e.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  decorators: [Story => {
+    return <Box maxWidth={'450px'}>
+          <Story />
+        </Box>;
+  }],
+  args: {
+    size: MEDIUM_USER_CARD,
+    menuActions: [{
+      field: 'Action 1',
+      callback: () => {
+        console.log('Action 1 clicked');
+      }
+    }, {
+      field: SEPERATOR
+    }, {
+      field: 'Action 2',
+      callback: () => {
+        console.log('Action 2 clicked');
+      }
+    }, {
+      field: 'Action 3',
+      callback: () => {
+        console.log('Action 3 clicked');
+      }
+    }]
+  }
+}`,...(_=(E=e.parameters)==null?void 0:E.docs)==null?void 0:_.source}}};var x,M,I;m.parameters={...m.parameters,docs:{...(x=m.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    size: LARGE_USER_CARD
+  }
+}`,...(I=(M=m.parameters)==null?void 0:M.docs)==null?void 0:I.source}}};const Ro=["Avatar","SmallUserCard","MediumUserCard","MediumUserCardWithActions","LargeUserCard"];export{o as Avatar,m as LargeUserCard,i as MediumUserCard,e as MediumUserCardWithActions,t as SmallUserCard,Ro as __namedExportsOrder,So as default};
