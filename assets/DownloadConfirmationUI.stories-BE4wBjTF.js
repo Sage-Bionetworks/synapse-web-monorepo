f163db39e9b0dbadd854a99c9b7a01745ed81ac6
jay-hodgson <jay.hodgson@sagebase.org>
2025-04-15 18:53:41 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@41f84e725ceda9530b194358493e47db8e1d50a2 🚀

---
+++ assets/DownloadConfirmationUI.stories-BE4wBjTF.js
@@ -0,0 +1,41 @@
+import{D as w}from"./DownloadConfirmationUI-Bc0KSjRO.js";import{f as a}from"./index-SqWy7oMY.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./useFiles-Bo-B-1VH.js";import"./VerificationSubmission-CWAFIXEx.js";import"./SynapseConstants-WPaa3dXz.js";import"./OrientationBanner-C-7EA0fi.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-BW-IV7Y-.js";import"./Alert-By3H5ehm.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-IuIKk-AJ.js";import"./Paper-Dap2c6xL.js";import"./IconButton-xnDYVMXw.js";import"./ButtonBase-BN-dALdo.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-CMgji9n-.js";import"./Box-sTghsUmj.js";import"./AlertTitle-C14X1--E.js";import"./Typography-u5-eOh5K.js";import"./useTheme-C2nLQS7p.js";import"./Grow-BjId_ape.js";import"./index-BIzb42Jq.js";import"./utils-nOa-7XVu.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-DghXNkD2.js";import"./index-pKvUPVqT.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-mTo7xxsU.js";import"./StringUtils-DBSsSsIU.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-Cctz1ZmX.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./calculateFriendlyFileSize-BBeMI-i_.js";import"./Skeleton-h9JRD3HJ.js";import"./inputBaseClasses-CTPjn8Z_.js";import"./Fade-Cxdmh2eZ.js";import"./utils-Bx1BRerw.js";import"./useQuery-BiN9FESP.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-CT__oUM4.js";import"./Link-CWXcK22Y.js";import"./Collapse-CmkIepef.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-B5GX6HSq.js";import"./merge-6t4vjKrJ.js";import"./_isIterateeCall-FuixZKJ7.js";import"./SynapseTableConstants-Bfds70xL.js";import"./useMutation-DjwfMZ6R.js";import"./useInfiniteQuery-80GXSAYy.js";import"./SkeletonInlineBlock-D4fNjiAE.js";import"./LayersTwoTone-fKfzxiPS.js";const Ot={title:"Download/DownloadConfirmationUI",component:w,tags:["autodocs"],argTypes:{isAuthenticated:{type:"boolean"}},args:{isAuthenticated:!0,onAddToDownloadCart:a(),onCancel:a()}},t={args:{isAuthenticated:!0,fileCount:1e4,fileSize:1e9,isLoadingStats:!1,isAddingToDownloadCart:!1}},i={args:{isAuthenticated:!0,fileCount:1e4,fileSize:void 0,isLoadingStats:!1,isAddingToDownloadCart:!1}},o={args:{isAuthenticated:!0,fileCount:0,fileSize:void 0,isLoadingStats:!0,isAddingToDownloadCart:!1}},r={args:{isAuthenticated:!0,fileCount:1e4,fileSize:void 0,isLoadingStats:!1,isAddingToDownloadCart:!0}},e={args:{isAuthenticated:!1,fileCount:1e4,fileSize:void 0,isLoadingStats:!1,isAddingToDownloadCart:!1}};var n,s,m;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: true,
+    fileCount: 10_000,
+    fileSize: 1_000_000_000,
+    isLoadingStats: false,
+    isAddingToDownloadCart: false
+  }
+}`,...(m=(s=t.parameters)==null?void 0:s.docs)==null?void 0:m.source}}};var d,p,l;i.parameters={...i.parameters,docs:{...(d=i.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: true,
+    fileCount: 10_000,
+    fileSize: undefined,
+    isLoadingStats: false,
+    isAddingToDownloadCart: false
+  }
+}`,...(l=(p=i.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var u,c,f;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: true,
+    fileCount: 0,
+    fileSize: undefined,
+    isLoadingStats: true,
+    isAddingToDownloadCart: false
+  }
+}`,...(f=(c=o.parameters)==null?void 0:c.docs)==null?void 0:f.source}}};var g,S,C;r.parameters={...r.parameters,docs:{...(g=r.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: true,
+    fileCount: 10_000,
+    fileSize: undefined,
+    isLoadingStats: false,
+    isAddingToDownloadCart: true
+  }
+}`,...(C=(S=r.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var A,h,D;e.parameters={...e.parameters,docs:{...(A=e.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    isAuthenticated: false,
+    fileCount: 10_000,
+    fileSize: undefined,
+    isLoadingStats: false,
+    isAddingToDownloadCart: false
+  }
+}`,...(D=(h=e.parameters)==null?void 0:h.docs)==null?void 0:D.source}}};const jt=["ConfirmationWithFileSize","ConfirmationWithoutFileSize","LoadingStats","AddingToCart","Unauthenticated"];export{r as AddingToCart,t as ConfirmationWithFileSize,i as ConfirmationWithoutFileSize,o as LoadingStats,e as Unauthenticated,jt as __namedExportsOrder,Ot as default};
