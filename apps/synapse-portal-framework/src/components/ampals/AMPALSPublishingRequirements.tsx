f6e9984eaffdf7ecd30917571987defd4735fc44
“kianamcc” <user.email=kianamccu@gmail.com>
2025-04-15 11:22:03 -0700

    publishing requirements slat, form link and resources page link unavailable

---
+++ apps/synapse-portal-framework/src/components/ampals/AMPALSPublishingRequirements.tsx
@@ -0,0 +1,53 @@
+import { CTASectionWrapper, PortalSectionHeader } from 'synapse-react-client'
+import { Link } from '@mui/material'
+
+function AMPALSPublishingRequirements() {
+  const content = (
+    <PortalSectionHeader
+      centered
+      reverseButtonAndText
+      title="Publishing Requirements"
+      summaryText={
+        <>
+          If you use AMP-ALS Knowledge Portal data in your manuscript, you must
+          first submit it to us for review. Use our {''}
+          <Link
+            href="https://example.com/path-to-publication-review-form"
+            sx={{
+              color: '#FFF !important',
+              whiteSpace: 'nowrap',
+              '&:hover': { textDecorationColor: '#FFF' },
+            }}
+          >
+            publication review form
+          </Link>{' '}
+          to send your manuscript."
+        </>
+      }
+      buttonText="Learn more"
+      link="Resources/Publishing Requirements"
+      sx={{
+        h2: {
+          color: 'primary.contrastText',
+          fontSize: '24px',
+          fontWeight: 600,
+          borderColor: 'rgba(255, 255, 255, 0.40)',
+        },
+        '.MuiButton-root': {
+          border: '1px solid rgba(255, 255, 255, 0.50)',
+          boxShadow: 'none',
+        },
+        '& p': {
+          fontSize: '16px',
+          paddingBottom: '10px',
+          color: 'primary.contrastText',
+          width: '480px',
+          lineHeight: '22.4px',
+        },
+      }}
+    />
+  )
+  return <CTASectionWrapper content={content} themeMode="dark" />
+}
+
+export default AMPALSPublishingRequirements
