b463e209f48aa8c8be0ffef1d86ca24497a8a7c8
jay-hodgson <jay.hodgson@sagebase.org>
2025-04-16 16:14:28 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@b1cced62c9b57e6d0fb2fae13d4590f7977ed2a7 🚀

---
+++ assets/ErrorBanner.stories-KFlyQRDx.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{H as g,Q as S}from"./useFiles-BFKs8i28.js";import"./index-Dl6G-zuu.js";import"./VerificationSubmission-CWAFIXEx.js";import"./SynapseConstants-WPaa3dXz.js";import"./OrientationBanner-C-7EA0fi.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-BW-IV7Y-.js";import"./Alert-By3H5ehm.js";import"./createTheme-DCAEEeQ9.js";import"./index-GEGPABih.js";import"./styled-GqwmQbHR.js";import"./mergeSlotProps-D-cfru4O.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-IuIKk-AJ.js";import"./Paper-Dap2c6xL.js";import"./IconButton-xnDYVMXw.js";import"./ButtonBase-BN-dALdo.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-CMgji9n-.js";import"./Box-sTghsUmj.js";import"./AlertTitle-C14X1--E.js";import"./Typography-u5-eOh5K.js";import"./useTheme-C2nLQS7p.js";import"./Grow-BjId_ape.js";import"./index-BIzb42Jq.js";import"./utils-nOa-7XVu.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-DghXNkD2.js";import"./index-pKvUPVqT.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-mTo7xxsU.js";import"./StringUtils-DBSsSsIU.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-Cctz1ZmX.js";import"./CheckCircleTwoTone-CDfc8b31.js";import"./calculateFriendlyFileSize-BBeMI-i_.js";import"./Skeleton-h9JRD3HJ.js";import"./inputBaseClasses-CTPjn8Z_.js";import"./Fade-Cxdmh2eZ.js";import"./utils-Bx1BRerw.js";import"./useQuery-BiN9FESP.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-CT__oUM4.js";import"./Link-CWXcK22Y.js";import"./Collapse-CmkIepef.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-B5GX6HSq.js";import"./merge-6t4vjKrJ.js";import"./_isIterateeCall-FuixZKJ7.js";const Cr={title:"UI/ErrorBanner",component:g},o={decorators:[u=>r.jsxs(r.Fragment,{children:[r.jsx("div",{children:"Note: login to see the unauthorized SynapseClientError."}),r.jsx(u,{})]})],args:{error:new S(403,"Some message about why access is not authorized.","some-url")}},t={args:{error:"Some error message",reloadButtonFn:()=>console.log("reload!")}},e={args:{error:new Error("Submission does not exist"),reloadButtonFn:()=>console.log("reload!")}};var m,i,s;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  decorators: [Story => <>
+        <div>Note: login to see the unauthorized SynapseClientError.</div>
+        <Story />
+      </>],
+  args: {
+    error: new SynapseClientError(403, 'Some message about why access is not authorized.', 'some-url')
+  }
+}`,...(s=(i=o.parameters)==null?void 0:i.docs)==null?void 0:s.source}}};var p,n,a;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    error: 'Some error message',
+    reloadButtonFn: () => console.log('reload!')
+  }
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var c,d,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    error: new Error('Submission does not exist'),
+    reloadButtonFn: () => console.log('reload!')
+  }
+}`,...(l=(d=e.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const Dr=["SynapseClientErrorDemo","StringDemo","ErrorDemo"];export{e as ErrorDemo,t as StringDemo,o as SynapseClientErrorDemo,Dr as __namedExportsOrder,Cr as default};
