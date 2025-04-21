3942254e1a1c0d267774adfa8631866ef5300bcd
jay-hodgson <jay.hodgson@sagebase.org>
2025-04-21 18:19:07 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@f3a42f4357fb71dfdd67548ec5f25f9b6c51293a 🚀

---
+++ assets/mockWikiPageKey-BDge-eD9.js
@@ -0,0 +1,1 @@
+import{O as e}from"./VerificationSubmission-CWAFIXEx.js";import{M as o}from"./mockFileEntity-BjfGiVfJ.js";import{d as t,e as a}from"./mockWiki-B78ox-lj.js";import{h as m}from"./mockAccessRequirements-C-gipOWb.js";const i={ownerObjectType:e.ENTITY,ownerObjectId:o,wikiPageId:t.id},s={...i,wikiPageId:a.id},y=[...m,i,s];export{s as a,y as b,i as m};
