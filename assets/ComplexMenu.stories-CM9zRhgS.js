c68bf26e3b2f1d158a16a6afd0ae26d4ebdc6ee3
lakikowolfe <loren.wolfe@sagebase.org>
2025-04-23 17:46:25 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@99c4fc33fd5b167371412f53f81e38fe04129679 🚀

---
+++ assets/ComplexMenu.stories-CM9zRhgS.js
@@ -0,0 +1,68 @@
+import{j as i}from"./jsx-runtime-Du8NFWEI.js";import{I as n}from"./IconSvg-DFKqE61C.js";import{C as a}from"./ComplexMenu-DnajOq0o.js";import"./index-Dl6G-zuu.js";import"./createSvgIcon-Cg71VSX1.js";import"./createTheme-BmUnpyjI.js";import"./index-GEGPABih.js";import"./styled-_IoEwjFQ.js";import"./spreadSx-CwcO6WA9.js";import"./useTheme-CxBmLnxz.js";import"./Tooltip-CbFhYBpv.js";import"./index-BByOA_q1.js";import"./Popper-BYhjzrIk.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-BJQmYc4X.js";import"./mergeSlotProps-D1c34xqb.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./index-BIzb42Jq.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./utils-Kl3ltpPj.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useTimeout-DgmtfV4_.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./LayersTwoTone-Bqhxg48d.js";import"./ErrorOutlined-DsIBW_UX.js";import"./GetAppTwoTone-LEv6oiyS.js";import"./InfoOutlined-CP7XpE9b.js";import"./DeleteTwoTone-46LZBzJE.js";import"./CheckCircleTwoTone-S1ZFwL3C.js";import"./DropdownMenu-SDGpdvgL.js";import"./Button-BkKUhIiJ.js";import"./ButtonBase-QEdCuq5m.js";import"./Typography-DUswyeAh.js";import"./Fade-DL1c9j4A.js";import"./Paper-sJzUMfPp.js";import"./MenuList-DcMoCfbK.js";import"./getScrollbarSize-Bqq2hMjQ.js";import"./List-CuEeKtjj.js";import"./MenuItem-BJNXNXXI.js";import"./dividerClasses-kfvcuVxh.js";import"./ListItemText-BiQbcWDf.js";import"./Divider-DZif6ozR.js";import"./ClickAwayListener-DMjw0JVT.js";import"./IconSvgButton-D6oxpugW.js";import"./IconButton-CxrGCLVD.js";import"./useMediaQuery-CMqr6Hlp.js";import"./Box-Tu95KWeq.js";const ro={title:"UI/ComplexMenu",component:a},o=s=>()=>{console.log("Item clicked",s)},t={args:{iconButtons:[{icon:"edit",tooltipText:"This is an icon button",onClick:o("edit icon")},{icon:"label",tooltipText:"View annotations",onClick:o("label icon")},{icon:"createVersion",tooltipText:"Create new version",onClick:o("createVersion icon")}],dropdownMenus:[{dropdownButtonText:"You can have...",buttonProps:{endIcon:i.jsx(n,{icon:"upload",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]},{dropdownButtonText:"...multiple dropdown menus!",buttonProps:{endIcon:i.jsx(n,{icon:"download",wrap:!1})},items:[[{text:"Do something cool",onClick:o(0)},{text:"This one has a tooltip",tooltipText:"Some more info",onClick:o(1)},{text:"This one is disabled",disabled:!0,onClick:o(3)},{text:"This one has both",disabled:!0,tooltipText:"You can't do this for reasons",onClick:o(4)}],[{text:"You can organize actions into groups",onClick:o(5)}]]}]}};var e,r,l;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    iconButtons: [{
+      icon: 'edit',
+      tooltipText: 'This is an icon button',
+      onClick: onClickHandler('edit icon')
+    }, {
+      icon: 'label',
+      tooltipText: 'View annotations',
+      onClick: onClickHandler('label icon')
+    }, {
+      icon: 'createVersion',
+      tooltipText: 'Create new version',
+      onClick: onClickHandler('createVersion icon')
+    }],
+    dropdownMenus: [{
+      dropdownButtonText: 'You can have...',
+      buttonProps: {
+        endIcon: <IconSvg icon="upload" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }, {
+      dropdownButtonText: '...multiple dropdown menus!',
+      buttonProps: {
+        endIcon: <IconSvg icon="download" wrap={false} />
+      },
+      items: [[{
+        text: 'Do something cool',
+        onClick: onClickHandler(0)
+      }, {
+        text: 'This one has a tooltip',
+        tooltipText: 'Some more info',
+        onClick: onClickHandler(1)
+      }, {
+        text: 'This one is disabled',
+        disabled: true,
+        onClick: onClickHandler(3)
+      }, {
+        text: 'This one has both',
+        disabled: true,
+        tooltipText: "You can't do this for reasons",
+        onClick: onClickHandler(4)
+      }], [{
+        text: 'You can organize actions into groups',
+        onClick: onClickHandler(5)
+      }]]
+    }]
+  }
+}`,...(l=(r=t.parameters)==null?void 0:r.docs)==null?void 0:l.source}}};const lo=["Demo"];export{t as Demo,lo as __namedExportsOrder,ro as default};
---
+++ assets/EntityPageBreadcrumb.stories-BR9LTAcZ.js
@@ -0,0 +1,61 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{B as u}from"./Breadcrumbs-Dsis0GHf.js";import"./index-Dl6G-zuu.js";import"./IconSvg-DFKqE61C.js";import"./createSvgIcon-Cg71VSX1.js";import"./createTheme-BmUnpyjI.js";import"./index-GEGPABih.js";import"./styled-_IoEwjFQ.js";import"./spreadSx-CwcO6WA9.js";import"./useTheme-CxBmLnxz.js";import"./Tooltip-CbFhYBpv.js";import"./index-BByOA_q1.js";import"./Popper-BYhjzrIk.js";import"./ownerDocument-DW-IO8s5.js";import"./Grow-BJQmYc4X.js";import"./mergeSlotProps-D1c34xqb.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./index-BIzb42Jq.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./utils-Kl3ltpPj.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useTimeout-DgmtfV4_.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./LayersTwoTone-Bqhxg48d.js";import"./ErrorOutlined-DsIBW_UX.js";import"./GetAppTwoTone-LEv6oiyS.js";import"./InfoOutlined-CP7XpE9b.js";import"./DeleteTwoTone-46LZBzJE.js";import"./CheckCircleTwoTone-S1ZFwL3C.js";import"./StringUtils-DBSsSsIU.js";import"./VerificationSubmission-CWAFIXEx.js";import"./chunk-K6AXKMTT-C6FuPAdF.js";import"./Breadcrumbs-DCNjccyk.js";import"./ButtonBase-QEdCuq5m.js";import"./Typography-DUswyeAh.js";import"./Link-D-YR7sBc.js";const x=32,f={marginTop:"0px",padding:"10px 40px",backgroundColor:"grey.200","& .MuiLink-root":{color:"text.secondary",letterSpacing:0,"&:visited":{color:"text.secondary"}},"& .MuiSvgIcon-root":{color:"grey.500"}};function a(t){const{items:d}=t;return r.jsx(u,{items:d,sx:f,maxBreadcrumbLength:x})}try{a.displayName="EntityPageBreadcrumbs",a.__docgenInfo={description:"",displayName:"EntityPageBreadcrumbs",props:{items:{defaultValue:null,description:"",name:"items",required:!0,type:{name:"BreadcrumbItem[]"}},sx:{defaultValue:null,description:"",name:"sx",required:!1,type:{name:"SxProps"}}}}}catch{}const te={title:"Synapse/EntityPage/Breadcrumb",component:a,render:t=>r.jsxs(r.Fragment,{children:[r.jsx("br",{}),r.jsx("br",{}),r.jsx(a,{items:[],...t})]})},e=t=>{t.preventDefault()},n={args:{items:[{onClick:e,text:"Files",href:"#area"},{onClick:e,text:"Root folder",href:"Synapse:syn123"},{onClick:e,text:"A very long subfolder name that gets truncated",href:"Synapse:syn456"},{onClick:e,text:"The file you are looking at",href:"Synapse:syn789",current:!0}]}},o={args:{items:[{onClick:e,text:"Files",href:"#area"},{onClick:e,text:"Folder A"},{onClick:e,text:"Folder B"},{onClick:e,text:"Folder C"},{onClick:e,text:"Folder D"},{onClick:e,text:"Folder E"},{onClick:e,text:"Folder F"},{onClick:e,text:"Folder G"},{onClick:e,text:"Folder H"},{onClick:e,text:"Folder I"},{onClick:e,text:"Leaf file",current:!0}]}};var l,i,p;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      onClick: preventDefaultHandler,
+      text: 'Files',
+      href: '#area'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Root folder',
+      href: 'Synapse:syn123'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'A very long subfolder name that gets truncated',
+      href: 'Synapse:syn456'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'The file you are looking at',
+      href: 'Synapse:syn789',
+      current: true
+    }]
+  }
+}`,...(p=(i=n.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var s,m,c;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    items: [{
+      onClick: preventDefaultHandler,
+      text: 'Files',
+      href: '#area'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder A'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder B'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder C'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder D'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder E'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder F'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder G'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder H'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Folder I'
+    }, {
+      onClick: preventDefaultHandler,
+      text: 'Leaf file',
+      current: true
+    }]
+  }
+}`,...(c=(m=o.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const re=["Demo","ManyItems"];export{n as Demo,o as ManyItems,re as __namedExportsOrder,te as default};
