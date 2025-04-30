c500657970209395daccc82c083b686df2f2019d
kianamcc <42532725+kianamcc@users.noreply.github.com>
2025-04-30 18:35:23 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@643bab337ea03fdd08f9a207535ce928f167bc28 🚀

--- assets/mockWikiPageKey-DEhvz7Ut.js
+++
@@ -1,1 +0,0 @@
-import{O as e}from"./VerificationSubmission-BUSqAuLu.js";import{M as o}from"./mockFileEntity-Cbrg5Wpv.js";import{d as t,e as a}from"./mockWiki-BhBIsQCe.js";import{h as m}from"./mockAccessRequirements-zrx3YRiE.js";const i={ownerObjectType:e.ENTITY,ownerObjectId:o,wikiPageId:t.id},s={...i,wikiPageId:a.id},y=[...m,i,s];export{s as a,y as b,i as m};
