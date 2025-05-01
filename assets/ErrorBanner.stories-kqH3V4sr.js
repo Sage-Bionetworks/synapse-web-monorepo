30bf4621a4f7ab86d14f0a04b6db24f5cce4b86d
nickgros <nickgrosenbacher@gmail.com>
2025-05-01 13:51:38 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@7e33537e5193b54803badb74340f0d8f899abaa4 🚀

---
+++ assets/ErrorBanner.stories-kqH3V4sr.js
@@ -0,0 +1,19 @@
+import{j as r}from"./jsx-runtime-Du8NFWEI.js";import{H as g}from"./useFiles-CxkCE0ws.js";import{S}from"./fetchWithExponentialTimeout-Cpvpsfq8.js";import"./index-Dl6G-zuu.js";import"./VerificationSubmission-BUSqAuLu.js";import"./SynapseConstants-Dk0Mz-dJ.js";import"./OrientationBanner-BOCf4qOE.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-B_rXn9qX.js";import"./Alert-u0n1NA7e.js";import"./createTheme-BmUnpyjI.js";import"./index-GEGPABih.js";import"./styled-_IoEwjFQ.js";import"./mergeSlotProps-D1c34xqb.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-Cg71VSX1.js";import"./Close-DdzXEaRM.js";import"./Paper-sJzUMfPp.js";import"./IconButton-CxrGCLVD.js";import"./ButtonBase-QEdCuq5m.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-D4Hv4xSj.js";import"./Box-Tu95KWeq.js";import"./AlertTitle-CgrA6e5t.js";import"./Typography-DUswyeAh.js";import"./useTheme-CxBmLnxz.js";import"./Grow-BJQmYc4X.js";import"./index-BIzb42Jq.js";import"./utils-Kl3ltpPj.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-CbFhYBpv.js";import"./index-BByOA_q1.js";import"./Popper-BYhjzrIk.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-BkKUhIiJ.js";import"./StringUtils-2e5lfr-1.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-B7fmYX5S.js";import"./CheckCircleTwoTone-S1ZFwL3C.js";import"./calculateFriendlyFileSize-1hZ7TxWp.js";import"./Skeleton-DiWSb2YA.js";import"./inputBaseClasses-Bc0CX7u-.js";import"./Fade-DL1c9j4A.js";import"./utils-DPkdG3Lw.js";import"./mutation-SJTTtyND.js";import"./useQuery-DFbKL1Tm.js";import"./Link-D-YR7sBc.js";import"./Collapse-z9mI71qO.js";import"./isNil-CFgMzoGa.js";import"./_initCloneObject-BAcHLPB8.js";import"./_baseTimes-M2nZWodf.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-CT__oUM4.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-C6FuPAdF.js";import"./merge-6t4vjKrJ.js";import"./_isIterateeCall-FuixZKJ7.js";const fr={title:"UI/ErrorBanner",component:g},o={decorators:[u=>r.jsxs(r.Fragment,{children:[r.jsx("div",{children:"Note: login to see the unauthorized SynapseClientError."}),r.jsx(u,{})]})],args:{error:new S(403,"Some message about why access is not authorized.","some-url")}},t={args:{error:"Some error message",reloadButtonFn:()=>console.log("reload!")}},e={args:{error:new Error("Submission does not exist"),reloadButtonFn:()=>console.log("reload!")}};var m,i,p;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  decorators: [Story => <>
+        <div>Note: login to see the unauthorized SynapseClientError.</div>
+        <Story />
+      </>],
+  args: {
+    error: new SynapseClientError(403, 'Some message about why access is not authorized.', 'some-url')
+  }
+}`,...(p=(i=o.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var s,n,a;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    error: 'Some error message',
+    reloadButtonFn: () => console.log('reload!')
+  }
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var c,d,l;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    error: new Error('Submission does not exist'),
+    reloadButtonFn: () => console.log('reload!')
+  }
+}`,...(l=(d=e.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};const zr=["SynapseClientErrorDemo","StringDemo","ErrorDemo"];export{e as ErrorDemo,t as StringDemo,o as SynapseClientErrorDemo,zr as __namedExportsOrder,fr as default};
