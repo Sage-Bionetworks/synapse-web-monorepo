c9c263303bd62312ce6696dee68745ba6a5ebc2c
jay-hodgson <jay.hodgson@sagebase.org>
2025-05-02 20:05:59 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@075f7564d6bcc393869f0c753600c9ad03ebdfbd 🚀

---
+++ assets/AccessRequirementRelatedProjectsList.stories-BaVXPh-K.js
@@ -0,0 +1,35 @@
+import{M as j,b as P}from"./useFiles-CRNYnzvZ.js";import{l as I}from"./index-BN0AhjWv.js";import{A as g}from"./AccessRequirementRelatedProjectsList-DHqLYIWI.js";import"./VerificationSubmission-BUSqAuLu.js";import"./SynapseConstants-Dk0Mz-dJ.js";import"./OrientationBanner-BOCf4qOE.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./index-CZCSLsz_.js";import"./spreadSx-CwcO6WA9.js";import"./FullWidthAlert-B_rXn9qX.js";import"./Alert-u0n1NA7e.js";import"./createTheme-BmUnpyjI.js";import"./index-GEGPABih.js";import"./styled-_IoEwjFQ.js";import"./mergeSlotProps-D1c34xqb.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-Cg71VSX1.js";import"./Close-DdzXEaRM.js";import"./Paper-sJzUMfPp.js";import"./IconButton-CxrGCLVD.js";import"./ButtonBase-QEdCuq5m.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-D4Hv4xSj.js";import"./Box-Tu95KWeq.js";import"./AlertTitle-CgrA6e5t.js";import"./Typography-DUswyeAh.js";import"./useTheme-CxBmLnxz.js";import"./Grow-BJQmYc4X.js";import"./index-BIzb42Jq.js";import"./utils-Kl3ltpPj.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-CbFhYBpv.js";import"./index-BByOA_q1.js";import"./Popper-BYhjzrIk.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-BkKUhIiJ.js";import"./fetchWithExponentialTimeout-Cpvpsfq8.js";import"./StringUtils-CCbjCejM.js";import"./tinycolor-Begke6kS.js";import"./InfoTwoTone-B7fmYX5S.js";import"./CheckCircleTwoTone-S1ZFwL3C.js";import"./calculateFriendlyFileSize-1hZ7TxWp.js";import"./Skeleton-DiWSb2YA.js";import"./inputBaseClasses-Bc0CX7u-.js";import"./Fade-DL1c9j4A.js";import"./utils-DPkdG3Lw.js";import"./mutation-SJTTtyND.js";import"./useQuery-DFbKL1Tm.js";import"./Link-D-YR7sBc.js";import"./Collapse-z9mI71qO.js";import"./isNil-c8QyzjdH.js";import"./_initCloneObject-CZ6qv1A1.js";import"./_baseTimes-36S_kd0L.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./cloneDeep-DwbK2PGz.js";import"./dayjs.min-d18Up55D.js";import"./chunk-K6AXKMTT-C6FuPAdF.js";import"./merge-BpojWDP4.js";import"./identity-DKeuBCMA.js";import"./util-BbuHFHEZ.js";import"./useAccessRequirements-S-0IcIlk.js";import"./useMediaQuery-CMqr6Hlp.js";import"./_baseFlatten-CZo0aWQS.js";import"./_baseOrderBy-C2grRRFd.js";import"./toString-C-tSuG3i.js";import"./isSymbol-VqtJwUUo.js";import"./hasIn-DYHo78HQ.js";import"./_baseIteratee-DsVKhaMU.js";import"./_baseEach-DDQVroiY.js";import"./useQueries-xW26vg9E.js";import"./useMutation-DU0JAunR.js";import"./useInfiniteQuery-kUAmfbvr.js";import"./QueryFilterUtils-DnG5KPOk.js";import"./isNumber-BR1aE1rr.js";import"./pluralize-CVwSCrA2.js";import"./_commonjs-dynamic-modules-TDtrdbi3.js";import"./EntityLink-htJ_fQmR.js";import"./useEntity-CvQjXiTg.js";import"./_baseIndexOf-rBnIfvEs.js";import"./pickBy-C-3ExsCj.js";import"./pick-BxC-3Ryr.js";import"./_baseSlice-F8doVSIJ.js";import"./toInteger-kto__FmK.js";import"./InfiniteQueryUtils-CKlRW-xB.js";import"./EntityIcon-CUfqHNlL.js";import"./IconSvg-B29FcmbQ.js";import"./LayersTwoTone-Bqhxg48d.js";import"./ErrorOutlined-DsIBW_UX.js";import"./GetAppTwoTone-LEv6oiyS.js";import"./InfoOutlined-CP7XpE9b.js";import"./DeleteTwoTone-46LZBzJE.js";import"./ErrorChip-NZ25xoDd.js";import"./Chip-ByQPbXSE.js";import"./Avatar-D2kdP2Ev.js";import"./List-CuEeKtjj.js";import"./ListItem-BBWF7PtT.js";import"./isMuiElement-DAcuSkv2.js";const It={title:"Governance/AccessRequirementRelatedProjectsList",component:g},r={args:{accessRequirementId:9603055}},t={args:{accessRequirementId:9605913}},o={args:{accessRequirementId:1234567},parameters:{stack:"mock",msw:{handlers:[I.rest.post(`${j}${P}`,async(A,u,e)=>{const l={results:[{id:"1234567",type:"org.sagebionetworks.repo.model.ManagedACTAccessRequirement",createdOn:"2017-08-23T18:48:20.892Z",modifiedOn:"2023-12-14T00:43:41.315Z",name:"Team AR without Related Projects",version:"1",relatedProjectIds:[],reviewerIds:[]}]};return u(e.status(200),e.json(l))})]}}};var m,i,p;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 9603055
+  }
+}`,...(p=(i=r.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var s,a,n;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 9605913
+  }
+}`,...(n=(a=t.parameters)==null?void 0:a.docs)==null?void 0:n.source}}};var c,d,R;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    accessRequirementId: 1234567
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: [
+      // searchAccessRequirements
+      rest.post(\`\${MOCK_REPO_ORIGIN}\${ACCESS_REQUIREMENT_SEARCH}\`, async (req, res, ctx) => {
+        const zeroRelatedProjects = {
+          results: [{
+            id: '1234567',
+            type: 'org.sagebionetworks.repo.model.ManagedACTAccessRequirement',
+            createdOn: '2017-08-23T18:48:20.892Z',
+            modifiedOn: '2023-12-14T00:43:41.315Z',
+            name: 'Team AR without Related Projects',
+            version: '1',
+            relatedProjectIds: [],
+            reviewerIds: []
+          }]
+        };
+        return res(ctx.status(200), ctx.json(zeroRelatedProjects));
+      })]
+    }
+  }
+}`,...(R=(d=o.parameters)==null?void 0:d.docs)==null?void 0:R.source}}};const gt=["ManyProjects","OneProject","ZeroProjectsMock"];export{r as ManyProjects,t as OneProject,o as ZeroProjectsMock,gt as __namedExportsOrder,It as default};
---
+++ assets/SharePageLinkButton.stories-BODn75em.js
@@ -0,0 +1,16 @@
+import{S as p}from"./SharePageLinkButton-BfbwQJPB.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./useMutation-DU0JAunR.js";import"./mutation-SJTTtyND.js";import"./utils-DPkdG3Lw.js";import"./ToastMessage-CBhzPong.js";import"./FullWidthAlert-B_rXn9qX.js";import"./spreadSx-CwcO6WA9.js";import"./Alert-u0n1NA7e.js";import"./createTheme-BmUnpyjI.js";import"./index-GEGPABih.js";import"./styled-_IoEwjFQ.js";import"./mergeSlotProps-D1c34xqb.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-Cg71VSX1.js";import"./Close-DdzXEaRM.js";import"./Paper-sJzUMfPp.js";import"./IconButton-CxrGCLVD.js";import"./ButtonBase-QEdCuq5m.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-D4Hv4xSj.js";import"./Box-Tu95KWeq.js";import"./AlertTitle-CgrA6e5t.js";import"./Typography-DUswyeAh.js";import"./useTheme-CxBmLnxz.js";import"./Grow-BJQmYc4X.js";import"./index-BIzb42Jq.js";import"./utils-Kl3ltpPj.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-CbFhYBpv.js";import"./index-BByOA_q1.js";import"./Popper-BYhjzrIk.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-BkKUhIiJ.js";import"./CSSTransition-BO_24ZZx.js";import"./uniqueId-CNPmaLAy.js";import"./toString-C-tSuG3i.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./isSymbol-VqtJwUUo.js";import"./IconSvg-B29FcmbQ.js";import"./LayersTwoTone-Bqhxg48d.js";import"./ErrorOutlined-DsIBW_UX.js";import"./GetAppTwoTone-LEv6oiyS.js";import"./InfoOutlined-CP7XpE9b.js";import"./DeleteTwoTone-46LZBzJE.js";import"./CheckCircleTwoTone-S1ZFwL3C.js";import"./StringUtils-CCbjCejM.js";import"./VerificationSubmission-BUSqAuLu.js";const et={title:"UI/SharePageLinkButton",component:p},t={args:{buttonProps:{sx:{position:"fixed",right:"20px"}}},parameters:{stack:"mock",msw:{handlers:[]}}};var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    buttonProps: {
+      sx: {
+        position: 'fixed',
+        right: '20px'
+      }
+    }
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: []
+    }
+  }
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const at=["SharePageLinkButtonStory"];export{t as SharePageLinkButtonStory,at as __namedExportsOrder,et as default};
