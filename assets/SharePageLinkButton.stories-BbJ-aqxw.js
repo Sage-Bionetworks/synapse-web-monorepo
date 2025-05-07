bc24f7d4c00e9d158bc5b54b84c627e4e736c625
kianamcc <42532725+kianamcc@users.noreply.github.com>
2025-05-07 17:29:16 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@19194593332951eabc4585ea3e2aa0d56ce55193 🚀

---
+++ assets/SharePageLinkButton.stories-BbJ-aqxw.js
@@ -0,0 +1,16 @@
+import{S as p}from"./SharePageLinkButton-CTkfN7vO.js";import"./jsx-runtime-Du8NFWEI.js";import"./index-Dl6G-zuu.js";import"./useMutation-DU0JAunR.js";import"./mutation-SJTTtyND.js";import"./utils-DPkdG3Lw.js";import"./ToastMessage-CmCdGkWE.js";import"./FullWidthAlert-CgFm9dfg.js";import"./spreadSx-CwcO6WA9.js";import"./Alert-BoiUXBFA.js";import"./createTheme-BoQYKx5k.js";import"./index-GEGPABih.js";import"./styled-BMGIoXl7.js";import"./mergeSlotProps-D_dLvIWz.js";import"./isHostComponent-DVu5iVWx.js";import"./useForkRef-BDoLG09A.js";import"./createSvgIcon-DrH8-ApS.js";import"./Close-N0BBw2gu.js";import"./Paper-B6Fumrsg.js";import"./IconButton-QXhlbi2Z.js";import"./ButtonBase-CTLL4EY5.js";import"./useTimeout-DgmtfV4_.js";import"./TransitionGroupContext-DetLSwnr.js";import"./useEnhancedEffect-DZnfS5Iy.js";import"./useIsFocusVisible-Bf3pHwKN.js";import"./Stack-BcBUfnl7.js";import"./Box-T6r2Z_yt.js";import"./AlertTitle-DrSQ0G21.js";import"./Typography-D3xYwoES.js";import"./useTheme-B4ZZkoF-.js";import"./Grow-BcJKh-mH.js";import"./index-BIzb42Jq.js";import"./utils-D16j0ClI.js";import"./ClickAwayListener-DMjw0JVT.js";import"./ownerDocument-DW-IO8s5.js";import"./Tooltip-CIKCv17A.js";import"./index-DRltcjjn.js";import"./Popper-YUaVwGty.js";import"./useControlled-1Y2rT-1r.js";import"./useId-BkqTTtmk.js";import"./Button-CgNBwHku.js";import"./CSSTransition-CRPvtSQ6.js";import"./uniqueId-CNPmaLAy.js";import"./toString-C-tSuG3i.js";import"./isObjectLike-Cu7zvyZq.js";import"./isArray-Dxzbedgu.js";import"./isSymbol-VqtJwUUo.js";import"./IconSvg-DSJ_maOf.js";import"./LayersTwoTone-CMTGtTmF.js";import"./ContentCopyTwoTone-Cz1EZlai.js";import"./HelpOutlineTwoTone-DAzxeOov.js";import"./ErrorOutlined-BQG7G4Lq.js";import"./GetAppTwoTone-Q9RFiIkz.js";import"./InfoOutlined-sdaAyk9N.js";import"./DeleteTwoTone-DdO2TNwv.js";import"./CheckCircleTwoTone-BxJ-qX7O.js";import"./StringUtils-DrtZBNIY.js";import"./VerificationSubmission-CX6Xq4We.js";const st={title:"UI/SharePageLinkButton",component:p},t={args:{buttonProps:{sx:{position:"fixed",right:"20px"}}},parameters:{stack:"mock",msw:{handlers:[]}}};var r,o,i;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    buttonProps: {
+      sx: {
+        position: 'fixed',
+        right: '20px'
+      }
+    }
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: []
+    }
+  }
+}`,...(i=(o=t.parameters)==null?void 0:o.docs)==null?void 0:i.source}}};const ct=["SharePageLinkButtonStory"];export{t as SharePageLinkButtonStory,ct as __namedExportsOrder,st as default};
