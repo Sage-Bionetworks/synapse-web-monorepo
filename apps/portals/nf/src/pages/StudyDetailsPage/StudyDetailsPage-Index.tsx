eda363e6d27a855d90cfd09c10684498f4b2c7ef
“kianamcc” <user.email=kianamccu@gmail.com>
2025-05-20 17:06:17 -0700

    cleanup

--- apps/portals/nf/src/pages/StudyDetailsPage/StudyDetailsPage-Index.tsx
+++
@@ -1,9 +0,0 @@
-import { STUDY_DETAILS_PAGE_DETAILS_TAB_PATH } from '@/config/routeConstants'
-import RedirectWithQuery from '@sage-bionetworks/synapse-portal-framework/components/RedirectWithQuery'
-
-function StudyDetailsPageIndex() {
-  // A tab is always shown -- redirect to the details tab
-  return <RedirectWithQuery to={STUDY_DETAILS_PAGE_DETAILS_TAB_PATH} />
-}
-
-export default StudyDetailsPageIndex
