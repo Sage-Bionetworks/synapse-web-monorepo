63eedf51cdf0f8be76da6725a5312963030ba4fa
Chelsea-Na <109613735+Chelsea-Na@users.noreply.github.com>
2025-05-21 00:37:08 -0700

    Update footerConfig.ts

--- apps/portals/genie/src/config/footerConfig.ts
+++ apps/portals/genie/src/config/footerConfig.ts
@@ -1,7 +1,7 @@
 import { FooterConfig } from '@sage-bionetworks/synapse-portal-framework/types/portal-config'
 
 const footer: FooterConfig = {
-  contactUs: 'genieinfo@aacr.org',
+  contactUs: 'mailto:genieinfo@aacr.org',
 }
 
 export default footer
