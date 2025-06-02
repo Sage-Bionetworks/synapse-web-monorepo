c6fce5a43739c11dd2af9352fddecec592bffb87
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-06-02 15:29:39 -0400

    Fix controlled/uncontrolled error log, hoping that will stabilize the test failing in CI but passing locally.

--- packages/synapse-react-client/src/components/AccessRequirementList/ManagedACTAccessRequirementRequestFlow/ResearchProjectForm/ResearchProjectForm.tsx
+++ packages/synapse-react-client/src/components/AccessRequirementList/ManagedACTAccessRequirementRequestFlow/ResearchProjectForm/ResearchProjectForm.tsx
@@ -19,6 +19,7 @@ import {
   ManagedACTAccessRequirement,
   ResearchProject,
 } from '@sage-bionetworks/synapse-types'
+import isEmpty from 'lodash-es/isEmpty'
 import { FormEvent, useEffect, useState } from 'react'
 import HelpPopover from '../../../HelpPopover'
 import IconSvg from '../../../IconSvg/IconSvg'
@@ -43,11 +44,10 @@ export type ResearchProjectFormProps = {
  */
 export default function ResearchProjectForm(props: ResearchProjectFormProps) {
   const { onSave, managedACTAccessRequirement, onHide } = props
-  const [projectLead, setProjectLead] = useState<string | undefined>(undefined)
-  const [institution, setInstitution] = useState<string | undefined>(undefined)
-  const [intendedDataUseStatement, setIntendedDataUseStatement] = useState<
-    string | undefined
-  >(undefined)
+  const [projectLead, setProjectLead] = useState<string>('')
+  const [institution, setInstitution] = useState<string>('')
+  const [intendedDataUseStatement, setIntendedDataUseStatement] =
+    useState<string>('')
   const [alert, setAlert] = useState<AlertProps | undefined>()
 
   const [showConfirmationScreen, setShowConfirmationScreen] = useState(false)
@@ -60,14 +60,14 @@ export default function ResearchProjectForm(props: ResearchProjectFormProps) {
 
   // Populate the form with existing data if it exists
   useEffect(() => {
-    if (projectLead == undefined && existingResearchProject?.projectLead) {
+    if (isEmpty(projectLead) && existingResearchProject?.projectLead) {
       setProjectLead(existingResearchProject?.projectLead)
     }
-    if (institution == undefined && existingResearchProject?.institution) {
+    if (isEmpty(institution) && existingResearchProject?.institution) {
       setInstitution(existingResearchProject?.institution)
     }
     if (
-      intendedDataUseStatement == undefined &&
+      isEmpty(intendedDataUseStatement) &&
       existingResearchProject?.intendedDataUseStatement
     ) {
       setIntendedDataUseStatement(
@@ -123,7 +123,7 @@ export default function ResearchProjectForm(props: ResearchProjectFormProps) {
         accessRequirementId: String(managedACTAccessRequirement.id),
         institution: institution,
         projectLead: projectLead,
-        intendedDataUseStatement: intendedDataUseStatement,
+        intendedDataUseStatement: intendedDataUseStatement || undefined,
       })
       setShowConfirmationScreen(false)
     }
