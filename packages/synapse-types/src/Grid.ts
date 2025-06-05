e966306387c43eb2b53c2711e10fdb875c82323e
afwillia <anthony.williams@sagebase.org>
2025-06-05 13:55:09 -0700

    Add synapse grid types

---
+++ packages/synapse-types/src/Grid.ts
@@ -0,0 +1,20 @@
+export type CreateGridRequest = {
+  concreteType?: string
+  csvFileHandleId?: number // The ID of a fileHandle of a CSV that defines the starting state of the grid. The first line of the CSV must be a header that defines the column names of the grid.
+  schema$id?: string // The $id of the JSON schema to be used for model validation. Note: If the $id includes a semantic version then specific version will be used. If the $id excludes the semantic version then the latest version of that schema.
+}
+
+export type CreateGridResponse = {
+  concreteType?: string
+  gridSession: string // Basic information about a grid session.
+}
+
+export type GridSession = {
+  sessionId: string // 	The unique sessionId that identifies the grid session.
+  startedBy: string // 	The user that started this session.
+  startedOn: string // 	The date-time when the session was started.
+  etag: string // 	Changes when the session chagnes.
+  modifiedOn: string // 	The date-time when the session was last changed.
+  lastReplicaIdClient: number // 	The last replica ID issued to a client. Client replica IDs are incremented.
+  lastReplicaIdService: number // The last replica ID issued to a service. Service replica IDs are decremented.
+}
