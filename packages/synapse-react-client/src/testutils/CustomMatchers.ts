9d1cc4c7397a0f52beb221a9628130cbf72ad1db
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-06-06 10:56:56 -0400

    Fix custom matcher message

--- packages/synapse-react-client/src/testutils/CustomMatchers.ts
+++ packages/synapse-react-client/src/testutils/CustomMatchers.ts
@@ -3,10 +3,11 @@ import { QueryKey } from '@tanstack/react-query'
 
 expect.extend({
   toMatchQueryKey(received: QueryKey, matcher: QueryKey) {
+    const { isNot } = this
     return {
       pass: doesQueryKeyMatch(received, matcher),
       message: () =>
-        `Expected query key to match: ${JSON.stringify(
+        `Expected query key to${isNot ? ' not' : ''} match: ${JSON.stringify(
           matcher,
         )}, but received: ${JSON.stringify(received)}`,
       actual: received,
