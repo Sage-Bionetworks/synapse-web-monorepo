6f768208457c086896d5407e4d3e9df430f9d549
“kianamcc” <user.email=kianamccu@gmail.com>
2025-06-17 10:32:42 -0700

    change link with icon to button

--- packages/synapse-react-client/src/components/LinkWithIcon/LinkWithIcon.tsx
+++ packages/synapse-react-client/src/components/LinkWithIcon/LinkWithIcon.tsx
@@ -1,5 +1,5 @@
 import { IsExternalLink } from '@/utils/functions/IsExternalLink'
-import { Box, SxProps, Typography, useTheme } from '@mui/material'
+import { Box, Button, SxProps, Typography, useTheme } from '@mui/material'
 import IconSvg, { IconName } from '../IconSvg'
 
 export type LinkWithIconProps = {
@@ -12,46 +12,47 @@ export type LinkWithIconProps = {
 const LinkWithIcon = (props: LinkWithIconProps) => {
   const { text, icon, href, sx } = props
   const isExternal = IsExternalLink(href)
-  const theme = useTheme()
   return (
-    <Box
-      component="a"
+    <Button
+      role="link"
+      variant="text"
       href={href}
       target={isExternal ? '_blank' : undefined}
       rel={isExternal ? 'noopener noreferrer' : undefined}
+      endIcon={
+        icon && (
+          <IconSvg
+            icon={icon}
+            sx={{
+              display: 'flex',
+              marginRight: '8px',
+              width: '20px',
+              height: '20px',
+              textAlign: 'center',
+            }}
+          />
+        )
+      }
       sx={{
-        display: 'flex',
-        alignItems: 'center',
+        '&:hover': {
+          background: 'none',
+          textDecorationColor: 'inherit',
+        },
         padding: '6px 16px',
         width: { xs: '100%', sm: 'initial' },
         justifyContent: { xs: 'center', sm: 'flex-start' },
-        '&:hover': {
-          textDecorationColor: `${theme.palette.primary.contrastText} !important`,
-        },
         ...sx,
       }}
     >
-      {icon && (
-        <IconSvg
-          icon={icon}
-          sx={{
-            marginRight: '8px',
-            width: '20px',
-            height: '20px',
-            color: theme.palette.primary.contrastText,
-          }}
-        />
-      )}
       <Typography
         variant="buttonLink"
         sx={{
           fontWeight: 700,
-          color: theme.palette.primary.contrastText,
         }}
       >
         {text}
       </Typography>
-    </Box>
+    </Button>
   )
 }
 
