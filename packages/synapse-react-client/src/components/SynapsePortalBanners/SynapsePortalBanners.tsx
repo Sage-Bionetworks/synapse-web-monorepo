e4848e844ba35fc48674828de0752ee106d24dd9
jay-hodgson <jay.hodgson@sagebase.org>
2025-06-27 14:52:25 -0700

    SWC-7374: in progress

---
+++ packages/synapse-react-client/src/components/SynapsePortalBanners/SynapsePortalBanners.tsx
@@ -0,0 +1,143 @@
+import { useGetQueryResultBundleWithAsyncStatus } from '@/synapse-queries'
+import { useSourceAppConfigs } from '@/utils/hooks'
+import { BUNDLE_MASK_QUERY_RESULTS } from '@/utils/SynapseConstants'
+import { ChevronRightTwoTone } from '@mui/icons-material'
+import { Box, Typography } from '@mui/material'
+import {
+  ColumnSingleValueQueryFilter,
+  ColumnSingleValueFilterOperator,
+} from '@sage-bionetworks/synapse-types'
+
+export type SynapsePortalBannersProps = {
+  entityId: string
+  dataCatalogEntityId?: string
+  sourceAppConfigTableID?: string
+}
+
+/**
+ *
+ * @param props
+ */
+export default function SynapsePortalBanners({
+  entityId,
+  dataCatalogEntityId = 'syn61609402',
+  sourceAppConfigTableID = 'syn45291362',
+}: SynapsePortalBannersProps) {
+  const dataCatalogAdditionalFilters: ColumnSingleValueQueryFilter[] = [
+    {
+      concreteType:
+        'org.sagebionetworks.repo.model.table.ColumnSingleValueQueryFilter',
+      columnName: 'id',
+      operator: ColumnSingleValueFilterOperator.EQUAL,
+      values: [entityId],
+    },
+  ]
+  const { data: dataCatalogData } = useGetQueryResultBundleWithAsyncStatus({
+    entityId: dataCatalogEntityId,
+    query: {
+      sql: `SELECT distinct appId FROM ${dataCatalogEntityId}`,
+      additionalFilters: dataCatalogAdditionalFilters,
+    },
+    partMask: BUNDLE_MASK_QUERY_RESULTS,
+    concreteType: 'org.sagebionetworks.repo.model.table.QueryBundleRequest',
+  })
+
+  const rowSet = dataCatalogData?.responseBody?.queryResult?.queryResults
+  const hasPortalBanners = !!rowSet && rowSet?.rows.length > 0
+
+  const appIds = rowSet?.rows[0].values as string[]
+  const sourceAppConfigFilters: ColumnSingleValueQueryFilter[] = [
+    {
+      concreteType:
+        'org.sagebionetworks.repo.model.table.ColumnSingleValueQueryFilter',
+      columnName: 'appId',
+      operator: ColumnSingleValueFilterOperator.EQUAL,
+      values: appIds,
+    },
+  ]
+  const sourceAppConfigs = useSourceAppConfigs(
+    sourceAppConfigTableID,
+    sourceAppConfigFilters,
+  )
+  const hasSourceAppConfigs = !!sourceAppConfigs && sourceAppConfigs.length > 0
+
+  if (!hasPortalBanners || !hasSourceAppConfigs) {
+    return <></>
+  }
+
+  // we have source app configs associated to this entity, let's render the banners
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '20px',
+        width: '100%',
+        alignItems: 'center',
+      }}
+    >
+      {sourceAppConfigs.map((appConfig, index) => (
+        <Box
+          key={index}
+          component="a"
+          href={appConfig.appURL}
+          target="_blank"
+          rel="noopener noreferrer"
+          flex={1}
+          sx={{
+            display: 'flex',
+            flexWrap: 'nowrap',
+            padding: '16px',
+            justifyContent: 'space-between',
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+            backgroundColor: '#f9f9f9',
+            textDecoration: 'none',
+            pointer: 'cursor',
+            '&:hover': {
+              textDecoration: 'none !important',
+            },
+            width: '100%',
+            alignItems: 'center',
+          }}
+        >
+          <Box
+            flex={1}
+            sx={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box
+              sx={{
+                '> *': {
+                  height: '60px',
+                },
+              }}
+            >
+              {appConfig?.logo}
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                mt: '10px',
+                mr: '10px',
+              }}
+            >
+              <Typography variant="body1" sx={{ fontWeight: 700 }}>
+                This resource is part of a Portal
+              </Typography>
+              <Typography variant="body1">
+                {' '}
+                Access the {appConfig.friendlyName}
+              </Typography>
+            </Box>
+          </Box>
+          <ChevronRightTwoTone />
+        </Box>
+      ))}
+    </Box>
+  )
+}
