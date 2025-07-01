9b37d51ad4aebc3a6ae3833cfc0934e7c5d1aaa3
Sigfried Gold <sigfried@sigfried.org>
2025-07-01 13:00:25 -0400

    The facet/col was supposed to be Maturity, not Mature

--- apps/portals/b2ai.standards/src/config/columnAliases.ts
+++ apps/portals/b2ai.standards/src/config/columnAliases.ts
@@ -2,7 +2,7 @@ import { DST_TABLE_COLUMN_NAMES } from '@/config/resources'
 
 export default {
   [DST_TABLE_COLUMN_NAMES.HAS_RELEVANT_ORGANIZATION]: 'Relevant Organizations',
-  [DST_TABLE_COLUMN_NAMES.MATURE]: 'Mature',
+  [DST_TABLE_COLUMN_NAMES.MATURE]: 'Maturity',
   standardName: 'Name of Standard',
   isOpen: 'Is Open?',
   registration: 'Requires Registration?',
