73af9c1e491a74c5a75e8d6e28db50d9bc6587c5
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-07-03 14:15:41 -0400

    SWC-7389
    Remove lazy-loading in SWC index file, which resolves an issue where users would always encounter the issue described in SWC-7390.

--- packages/synapse-react-client/src/SWC.index.ts
+++ packages/synapse-react-client/src/SWC.index.ts
@@ -5,12 +5,76 @@
  * If you wish to export a component or other object to be used by SynapseWebClient, you should add it to this file.
  */
 
-import { lazy } from 'react'
 import { version } from '../package.json'
+import AccessRequirementAclEditor from './components/AccessRequirementAclEditor'
+import AccessRequirementList from './components/AccessRequirementList/AccessRequirementList'
+import AccessRequirementRelatedProjectsList from './components/AccessRequirementRelatedProjectsList'
+import AccountLevelBadges from './components/AccountLevelBadges/AccountLevelBadges'
+import CertificationQuiz from './components/CertificationQuiz/CertificationQuiz'
+import AvailableEvaluationQueueList from './components/ChallengeSubmission/AvailableEvaluationQueueList'
+import CookiesNotification from './components/CookiesNotification'
+import CreateOrUpdateAccessRequirementWizard from './components/CreateOrUpdateAccessRequirementWizard'
+import CreateTableViewWizard from './components/CreateTableViewWizard/CreateTableViewWizard'
+import RejectProfileValidationRequestModal from './components/dataaccess/RejectProfileValidationRequestModal'
+import ReviewerDashboard from './components/dataaccess/ReviewerDashboard'
+import UserAccessRequestHistoryPlace from './components/dataaccess/UserAccessRequestHistory/UserAccessRequestHistoryPlace'
+import CreateOrUpdateDoiModal from './components/doi/CreateOrUpdateDoiModal'
+import DownloadConfirmation from './components/download_list/FolderDownloadConfirmation'
+import DownloadCartPage from './components/DownloadCart/DownloadCartPage'
+import EntityModal from './components/entity/metadata/EntityModal'
+import EntityActionMenu from './components/entity/page/action_menu/EntityActionMenu'
+import EntityPageBreadcrumbs from './components/entity/page/breadcrumbs/EntityPageBreadcrumbs'
+import CreatedByModifiedBy from './components/entity/page/CreatedByModifiedBy'
+import EntityPageTitleBar from './components/entity/page/title_bar/EntityPageTitleBar'
+import EntityAclEditorModal from './components/EntityAclEditor/EntityAclEditorModal'
+import EntityBadgeIcons from './components/EntityBadgeIcons/EntityBadgeIcons'
+import EntityCitation from './components/EntityCitation'
+import EntityFinder from './components/EntityFinder/EntityFinder'
+import EntityForm from './components/EntityForm/EntityForm'
+import EntityHeaderTable from './components/EntityHeaderTable'
+import EntityTypeIcon from './components/EntityIcon'
+import EntityUploadModal from './components/EntityUpload/EntityUploadModal'
+import EntityViewScopeEditorModal from './components/EntityViewScopeEditor/EntityViewScopeEditorModal'
+import ErrorPage from './components/error/ErrorPage'
+import EvaluationCard from './components/Evaluation/EvaluationCard'
+import EvaluationEditorPage from './components/Evaluation/EvaluationEditorPage'
+import FavoritesPage from './components/favorites/FavoritesPage'
+import HtmlPreview from './components/FilePreview/HtmlPreview/HtmlPreview'
+import DiscussionThread from './components/Forum/DiscussionThread'
+import ForumSearch from './components/ForumSearch/ForumSearch'
+import FullWidthAlert from './components/FullWidthAlert/FullWidthAlert'
+import GoogleAnalytics from './components/GoogleAnalytics/GoogleAnalytics'
+import HasAccess from './components/HasAccess/HasAccessV2'
+import HelpPopover from './components/HelpPopover/HelpPopover'
+import IconSvg from './components/IconSvg/IconSvg'
+import IDUReport from './components/IDUReport/IDUReport'
+import GovernanceMarkdownGithub from './components/Markdown/MarkdownGithub'
+import OAuthManagement from './components/OAuthClientManagement/OAuthManagement'
+import OrientationBanner from './components/OrientationBanner/OrientationBanner'
+import PageProgress from './components/PageProgress/PageProgress'
+import ProjectDataAvailability from './components/ProjectStorage/ProjectDataAvailability'
+import ProvenanceGraph from './components/ProvenanceGraph/ProvenanceGraph'
+import QueryWrapperPlotNav from './components/QueryWrapperPlotNav/QueryWrapperPlotNav'
+import SkeletonButton from './components/Skeleton/SkeletonButton'
+import SqlDefinedTableEditorModal from './components/SqlDefinedTableEditor/SqlDefinedTableEditorModal'
+import StandaloneQueryWrapper from './components/StandaloneQueryWrapper/StandaloneQueryWrapper'
+import StatisticsPlot from './components/StatisticsPlot'
+import SubmissionViewScopeEditorModal from './components/SubmissionViewScopeEditor/SubmissionViewScopeEditorModal'
+import SubscriptionPage from './components/SubscriptionPage'
+import SynapseChat from './components/SynapseChat/SynapseChat'
+import SynapseFooter from './components/SynapseFooter/SynapseFooter'
+import SynapseHomepageV2 from './components/SynapseHomepageV2/SynapseHomepageV2'
+import SynapsePlansPage from './components/SynapseHomepageV2/SynapsePlansPage'
+import SynapseNavDrawer from './components/SynapseNavDrawer/SynapseNavDrawer'
+import DatasetItemsEditor from './components/SynapseTable/datasets/DatasetItemsEditor'
+import TableColumnSchemaEditor from './components/TableColumnSchemaEditor/TableColumnSchemaEditor'
 import {
   displayToast,
   SynapseToastContainer,
 } from './components/ToastMessage/ToastMessage'
+import TrashCanList from './components/trash/TrashCanList'
+import UserCard from './components/UserCard/UserCard'
+import UserProfileLinks from './components/UserProfileLinks/UserProfileLinks'
 import SynapseClient, { HttpClient } from './synapse-client'
 import * as SynapseQueries from './synapse-queries'
 import Palettes from './theme/palette/Palettes'
@@ -47,166 +111,74 @@ const SynapseContext = {
 }
 
 const SynapseComponents = {
+  AccessRequirementAclEditor,
+  AccessRequirementList,
+  AccessRequirementRelatedProjectsList,
+  AccountLevelBadges,
+  AvailableEvaluationQueueList,
+  CertificationQuiz,
+  CookiesNotification,
+  CreateOrUpdateAccessRequirementWizard,
+  CreateOrUpdateDoiModal,
+  CreateTableViewWizard,
+  CreatedByModifiedBy,
+  DatasetItemsEditor,
+  DiscussionThread,
+  DownloadCartPage,
+  DownloadConfirmation,
+  EntityAclEditorModal,
+  EntityActionMenu,
+  EntityBadgeIcons,
+  EntityCitation,
+  EntityFinder,
+  EntityForm,
+  EntityHeaderTable,
+  EntityModal,
+  EntityPageBreadcrumbs,
+  EntityPageTitleBar,
+  EntityTypeIcon,
+  EntityUploadModal,
+  EntityViewScopeEditorModal,
+  ErrorPage,
+  EvaluationCard,
+  EvaluationEditorPage,
+  FavoritesPage,
+  ForumSearch,
+  FullWidthAlert,
+  GoogleAnalytics,
+  GovernanceMarkdownGithub,
+  HasAccess,
+  HelpPopover,
+  HtmlPreview,
+  IDUReport,
+  IconSvg,
+  OAuthManagement,
+  OrientationBanner,
+  PageProgress,
+  ProjectDataAvailability,
+  ProvenanceGraph,
+  QueryWrapperPlotNav,
+  RejectProfileValidationRequestModal,
+  ReviewerDashboard,
+  SkeletonButton,
+  SqlDefinedTableEditorModal,
+  StandaloneQueryWrapper,
+  StatisticsPlot,
+  SubmissionViewScopeEditorModal,
+  SubscriptionPage,
+  SynapseChat,
+  SynapseFooter,
+  SynapseHomepageV2,
+  SynapseNavDrawer,
+  SynapsePlansPage,
   SynapseToastContainer,
+  TableColumnSchemaEditor,
+  TrashCanList,
+  UserAccessRequestHistoryPlace,
+  UserCard,
+  UserProfileLinks,
   displayToast,
   getCurrentCookiePreferences,
-  EntityForm: lazy(() => import('./components/EntityForm/EntityForm')),
-  UserCard: lazy(() => import('./components/UserCard/UserCard')),
-  StatisticsPlot: lazy(() => import('./components/StatisticsPlot')),
-  HasAccess: lazy(() => import('./components/HasAccess/HasAccessV2')),
-  EvaluationCard: lazy(() => import('./components/Evaluation/EvaluationCard')),
-  EvaluationEditorPage: lazy(
-    () => import('./components/Evaluation/EvaluationEditorPage'),
-  ),
-  AccountLevelBadges: lazy(
-    () => import('./components/AccountLevelBadges/AccountLevelBadges'),
-  ),
-  PageProgress: lazy(() => import('./components/PageProgress/PageProgress')),
-  SynapseHomepageV2: lazy(
-    () => import('./components/SynapseHomepageV2/SynapseHomepageV2'),
-  ),
-  SynapsePlansPage: lazy(
-    () => import('./components/SynapseHomepageV2/SynapsePlansPage'),
-  ),
-  SynapseFooter: lazy(() => import('./components/SynapseFooter/SynapseFooter')),
-  EntityFinder: lazy(() => import('./components/EntityFinder/EntityFinder')),
-  ErrorPage: lazy(() => import('./components/error/ErrorPage')),
-  EntityBadgeIcons: lazy(
-    () => import('./components/EntityBadgeIcons/EntityBadgeIcons'),
-  ),
-  DownloadCartPage: lazy(
-    () => import('./components/DownloadCart/DownloadCartPage'),
-  ),
-  DownloadConfirmation: lazy(
-    () => import('./components/download_list/FolderDownloadConfirmation'),
-  ),
-  FullWidthAlert: lazy(
-    () => import('./components/FullWidthAlert/FullWidthAlert'),
-  ),
-  SynapseNavDrawer: lazy(
-    () => import('./components/SynapseNavDrawer/SynapseNavDrawer'),
-  ),
-  FavoritesPage: lazy(() => import('./components/favorites/FavoritesPage')),
-  EntityCitation: lazy(() => import('./components/EntityCitation')),
-  EntityModal: lazy(() => import('./components/entity/metadata/EntityModal')),
-  IconSvg: lazy(() => import('./components/IconSvg/IconSvg')),
-  UserProfileLinks: lazy(
-    () => import('./components/UserProfileLinks/UserProfileLinks'),
-  ),
-  DatasetItemsEditor: lazy(
-    () => import('./components/SynapseTable/datasets/DatasetItemsEditor'),
-  ),
-  EntityTypeIcon: lazy(() => import('./components/EntityIcon')),
-  HelpPopover: lazy(() => import('./components/HelpPopover/HelpPopover')),
-  SkeletonButton: lazy(() => import('./components/Skeleton/SkeletonButton')),
-  QueryWrapperPlotNav: lazy(
-    () => import('./components/QueryWrapperPlotNav/QueryWrapperPlotNav'),
-  ),
-  StandaloneQueryWrapper: lazy(
-    () => import('./components/StandaloneQueryWrapper/StandaloneQueryWrapper'),
-  ),
-  ForumSearch: lazy(() => import('./components/ForumSearch/ForumSearch')),
-  DiscussionThread: lazy(() => import('./components/Forum/DiscussionThread')),
-  ReviewerDashboard: lazy(
-    () => import('./components/dataaccess/ReviewerDashboard'),
-  ),
-  ProvenanceGraph: lazy(
-    () => import('./components/ProvenanceGraph/ProvenanceGraph'),
-  ),
-  TrashCanList: lazy(() => import('./components/trash/TrashCanList')),
-  OAuthManagement: lazy(
-    () => import('./components/OAuthClientManagement/OAuthManagement'),
-  ),
-  CertificationQuiz: lazy(
-    () => import('./components/CertificationQuiz/CertificationQuiz'),
-  ),
-  HtmlPreview: lazy(
-    () => import('./components/FilePreview/HtmlPreview/HtmlPreview'),
-  ),
-  IDUReport: lazy(() => import('./components/IDUReport/IDUReport')),
-  EntityPageBreadcrumbs: lazy(
-    () => import('./components/entity/page/breadcrumbs/EntityPageBreadcrumbs'),
-  ),
-  EntityActionMenu: lazy(
-    () => import('./components/entity/page/action_menu/EntityActionMenu'),
-  ),
-  EntityPageTitleBar: lazy(
-    () => import('./components/entity/page/title_bar/EntityPageTitleBar'),
-  ),
-  CreatedByModifiedBy: lazy(
-    () => import('./components/entity/page/CreatedByModifiedBy'),
-  ),
-  SubscriptionPage: lazy(() => import('./components/SubscriptionPage')),
-  OrientationBanner: lazy(
-    () => import('./components/OrientationBanner/OrientationBanner'),
-  ),
-  AvailableEvaluationQueueList: lazy(
-    () =>
-      import('./components/ChallengeSubmission/AvailableEvaluationQueueList'),
-  ),
-  AccessRequirementList: lazy(
-    () => import('./components/AccessRequirementList/AccessRequirementList'),
-  ),
-  EntityHeaderTable: lazy(() => import('./components/EntityHeaderTable')),
-  AccessRequirementRelatedProjectsList: lazy(
-    () => import('./components/AccessRequirementRelatedProjectsList'),
-  ),
-  CreateTableViewWizard: lazy(
-    () => import('./components/CreateTableViewWizard/CreateTableViewWizard'),
-  ),
-  TableColumnSchemaEditor: lazy(
-    () =>
-      import('./components/TableColumnSchemaEditor/TableColumnSchemaEditor'),
-  ),
-  SqlDefinedTableEditorModal: lazy(
-    () =>
-      import('./components/SqlDefinedTableEditor/SqlDefinedTableEditorModal'),
-  ),
-  EntityViewScopeEditorModal: lazy(
-    () =>
-      import('./components/EntityViewScopeEditor/EntityViewScopeEditorModal'),
-  ),
-  SubmissionViewScopeEditorModal: lazy(
-    () =>
-      import(
-        './components/SubmissionViewScopeEditor/SubmissionViewScopeEditorModal'
-      ),
-  ),
-  AccessRequirementAclEditor: lazy(
-    () => import('./components/AccessRequirementAclEditor'),
-  ),
-  CreateOrUpdateAccessRequirementWizard: lazy(
-    () => import('./components/CreateOrUpdateAccessRequirementWizard'),
-  ),
-  GoogleAnalytics: lazy(
-    () => import('./components/GoogleAnalytics/GoogleAnalytics'),
-  ),
-  CookiesNotification: lazy(() => import('./components/CookiesNotification')),
-  EntityAclEditorModal: lazy(
-    () => import('./components/EntityAclEditor/EntityAclEditorModal'),
-  ),
-  SynapseChat: lazy(() => import('./components/SynapseChat/SynapseChat')),
-  RejectProfileValidationRequestModal: lazy(
-    () => import('./components/dataaccess/RejectProfileValidationRequestModal'),
-  ),
-  GovernanceMarkdownGithub: lazy(
-    () => import('./components/Markdown/MarkdownGithub'),
-  ),
-  ProjectDataAvailability: lazy(
-    () => import('./components/ProjectStorage/ProjectDataAvailability'),
-  ),
-  EntityUploadModal: lazy(
-    () => import('./components/EntityUpload/EntityUploadModal'),
-  ),
-  CreateOrUpdateDoiModal: lazy(
-    () => import('./components/doi/CreateOrUpdateDoiModal'),
-  ),
-  UserAccessRequestHistoryPlace: lazy(
-    () =>
-      import(
-        './components/dataaccess/UserAccessRequestHistory/UserAccessRequestHistoryPlace'
-      ),
-  ),
 }
 
 const Analytics = {
