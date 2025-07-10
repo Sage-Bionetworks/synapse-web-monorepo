61dfa7ec7d434386f95070283ede97127e602ff1
jay-hodgson <jay.hodgson@sagebase.org>
2025-07-10 09:57:53 -0700

    PORTALS-3674: link to form is in the wiki content (as a buttonlink widget)

--- apps/portals/ampals/src/pages/resources/RequirementsForPublication.tsx
+++ apps/portals/ampals/src/pages/resources/RequirementsForPublication.tsx
@@ -1,7 +1,6 @@
 import AMPALSResearchPageLayout from '@sage-bionetworks/synapse-portal-framework/components/ampals/AMPALSResearchPageLayout'
 import publishingRequirementsSlat from '@/assets/publishing_requirements.png'
 import { MarkdownSynapse } from 'synapse-react-client/components/Markdown/MarkdownSynapse'
-import { Button } from '@mui/material'
 
 function RequirementsForPublication() {
   return (
@@ -15,16 +14,6 @@ function RequirementsForPublication() {
         wikiId="632170"
         loadingSkeletonRowCount={50}
       />
-      <Button
-        variant="contained"
-        size="large"
-        href="https://docs.google.com/forms/d/e/1FAIpQLSfjsLpVDzxRSs83qL_i4G8cq_DUYMGK8J10PKj3p9vve-0ftw/viewform"
-        target="_blank"
-        rel="noopener noreferrer"
-        sx={{ marginTop: '40px' }}
-      >
-        Fill Out ALSKP Publication Intent Form
-      </Button>
     </AMPALSResearchPageLayout>
   )
 }
