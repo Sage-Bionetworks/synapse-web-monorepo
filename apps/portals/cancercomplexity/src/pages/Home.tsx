a59210f6d25c7f27308f47b210bd92f03d8f8dfe
jay-hodgson <jay.hodgson@sagebase.org>
2025-07-11 09:20:00 -0700

    and remove import

--- apps/portals/cancercomplexity/src/pages/Home.tsx
+++ apps/portals/cancercomplexity/src/pages/Home.tsx
@@ -6,7 +6,6 @@ import {
 } from '@/config/synapseConfigs/onPointClick'
 import CancerComplexityHeader from '@sage-bionetworks/synapse-portal-framework/components/cancercomplexity/CancerComplexityHeader'
 import CancerComplexityIntro from '@sage-bionetworks/synapse-portal-framework/components/cancercomplexity/CancerComplexityIntro'
-import AboutPortal from '@sage-bionetworks/synapse-portal-framework/components/csbc-home-page/AboutPortal'
 import ConsortiaGoals from '@sage-bionetworks/synapse-portal-framework/components/csbc-home-page/ConsortiaGoals'
 import DevelopedBySage from '@sage-bionetworks/synapse-portal-framework/components/csbc-home-page/DevelopedBySage'
 import { SectionLayout } from '@sage-bionetworks/synapse-portal-framework/components/SectionLayout'
