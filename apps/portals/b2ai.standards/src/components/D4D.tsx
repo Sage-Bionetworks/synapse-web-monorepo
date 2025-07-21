8a629d433b822869190670d02dee9c3660553aaa
Sigfried Gold <sigfried@sigfried.org>
2025-07-21 11:12:42 -0400

    D4D using new html and styles
    - Copied html and css from b2ai-standards-registry/project/data/sheets/
      to assets/d4d
    - Recombining css and html for display. Probably don't need to separate
      them into different files in the html generation script. (I had
      thought we would need to.)

--- apps/portals/b2ai.standards/src/components/D4D.tsx
+++ apps/portals/b2ai.standards/src/components/D4D.tsx
@@ -3,25 +3,18 @@ import { GCInfo } from '@/config/GrandChallengeResources'
 type D4DProps = {
   org_id: string
 }
+
 export function D4D(props: D4DProps) {
   const { org_id } = props
   const rawHtml = GCInfo[org_id]?.d4dHtml ?? ''
+  const rawCss = GCInfo[org_id]?.d4dCss ?? ''
+
   if (!rawHtml) {
-    return (
-      <div className="d4d-html-style">
-        No datasheet available for organization: {org_id}
-      </div>
-    )
+    return <div>No datasheet available for organization: {org_id}</div>
   }
 
-  // const reactReadyHtml = rawHtml.replace(/\bclass=/g, 'className=')
+  // Combine CSS and HTML
+  const styledContent = rawCss ? `<style>${rawCss}</style>${rawHtml}` : rawHtml
 
-  return (
-    <div
-      className="d4d-html-style"
-      dangerouslySetInnerHTML={{ __html: rawHtml }}
-    />
-  )
+  return <div dangerouslySetInnerHTML={{ __html: styledContent }} />
 }
-
-// const quickTransform = (html) => html.replace(/\bclass=/g, 'className=').replace('class="container"', 'className="d4d-html-style"');
