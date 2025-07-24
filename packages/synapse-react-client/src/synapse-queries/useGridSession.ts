df1aeaf12435cb15d48c931dd698ca932d308f6b
afwillia <anthony.williams@sagebase.org>
2025-07-24 10:57:52 -0700

    Use infinite query to list grid sessions

--- packages/synapse-react-client/src/synapse-queries/useGridSession.ts
+++ packages/synapse-react-client/src/synapse-queries/useGridSession.ts
@@ -4,7 +4,12 @@ import {
   useMutation,
   UseMutationOptions,
   useQueryClient,
+  InfiniteData,
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  QueryKey,
 } from '@tanstack/react-query'
+import { ListGridSessionsResponse } from '@sage-bionetworks/synapse-client/generated/models/ListGridSessionsResponse'
 
 export function useDeleteGridSession(
   options?: Partial<UseMutationOptions<void, SynapseClientError, string>>,
@@ -26,5 +31,38 @@ export function useDeleteGridSession(
         await options.onSuccess(updatedClient, sessionId, ctx)
       }
     },
+  })
+}
+
+export function useGetGridSessionsInfinite<
+  TData = InfiniteData<ListGridSessionsResponse>,
+>(
+  options?: Partial<
+    UseInfiniteQueryOptions<
+      ListGridSessionsResponse,
+      SynapseClientError,
+      TData,
+      QueryKey,
+      ListGridSessionsResponse['nextPageToken']
+    >
+  >,
+) {
+  const { keyFactory, synapseClient } = useSynapseContext()
+
+  return useInfiniteQuery<
+    ListGridSessionsResponse,
+    SynapseClientError,
+    TData,
+    QueryKey,
+    ListGridSessionsResponse['nextPageToken']
+  >({
+    ...options,
+    queryKey: keyFactory.getGridSessionListKey(),
+    queryFn: async context =>
+      await synapseClient.gridServicesClient.postRepoV1GridSessionList({
+        listGridSessionsRequest: { nextPageToken: context.pageParam },
+      }),
+    initialPageParam: undefined,
+    getNextPageParam: page => page.nextPageToken,
   })
 }
