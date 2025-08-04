a9a3cd5322fa8ccd12297e975e8b47a34ca8f48d
Sigfried Gold <sigfried@sigfried.org>
2025-08-04 09:23:27 -0400

    Forgot to include this comment change in last commit

--- apps/portals/b2ai.standards/src/config/GrandChallengeResources.ts
+++ apps/portals/b2ai.standards/src/config/GrandChallengeResources.ts
@@ -8,18 +8,9 @@ The d4d html will be updated sometimes. The process for doing this hasn't been d
 yet, but there is discussion about it in this Slack thread around 2025-07-21:
 https://b2aiworkspace.slack.com/archives/C0738H2KVGB/p1752187350029169
 
-Particularly (from @sigfried):
-I don't think the D4D html/css necessarily needs to be copied into the
-standards-registry repo (https://github.com/bridge2ai/b2ai-standards-registry)
-but they do need to be copied (and updated) here on the Synapse monorepo
-(https://github.com/Sigfried/synapse-web-monorepo/tree/b2ai-org-detail-GC-landing-page/apps/portals/b2ai.standards/src/assets/d4d).
-That link is to my fork. Whoever puts this copying process together will need
-to figure out if the copies go into a fork or the main repo, and whether to
-create a new branch each time or use a dedicated branch for this. In either
-case, after copying, a PR will need to be made and merged. If we want to do
-face validation before merging to staging, we will need someone to do it on a
-local development environment, in which case the files should go to their fork.
- */
+Probably we will be importing html and css content into Synapse tables instead
+of as static files like we're doing now.
+*/
 import B2AI_ORG_114_html from '@/assets/d4d/B2AI_ORG_114_d4d.html?raw'
 import B2AI_ORG_116_html from '@/assets/d4d/B2AI_ORG_116_d4d.html?raw'
 import B2AI_ORG_117_html from '@/assets/d4d/B2AI_ORG_117_d4d.html?raw'
