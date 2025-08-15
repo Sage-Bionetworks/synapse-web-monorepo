7acb399a1b013f2c2a25c528a55cbed4bd46ee15
“kianamcc” <user.email=kianamccu@gmail.com>
2025-08-14 17:29:54 -0700

    update stories

--- packages/synapse-react-client/src/components/DataGrid/SynapseGrid.stories.tsx
+++ packages/synapse-react-client/src/components/DataGrid/SynapseGrid.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
-import SynapseGrid from './SynapseGrid'
+import SynapseGrid, { SynapseGridProps } from './SynapseGrid'
+import { useRef } from 'react'
+import { Button } from '@mui/material'
 
 const meta = {
   title: 'Components/SynapseGrid',
@@ -19,7 +21,24 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Default: Story = {
+const GridWithButton = (args: SynapseGridProps) => {
+  const gridRef = useRef<{ initializeGrid: () => void }>(null)
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        onClick={() => gridRef.current?.initializeGrid()}
+        sx={{ color: 'inherit', margin: '10px 0' }}
+      >
+        Start Grid Session
+      </Button>
+      <SynapseGrid ref={gridRef} {...args} />
+    </div>
+  )
+}
+
+export const GridWithQuery: Story = {
+  render: GridWithButton,
   args: {
     query: 'SELECT * FROM syn35295069',
   },
@@ -30,7 +49,7 @@ export const Default: Story = {
   },
 }
 
-export const GridWithDebugInfo: Story = {
+export const GridWithUserInputAndDebugInfo: Story = {
   args: {
     query: '',
     showDebugInfo: true,
