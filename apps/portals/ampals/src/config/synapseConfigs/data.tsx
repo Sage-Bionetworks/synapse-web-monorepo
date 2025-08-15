035015d3f491958a9a28ce95ad6f2affbcdc0c10
jay-hodgson <jay.hodgson@sagebase.org>
2025-08-15 14:11:52 -0700

    add a special link for name (using another column as the source URL)

--- apps/portals/ampals/src/config/synapseConfigs/data.tsx
+++ apps/portals/ampals/src/config/synapseConfigs/data.tsx
@@ -1,19 +1,13 @@
-import { ColumnSingleValueFilterOperator } from '@sage-bionetworks/synapse-types'
-import {
-  QueryWrapperPlotNavProps,
-  StandaloneQueryWrapperProps,
-  LabelLinkConfig,
-} from 'synapse-react-client'
+import { QueryWrapperPlotNavProps, LabelLinkConfig } from 'synapse-react-client'
 import columnAliases from '../columnAliases'
 import { filesSql } from '../resources'
 
 const rgbIndex = 0
 export const dataColumnLinks: LabelLinkConfig = [
   {
-    matchColumnName: 'dataset',
+    matchColumnName: 'name',
     isMarkdown: false,
-    baseURL: 'Explore/Datasets/DetailsPage',
-    URLColumnName: 'id',
+    linkColumnName: 'dataFileKey',
   },
 ]
 
@@ -29,13 +23,5 @@ export const filesQueryWrapperPlotNavProps: QueryWrapperPlotNavProps = {
     showAccessColumn: true,
     showExternalAccessIcon: true,
   },
-  // facetsToPlot: ['program', 'project'],
-}
-
-export const filesDetailPageProps: StandaloneQueryWrapperProps = {
-  sql: filesSql,
-  rgbIndex,
-  // columnLinks: dataColumnLinks,
-  hideDownload: true,
-  sqlOperator: ColumnSingleValueFilterOperator.EQUAL,
+  visibleColumnCount: 7,
 }
