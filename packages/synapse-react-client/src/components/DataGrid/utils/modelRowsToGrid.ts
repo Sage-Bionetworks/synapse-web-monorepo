45e6d1edb95ed502850827dd480ba9e1c52a8cff
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-09-03 16:00:33 -0400

    Use row object logical timestamp as grid row React key

---
+++ packages/synapse-react-client/src/components/DataGrid/utils/modelRowsToGrid.ts
@@ -0,0 +1,38 @@
+import {
+  DataGridRow,
+  GridModel,
+  GridModelSnapshot,
+} from '@/components/DataGrid/DataGridTypes'
+import { GRID_ROW_REACT_KEY_PROPERTY } from '@/components/DataGrid/DataGridUtils'
+import logicalTimestampToString from '@/components/DataGrid/utils/logicalTimestampToString'
+
+/**
+ * Convert model rows to a format suitable for DataSheetGrid
+ * @param model
+ * @param modelSnapshot
+ */
+export default function modelRowsToGrid(
+  model: GridModel | null,
+  modelSnapshot: GridModelSnapshot,
+): DataGridRow[] {
+  if (!model || !modelSnapshot) return []
+  const { columnNames, columnOrder, rows } = modelSnapshot
+  const gridRows = rows.map((row): DataGridRow => {
+    const rowObj: DataGridRow = {}
+    columnOrder.forEach((index: number) => {
+      const columnName = columnNames[index]
+      if (columnName) {
+        rowObj[columnName] = row.data[index]
+      }
+    })
+
+    // Embed the logical timestamp as a string for use as the React key (a stable value unique to each row)
+    rowObj[GRID_ROW_REACT_KEY_PROPERTY] = logicalTimestampToString(
+      model.api.obj(['rows', String(rows.indexOf(row))]).node.id,
+    )
+
+    rowObj.__validationResults = row.metadata?.rowValidation
+    return rowObj
+  })
+  return gridRows
+}
