86ad51f7651ce4ddf8425cc579da7ef29c87fc1d
afwillia <anthony.williams@sagebase.org>
2025-09-10 11:20:12 -0700

    remove the add rows button and display validation messages when grid is not actively selected

--- packages/synapse-react-client/src/components/DataGrid/SynapseGrid.tsx
+++ packages/synapse-react-client/src/components/DataGrid/SynapseGrid.tsx
@@ -325,8 +325,11 @@ const SynapseGrid = forwardRef<
                     ...rowData,
                   })}
                   onChange={handleChange}
+                  addRowsComponent={false}
                   onActiveCellChange={({ cell }) => {
-                    setSelectedRowIndex(cell ? cell.row : null)
+                    if (cell) {
+                      setSelectedRowIndex(cell.row)
+                    }
                   }}
                 />
                 {/* Show validation messages for selected row */}
