a1b987796f9e28899421f9796a7816f8eee3313d
afwillia <anthony.williams@sagebase.org>
2025-09-12 13:08:24 -0700

    getCellClassName uses extractColumnValidationMessages to set cell validation status

--- packages/synapse-react-client/src/components/DataGrid/utils/getCellClassName.ts
+++ packages/synapse-react-client/src/components/DataGrid/utils/getCellClassName.ts
@@ -1,5 +1,4 @@
 import classNames from 'classnames'
-import { extractUniqueValidationFieldNames } from './parseValidationColumns'
 import { DataGridRow } from '../DataGridTypes'
 
 export function getCellClassName(params: {
@@ -11,21 +10,16 @@ export function getCellClassName(params: {
   const { rowData, rowIndex, columnId, selectedRowIndex } = params
 
   const isSelected = selectedRowIndex === rowIndex
-  const validationMessages = rowData.__validationResults?.allValidationMessages
+  const cellValidationResults = rowData.__cellValidationResults
 
   const classList: string[] = []
   if (isSelected) {
     classList.push('cell-row-selected')
   }
 
-  if (validationMessages && columnId) {
-    const invalidFields =
-      extractUniqueValidationFieldNames(validationMessages) || []
-    if (invalidFields.length) {
-      const invalidSet = new Set(invalidFields.map(f => f.toLowerCase()))
-      if (invalidSet.has(columnId.toLowerCase())) {
-        classList.push('cell-invalid')
-      }
+  if (cellValidationResults && columnId) {
+    if (cellValidationResults.has(columnId)) {
+      classList.push('cell-invalid')
     }
   }
 
