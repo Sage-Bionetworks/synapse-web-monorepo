b7ca0909dca054a59a8b1570d8cba2808be0a02c
afwillia <anthony.williams@sagebase.org>
2025-09-19 15:12:56 -0700

    remove AutocompleteMultipleColumn put in a different file

--- packages/synapse-react-client/src/components/DataGrid/columns/AutocompleteColumn.tsx
+++ packages/synapse-react-client/src/components/DataGrid/columns/AutocompleteColumn.tsx
@@ -1,5 +1,5 @@
 import parseFreeTextGivenJsonSchemaType from '@/components/DataGrid/utils/parseFreeTextUsingJsonSchemaType'
-import { Autocomplete, TextField, Chip } from '@mui/material'
+import { Autocomplete, TextField } from '@mui/material'
 import { JSONSchema7Type } from 'json-schema'
 import { useState } from 'react'
 import { CellComponent, CellProps, Column } from 'react-datasheet-grid'
@@ -19,7 +19,7 @@ export type AutocompleteCellProps = CellProps & {
   colType?: JSONSchema7Type
 }
 
-function castCellValueToString(toCast: any): string {
+export function castCellValueToString(toCast: any): string {
   if (typeof toCast === 'object') {
     return JSON.stringify(toCast)
   }
@@ -105,115 +105,6 @@ export function autocompleteColumn({
     // If we update our enums to support labels, then we can update copy to copy the label and paste to lookup the mapping from label -> value
     copyValue: ({ rowData }) => rowData,
     pasteValue: ({ value }) => value,
-    disableKeys: true,
-    keepFocus: true,
-  }
-}
-
-export type AutocompleteMultipleCellProps = CellProps & {
-  rowData: AutocompleteOption[]
-  setRowData: (value: AutocompleteOption[]) => void
-  choices: AutocompleteOption[]
-  colType?: JSONSchema7Type
-  limitTags?: number
-}
-
-function AutocompleteMultipleCell({
-  rowData,
-  setRowData,
-  choices,
-  colType,
-  limitTags,
-}: AutocompleteMultipleCellProps) {
-  const [localInputState, setLocalInputState] = useState<string>('')
-
-  return (
-    <Autocomplete
-      multiple
-      freeSolo
-      disablePortal={false}
-      disableCloseOnSelect
-      limitTags={limitTags}
-      size="small"
-      options={choices}
-      getOptionLabel={option => castCellValueToString(option)}
-      value={rowData || []}
-      onInputChange={(_, newInputValue) => {
-        setLocalInputState(newInputValue)
-      }}
-      onChange={(_e, newVal, reason) => {
-        if (reason === 'createOption') {
-          const lastValue = newVal[newVal.length - 1] as string
-          const parsedValue = parseFreeTextGivenJsonSchemaType(
-            lastValue,
-            colType,
-          )
-          const updatedValues = [...newVal.slice(0, -1), parsedValue]
-          setRowData(updatedValues)
-        } else {
-          setRowData(newVal as AutocompleteOption[])
-        }
-      }}
-      onBlur={() => {
-        if (localInputState.trim()) {
-          const parsedValue = parseFreeTextGivenJsonSchemaType(
-            localInputState,
-            colType,
-          )
-          setRowData([...(rowData || []), parsedValue])
-          setLocalInputState('')
-        }
-      }}
-      renderValue={(value: readonly string[], getItemProps) =>
-        value.map((option: string, index: number) => {
-          const { key, ...itemProps } = getItemProps({ index })
-          return <Chip label={option} key={key} {...itemProps} />
-        })
-      }
-      renderInput={params => <TextField {...params} />}
-      sx={{
-        width: '100%',
-        height: '100%',
-        '& .MuiAutocomplete-inputRoot': {
-          padding: '0 10px',
-          backgroundColor: 'inherit',
-        },
-        '& .MuiFormControl-root': { height: '100%' },
-      }}
-    />
-  )
-}
-
-export type AutocompleteMultipleColumnProps = {
-  choices: AutocompleteOption[]
-  colType?: JSONSchema7Type
-  limitTags?: number
-}
-
-export function autocompleteMultipleColumn({
-  choices,
-  colType,
-  limitTags,
-}: AutocompleteMultipleColumnProps): Partial<Column> {
-  return {
-    component: ((
-      props: Omit<AutocompleteMultipleCellProps, 'choices' | 'limitTags'>,
-    ) => (
-      <AutocompleteMultipleCell
-        {...props}
-        choices={choices}
-        colType={colType}
-        limitTags={limitTags}
-      />
-    )) as CellComponent,
-    copyValue: ({ rowData }) => JSON.stringify(rowData || []),
-    pasteValue: ({ value }) => {
-      try {
-        return JSON.parse(value)
-      } catch {
-        return [value]
-      }
-    },
     disableKeys: true,
     keepFocus: true,
   }
