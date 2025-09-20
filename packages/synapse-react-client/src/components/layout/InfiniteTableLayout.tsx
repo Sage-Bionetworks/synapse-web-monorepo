ebda40cca4c93129323d9ee985ebda4653525da1
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-09-20 14:48:49 -0400

    SWC-7468
    - Refactor to create shared InfiniteTableLayout and use in components that all use a similar layout.

---
+++ packages/synapse-react-client/src/components/layout/InfiniteTableLayout.tsx
@@ -0,0 +1,71 @@
+import { SynapseSpinner } from '@/components/LoadingScreen/LoadingScreen'
+import { Button, Typography } from '@mui/material'
+
+export type InfiniteTableLayoutProps = {
+  /** The table of data to display */
+  table: React.ReactNode
+  /** Whether the first page of data is loading. If true, a spinner will be shown */
+  isLoading: boolean
+  /** Optional loader to display when loading. Default is a spinner */
+  loader?: React.ReactNode
+  /** Whether the first page of data is empty. If true, "No Results" will be shown */
+  isEmpty: boolean
+  /** Whether there is a next page of data. If false, the "Show More" button will not be shown */
+  hasNextPage: boolean
+  /** Callback for when the "Show More" button is clicked */
+  onFetchNextPageClicked: () => void
+  /** Whether the next page of data is loading. If true, the "Show More" button will show a loading state */
+  isFetchingNextPage: boolean
+  /** Optional node to display when there are no results. Default is "No results" */
+  noResults?: React.ReactNode
+}
+
+const DEFAULT_LOADER = (
+  <div className="SRC-center-text">
+    <SynapseSpinner size={40} />
+  </div>
+)
+
+const DEFAULT_NO_RESULTS = (
+  <Typography className="SRC-center-text" variant="body1">
+    No Results
+  </Typography>
+)
+
+/**
+ * Provides a standardized layout of a table with infinite data.
+ */
+export default function InfiniteTableLayout(props: InfiniteTableLayoutProps) {
+  const {
+    table,
+    isLoading,
+    isEmpty,
+    hasNextPage,
+    onFetchNextPageClicked,
+    isFetchingNextPage,
+    noResults = DEFAULT_NO_RESULTS,
+    loader = DEFAULT_LOADER,
+  } = props
+
+  return (
+    <div>
+      {table}
+      {isLoading && loader}
+      {!isLoading && isEmpty && noResults}
+      {!hasNextPage ? (
+        <></>
+      ) : (
+        <Button
+          variant="outlined"
+          color="primary"
+          loading={isFetchingNextPage}
+          onClick={() => {
+            onFetchNextPageClicked()
+          }}
+        >
+          Show More
+        </Button>
+      )}
+    </div>
+  )
+}
