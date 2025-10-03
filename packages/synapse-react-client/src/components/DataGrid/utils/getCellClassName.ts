b23b8cf29d64ec1de1608f3cb29bd7db3e2a25f7
afwillia <anthony.williams@sagebase.org>
2025-10-03 11:51:25 -0700

    set cell selection class in getCellClassName

--- packages/synapse-react-client/src/components/DataGrid/utils/getCellClassName.ts
+++ packages/synapse-react-client/src/components/DataGrid/utils/getCellClassName.ts
@@ -1,13 +1,24 @@
 import classNames from 'classnames'
 import { DataGridRow } from '../DataGridTypes'
+import { SelectionWithId } from 'react-datasheet-grid/dist/types'
+import { Column } from 'react-datasheet-grid'
 
 export function getCellClassName(params: {
   rowData: DataGridRow
   rowIndex: number
   columnId?: string
   selectedRowIndex: number | null
+  lastSelection?: SelectionWithId | null
+  colValues?: Column[]
 }): string | undefined {
-  const { rowData, rowIndex, columnId, selectedRowIndex } = params
+  const {
+    rowData,
+    rowIndex,
+    columnId,
+    selectedRowIndex,
+    lastSelection,
+    colValues,
+  } = params
 
   const isSelected = selectedRowIndex === rowIndex
   const cellValidationResults = rowData.__cellValidationResults
@@ -20,6 +31,20 @@ export function getCellClassName(params: {
   if (cellValidationResults && columnId) {
     if (cellValidationResults.has(columnId)) {
       classList.push('cell-invalid')
+    }
+  }
+
+  // Add selection styling based on lastSelection
+  if (lastSelection && columnId && colValues) {
+    const isInSelection =
+      rowIndex >= lastSelection.min.row &&
+      rowIndex <= lastSelection.max.row &&
+      colValues.findIndex(col => col.id === columnId) >=
+        lastSelection.min.col &&
+      colValues.findIndex(col => col.id === columnId) <= lastSelection.max.col
+
+    if (isInSelection) {
+      classList.push('cell-selected')
     }
   }
 
