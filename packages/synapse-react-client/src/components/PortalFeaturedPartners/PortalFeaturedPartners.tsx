d1a99be1150ad82465ee7f7c45d532c2887b2769
“kianamcc” <user.email=kianamccu@gmail.com>
2025-10-15 10:09:35 -0700

    partners component

--- packages/synapse-react-client/src/components/PortalFeaturedPartners/PortalFeaturedPartners.tsx
+++ packages/synapse-react-client/src/components/PortalFeaturedPartners/PortalFeaturedPartners.tsx
@@ -3,13 +3,22 @@ import { SynapseConstants } from '@/utils'
 import { parseEntityIdFromSqlStatement } from '@/utils/functions'
 import { getFieldIndex } from '@/utils/functions/queryUtils'
 import { useImageUrl } from '@/utils/hooks/useImageUrlUtils'
-import { Box, CardMedia, Link, Skeleton, Typography } from '@mui/material'
+import {
+  Box,
+  CardMedia,
+  Link,
+  Skeleton,
+  SxProps,
+  Typography,
+} from '@mui/material'
 import { QueryBundleRequest, Row } from '@sage-bionetworks/synapse-types'
 import PortalSectionHeader from '../PortalSectionHeader'
 
 export type PortalFeaturedPartnersProps = {
+  variation?: 'default' | 'centered'
   sql: string
   titleText?: string
+  sx?: SxProps
 }
 
 type Icons = {
@@ -41,74 +50,141 @@ const PartnerIcon = ({
   }
 
   return (
-    <Link
-      href={partner.values[websiteColIndex] ?? ''}
-      target="_blank"
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textDecoration: 'none !important',
-      }}
-    >
-      {imageUrl ? (
-        <CardMedia
-          component="img"
-          image={imageUrl}
-          aria-hidden="true"
+    <>
+      {partner.values[websiteColIndex] ? (
+        <Link
+          href={partner.values[websiteColIndex] ?? ''}
+          target="_blank"
           sx={{
-            width: '156px',
-            height: '101px',
-            objectFit: 'contain',
-            borderRadius: '10px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textDecoration: 'none !important',
           }}
-        />
+        >
+          {imageUrl ? (
+            <CardMedia
+              component="img"
+              image={imageUrl}
+              aria-hidden="true"
+              sx={{
+                width: '156px',
+                height: '101px',
+                objectFit: 'contain',
+                borderRadius: '10px',
+              }}
+            />
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                width: '165px',
+                padding: '3px',
+                flexDirection: 'column',
+                gap: '10px',
+                border: '2px solid',
+                borderColor: 'grey.900',
+                backgroundColor: '#FFF',
+                textAlign: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  padding: '10px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: '10px',
+                  border: '1px solid',
+                  borderColor: 'grey.900',
+                }}
+              >
+                <Typography
+                  variant="headline3"
+                  sx={{
+                    fontSize: '14px',
+                    fontWeight: 400,
+                    lineHeight: '130%',
+                    color: 'grey.900',
+                  }}
+                >
+                  {partner?.values[organizationNameColIndex ?? -1]}
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        </Link>
       ) : (
         <Box
           sx={{
             display: 'flex',
-            width: '165px',
-            padding: '3px',
-            flexDirection: 'column',
-            gap: '10px',
-            border: '2px solid',
-            borderColor: 'grey.900',
-            backgroundColor: '#FFF',
-            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textDecoration: 'none !important',
           }}
         >
-          <Box
-            sx={{
-              display: 'flex',
-              padding: '10px',
-              justifyContent: 'center',
-              alignItems: 'center',
-              gap: '10px',
-              border: '1px solid',
-              borderColor: 'grey.900',
-            }}
-          >
-            <Typography
-              variant="headline3"
+          {imageUrl ? (
+            <CardMedia
+              component="img"
+              image={imageUrl}
+              aria-hidden="true"
               sx={{
-                fontSize: '14px',
-                fontWeight: 400,
-                lineHeight: '130%',
-                color: 'grey.900',
+                width: '156px',
+                height: '101px',
+                objectFit: 'contain',
+                borderRadius: '10px',
+              }}
+            />
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                width: '165px',
+                padding: '3px',
+                flexDirection: 'column',
+                gap: '10px',
+                border: '2px solid',
+                borderColor: 'grey.900',
+                backgroundColor: '#FFF',
+                textAlign: 'center',
               }}
             >
-              {partner?.values[organizationNameColIndex ?? -1]}
-            </Typography>
-          </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  padding: '10px',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: '10px',
+                  border: '1px solid',
+                  borderColor: 'grey.900',
+                }}
+              >
+                <Typography
+                  variant="headline3"
+                  sx={{
+                    fontSize: '14px',
+                    fontWeight: 400,
+                    lineHeight: '130%',
+                    color: 'grey.900',
+                  }}
+                >
+                  {partner?.values[organizationNameColIndex ?? -1]}
+                </Typography>
+              </Box>
+            </Box>
+          )}
         </Box>
       )}
-    </Link>
+    </>
   )
 }
 
 const PortalFeaturedPartners = ({
   sql,
   titleText = 'Our Partners',
+  variation = 'default',
+  sx,
 }: PortalFeaturedPartnersProps) => {
   const entityId = parseEntityIdFromSqlStatement(sql)
 
@@ -153,20 +229,35 @@ const PortalFeaturedPartners = ({
       sx={{
         display: 'grid',
         padding: { xs: '40px', lg: '20px 80px' },
-        gridTemplateColumns: { xs: '1fr', md: '1fr 3fr' },
+        gridTemplateColumns:
+          variation === 'centered'
+            ? { xs: '1fr' }
+            : { xs: '1fr', md: '1fr 3fr' },
         gap: '50px',
+        textAlign: variation === 'centered' ? 'center' : 'left',
+        ...sx,
       }}
     >
-      <PortalSectionHeader
-        title={titleText}
-        sx={{ h2: { fontSize: '24px', paddingTop: '26px', width: '100%' } }}
-      />
+      {variation === 'default' ? (
+        <PortalSectionHeader
+          title={titleText}
+          sx={{ h2: { fontSize: '24px', paddingTop: '26px', width: '100%' } }}
+        />
+      ) : (
+        <Typography
+          variant="headline1"
+          sx={{ fontSize: '48px', color: 'grey.900' }}
+        >
+          {titleText}
+        </Typography>
+      )}
       <Box
         sx={theme => ({
           display: 'flex',
           flexWrap: 'wrap',
           gap: '64px',
           flex: '1 1 0',
+          justifyContent: variation === 'centered' ? 'center' : 'flex-start',
           [theme.breakpoints.down('sm')]: {
             justifyContent: 'center',
           },
