7cbeb6dd719ea707552a4d9fe9a031dcd5a61b11
“kianamcc” <user.email=kianamccu@gmail.com>
2025-11-05 12:53:41 -0800

    cleanup

--- packages/synapse-react-client/src/components/BasePortalCard/index.ts
+++ packages/synapse-react-client/src/components/BasePortalCard/index.ts
@@ -1,4 +1,2 @@
 import ColorfulPortalCardWithChips from './ColorfulPortalCardWithChips/ColorfulPortalCardWithChips'
-import type { PortalCardColumnMapping } from './ColorfulPortalCardWithChips/ColorfulPortalCardWithChips'
 export { ColorfulPortalCardWithChips }
-export type { PortalCardColumnMapping }
