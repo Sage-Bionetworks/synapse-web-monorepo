a95e29d5c2a92921585dac801bab0160096c0058
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2025-11-07 11:29:31 -0500

    PLFM-9342 - Compare validation timestamp to data timestamps to determine if validation is up-to-date

--- packages/synapse-react-client/src/components/DataGrid/utils/modelRowsToGrid.ts
+++ packages/synapse-react-client/src/components/DataGrid/utils/modelRowsToGrid.ts
@@ -3,6 +3,7 @@ import {
   GridModel,
   GridModelSnapshot,
 } from '@/components/DataGrid/DataGridTypes'
+import { isValidationUpToDateForRow } from '@/components/DataGrid/utils/isValidationUpToDateForRow'
 import { GRID_ROW_REACT_KEY_PROPERTY } from './DataGridUtils'
 import logicalTimestampToString from '@/components/DataGrid/utils/logicalTimestampToString'
 import { extractColumnValidationMessages } from './extractColumnValidationMessages'
@@ -18,7 +19,7 @@ export default function modelRowsToGrid(
 ): DataGridRow[] {
   if (!model || !modelSnapshot) return []
   const { columnNames, columnOrder, rows } = modelSnapshot
-  const gridRows = rows.map((row): DataGridRow => {
+  const gridRows = rows.map((row, rowIndex): DataGridRow => {
     const rowObj: DataGridRow = {}
     columnOrder.forEach((index: number) => {
       const columnName = columnNames[index]
@@ -29,10 +30,19 @@ export default function modelRowsToGrid(
 
     // Embed the logical timestamp as a string for use as the React key (a stable value unique to each row)
     rowObj[GRID_ROW_REACT_KEY_PROPERTY] = logicalTimestampToString(
-      model.api.obj(['rows', String(rows.indexOf(row))]).node.id,
+      model.api.obj(['rows', String(rowIndex)]).node.id,
     )
 
     rowObj.__validationResults = row.metadata?.rowValidation
+    if (rowObj.__validationResults) {
+      rowObj.__validationStatus = rowObj.__validationResults?.isValid
+        ? 'valid'
+        : 'invalid'
+    }
+
+    if (!isValidationUpToDateForRow(model, rowIndex)) {
+      rowObj.__validationStatus = 'pending'
+    }
 
     // Parse the validation messages into a Map of column name to messages
     if (rowObj.__validationResults?.allValidationMessages) {
