29c28328394ea334cd0d8cf6c7c3be8f43036933
jay-hodgson <jay.hodgson@sagebase.org>
2025-11-25 14:55:01 -0800

    comment out NF tools card CardTypeAdornment (CompletenessIndicator)

--- apps/portals/nf/src/config/synapseConfigs/tools.tsx
+++ apps/portals/nf/src/config/synapseConfigs/tools.tsx
@@ -53,11 +53,11 @@ export const toolsCardConfiguration: CardConfiguration = {
   },
   secondaryLabelLimit: 4,
   genericCardSchema: toolsSchema,
-  CardTypeAdornment: ({ schema, data }) => {
-    const completenessCategory = data[schema['completenessCategory']]
-    if (!completenessCategory) return null
-    return <CompletenessIndicator completenessCategory={completenessCategory} />
-  },
+  // CardTypeAdornment: ({ schema, data }) => {
+  //   const completenessCategory = data[schema['completenessCategory']]
+  //   if (!completenessCategory) return null
+  //   return <CompletenessIndicator completenessCategory={completenessCategory} />
+  // },
 }
 const rgbIndex = 6
 const tools: QueryWrapperPlotNavProps = {
