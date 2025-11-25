4a857bacce4b1bf559c78e345d1e82d4aae7c34f
“kianamcc” <user.email=kianamccu@gmail.com>
2025-11-25 15:18:56 -0800

    cleanup

--- apps/portals/eliteportal/src/pages/RootApp.tsx
+++ apps/portals/eliteportal/src/pages/RootApp.tsx
@@ -1,25 +1,9 @@
 import App from '@sage-bionetworks/synapse-portal-framework/App'
 import RepositoryUnderReviewAlert from '@sage-bionetworks/synapse-portal-framework/components/RepositoryUnderReviewAlert'
 import TemporaryBanner from 'synapse-react-client/components/TemporaryBanner/TemporaryBanner'
-import Link from '@mui/material/Link'
 import FundingBanner from '@sage-bionetworks/synapse-portal-framework/components/eliteportal/FundingBanner'
 
 function RootApp() {
-  const description = (
-    <>
-      Seeking U.S. Researchers: Apply by January 26, 2026 for up to $132,500 to
-      expand Exceptional Longevity research with NIA-funded data. View the full
-      FOA and submission requirements{' '}
-      <Link
-        href="https://www.longevityconsortium.org/funding-opportunity/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        here
-      </Link>
-      .
-    </>
-  )
   return (
     <App>
       {/* PORTALS-3508 */}
