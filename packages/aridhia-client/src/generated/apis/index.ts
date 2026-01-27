d9cb012c6c4884a47431dc45090a7b468986f4f1
jay-hodgson <jay.hodgson@sagebase.org>
2026-01-27 13:33:26 -0800

    need to check in generated client

---
+++ packages/aridhia-client/src/generated/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationApi'
+export * from './DatasetsApi'
+export * from './RequestsApi'
+export * from './WorkflowsApi'
+export * from './WorkspaceApi'
