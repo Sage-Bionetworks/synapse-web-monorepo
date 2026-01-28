2e3bee8b92fe5125b31bcf3ea9a8c131542af8ba
jay-hodgson <jay.hodgson@sagebase.org>
2026-01-28 14:10:44 -0800

    attempt to remove aridhia client generated

--- packages/aridhia-client/src/generated/apis/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthenticationApi'
-export * from './DatasetsApi'
-export * from './RequestsApi'
-export * from './WorkflowsApi'
-export * from './WorkspaceApi'
