a713612822b1d8de95a1c0950477675f55d0e6ba
Anthony Williams <anthony.williams@sagebase.org>
2026-03-03 13:52:12 -0800

    update header component so icons are always visible when shrinking column width

--- packages/synapse-react-client/src/components/DataGrid/components/ColumnHeaderWithTooltip.tsx
+++ packages/synapse-react-client/src/components/DataGrid/components/ColumnHeaderWithTooltip.tsx
@@ -22,45 +22,70 @@ export function ColumnHeaderWithTooltip({
   onTogglePin,
 }: ColumnHeaderWithTooltipProps) {
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-      <span>{name}</span>
-      {showPinIcon && (
-        <Tooltip
-          title={isPinned ? 'Unpin column' : 'Pin column'}
-          placement="top"
-          enterNextDelay={TOOLTIP_DELAY_SHOW}
-        >
-          <IconButton
-            size="small"
-            color="inherit"
-            onClick={onTogglePin}
-            sx={{
-              color: 'inherit',
-              opacity: isPinned ? 1 : 0.7,
-              '&:hover': {
-                opacity: 1,
-              },
-            }}
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: 'minmax(0, 1fr) auto',
+        gap: 0.5,
+        width: '100%',
+        alignItems: 'center',
+        minWidth: 0,
+      }}
+    >
+      {/* Text area - shrinks first */}
+      <Box
+        component="span"
+        sx={{
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+          whiteSpace: 'nowrap',
+          minWidth: 0,
+        }}
+        title={name} // Native tooltip for truncated text
+      >
+        {name}
+      </Box>
+
+      {/* Icons area - fixed size, never shrinks */}
+      <Box sx={{ display: 'flex', gap: 0.5, flexShrink: 0 }}>
+        {showPinIcon && (
+          <Tooltip
+            title={isPinned ? 'Unpin column' : 'Pin column'}
+            placement="top"
+            enterNextDelay={TOOLTIP_DELAY_SHOW}
           >
-            {isPinned ? (
-              <PushPin fontSize="inherit" />
-            ) : (
-              <PushPinOutlined fontSize="inherit" />
-            )}
-          </IconButton>
-        </Tooltip>
-      )}
-      {description && (
-        <Tooltip
-          title={description}
-          placement="top"
-          enterNextDelay={TOOLTIP_DELAY_SHOW}
-        >
-          <IconButton size="small" color="inherit">
-            <HelpTwoTone fontSize="inherit" />
-          </IconButton>
-        </Tooltip>
-      )}
+            <IconButton
+              size="small"
+              color="inherit"
+              onClick={onTogglePin}
+              sx={{
+                color: 'inherit',
+                opacity: isPinned ? 1 : 0.7,
+                '&:hover': {
+                  opacity: 1,
+                },
+              }}
+            >
+              {isPinned ? (
+                <PushPin fontSize="inherit" />
+              ) : (
+                <PushPinOutlined fontSize="inherit" />
+              )}
+            </IconButton>
+          </Tooltip>
+        )}
+        {description && (
+          <Tooltip
+            title={description}
+            placement="top"
+            enterNextDelay={TOOLTIP_DELAY_SHOW}
+          >
+            <IconButton size="small" color="inherit">
+              <HelpTwoTone fontSize="inherit" />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Box>
     </Box>
   )
 }
