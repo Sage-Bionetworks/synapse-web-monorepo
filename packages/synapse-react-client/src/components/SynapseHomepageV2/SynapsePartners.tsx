6626711458beb3f5d19e51247c0d7b979674d17c
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-02 16:36:37 -0700

    nih size fix

--- packages/synapse-react-client/src/components/SynapseHomepageV2/SynapsePartners.tsx
+++ packages/synapse-react-client/src/components/SynapseHomepageV2/SynapsePartners.tsx
@@ -4,17 +4,15 @@ import { ReactComponent as Ctf } from '@/assets/homepage/ctf.svg'
 import { ReactComponent as Gff } from '@/assets/homepage/gff.svg'
 import { ReactComponent as Gray } from '@/assets/homepage/gray.svg'
 import { ReactComponent as MlCommons } from '@/assets/homepage/ml-commons.svg'
-import { ReactComponent as Nci } from '@/assets/homepage/nci.svg'
+import { ReactComponent as Nci } from '@/assets/homepage/nih-nci.svg'
 import { ReactComponent as NihAging } from '@/assets/homepage/nih-aging.svg'
 import { ReactComponent as NihHeart } from '@/assets/homepage/nih-heart.svg'
-import { ReactComponent as NihMental } from '@/assets/homepage/nih-mental.svg'
+import { ReactComponent as NihMental } from '@/assets/homepage/nih-nimh.svg'
 import { ReactComponent as Ntap } from '@/assets/homepage/ntap.svg'
 import { ReactComponent as Sloan } from '@/assets/homepage/sloan.svg'
 import { Box } from '@mui/material'
 
 export function SynapsePartners() {
-  const PARTNER_LOGO_HEIGHT = 60
-
   return (
     <Box sx={{ m: 'auto', maxWidth: '1200px' }}>
       <Box
@@ -33,48 +31,48 @@ export function SynapsePartners() {
         }}
       >
         <a href="https://sloan.org/" target="_blank" rel="noreferrer">
-          <Sloan height={PARTNER_LOGO_HEIGHT} />
+          <Sloan />
         </a>
         <a href="https://www.aacr.org/" target="_blank" rel="noreferrer">
-          <Aacr height={PARTNER_LOGO_HEIGHT} />
+          <Aacr />
         </a>
         <a
           href="https://www.cancerresearch.org/"
           target="_blank"
           rel="noreferrer"
         >
-          <Cri height={PARTNER_LOGO_HEIGHT} />
+          <Cri />
         </a>
         <a href="https://www.ctf.org/" target="_blank" rel="noreferrer">
-          <Ctf height={PARTNER_LOGO_HEIGHT} />
+          <Ctf />
         </a>
         <a
           href="https://gilbertfamilyfoundation.org/"
           target="_blank"
           rel="noreferrer"
         >
-          <Gff height={PARTNER_LOGO_HEIGHT} />
+          <Gff />
         </a>
         <a href="http://grayfoundation.org/" target="_blank" rel="noreferrer">
-          <Gray height={PARTNER_LOGO_HEIGHT} />
+          <Gray />
         </a>
         <a href="https://mlcommons.org/" target="_blank" rel="noreferrer">
-          <MlCommons height={PARTNER_LOGO_HEIGHT} />
+          <MlCommons />
         </a>
         <a href="https://www.cancer.gov/" target="_blank" rel="noreferrer">
-          <Nci height={PARTNER_LOGO_HEIGHT} />
+          <Nci />
         </a>
         <a href="https://www.nhlbi.nih.gov/" target="_blank" rel="noreferrer">
-          <NihHeart height={PARTNER_LOGO_HEIGHT} />
+          <NihHeart />
         </a>
         <a href="https://www.nia.nih.gov/" target="_blank" rel="noreferrer">
-          <NihAging height={PARTNER_LOGO_HEIGHT} />
+          <NihAging />
         </a>
         <a href="https://www.nimh.nih.gov/" target="_blank" rel="noreferrer">
-          <NihMental height={PARTNER_LOGO_HEIGHT} />
+          <NihMental />
         </a>
         <a href="https://www.n-tap.org/" target="_blank" rel="noreferrer">
-          <Ntap height={PARTNER_LOGO_HEIGHT} />
+          <Ntap />
         </a>
       </Box>
     </Box>
