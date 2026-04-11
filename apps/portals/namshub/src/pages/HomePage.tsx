241b9068dea268fd9e9909a13a4b12391646076b
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-10 17:26:59 -0700

    portal header update, included in e2e

--- apps/portals/namshub/src/pages/HomePage.tsx
+++ apps/portals/namshub/src/pages/HomePage.tsx
@@ -1,16 +1,9 @@
-import PortalHomePageHeader from 'synapse-react-client/components/PortalHomePageHeader/PortalHomePageHeader'
+import NamshubHeader from '@sage-bionetworks/synapse-portal-framework/components/namshub/NamshubHeader'
 
 export default function Homepage() {
   return (
     <>
-      <PortalHomePageHeader
-        backgroundCss={`linear-gradient(90deg, #F6FCFF 11.06%, #DCEFF9 100%)`}
-        title={'Advancing NAMs through shared dialogue'}
-        subTitle={''}
-        description={
-          'Explore Data, Models, and more generated from the New Approach Methodologies (NAMs) in the Complement-ARIE NAMHub'
-        }
-      />
+      <NamshubHeader />
     </>
   )
 }
