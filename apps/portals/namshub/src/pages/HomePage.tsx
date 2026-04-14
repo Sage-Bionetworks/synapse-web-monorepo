31215dfe2233c5ed4134db71845c07be4c5e7fda
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-14 14:31:41 -0700

    rss feed cards

--- apps/portals/namshub/src/pages/HomePage.tsx
+++ apps/portals/namshub/src/pages/HomePage.tsx
@@ -1,11 +1,23 @@
 import NamshubHeader from '@sage-bionetworks/synapse-portal-framework/components/namshub/NamshubHeader'
 import NamshubAbout from '@sage-bionetworks/synapse-portal-framework/components/namshub/NamshubAbout'
+import { SectionLayout } from '@sage-bionetworks/synapse-portal-framework/components/SectionLayout'
+import { RssFeedCards } from 'synapse-react-client'
+import styles from './Homepage.module.scss'
 
 export default function Homepage() {
   return (
     <>
       <NamshubHeader />
       <NamshubAbout />
+      <div className={styles.eventSection}>
+        <SectionLayout title="Events" centerTitle>
+          <RssFeedCards
+            url="https://nhdcc.wpenginepowered.com"
+            itemsToShow={2}
+            allowCategories={[]}
+          />
+        </SectionLayout>
+      </div>
     </>
   )
 }
