50436a5722c09b662329cfa8aafe910cccfcd017
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-14 13:33:54 -0700

    terms and services, logos

--- apps/portals/namshub/src/pages/HomePage.tsx
+++ apps/portals/namshub/src/pages/HomePage.tsx
@@ -1,11 +1,31 @@
 import NamshubHeader from '@sage-bionetworks/synapse-portal-framework/components/namshub/NamshubHeader'
 import NamshubAbout from '@sage-bionetworks/synapse-portal-framework/components/namshub/NamshubAbout'
+import DevelopedBySage from '@sage-bionetworks/synapse-portal-framework/components/DevelopedBySage'
+import { ReactComponent as NYUGSOMLogo } from '../assets/NYUGSOM.svg'
+import { ReactComponent as SageLogo } from '@sage-bionetworks/synapse-portal-framework/components/assets/sage-logo.svg'
+import { Link } from '@mui/material'
+import styles from './Homepage.module.scss'
 
 export default function Homepage() {
   return (
     <>
       <NamshubHeader />
       <NamshubAbout />
+      <div className={styles.termsAndConditions}>
+        <DevelopedBySage />
+      </div>
+      <div className={styles.logoContainer}>
+        <Link href="https://med.nyu.edu" target="_blank" rel="noreferrer">
+          <NYUGSOMLogo />
+        </Link>
+        <Link
+          href="https://sagebionetworks.org/platform/synapse"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SageLogo />
+        </Link>
+      </div>
     </>
   )
 }
