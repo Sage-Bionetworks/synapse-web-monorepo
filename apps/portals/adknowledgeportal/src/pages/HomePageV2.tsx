52c2d5bec41b3c186aad000a3fcb1a2f9468670a
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-16 14:19:00 -0700

    feature flag, new homepage setup, contribution card component

---
+++ apps/portals/adknowledgeportal/src/pages/HomePageV2.tsx
@@ -0,0 +1,11 @@
+import AdknowledgeContributeCard from '@sage-bionetworks/synapse-portal-framework/components/adknowledge/AdknowledgeContributeCard/AdknowledgeContributeCard'
+
+function HomePageV2() {
+  return (
+    <div className="HomePageV2">
+      <AdknowledgeContributeCard />
+    </div>
+  )
+}
+
+export default HomePageV2
