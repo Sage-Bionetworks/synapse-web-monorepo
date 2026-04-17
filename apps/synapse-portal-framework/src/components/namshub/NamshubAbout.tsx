6f0d28c35a78f79040ab6887aab15e38cf4fd5f3
“kianamcc” <user.email=kianamccu@gmail.com>
2026-04-17 16:34:08 -0700

    small fix for spacing adjustment

--- apps/synapse-portal-framework/src/components/namshub/NamshubAbout.tsx
+++ apps/synapse-portal-framework/src/components/namshub/NamshubAbout.tsx
@@ -21,26 +21,24 @@ function NamshubAbout() {
             }
           />
         </div>
-        <span className={styles.aboutDescription}>
-          <Typography variant="body1">
-            Researchers developing new drugs or studying human diseases often
-            rely on animal models that don't accurately reflect human biology. A
-            growing body of research points to human-based alternatives, such as
-            organ-on-chip devices and organoids grown from patient cells,
-            collectively called <b>N</b>ew <b>A</b>pproach <b>M</b>ethodologies
-            (NAMs), as more reliable tools for modeling human health and
-            disease.
-          </Typography>
-          <br />
-          <Typography variant="body1">
-            The NAM Hub will enable the standardization, harmonization, and
-            sharing of datasets - from molecular tests to 3D cultures to
-            simulated population outcomes. The hub will serve as the
-            consortium’s backbone for data, metadata, code, and computational
-            models; provide AI-augmented data curation and a framework for
-            harmonizing NAMs data across the consortium; and be a source of
-            analytical tools to help researchers work with datasets.
-          </Typography>
+        <Typography variant="body1" className={styles.aboutTextColor}>
+          Researchers developing new drugs or studying human diseases often rely
+          on animal models that don't accurately reflect human biology. A
+          growing body of research points to human-based alternatives, such as
+          organ-on-chip devices and organoids grown from patient cells,
+          collectively called <b>N</b>ew <b>A</b>pproach <b>M</b>ethodologies
+          (NAMs), as more reliable tools for modeling human health and disease.
+        </Typography>
+        <Typography variant="body1" className={styles.aboutTextColor}>
+          The NAM Hub will enable the standardization, harmonization, and
+          sharing of datasets - from molecular tests to 3D cultures to simulated
+          population outcomes. The hub will serve as the consortium’s backbone
+          for data, metadata, code, and computational models; provide
+          AI-augmented data curation and a framework for harmonizing NAMs data
+          across the consortium; and be a source of analytical tools to help
+          researchers work with datasets.
+        </Typography>
+        <span className={styles.aboutTextColor}>
           <ColoredBulletListItem
             bulletColor="#b9c83c"
             mainText="Join our growing network of researchers"
