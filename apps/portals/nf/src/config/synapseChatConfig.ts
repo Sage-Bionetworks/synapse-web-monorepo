51bc407ffc6b36b15337ec79b857a67e39c08d0a
Anh Nguyet Vu <32753274+anngvu@users.noreply.github.com>
2026-04-21 10:25:08 -0700

    Update synapseChatConfig.ts

--- apps/portals/nf/src/config/synapseChatConfig.ts
+++ apps/portals/nf/src/config/synapseChatConfig.ts
@@ -1,7 +1,7 @@
 import { SynapseChatProps } from 'synapse-react-client'
 
 const synapseChatConfig: SynapseChatProps = {
-  agentRegistrationId: '238',
+  agentRegistrationId: '247',
   chatbotName: 'NF Portal Assistant',
 }
 export default synapseChatConfig
