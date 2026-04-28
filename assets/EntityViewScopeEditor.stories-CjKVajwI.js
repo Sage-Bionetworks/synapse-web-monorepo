0bf6096270fdb40130ffb9e0d4178629492824a8
kianamcc <kianamccu@gmail.com>
2026-04-28 18:17:19 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@114fecbb8ab884fce051d18dc262f3fae28aa5ce 🚀

--- assets/EntityViewScopeEditor.stories-CjKVajwI.js
+++
@@ -1,9 +0,0 @@
-import{n as e,o as t}from"./chunk-zsgVPwQN.js";import{t as n}from"./react-Bxo1Isko.js";import{Kt as r}from"./TextField-BrxDFlyn.js";import{t as i}from"./jsx-runtime-CR4qI0Ep.js";import{t as a}from"./esm-Bc_ZnX0H.js";import{n as o,t as s}from"./mockProject-BNR0UZ5B.js";import{i as c,r as l}from"./mockEntity-B1Z2lMLk.js";import{n as u,t as d}from"./EntityViewScopeEditor-DEShmeQz.js";var f,p,m,h,g,_;e((()=>{l(),s(),a(),f=t(n(),1),u(),p=i(),m={title:`Synapse/EntityView/Scope Editor`,component:d,decorators:[e=>(0,p.jsx)(r,{sx:{mx:`auto`,p:4,maxWidth:`720px`},children:(0,p.jsx)(e,{})})],render:function(e){let[t,n]=(0,f.useState)([]);return(0,p.jsx)(d,{...e,scopeIds:t,onChange:n})}},h={args:{scopeIds:[]}},g={args:{scopeIds:[o.id,c.id]}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
-  args: {
-    scopeIds: []
-  }
-}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
-  args: {
-    scopeIds: [mockProject.id, mockFolderEntity.id!]
-  }
-}`,...g.parameters?.docs?.source}}},_=[`Empty`,`HasItems`]}))();export{h as Empty,g as HasItems,_ as __namedExportsOrder,m as default};
--- assets/LoginMethodButton.stories-DNs72O-K.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{i as t,o as n}from"./LastLoginInfo-36FF0vLY.js";import{r,t as i}from"./IconSvg-BGK7UYz_.js";import{n as a,t as o}from"./LoginMethodButton-rNsC2gGx.js";var s,c,l,u;e((()=>{n(),a(),r(),{fn:s}=__STORYBOOK_MODULE_TEST__,c={title:`Authentication/LoginMethodButton`,component:o,argTypes:{loginMethod:{options:t,control:`radio`},iconName:{options:i,control:`select`}}},l={args:{loginMethod:`GOOGLE`,iconName:`google24`,onClick:s()}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
-  args: {
-    loginMethod: 'GOOGLE',
-    iconName: 'google24',
-    onClick: fn()
-  }
-}`,...l.parameters?.docs?.source}}},u=[`Demo`]}))();export{l as Demo,u as __namedExportsOrder,c as default};
