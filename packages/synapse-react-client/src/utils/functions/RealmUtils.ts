44aebaef31dcaf570a8403b23a354335f7f8afc1
jay-hodgson <jay.hodgson@sagebase.org>
2026-04-29 16:35:25 -0700

    PORTALS-4199

--- packages/synapse-react-client/src/utils/functions/RealmUtils.ts
+++ packages/synapse-react-client/src/utils/functions/RealmUtils.ts
@@ -8,10 +8,24 @@ import {
  * Check if a realm has the ARCUS_BIOSCIENCES identity provider
  */
 export function hasArcusProvider(realm?: Realm): boolean {
+  return hasProvider(realm, OAuthIdentityProviderProviderEnum.ARCUS_BIOSCIENCES)
+}
+
+/**
+ * Check if a realm has the SAGE_BIONETWORKS identity provider
+ */
+export function hasSageBionetworksProvider(realm?: Realm): boolean {
+  return hasProvider(realm, OAuthIdentityProviderProviderEnum.SAGE_BIONETWORKS)
+}
+
+function hasProvider(
+  realm?: Realm,
+  providerEnum?: OAuthIdentityProviderProviderEnum,
+): boolean {
   if (!realm?.identityProvider) return false
   return realm.identityProvider.some(
     (provider: any) =>
       instanceOfOAuthIdentityProvider(provider) &&
-      provider.provider === OAuthIdentityProviderProviderEnum.ARCUS_BIOSCIENCES,
+      provider.provider === providerEnum,
   )
 }
