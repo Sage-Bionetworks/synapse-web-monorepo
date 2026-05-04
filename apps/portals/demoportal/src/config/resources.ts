0216722a9114051b0c6daad13d66a0ba9539e946
jay-hodgson <jay.hodgson@sagebase.org>
2026-05-04 13:55:33 -0700

    functional files and datasets explore pages

--- apps/portals/demoportal/src/config/resources.ts
+++ apps/portals/demoportal/src/config/resources.ts
@@ -1,5 +1,5 @@
-export const whatWeDoSql = 'SELECT * FROM syn64130706'
+export const whatWeDoSql = 'SELECT * FROM syn74802793'
 
 // REPLACE these with your actual Synapse table IDs
-export const datasetsSql = 'SELECT * FROM syn_REPLACE_WITH_DATASETS_TABLE_ID'
-export const filesSql = 'SELECT * FROM syn_REPLACE_WITH_FILES_TABLE_ID'
+export const datasetsSql = 'SELECT * FROM syn74803253'
+export const filesSql = 'SELECT * FROM syn74803383'
