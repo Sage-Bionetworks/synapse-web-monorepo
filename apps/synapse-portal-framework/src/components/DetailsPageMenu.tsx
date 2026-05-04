48ff41fb21e3886e0dde3ac90e8b99175302aab7
verena <9377970+vpchung@users.noreply.github.com>
2026-05-04 13:01:18 -0700

    fix import

--- apps/synapse-portal-framework/src/components/DetailsPageMenu.tsx
+++ apps/synapse-portal-framework/src/components/DetailsPageMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { DetailsPageSectionLayoutType } from '@/components/DetailsPage/DetailsPageSectionLayout'
 import { scrollToWithOffset } from '@/utils'
 import { Button, Stack, Typography } from '@mui/material'
