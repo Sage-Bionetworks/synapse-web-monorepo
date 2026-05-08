9f4849b5c1b3ccc7331c260e8f39ab6595c7b8ff
jay-hodgson <jay.hodgson@sagebase.org>
2026-05-08 14:26:34 -0700

    refactored after code review, and tested doi redirect (locally)

--- apps/portals/nf/src/pages/DoiRedirect.tsx
+++ apps/portals/nf/src/pages/DoiRedirect.tsx
@@ -1,29 +1,11 @@
-import { Navigate, useSearchParams } from 'react-router'
+import { DoiRedirectComponent } from '@sage-bionetworks/synapse-portal-framework/shared-config/DoiRedirectRoute'
 import { doiRedirector, doiSerializer } from '@/config/doiRedirector'
 
-const OBJECT_ID_QUERY_PARAM = 'objectId'
-
 export default function DoiRedirect() {
-  const [searchParams] = useSearchParams()
-  const doiId = searchParams.get(OBJECT_ID_QUERY_PARAM)
-
-  let redirectUrl: string = '/'
-
-  if (!doiId) {
-    console.error(
-      'Could not redirect to DOI: no id provided in the URLSearchParams.',
-    )
-  } else {
-    try {
-      const [resourceType, keyAttributes] = doiSerializer.deserialize(doiId)
-      redirectUrl = doiRedirector(resourceType, keyAttributes)
-    } catch (e) {
-      console.error(
-        `Could not redirect to DOI: error deserializing id '${doiId}' provided in the URLSearchParams.`,
-        e,
-      )
-    }
-  }
-
-  return <Navigate to={redirectUrl} replace={true} />
+  return (
+    <DoiRedirectComponent
+      deserializer={doiSerializer}
+      redirector={doiRedirector}
+    />
+  )
 }
