44d52459b9645a357eee6abcc08b5a0e6e4549c4
jay-hodgson <jay.hodgson@sagebase.org>
2026-05-12 13:37:00 -0700

    update friendly file size logic, and add tests

--- packages/synapse-react-client/src/utils/functions/calculateFriendlyFileSize.ts
+++ packages/synapse-react-client/src/utils/functions/calculateFriendlyFileSize.ts
@@ -12,16 +12,18 @@ const suffixes: string[] = [
 
 export function calculateFriendlyFileSize(
   bytes: number | null | undefined,
-  fractionDigits: number = 2,
+  fractionDigits?: number,
 ) {
   if (bytes == null) {
     return ''
   }
   // https://stackoverflow.com/questions/10420352/converting-file-size-in-bytes-to-human-readable-string
   const i = Math.floor(Math.log(bytes) / Math.log(1024))
+  // Use 2 decimal places for GB and above, 0 for smaller units
+  const digits = fractionDigits ?? (i >= 3 ? 2 : 0)
   // tslint:disable-next-line
   return (
     (bytes == 0 && '0 Bytes') ||
-    (bytes / Math.pow(1024, i)).toFixed(fractionDigits) + ' ' + suffixes[i]
+    (bytes / Math.pow(1024, i)).toFixed(digits) + ' ' + suffixes[i]
   )
 }
