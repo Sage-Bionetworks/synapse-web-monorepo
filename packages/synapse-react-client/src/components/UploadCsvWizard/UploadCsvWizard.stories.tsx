8c90333307f6c0f288f6cd9bb107775160e53bed
Anthony Williams <anthony.williams@sagebase.org>
2026-05-14 09:18:27 -0700

    add storybook for UploadCsvWizard

---
+++ packages/synapse-react-client/src/components/UploadCsvWizard/UploadCsvWizard.stories.tsx
@@ -0,0 +1,34 @@
+import UploadCsvWizard, { UploadCsvWizardProps } from './UploadCsvWizard'
+import { Meta, StoryObj } from '@storybook/react-vite'
+import { fn } from 'storybook/test'
+
+const meta: Meta<UploadCsvWizardProps> = {
+  title: 'Synapse/Upload CSV Wizard',
+  component: UploadCsvWizard,
+  args: {
+    open: true,
+    onClose: fn(),
+    onComplete: fn(),
+  },
+  parameters: {
+    requireLogin: true,
+  },
+}
+export default meta
+type Story = StoryObj<typeof meta>
+
+/** Create a new Synapse Table from an uploaded CSV. */
+export const CreateMode: Story = {
+  name: 'Create new table',
+  args: {
+    parentId: 'syn00000000',
+  },
+}
+
+/** Append an uploaded CSV to an existing Synapse Table. */
+export const AppendMode: Story = {
+  name: 'Append to existing table',
+  args: {
+    tableId: 'syn00000000',
+  },
+}
