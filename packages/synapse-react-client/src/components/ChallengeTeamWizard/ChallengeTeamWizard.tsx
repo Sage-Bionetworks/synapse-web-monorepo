8928085218ac4aa5c13a84af14e3c5f1b3a3a2a9
verena <9377970+vpchung@users.noreply.github.com>
2026-05-15 11:08:14 -0700

    use "Submission team" for consistency

--- packages/synapse-react-client/src/components/ChallengeTeamWizard/ChallengeTeamWizard.tsx
+++ packages/synapse-react-client/src/components/ChallengeTeamWizard/ChallengeTeamWizard.tsx
@@ -39,7 +39,7 @@ enum ChallengeTeamWizardStep {
 function getStepDialogTitle(step: ChallengeTeamWizardStep) {
   switch (step) {
     case ChallengeTeamWizardStep.SELECT_YOUR_CHALLENGE_TEAM:
-      return 'Select Your Challenge Team'
+      return 'Select Your Submission Team'
     case ChallengeTeamWizardStep.ACCEPT_INVITATION:
       return 'Invitation to Join Team'
     case ChallengeTeamWizardStep.JOIN_REQUEST_FORM:
