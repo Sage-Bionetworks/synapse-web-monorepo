51eee959c4bbda3e6885154e754ed181d9e3b98e
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2026-05-17 11:27:15 -0400

    PORTALS-4189 - SSG for CCKP

--- apps/portals/cancercomplexity/src/pages/EducationalResourcesDetailsPage.tsx
+++ apps/portals/cancercomplexity/src/pages/EducationalResourcesDetailsPage.tsx
@@ -1,9 +1,11 @@
 import { DetailsPageContent } from '@sage-bionetworks/synapse-portal-framework/components/DetailsPage/DetailsPageContentLayout'
 import { DetailsPageContextConsumer } from '@sage-bionetworks/synapse-portal-framework/components/DetailsPage/DetailsPageContext'
 import DetailsPage from '@sage-bionetworks/synapse-portal-framework/components/DetailsPage/index'
-import { useGetPortalComponentSearchParams } from '@sage-bionetworks/synapse-portal-framework/utils/UseGetPortalComponentSearchParams'
+import { createDetailPageRouteExports } from '@sage-bionetworks/synapse-portal-framework/utils/detailPageRouteUtils'
 import { ColumnSingleValueFilterOperator } from '@sage-bionetworks/synapse-types'
+import { useParams } from 'react-router'
 import columnAliases from '../config/columnAliases'
+import { portalMetadata } from '../config/portalMetadata'
 import { educationSql, grantsSql } from '../config/resources'
 import { educationDetailsCardConfiguration } from '../config/synapseConfigs/education'
 import { grantsCardConfiguration } from '../config/synapseConfigs/grants'
@@ -11,9 +13,20 @@ import ErrorPage, {
   SynapseErrorType,
 } from 'synapse-react-client/components/error/ErrorPage'
 import CardContainerLogic from 'synapse-react-client/components/CardContainerLogic/index'
+import { metadataConfig } from './EducationalResourcesDetailsPage.config'
+
+export { metadataConfig }
+
+const _routeExports = createDetailPageRouteExports(
+  metadataConfig,
+  portalMetadata,
+)
+export const loader = _routeExports.loader
+export const clientLoader = _routeExports.clientLoader
+export const meta = _routeExports.meta
 
 function EducationalResourcesDetailsPage() {
-  const { title } = useGetPortalComponentSearchParams()
+  const { title } = useParams<{ title: string }>()
 
   if (!title) {
     return <ErrorPage type={SynapseErrorType.NOT_FOUND} gotoPlace={() => {}} />
@@ -37,8 +50,10 @@ function EducationalResourcesDetailsPage() {
         </>
       }
       sql={educationSql}
+      searchParams={{ title }}
       sqlOperator={ColumnSingleValueFilterOperator.LIKE}
       resourcePrimaryKey={['title']}
+      disableCanonicalUrl
     >
       <DetailsPageContent
         content={[
