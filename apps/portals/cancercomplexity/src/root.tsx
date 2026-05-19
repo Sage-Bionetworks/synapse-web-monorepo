a9fc989b12fcfb56c19a184eeecce82c36c77cc0
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2026-05-19 09:44:36 -0400

    Provide custom `script` for service desk

--- apps/portals/cancercomplexity/src/root.tsx
+++ apps/portals/cancercomplexity/src/root.tsx
@@ -1,6 +1,6 @@
 import {
+  createLayout,
   createRootMeta,
-  Layout,
   links,
 } from '@sage-bionetworks/synapse-portal-framework/ssg/root'
 
@@ -9,5 +9,17 @@ export const meta = createRootMeta({
   portalDescription: import.meta.env.VITE_PORTAL_DESCRIPTION,
   portalKey: import.meta.env.VITE_PORTAL_KEY,
 })
-export { Layout, links }
+
+export const Layout = createLayout({
+  headChildren: (
+    <script
+      data-jsd-embedded
+      data-key="885c2536-2aaa-4f84-a9af-f39f15d44e4a"
+      data-base-url="https://jsd-widget.atlassian.com"
+      src="https://jsd-widget.atlassian.com/assets/embed.js"
+    />
+  ),
+})
+
+export { links }
 export { default } from '@sage-bionetworks/synapse-portal-framework/ssg/root'
