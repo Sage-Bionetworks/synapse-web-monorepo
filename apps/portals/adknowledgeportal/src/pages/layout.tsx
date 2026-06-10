9dd467673d2a66246037a87b6e2783e173ef90e3
jay-hodgson <jay.hodgson@sagebase.org>
2026-06-10 12:14:22 -0700

    DESIGN-1739: Add SurveyDialog, used in ADKP but can be easily reused by other portals

--- apps/portals/adknowledgeportal/src/pages/layout.tsx
+++ apps/portals/adknowledgeportal/src/pages/layout.tsx
@@ -1,5 +1,7 @@
 import App from '@sage-bionetworks/synapse-portal-framework/App'
 import RepositoryUnderReviewAlert from '@sage-bionetworks/synapse-portal-framework/components/RepositoryUnderReviewAlert'
+import SurveyDialog from '@sage-bionetworks/synapse-portal-framework/components/SurveyDialog'
+import { ReactComponent as SurveyDialogImage } from '../assets/survey-dialog.svg'
 
 export function Layout() {
   return (
@@ -9,6 +11,22 @@ export function Layout() {
         portalSpecificDisclaimer={
           "This repository is developed by Sage Bionetworks to host and share resources related to Alzheimer's Disease research, and remains fully operational. We continue to maintain and accept Alzheimer's disease-related data and resources throughout this review process."
         }
+      />
+      <SurveyDialog
+        localStorageKey="adkp-survey-dialog-dismissed"
+        Svg={SurveyDialogImage}
+        title="Help Us Improve the ADKP Ecosystem!"
+        description={
+          <>
+            We need your input! Please complete this 10-minute survey on how the
+            ADKP influences your research outputs and career development.{' '}
+            <strong>
+              Your anonymous feedback helps us enhance our support for the AD
+              research community.
+            </strong>
+          </>
+        }
+        surveyURL="https://docs.google.com/forms/d/e/1FAIpQLSewZ479nxfOpGvahaPG658aL0CjY_M3Ld9i6uDqIzyQhObC6w/viewform"
       />
     </App>
   )
