7e244b969e09ab5733430a852e70aec97fce496d
afwillia <anthony.williams@sagebase.org>
2026-06-12 17:45:15 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@2389f2918520c673f61fe092ab3fcb0d87c5f992 🚀

---
+++ assets/SubscriptionPage.stories-BPddqZye.js
@@ -0,0 +1,11 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{Tn as t,kn as n}from"./synapse-client-DNripIzl.js";import{n as r,t as i}from"./iframe-Da47KvfU.js";import{n as a,t as o}from"./SubscriptionPage-DLUf0Bwe.js";var s,c,l;e((()=>{r(),n(),a(),s={title:`Synapse/Following/SubscriptionPage`,component:o,parameters:{stack:`mock`}},c={name:`SubscriptionPage`,args:{},parameters:{msw:{handlers:{...i(t)}}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'SubscriptionPage',
+  args: {},
+  parameters: {
+    msw: {
+      handlers: {
+        ...getHandlers(MOCK_REPO_ORIGIN)
+      }
+    }
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Demo`]}))();export{c as Demo,l as __namedExportsOrder,s as default};
