7c362cffaf89180ca89daf369e3788f7d1e276b0
jay-hodgson <jay.hodgson@sagebase.org>
2026-06-15 17:08:39 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@b0f17be7b669e95669d0c834e46ddd8a043a4d88 🚀

--- assets/Goals.stories-CQzh_1Dq.js
+++
@@ -1,5 +0,0 @@
-import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t,t as n}from"./Goals-Bfw2jz3I.js";var r,i,a;e((()=>{t(),r={title:`Home Page/Goals`,component:n,parameters:{chromatic:{viewports:[600,1200]}}},i={args:{entityId:`syn22315959`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
-  args: {
-    entityId: 'syn22315959'
-  }
-}`,...i.parameters?.docs?.source}}},a=[`Demo`]}))();export{i as Demo,a as __namedExportsOrder,r as default};
