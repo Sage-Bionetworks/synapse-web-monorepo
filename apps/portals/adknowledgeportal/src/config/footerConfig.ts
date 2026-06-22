f04a4308377dd1002fef31d555df88e5a3c2ff5c
“kianamcc” <user.email=kianamccu@gmail.com>
2026-06-22 13:31:24 -0700

    address figma comments

--- apps/portals/adknowledgeportal/src/config/footerConfig.ts
+++ apps/portals/adknowledgeportal/src/config/footerConfig.ts
@@ -1,6 +1,8 @@
 import { FooterConfig } from '@sage-bionetworks/synapse-portal-framework/types/portal-config'
 
 const footer: FooterConfig = {
+  contactUs: 'https://sagebionetworks.jira.com/servicedesk/customer/portal/12',
+  help: 'https://help.adknowledgeportal.org/apd/',
   about: 'https://help.adknowledgeportal.org/apd/About.1994096680.html',
   variant: 'adkp',
 }
