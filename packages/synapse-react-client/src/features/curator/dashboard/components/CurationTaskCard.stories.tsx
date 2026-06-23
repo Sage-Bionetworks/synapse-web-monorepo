034cfbc472ffe2bf326aeafa4230b472304f3de3
Anthony Williams <anthony.williams@sagebase.org>
2026-06-23 14:59:14 -0700

    add task status toggles to CurationTaskCard stories

--- packages/synapse-react-client/src/features/curator/dashboard/components/CurationTaskCard.stories.tsx
+++ packages/synapse-react-client/src/features/curator/dashboard/components/CurationTaskCard.stories.tsx
@@ -1,3 +1,4 @@
+import { TaskStatusStateEnum } from '@sage-bionetworks/synapse-client'
 import { Meta, StoryObj } from '@storybook/react-vite'
 import CurationTaskCard, { CurationTaskCardProps } from './CurationTaskCard'
 
@@ -6,25 +7,35 @@ const meta = {
   component: CurationTaskCard,
 } satisfies Meta<CurationTaskCardProps>
 export default meta
-type Story = StoryObj<typeof meta>
 
-export const Demo: Story = {
-  args: {
-    taskBundle: {
-      task: {
-        dataType: 'metadata_clinical',
-        taskId: 123,
-        instructions:
-          'Project: Precision Drug Treatment Profiling in Human Pancreatic Tissue',
-        assigneePrincipalId: '273957',
-        taskProperties: {
-          concreteType:
-            'org.sagebionetworks.repo.model.curation.metadata.FileBasedMetadataTaskProperties',
-        },
-      },
-      status: {
-        state: 'NOT_STARTED',
-      },
+const baseTask = {
+  dataType: 'metadata_clinical',
+  taskId: 123,
+  instructions:
+    'Project: Precision Drug Treatment Profiling in Human Pancreatic Tissue',
+  assigneePrincipalId: '273957',
+  taskProperties: {
+    concreteType:
+      'org.sagebionetworks.repo.model.curation.metadata.FileBasedMetadataTaskProperties' as const,
+  },
+}
+
+export const Demo: StoryObj<{ state: TaskStatusStateEnum }> = {
+  argTypes: {
+    state: {
+      control: 'select',
+      options: Object.values(TaskStatusStateEnum),
     },
   },
+  args: {
+    state: TaskStatusStateEnum.NOT_STARTED,
+  },
+  render: ({ state }) => (
+    <CurationTaskCard
+      taskBundle={{
+        task: baseTask,
+        status: { state },
+      }}
+    />
+  ),
 }
