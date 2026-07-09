2ae0f664d5645ca1468064488a7615a89d7be2d0
jay-hodgson <jay.hodgson@sagebase.org>
2026-07-09 15:41:05 -0700

    fix import

--- packages/synapse-react-client/src/assets/icons/NIHIcon.tsx
+++ packages/synapse-react-client/src/assets/icons/NIHIcon.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
 import { Box } from '@mui/material'
 import { SvgIconProps } from '@mui/material/SvgIcon'
-import NIHSvg from './nih.svg?react'
+import React from 'react'
+import { ReactComponent as NIHSvg } from './nih.svg'
 
 export const NIHIcon = ({ sx }: SvgIconProps): React.ReactNode => {
   return (
     <Box
-      component={NIHSvg as React.ElementType}
       sx={[
-        { display: 'inline-block', verticalAlign: 'middle', height: '1em' },
+        {
+          display: 'inline-flex',
+          verticalAlign: 'middle',
+          height: '1em',
+          lineHeight: 0,
+          width: 'auto',
+        },
         sx ?? {},
+        // Always keep width auto to preserve the landscape aspect ratio
         { width: 'auto' },
       ]}
-    />
+    >
+      <NIHSvg height="100%" />
+    </Box>
   )
 }
