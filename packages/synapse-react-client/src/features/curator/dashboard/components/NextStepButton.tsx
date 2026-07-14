481ff2d57f5725712ca21632665617b5966368ba
Anthony Williams <anthony.williams@sagebase.org>
2026-07-14 13:43:52 -0700

    add an expanded version of the NextStepButton

--- packages/synapse-react-client/src/features/curator/dashboard/components/NextStepButton.tsx
+++ packages/synapse-react-client/src/features/curator/dashboard/components/NextStepButton.tsx
@@ -1,6 +1,6 @@
 import styles from './NextStepButton.module.scss'
-import { Typography } from '@mui/material'
-import { ArrowForwardIos } from '@mui/icons-material'
+import { Typography, useTheme } from '@mui/material'
+import { ArrowForwardIos, TableChartOutlined } from '@mui/icons-material'
 import { SynapseSpinner } from '@/components/LoadingScreen/LoadingScreen'
 import classNames from 'classnames'
 
@@ -10,6 +10,7 @@ type NextStepButtonProps = {
   onClick: () => void
   disabled?: boolean
   loading?: boolean
+  expanded?: boolean
 }
 
 /**
@@ -17,7 +18,41 @@ type NextStepButtonProps = {
  * Displays an arrow icon or loading spinner when appropriate.
  */
 export default function NextStepButton(props: NextStepButtonProps) {
-  const { className, buttonText, onClick, disabled, loading } = props
+  const { className, buttonText, onClick, disabled, loading, expanded } = props
+  const theme = useTheme()
+
+  if (expanded) {
+    return (
+      <button
+        type="button"
+        className={classNames(styles.button, className)}
+        onClick={onClick}
+        disabled={disabled || loading}
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '8px',
+          width: '100%',
+          backgroundColor: theme.palette.primary.main,
+          color: 'white',
+          border: 'none',
+          padding: '12px 24px',
+          borderRadius: '4px',
+          cursor: 'pointer',
+        }}
+      >
+        <TableChartOutlined sx={{ fontSize: 20 }} />
+        <Typography
+          variant="buttonLink"
+          sx={{ color: 'inherit', fontWeight: 700 }}
+        >
+          {buttonText}
+        </Typography>
+      </button>
+    )
+  }
+
   return (
     <button
       type="button"
