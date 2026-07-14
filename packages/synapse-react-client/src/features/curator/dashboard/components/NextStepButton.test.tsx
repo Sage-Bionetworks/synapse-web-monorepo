86a67dfba24e36efa759fbfa2576dcde6a46e755
Anthony Williams <anthony.williams@sagebase.org>
2026-07-14 14:39:03 -0700

    fix imports in NextStepButton tests

--- packages/synapse-react-client/src/features/curator/dashboard/components/NextStepButton.test.tsx
+++ packages/synapse-react-client/src/features/curator/dashboard/components/NextStepButton.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import NextStepButton from './NextStepButton'
 
 describe('NextStepButton', () => {
