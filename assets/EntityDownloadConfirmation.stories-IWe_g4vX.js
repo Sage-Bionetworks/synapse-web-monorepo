8084b9d47239f94b07805bc2e03a4c148f973e1f
jay-hodgson <jay.hodgson@sagebase.org>
2026-07-17 22:28:15 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@e22fe576d3d8bba01ab306e7708acf118a0e6d0d 🚀

---
+++ assets/EntityDownloadConfirmation.stories-IWe_g4vX.js
@@ -0,0 +1,7 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{a as t,o as n}from"./CardContainerLogic-B80BT4PC.js";var r,i,a,o;e((()=>{n(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Synapse/Entity/EntityDownloadConfirmation`,component:t,parameters:{requireLogin:!0}},a={args:{entityId:`syn64136446`,onIsLoadingChange:r(),handleClose:r()}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    entityId: 'syn64136446',
+    onIsLoadingChange: fn(),
+    handleClose: fn()
+  }
+}`,...a.parameters?.docs?.source}}},o=[`Dataset`]}))();export{a as Dataset,o as __namedExportsOrder,i as default};
--- assets/error-ClpdyBUi.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./preload-helper-CsHsquCd.js";import{h as t}from"./SynapseContext-D2HeodXq.js";var n=e((()=>{t()}));export{n as t};
