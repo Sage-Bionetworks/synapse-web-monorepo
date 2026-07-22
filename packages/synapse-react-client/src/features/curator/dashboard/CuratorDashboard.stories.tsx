46909d0d559f4a61494863e8fe184eeb6d390a28
Anthony Williams <anthony.williams@sagebase.org>
2026-07-22 13:37:15 -0700

    fix CuratorDashboard.stories.tsx

--- packages/synapse-react-client/src/features/curator/dashboard/CuratorDashboard.stories.tsx
+++ packages/synapse-react-client/src/features/curator/dashboard/CuratorDashboard.stories.tsx
@@ -1,19 +1,18 @@
 import { Meta, StoryObj } from '@storybook/react-vite'
-import { CuratorDashboardRouter } from './CuratorDashboardRouter'
+import { CuratorDashboard } from './CuratorDashboard'
 
 const meta = {
   title: 'Curator/Dashboard',
-  component: CuratorDashboardRouter,
+  component: CuratorDashboard,
   parameters: {
     requireLogin: true,
+  },
+  args: {
+    useMemoryRouter: true,
+    routerBaseName: '/',
   },
 } satisfies Meta<typeof CuratorDashboardRouter>
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Dashboard: Story = {
-  args: {
-    useMemoryRouter: true,
-    routerBaseName: '/',
-  },
-}
+export const Dashboard: Story = {}
