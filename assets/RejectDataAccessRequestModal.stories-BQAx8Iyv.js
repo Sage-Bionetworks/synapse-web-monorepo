498aaf2daea9923c95c051f3c80f642b276b72b8
nickgros <nickgrosenbacher@gmail.com>
2026-08-10 16:53:47 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@559b93377669c95c725241f60ffc2e02c3fcef0f 🚀

--- assets/RejectDataAccessRequestModal.stories-BQAx8Iyv.js
+++
@@ -1,9 +0,0 @@
-import{o as e}from"./preload-helper-CsHsquCd.js";import{tn as t,wt as n}from"./SynapseConstants-C6NyDJfS.js";import{An as r,En as i}from"./synapse-client-CN8jHcZ7.js";import{_ as a,b as o,v as s,y as c}from"./iframe-vkEV8oeF.js";import{n as l,t as u}from"./mockRejectionReasonsTableQueryResultBundle-CEMeEMmF.js";import{n as d,t as f}from"./RejectDataAccessRequestModal-DfLJyuiK.js";var p,m,h,g;e((()=>{s(),c(),u(),r(),t(),d(),{fn:p}=__STORYBOOK_MODULE_TEST__,m={title:`Governance/RejectDataAccessRequestModal`,component:f,loaders:[()=>o({sql:`SELECT * FROM ${n}`},l)],parameters:{stack:`mock`,msw:{handlers:[...a(i)]}}},h={name:`RejectDataAccessRequestModal`,args:{open:!0,tableId:`syn50683097`,submissionId:``,onClose:p()}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
-  name: 'RejectDataAccessRequestModal',
-  args: {
-    open: true,
-    tableId: 'syn50683097',
-    submissionId: '',
-    onClose: fn()
-  }
-}`,...h.parameters?.docs?.source}}},g=[`Demo`]}))();export{h as Demo,g as __namedExportsOrder,m as default};
