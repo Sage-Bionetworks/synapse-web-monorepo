68e5c4f4c5540d68f93fced11d5da5d703ec130b
afwillia <anthony.williams@sagebase.org>
2026-08-11 17:28:41 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@3c2cb866ee5632fc92dd8333eba812958dcbea1b 🚀

---
+++ assets/Forum-BiSm5vcC.js
@@ -0,0 +1,1 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t}from"./DiscussionThread-PC8RuHPF.js";import{n}from"./ForumPage-DdaijU7s.js";var r=e((()=>{t(),n()}));export{r as t};
---
+++ assets/SynapseChat.stories-CR6X7GYY.js
@@ -0,0 +1,23 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{An as t,En as n}from"./synapse-client-T1VPIVgj.js";import{D as r,O as i,R as a,d as o,f as s,z as c}from"./iframe-xGE8maXL.js";import{n as l,t as u}from"./SynapseChat-BIR6t4mP.js";import{n as d,t as f}from"./chatHandlers-Dx-P5E5h.js";var p,m,h,g,_;e((()=>{d(),c(),i(),s(),t(),l(),p={title:`Synapse/Chat`,component:u,parameters:{requireLogin:!0,chromatic:{viewports:[600,1200]}},argTypes:{isAuthenticated:{type:`boolean`}},args:{isAuthenticated:!0}},m={userProfile:o(n),entity:a(n),chatbot:f(n)},h={args:{initialMessage:`hello`},parameters:{stack:`mock`,msw:{handlers:m}}},g={args:{allowAttachments:!0},parameters:{stack:`mock`,msw:{handlers:[...o(n),...a(n),...f(n),...r(n)]}}},h.parameters={...h.parameters,docs:{...h.parameters?.docs,source:{originalSource:`{
+  args: {
+    initialMessage: 'hello'
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers
+    }
+  }
+}`,...h.parameters?.docs?.source}}},g.parameters={...g.parameters,docs:{...g.parameters?.docs,source:{originalSource:`{
+  args: {
+    allowAttachments: true
+  },
+  parameters: {
+    stack: 'mock',
+    msw: {
+      handlers: [...getUserProfileHandlers(MOCK_REPO_ORIGIN), ...getEntityHandlers(MOCK_REPO_ORIGIN), ...getChatbotHandlers(MOCK_REPO_ORIGIN), ...getFileHandlers(MOCK_REPO_ORIGIN)]
+    }
+  }
+}`,...g.parameters?.docs?.source},description:{story:`Demonstrates attaching local files to a chat message. Use the "+" button to upload one or
+more files, then send the message -- the mocked response reports the first attachment as
+FAILED and the rest as STAGED (see chatHandlers.ts) so both outcomes can be exercised.`,...g.parameters?.docs?.description}}},_=[`ChatWithSynapse`,`WithAttachments`]}))();export{h as ChatWithSynapse,g as WithAttachments,_ as __namedExportsOrder,p as default};
