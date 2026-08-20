707b467e8bb93f47887ef9d3bba34577c20c5bfe
jay-hodgson <jay.hodgson@sagebase.org>
2026-08-20 23:39:54 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@2c8ba1d68fa63d3fe8b702e5de7021c42432a5e4 🚀

--- assets/Forum-Bp-vyB2A.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t}from"./DiscussionThread-B6O_a-Dn.js";import{n}from"./ForumPage-H1gSSWKz.js";var r=e((()=>{t(),n()}));export{r as t};
---
+++ assets/SubscriptionPage.stories-BcG3itAa.js
@@ -0,0 +1,11 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{Fn as t,jn as n}from"./synapse-client-DJe3j8_l.js";import{n as r,t as i}from"./iframe-s2yGHo7n.js";import{n as a,t as o}from"./SubscriptionPage-Bboenkbv.js";var s,c,l;e((()=>{r(),t(),a(),s={title:`Synapse/Following/SubscriptionPage`,component:o,parameters:{stack:`mock`}},c={name:`SubscriptionPage`,args:{},parameters:{msw:{handlers:{...i(n)}}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: 'SubscriptionPage',
+  args: {},
+  parameters: {
+    msw: {
+      handlers: {
+        ...getHandlers(MOCK_REPO_ORIGIN)
+      }
+    }
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Demo`]}))();export{c as Demo,l as __namedExportsOrder,s as default};
