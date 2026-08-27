03be03ed241930ed4afadd72b69eaa2bd0cef37e
jay-hodgson <jay.hodgson@sagebase.org>
2026-08-27 17:30:52 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@f8cf936a359a406a292a6dc81a6456b66a9b240f 🚀

---
+++ assets/Forum-D8-y28Ly.js
@@ -0,0 +1,1 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t}from"./DiscussionThread-Bc1UjNsI.js";import{n}from"./ForumPage-BX3ooDGT.js";var r=e((()=>{t(),n()}));export{r as t};
---
+++ assets/LoginMethodButton.stories-CS1LBQlf.js
@@ -0,0 +1,7 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{i as t,o as n}from"./LastLoginInfo-B11YQYKs.js";import{r,t as i}from"./IconSvg-BZp2QC9G.js";import{on as a,sn as o}from"./iframe-Thp0mAsS.js";var s,c,l,u;e((()=>{n(),o(),r(),{fn:s}=__STORYBOOK_MODULE_TEST__,c={title:`Authentication/LoginMethodButton`,component:a,argTypes:{loginMethod:{options:t,control:`radio`},iconName:{options:i,control:`select`}}},l={args:{loginMethod:`GOOGLE`,iconName:`google24`,onClick:s()}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    loginMethod: 'GOOGLE',
+    iconName: 'google24',
+    onClick: fn()
+  }
+}`,...l.parameters?.docs?.source}}},u=[`Demo`]}))();export{l as Demo,u as __namedExportsOrder,c as default};
