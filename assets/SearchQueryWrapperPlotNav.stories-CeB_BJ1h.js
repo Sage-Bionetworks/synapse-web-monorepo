cd94a62f5fd36c840a3bcd56aac2dff3b353e18c
jay-hodgson <jay.hodgson@sagebase.org>
2026-08-28 20:49:36 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@47c84845524a8b1046dbcbe3e2c95840e2aaef60 🚀

---
+++ assets/SearchQueryWrapperPlotNav.stories-CeB_BJ1h.js
@@ -0,0 +1,37 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{U as t,tn as n}from"./SynapseConstants-C6NyDJfS.js";import{Pn as r,Rn as i}from"./synapse-client-CmSRto0s.js";import{H as a,W as o,et as s,i as c,n as l,nt as u,r as d,t as f,tt as p}from"./iframe-Cn2wcqhG.js";import{m,p as h}from"./CardContainerLogic-C3AboN6R.js";var g,_,v,y,b,x;e((()=>{o(),c(),l(),p(),i(),n(),m(),{fn:g}=__STORYBOOK_MODULE_TEST__,_={title:`Explore/SearchQueryWrapperPlotNav`,component:h,parameters:{stack:`mock`,msw:{handlers:[a(r,{tableBundle:{columnModels:u.columnModels,maxRowsPerPage:25}}),...f(r),...d(r)]}},args:{searchIndexId:s,onQueryResultBundleChange:g()}},v={args:{name:`Search Results`,unitDescription:`result`,tableConfiguration:{columnLinks:[]},facetsToPlot:[`category`,`collections`,`topic`],availableFacets:[`category`,`collections`,`topic`,`dataTypes`,`isOpen`,`mature`,`registration`]}},y={args:{name:`Search Results`,cardConfiguration:{type:t,genericCardSchema:{type:`Standard`,title:`name`,subTitle:`acronym`,secondaryLabels:[`category`,`collections`,`topic`,`isOpen`]}},facetsToPlot:[`category`,`collections`,`topic`],availableFacets:[`category`,`collections`,`topic`,`dataTypes`,`isOpen`,`mature`,`registration`],defaultShowPlots:!0}},b={args:{name:`Search Results`,unitDescription:`result`,tableConfiguration:{},availableFacets:[`category`,`collections`,`topic`,`dataTypes`,`isOpen`,`mature`,`registration`],defaultShowPlots:!1,hideTopLevelControls:!1,hideQueryCount:!1}},v.parameters={...v.parameters,docs:{...v.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: 'Search Results',
+    unitDescription: 'result',
+    tableConfiguration: {
+      columnLinks: []
+    },
+    facetsToPlot: ['category', 'collections', 'topic'],
+    availableFacets: ['category', 'collections', 'topic', 'dataTypes', 'isOpen', 'mature', 'registration']
+  }
+}`,...v.parameters?.docs?.source}}},y.parameters={...y.parameters,docs:{...y.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: 'Search Results',
+    cardConfiguration: {
+      type: GENERIC_CARD,
+      genericCardSchema: {
+        type: 'Standard',
+        title: 'name',
+        subTitle: 'acronym',
+        secondaryLabels: ['category', 'collections', 'topic', 'isOpen']
+      }
+    },
+    facetsToPlot: ['category', 'collections', 'topic'],
+    availableFacets: ['category', 'collections', 'topic', 'dataTypes', 'isOpen', 'mature', 'registration'],
+    defaultShowPlots: true
+  }
+}`,...y.parameters?.docs?.source}}},b.parameters={...b.parameters,docs:{...b.parameters?.docs,source:{originalSource:`{
+  args: {
+    name: 'Search Results',
+    unitDescription: 'result',
+    tableConfiguration: {},
+    availableFacets: ['category', 'collections', 'topic', 'dataTypes', 'isOpen', 'mature', 'registration'],
+    defaultShowPlots: false,
+    hideTopLevelControls: false,
+    hideQueryCount: false
+  }
+}`,...b.parameters?.docs?.source}}},x=[`TableView`,`CardView`,`TableWithFacetsOnly`]}))();export{y as CardView,v as TableView,b as TableWithFacetsOnly,x as __namedExportsOrder,_ as default};
