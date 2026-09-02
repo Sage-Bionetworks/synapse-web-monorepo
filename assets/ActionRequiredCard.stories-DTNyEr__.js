f4502ea0008bcd96f6a10457ed08af974d7f3744
kianamcc <kianamccu@gmail.com>
2026-09-02 17:02:13 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@4f0522981cee0802c6d981b1fcb11a074553cae2 🚀

---
+++ assets/ActionRequiredCard.stories-DTNyEr__.js
@@ -0,0 +1,14 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{pn as t}from"./TextField-DshmLCH7.js";import{t as n}from"./jsx-runtime-l3w3GfrB.js";import{t as r,yn as i}from"./esm-BSTH_Qsy.js";import{D as a,tn as o}from"./SynapseConstants-C6NyDJfS.js";import{n as s,t as c}from"./WideButton-BH-UxoFM.js";import{r as l,t as u}from"./ActionRequiredCard-CBfhF28E.js";var d,f,p,m,h;e((()=>{s(),o(),r(),l(),d=n(),f={title:`Download/ActionRequiredCard`,component:u,tags:[`autodocs`],argTypes:{actionNode:{options:[`Button`,`Button with text (Sharing Settings)`],mapping:{Button:(0,d.jsx)(c,{variant:`contained`,children:`Start`}),"Button with text (Sharing Settings)":(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(t,{variant:`smallText1`,sx:{color:`grey.700`},children:`Contact an administrator to request download permission`}),(0,d.jsx)(i,{variant:`outlined`,children:`View Sharing Settings`})]})}}}},p={args:{isLoading:!0}},m={args:{isLoading:!1,title:`The title of the card`,description:`The description of the card`,actionNode:`Button`,iconType:a,count:42}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLoading: true
+  }
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLoading: false,
+    title: 'The title of the card',
+    description: 'The description of the card',
+    actionNode: 'Button',
+    iconType: EASY_DIFFICULTY,
+    count: 42
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Loading`,`Card`]}))();export{m as Card,p as Loading,h as __namedExportsOrder,f as default};
