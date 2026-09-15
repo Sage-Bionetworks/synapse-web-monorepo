332f5a6ad7a05d05ab2d7d4cd42b38cc31da0a93
jay-hodgson <jay.hodgson@sagebase.org>
2026-09-15 11:56:23 -0700

    make one line

--- apps/synapse-portal-framework/src/components/curie-chat-widget/CurieChatWidget.tsx
+++ apps/synapse-portal-framework/src/components/curie-chat-widget/CurieChatWidget.tsx
@@ -26,11 +26,7 @@ function CurieChatDialogLauncher() {
       onClick={onClick}
       aria-label="Open Curie chat"
     >
-      <span className={styles.speechBubble}>
-        Hi!
-        <br />
-        How can I help you?
-      </span>
+      <span className={styles.speechBubble}>Hi! How can I help you?</span>
       <CurieLogo className={styles.curieLogo} />
     </button>
   )
