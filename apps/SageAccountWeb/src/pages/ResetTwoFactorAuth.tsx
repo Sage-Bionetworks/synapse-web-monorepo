00e85758c928136e9bbe6f5e3f88f81b478a7598
“kianamcc” <user.email=kianamccu@gmail.com>
2026-09-23 12:44:16 -0700

    reverting a change, I think unrelated to ticket

--- apps/SageAccountWeb/src/pages/ResetTwoFactorAuth.tsx
+++ apps/SageAccountWeb/src/pages/ResetTwoFactorAuth.tsx
@@ -67,18 +67,6 @@ function ResetTwoFactorAuth() {
         className={'ResetPasswords'}
         leftContent={
           <>
-            {promptConfirmDisable2FA && showPasswordField && (
-              <BackButton
-                onClick={() => {
-                  resetDisable2FAMutation()
-                  setShowPasswordField(false)
-                }}
-                sx={{
-                  alignSelf: 'flex-start',
-                  marginBottom: 2,
-                }}
-              />
-            )}
             <div className={'panel-logo'}>
               <SourceAppLogo />
             </div>
@@ -109,6 +97,14 @@ function ResetTwoFactorAuth() {
               )}
               {promptConfirmDisable2FA && (
                 <>
+                  {showPasswordField && (
+                    <BackButton
+                      onClick={() => {
+                        resetDisable2FAMutation()
+                        setShowPasswordField(false)
+                      }}
+                    />
+                  )}
                   <Typography
                     variant={'body1'}
                     sx={{
