0cb5dda3eed1097a3df9d7c4a002fe9f954c4753
nickgros <nickgrosenbacher@gmail.com>
2026-09-24 01:58:38 +0000

    Deploying to gh-pages from @ Sage-Bionetworks/synapse-web-monorepo@34aedc403c8d9d626f38491327efb421e27c9a56 🚀

---
+++ assets/ChoiceOptionsEditor.stories-CLRw--Mi.js
@@ -0,0 +1,9 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t,t as n}from"./ChoiceOptionsEditor-balXFYtk.js";var r,i,a,o,s;e((()=>{t(),r={title:`Components/FormTemplateEditor/ChoiceOptionsEditor`,component:n,args:{options:[`Option 1`,`Option 2`],onChange:e=>console.log(`onChange`,e)}},i={},a={args:{options:[`Option 1`]}},o={args:{options:[`United States`,`Canada`,`United Kingdom`,`Germany`,`Japan`,`Australia`]}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    options: ['Option 1']
+  }
+}`,...a.parameters?.docs?.source},description:{story:`A single remaining choice -- its delete button is disabled so the list can't go empty.`,...a.parameters?.docs?.description}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    options: ['United States', 'Canada', 'United Kingdom', 'Germany', 'Japan', 'Australia']
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`SingleOption`,`ManyOptions`]}))();export{i as Default,o as ManyOptions,a as SingleOption,s as __namedExportsOrder,r as default};
---
+++ assets/Forum-CRpj6QTZ.js
@@ -0,0 +1,1 @@
+import{o as e}from"./preload-helper-CsHsquCd.js";import{n as t}from"./DiscussionThread-DY9Q-B5W.js";import{n}from"./ForumPage-C4nHDYgJ.js";var r=e((()=>{t(),n()}));export{r as t};
